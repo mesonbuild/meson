18ad46d6efe732a2643df8186e2a1befd1748d35
Jussi Pakkanen <jpakkane@gmail.com>
2013-01-13 21:01:53 +0200

    Test for C++ pch.

---
+++ test cases/14 cxx pch/prog.cc
@@ -0,0 +1,8 @@
+void func() {
+    std::cout << "This is a function that fails to compile if iostream is not included."
+              << std::endl;
+}
+
+int main(int argc, char **argv) {
+    return 0;
+}
