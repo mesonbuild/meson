73f8a69d3942c1ce6ecb0fa19afca3a46341dee3
Jussi Pakkanen <jpakkane@gmail.com>
2013-01-13 21:11:56 +0200

    Added test for mixed C/C++ pch usage.

---
+++ test cases/15 mixed pch/func.c
@@ -0,0 +1,7 @@
+void tmp_func() {
+    fprintf(stdout, "This is a function that fails if stdio is not #included.\n");
+}
+
+int cfunc() {
+    return 0;
+}
---
+++ test cases/15 mixed pch/main.cc
@@ -0,0 +1,10 @@
+extern "C" int cfunc();
+
+void func() {
+    std::cout << "This is a function that fails to compile if iostream is not included."
+              << std::endl;
+}
+
+int main(int argc, char **argv) {
+    return cfunc();
+}
