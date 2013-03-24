a05021f7495f57f103eff7154b60441865de48cf
Jussi Pakkanen <jpakkane@gmail.com>
2013-03-24 21:11:02 +0200

    Helper app to delete files with a given suffix.

---
+++ delwithsuffix.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+# Copyright 2013 Jussi Pakkanen
+
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+
+#     http://www.apache.org/licenses/LICENSE-2.0
+
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os, sys
+
+if len(sys.argv) != 3:
+    print('%s <root of subdir to process> <suffix to delete>' % sys.argv[0])
+    sys.exit(1)
+
+topdir = sys.argv[1]
+suffix = sys.argv[2]
+if suffix[0] != '.':
+    suffix = '.' + suffix
+
+for (root, dirs, files) in os.walk(topdir):
+    for f in files:
+        if f.endswith(suffix):
+            fullname = os.path.join(root, f)
+            os.unlink(fullname)
