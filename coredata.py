7ac68e0d6e88ff091567496b4ed25b605b08b638
Jussi Pakkanen <jpakkane@gmail.com>
2013-06-09 21:06:30 +0300

    Bumped version number for release.

--- coredata.py
+++ coredata.py
@@ -16,7 +16,7 @@
 
 import pickle
 
-version = '0.3.0-research'
+version = '0.4.0'
 
 # This class contains all data that must persist over multiple
 # invocations of Meson. It is roughly the same thing as
