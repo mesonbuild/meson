735bacb050b7bb0da3d8e9459bb68bdb815357a7
Jussi Pakkanen <jpakkane@gmail.com>
2014-07-30 13:17:58 +0300

    Add test that I forgot.

---
+++ test cases/common/61 custom target source output/generator.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+import sys, os
+
+if len(sys.argv) != 2:
+    print(sys.argv[0], '<output dir>')
+
+odir = sys.argv[1]
+
+open(os.path.join(odir, 'mylib.h'), 'w').write('int func();\n')
+open(os.path.join(odir, 'mylib.c'), 'w').write('''int func() {
+    return 0;
+}
+''')
