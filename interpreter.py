c4860ff60f7d147683486f012dba742f3c460a0b
Jussi Pakkanen <jpakkane@gmail.com>
2014-08-11 00:20:17 +0300

    Updated copyright years.

--- interpreter.py
+++ interpreter.py
@@ -1,4 +1,4 @@
-# Copyright 2012 Jussi Pakkanen
+# Copyright 2012-2014 Jussi Pakkanen
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
