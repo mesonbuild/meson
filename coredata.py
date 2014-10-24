ad2f03da7b43467af5ab7b155ee1c1eefb6afa94
Jussi Pakkanen <jpakkane@gmail.com>
2014-10-24 04:58:41 +0300

    Bump version number for release.

--- coredata.py
+++ coredata.py
@@ -14,7 +14,7 @@
 
 import pickle, os, uuid
 
-version = '0.21.0-research'
+version = '0.21.0'
 
 # This class contains all data that must persist over multiple
 # invocations of Meson. It is roughly the same thing as
