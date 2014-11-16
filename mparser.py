525f0ae6d252f947a512c5e0cb532617486ad649
Jussi Pakkanen <jpakkane@gmail.com>
2014-11-16 20:19:12 +0200

    Harmonised copyright declarations.

--- mparser.py
+++ mparser.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 
-# Copyright 2014 Jussi Pakkanen
-# Copyright 2014 Robin McCorkell
+# Copyright 2014 The Meson development team
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
--- optinterpreter.py
+++ optinterpreter.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2014 Jussi Pakkanen
+# Copyright 2013-2014 The Meson development team
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
