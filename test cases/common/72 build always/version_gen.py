24484412b570f53e447b81017a15ec23b6417106
Jussi Pakkanen <jpakkane@gmail.com>
2015-02-07 14:33:43 +0200

    Start proper approach to determining how build_always targets should work by writing a proper test case illustrating how it should behave.

---
+++ test cases/common/72 build always/version_gen.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+
+import sys, os, subprocess
+
+def generate(infile, outfile, fallback):
+    workdir = os.path.split(infile)[0]
+    if workdir == '':
+        workdir = '.'
+    p = subprocess.Popen(['git', 'describe'], cwd=workdir, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    (stdo, _) = p.communicate()
+    # If we are working off an extracted tarball, git version number is not available.
+    if p.returncode == 0:
+        version = stdo.decode().strip()
+    else:
+        version = fallback
+    newdata = open(infile).read().replace('@VERSION@', version)
+    try:
+        olddata = open(outfile).read()
+        if olddata == newdata:
+            return
+    except Exception:
+        pass
+    open(outfile, 'w').write(newdata)
+
+if __name__ == '__main__':
+    infile = sys.argv[1]
+    outfile = sys.argv[2]
+    fallback = sys.argv[3]
+    generate(infile, outfile, fallback)
