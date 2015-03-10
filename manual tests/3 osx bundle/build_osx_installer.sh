b4b2e8d5331878f9b2dcf807c0d277b6dc880959
Jussi Pakkanen <jpakkane@gmail.com>
2015-03-10 16:45:39 +0200

    Added an example of how to create OSX installers.

---
+++ manual tests/3 osx bundle/build_osx_installer.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+rm -rf buildtmp
+mkdir buildtmp
+~/meson/meson.py buildtmp --prefix=/tmp/myapp.app --bindir=Contents/MacOS
+ninja -C buildtmp install
+rm -rf buildtmp
+mkdir -p mnttmp
+rm -f working.dmg
+gunzip < template.dmg.gz > working.dmg
+hdiutil attach working.dmg -noautoopen -quiet -mountpoint mnttmp
+# NOTE: output of hdiutil changes every now and then.
+# Verify that this is still working.
+DEV=`hdiutil info|tail -1|awk '{print $1}'`
+rm -rf mnttmp/myapp.app
+mv /tmp/myapp.app mnttmp
+hdiutil detach ${DEV}
+rm -rf mnttmp
+rm -f myapp.dmg
+hdiutil convert working.dmg -quiet -format UDZO -imagekey zlib-level=9 -o myapp.dmg
+rm -f working.dmg
