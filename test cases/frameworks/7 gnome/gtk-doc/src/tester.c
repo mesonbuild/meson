8b0469e27690a4708d0a5eefe0b473444cb8a5db
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2015-05-25 19:11:08 +0300

    gnome: implement basic gtkdoc_scan
    Signed-off-by: Igor Gnatenko <i.gnatenko.brain@gmail.com>

---
+++ test cases/frameworks/7 gnome/gtk-doc/src/tester.c
@@ -0,0 +1,35 @@
+/**
+ * SECTION:tester
+ * @short_description: module for gtk-doc unit test
+ *
+ * This file contains non-sense code for the sole purpose of testing the docs.
+ *
+ * As described in http://bugzilla.gnome.org/show_bug.cgi?id=457077 it
+ * returns nothing.
+ */
+
+#include <glib.h>
+
+#include "tester.h"
+
+/**
+ * test:
+ * @a: arg
+ *
+ * lonely function
+ */
+void test (gint a) {
+}
+
+/**
+ * GtkDocTestIf:
+ *
+ * test object
+ */
+/**
+ * GtkDocTestIfInterface:
+ * @parent: parent interface type.
+ *
+ * test interface
+ */
+
