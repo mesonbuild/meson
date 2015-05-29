d08610d50790c9a6dac9398b6015aaf3396ecd86
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2015-05-29 21:57:11 +0300

    implement getting versions for projects
    Signed-off-by: Igor Gnatenko <i.gnatenko.brain@gmail.com>

--- mesonwrapdb.py
+++ mesonwrapdb.py
@@ -16,6 +16,7 @@
 
 import argparse
 import requests
+import sys
 
 import mlog
 
@@ -23,8 +24,10 @@ API_URL = "http://wrapdb.mesonbuild.com/v1"
 
 parser = argparse.ArgumentParser()
 group = parser.add_mutually_exclusive_group()
-group.add_argument("--list-wraps", action="store_true", dest="list_projects", default=True,
-                    help="List all available projects with wraps.")
+group.add_argument("--list-projects", action="store_true", dest="list_projects", default=False,
+                   help="List all available projects with wraps.")
+group.add_argument("--list-wraps", action="store", dest="get_wraps", default=[], metavar="PKG",
+                   help="Get list with available versions of wrap for given package.")
 
 def list_projects():
     j = requests.get("%s/projects" % API_URL).json()
@@ -32,7 +35,21 @@ def list_projects():
     for p in j["projects"]:
         mlog.log("-", mlog.bold(p))
 
+def list_versions(project):
+    j = requests.get("%s/projects/%s" % (API_URL, project)).json()
+    if j.get("output") == "ok":
+        mlog.log("List available versions for", mlog.bold(project), "(branch/revision)")
+        for v in j["versions"]:
+            mlog.log("-", mlog.bold("{branch}/{revision}".format(**v)))
+    else:
+        mlog.log("Something went wrong.", j.get("error"))
+
 if __name__ == "__main__":
     options = parser.parse_args()
     if options.list_projects:
         list_projects()
+    elif options.get_wraps:
+        list_versions(options.get_wraps)
+    else:
+        print("Please specify at least one option!")
+        sys.exit(1)
