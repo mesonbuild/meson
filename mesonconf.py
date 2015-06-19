f1bc1e2757db5d719a2b7c51fb4936d0f79d8767
Igor Gnatenko <i.gnatenko.brain@gmail.com>
2015-06-19 10:36:27 +0300

    mesonconf: fix tb and parsing options
    Traceback (most recent call last):
      File "./mesonconf.py", line 202, in <module>
        print(args)
    NameError: name 'args' is not defined
    
    Signed-off-by: Igor Gnatenko <i.gnatenko.brain@gmail.com>

--- mesonconf.py
+++ mesonconf.py
@@ -198,8 +198,7 @@ class Conf:
 
 if __name__ == '__main__':
     options = parser.parse_args()
-    if len(options.directory) > 1:
-        print(args)
+    if len(options.directory) < 1:
         print('%s <build directory>' % sys.argv[0])
         print('If you omit the build directory, the current directory is substituted.')
         sys.exit(1)
