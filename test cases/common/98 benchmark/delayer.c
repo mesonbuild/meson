3daf0b8a819ae99a15914d6b0445e5ca5ab148c9
Jussi Pakkanen <jpakkane@gmail.com>
2015-11-26 00:22:46 +0200

    Benchmark test binary runs on Windows.

--- test cases/common/98 benchmark/delayer.c
+++ test cases/common/98 benchmark/delayer.c
@@ -1,21 +1,20 @@
 /* Simple prog that sleeps for a random time. */
 
 #include<stdlib.h>
-#if !defined(_MSC_VER)
 #include<time.h>
-#else
+#if defined(_WIN32)
 #include<windows.h>
 #endif
 
 int main(int argc, char **argv) {
     srand(time(NULL));
-#if !defined(_MSC_VER)
+#if !defined(_WIN32)
     struct timespec t;
     t.tv_sec = 0;
     t.tv_nsec = 199999999.0*rand()/RAND_MAX;
     nanosleep(&t, NULL);
 #else
-    Sleep(500.0*rand()/RAND_MAX);
+    Sleep(50.0*rand()/RAND_MAX);
 #endif
     return 0;
 }
