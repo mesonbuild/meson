f89023c58be0abe1fb68900cb6ceacfcdaa474d9
Jussi Pakkanen <jpakkane@gmail.com>
2016-01-06 15:30:44 +0200

    Added missing shebang line.

--- dirchanger.py
+++ dirchanger.py
@@ -1,4 +1,6 @@
-# Copyright 2015 The Meson development team
+#!/usr/bin/env python3
+
+# Copyright 2015-2016 The Meson development team
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
