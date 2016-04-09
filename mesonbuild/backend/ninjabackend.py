48e678db76948cc4b02369b64bc072810535f12f
Jussi Pakkanen <jpakkane@gmail.com>
2016-04-09 12:04:06 +0300

    Strip leading source tree dir name from install files if it exists.

--- mesonbuild/backend/ninjabackend.py
+++ mesonbuild/backend/ninjabackend.py
@@ -547,12 +547,13 @@ int dummy;
             assert(isinstance(de, build.Data))
             subdir = de.install_dir
             for f in de.sources:
+                plain_f = os.path.split(f)[1]
                 if de.in_sourcetree:
                     srcprefix = self.environment.get_source_dir()
                 else:
                     srcprefix = self.environment.get_build_dir()
                 srcabs = os.path.join(srcprefix, de.source_subdir, f)
-                dstabs = os.path.join(subdir, f)
+                dstabs = os.path.join(subdir, plain_f)
                 i = [srcabs, dstabs]
                 d.data.append(i)
 
