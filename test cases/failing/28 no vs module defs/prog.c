de73d85ad80a375cef12fb411c66a9654fa9899b
Jussi Pakkanen <jpakkane@gmail.com>
2016-06-05 20:35:45 +0300

    Removed vs nomod test because it fails at compile time but the framework needs it to fail at configure time.

--- test cases/failing/28 no vs module defs/prog.c
+++
@@ -1,5 +0,0 @@
-int somedllfunc();
-
-int main(int argc, char **argv) {
-    return somedllfunc() == 42 ? 0 : 1;
-}
--- test cases/failing/28 no vs module defs/subdir/somedll.c
+++
@@ -1,7 +0,0 @@
-/* With MSVC, the DLL created from this will not export any symbols
- * without a module definitions file specified while linking */
-#ifdef _MSC_VER
-int somedllfunc() {
-    return 42;
-}
-#endif
