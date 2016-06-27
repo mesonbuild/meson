bc347aed0be4d8ea6210a546fb350f7bc1eee529
Zhe Wang <0x1998@gmail.com>
2016-06-28 00:31:07 +0800

    GnuCPPCompiler: allow usage of GNU extensions (#619)
    * GnuCPPCompiler: allow usage of GNU extensions
    
    * Add myself to authors.txt

--- mesonbuild/compilers.py
+++ mesonbuild/compilers.py
@@ -1828,7 +1828,8 @@ class GnuCPPCompiler(CPPCompiler):
 
     def get_options(self):
         opts = {'cpp_std' : coredata.UserComboOption('cpp_std', 'C++ language standard to use',
-                                                     ['none', 'c++03', 'c++11', 'c++14', 'c++1z'],
+                                                     ['none', 'c++03', 'c++11', 'c++14', 'c++1z',
+                                                      'gnu++03', 'gnu++11', 'gnu++14', 'gnu++1z'],
                                                      'none'),
                 'cpp_debugstl': coredata.UserBooleanOption('cpp_debugstl',
                                                            'STL debug mode',
