efc986ee93c4a509ce087370d645a816393b7966
Nirbheek Chauhan <nirbheek@centricular.com>
2016-10-03 12:58:10 +0530

    swift tests: Port to Swift 3
    Should remain compatible with Swift 2.2

--- test cases/swift/4 generate/gen/main.swift
+++ test cases/swift/4 generate/gen/main.swift
@@ -4,7 +4,11 @@
     import Glibc
 #endif
 
+#if swift(>=3.0)
+let fname = CommandLine.arguments[1]
+#else
 let fname = Process.arguments[1]
+#endif
 let code = "public func getGenerated() -> Int {\n    return 42\n}\n"
 
 let f = fopen(fname, "w")
