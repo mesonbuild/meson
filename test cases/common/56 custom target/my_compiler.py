5fdac48250c0e4a7ad6e9b92041ab7b4d1bd02ec
Nirbheek Chauhan <nirbheek@centricular.com>
2016-11-08 18:55:15 +0530

    custom_target: Substitute @OUTPUT@ and @INPUT properly
    They weren't being substituted if they were a part of a command
    argument, ala --output=@OUTPUT@, etc.
    
    Closes https://github.com/mesonbuild/meson/issues/824

--- test cases/common/56 custom target/my_compiler.py
+++ test cases/common/56 custom target/my_compiler.py
@@ -3,13 +3,14 @@
 import sys
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print(sys.argv[0], 'input_file output_file')
+    if len(sys.argv) != 3 or not sys.argv[1].startswith('--input') or \
+       not sys.argv[2].startswith('--output'):
+        print(sys.argv[0], '--input=input_file --output=output_file')
         sys.exit(1)
-    with open(sys.argv[1]) as f:
+    with open(sys.argv[1].split('=')[1]) as f:
         ifile = f.read()
     if ifile != 'This is a text only input file.\n':
         print('Malformed input')
         sys.exit(1)
-    with open(sys.argv[2], 'w') as ofile:
+    with open(sys.argv[2].split('=')[1], 'w') as ofile:
         ofile.write('This is a binary output file.\n')
