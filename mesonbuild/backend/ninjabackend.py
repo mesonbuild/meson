1e0ae0a083cc454dad64f9195e7b5e08b80f9bd9
Jussi Pakkanen <jpakkane@gmail.com>
2016-11-12 18:45:24 +0200

    Revert "vala: Build with '--nostdpkg'", MR #920
    This reverts commit 0f098c37ae3f257649eae7566391ad74b7010c45.

--- mesonbuild/backend/ninjabackend.py
+++ mesonbuild/backend/ninjabackend.py
@@ -1024,7 +1024,6 @@ int dummy;
         # found inside the build tree (generated sources).
         args += ['-d', c_out_dir]
         args += ['-C']
-        args += ['--nostdpkg']
         if not isinstance(target, build.Executable):
             # Library name
             args += ['--library=' + target.name]
