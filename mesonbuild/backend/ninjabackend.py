c9a46fcc93461ece4ac6d83b1875cd0f5fe0c684
Jussi Pakkanen <jpakkane@gmail.com>
2016-11-20 00:26:45 +0200

    Can set compiler arguments in Java.

--- mesonbuild/backend/ninjabackend.py
+++ mesonbuild/backend/ninjabackend.py
@@ -859,6 +859,9 @@ int dummy;
     def generate_single_java_compile(self, src, target, compiler, outfile):
         args = []
         args += compiler.get_buildtype_args(self.environment.coredata.get_builtin_option('buildtype'))
+        args += self.build.get_global_args(compiler)
+        args += self.build.get_project_args(compiler, target.subproject)
+        args += target.get_java_args()
         args += compiler.get_output_args(self.get_target_private_dir(target))
         for i in target.include_dirs:
             for idir in i.get_incdirs():
