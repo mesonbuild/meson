4f43709068b516453579206ae63c35ba0402813b
Nirbheek Chauhan <nirbheek@centricular.com>
2016-12-18 19:59:48 +0530

    Allow passing arguments to install scripts
    Closes #1213

---
+++ test cases/common/60 install script/myinstall.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+import os
+import sys
+
+prefix = os.environ['MESON_INSTALL_DESTDIR_PREFIX']
+
+dirname = os.path.join(prefix, sys.argv[1])
+
+os.makedirs(dirname)
+with open(os.path.join(dirname, sys.argv[2]), 'w') as f:
+    f.write('')
