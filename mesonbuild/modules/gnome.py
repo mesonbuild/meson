af85d0e65ef99c6e3db0f7f785968a675340edd2
Nirbheek Chauhan <nirbheek@centricular.com>
2017-02-19 00:02:28 +0530

    gnome: Fix minimum gresource dependency required
    This is the latest release of glib that exists and has the required
    dependency-generation fixes.
    
    Without this GNOME Recipes cannot even configure.

--- mesonbuild/modules/gnome.py
+++ mesonbuild/modules/gnome.py
@@ -37,7 +37,7 @@ from . import ExtensionModule
 #
 # https://github.com/ninja-build/ninja/issues/1184
 # https://bugzilla.gnome.org/show_bug.cgi?id=774368
-gresource_dep_needed_version = '>= 2.52.0'
+gresource_dep_needed_version = '>= 2.51.1'
 
 native_glib_version = None
 girwarning_printed = False
