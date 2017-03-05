48d371bd19ce990d04c42ca6f303d1180b843b6b
Jussi Pakkanen <jpakkane@gmail.com>
2017-03-05 23:46:21 +0200

    Bump version number for new development.

--- mesonbuild/coredata.py
+++ mesonbuild/coredata.py
@@ -17,7 +17,7 @@ from pathlib import PurePath
 from .mesonlib import MesonException, commonpath
 from .mesonlib import default_libdir, default_libexecdir, default_prefix
 
-version = '0.39.0'
+version = '0.40.0.dev1'
 backendlist = ['ninja', 'vs2010', 'vs2015', 'xcode']
 
 class UserOption:
