a4255d74f54f6d74b12f0cadcaa29a7584079503
Nirbheek Chauhan <nirbheek@centricular.com>
2017-03-14 19:17:24 +0530

    Don't generate import library for shared modules
    Also add a test for this on all platforms.

--- mesonbuild/build.py
+++ mesonbuild/build.py
@@ -1246,6 +1246,7 @@ class SharedModule(SharedLibrary):
         if 'soversion' in kwargs:
             raise MesonException('Shared modules must not specify the soversion kwarg.')
         super().__init__(name, subdir, subproject, is_cross, sources, objects, environment, kwargs)
+        self.import_filename = None
 
 class CustomTarget(Target):
     known_kwargs = {'input': True,
