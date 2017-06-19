15d0851443d98fcdf9bd7960cf17491f8dfa7e68
Adam C. Foltzer <acfoltzer@galois.com>
2017-06-19 16:52:31 -0700

    fix indentation per @ignatenkobrain

--- mesonbuild/backend/ninjabackend.py
+++ mesonbuild/backend/ninjabackend.py
@@ -1200,7 +1200,7 @@ int dummy;
                 # unfortunately we did not check for that and some downstream projects
                 # now have them. Once slashes are forbidden, remove this bit.
                 target_slashname_workaround_dir = os.path.join(os.path.split(target.name)[0],
-                                                           self.get_target_dir(target))
+                                                               self.get_target_dir(target))
             else:
                 target_slashname_workaround_dir = self.get_target_dir(target)
             rpath_args = rustc.build_rpath_args(self.environment.get_build_dir(),
