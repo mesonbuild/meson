c5e7774e27c56f09c3a16458c6d9931e01cee100
Nirbheek Chauhan <nirbheek@centricular.com>
2017-07-19 05:03:14 +0530

    Update all versions to 0.41.2

--- mesonbuild/coredata.py
+++ mesonbuild/coredata.py
@@ -19,7 +19,7 @@ from .mesonlib import MesonException, commonpath
 from .mesonlib import default_libdir, default_libexecdir, default_prefix
 import ast
 
-version = '0.41.1'
+version = '0.41.2'
 backendlist = ['ninja', 'vs', 'vs2010', 'vs2015', 'vs2017', 'xcode']
 
 class UserOption:
