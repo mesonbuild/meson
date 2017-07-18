7c2ae55fab2f0837531188373d276f168845e62f
Nirbheek Chauhan <nirbheek@centricular.com>
2017-07-18 18:19:55 +0530

    Revert "Revert "Merge pull request #1931 from centricular/use-patched-ninja""
    This reverts commit 5eb64a6f3e47b570f544524ef48dc6ef0e4dce59.
    
    Let's try again, with a fixed Ninja by QuLogic.

--- ci/appveyor-install.bat
+++ ci/appveyor-install.bat
@@ -1,5 +1,7 @@
 set CACHE=C:\cache
 set CYGWIN_MIRROR="http://cygwin.mirror.constant.com"
+set CYGWIN_ADDITIONAL_REPO="http://www.dronecode.org.uk/cygwin/"
+set CYGWIN_ADDITIONAL_REPO_KEY="http://www.dronecode.org.uk/cygwin/dronecode.gpg"
 
 if _%arch%_ == _x64_ set SETUP=setup-x86_64.exe && set CYGWIN_ROOT=C:\cygwin64
 if _%arch%_ == _x86_ set SETUP=setup-x86.exe && set CYGWIN_ROOT=C:\cygwin
@@ -7,5 +9,5 @@ if _%arch%_ == _x86_ set SETUP=setup-x86.exe && set CYGWIN_ROOT=C:\cygwin
 if not exist %CACHE% mkdir %CACHE%
 
 echo Updating Cygwin and installing ninja and test prerequisites
-%CYGWIN_ROOT%\%SETUP% -qnNdO -R "%CYGWIN_ROOT%" -s "%CYGWIN_MIRROR%" -l "%CACHE%" -g -P "ninja,gcc-objc,gcc-objc++,libglib2.0-devel,zlib-devel,python3-pip"
+%CYGWIN_ROOT%\%SETUP% -qnNdO -R "%CYGWIN_ROOT%" -s "%CYGWIN_MIRROR%" -s "%CYGWIN_ADDITIONAL_REPO%" -K "%CYGWIN_ADDITIONAL_REPO_KEY%" -l "%CACHE%" -g -P "ninja,gcc-objc,gcc-objc++,libglib2.0-devel,zlib-devel,python3-pip"
 echo Install done
