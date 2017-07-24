0aa71d680898fd2812a3038a57eb9f9231af1de4
nyorain <nyorain@gmail.com>
2017-07-24 21:03:26 +0200

    Fix vulkan test

--- test cases/frameworks/17 vulkan/vulkanprog.c
+++ test cases/frameworks/17 vulkan/vulkanprog.c
@@ -3,21 +3,11 @@
 
 int main()
 {
-    VkApplicationInfo application_info = {
-        VK_STRUCTURE_TYPE_APPLICATION_INFO,
-        	NULL,
-        	"sway",
-        	VK_MAKE_VERSION(1,0,0),
-        	"wlroots",
-        	VK_MAKE_VERSION(1,0,0),
-        	VK_MAKE_VERSION(1,0,0)
-    };
-
     VkInstanceCreateInfo instance_create_info = {
         	VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO,
         	NULL,
         	0,
-        	&application_info,
+        	NULL,
         	0,
         	NULL,
         	0,
@@ -31,5 +21,6 @@ int main()
       	return ret;
     }
 
+    vkDestroyInstance(instance, NULL);
     return 0;    
 }
