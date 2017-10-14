mesonbuild/meson

commit 68bd64cd38f40eedddbe8e96ea6a61c746d1b78b

Prevent projects from directly grabbing files from other subprojects.

mesonbuild/interpreter.py
@@ -2785,6 +2785,40 @@ different subdirectory.
         super().run()
         mlog.log('Build targets in project:', mlog.bold(str(len(self.build.targets))))
 
+
+    # Check that the indicated file is within the same subproject
+    # as we currently are. This is to stop people doing
+    # nasty things like:
+    #
+    # f = files('../../master_src/file.c')
+    #
+    # Note that this is validated only when the file
+    # object is generated. The result can be used in a different
+    # subproject than it is defined in (due to e.g. a
+    # declare_dependency).
+    def validate_within_subproject(self, subdir, fname):
+        norm = os.path.normpath(os.path.join(subdir, fname))
+        if os.path.isabs(norm):
+            if not norm.startswith(self.environment.source_dir):
+                # Grabbing files outside the source tree is ok.
+                # This is for vendor stuff like:
+                #
+                # /opt/vendorsdk/src/file_with_license_restrictions.c
+                return
+            norm = os.path.relpath(norm, self.environment.source_dir)
+            assert(not os.path.isabs(norm))
+        segments = norm.split(os.path.sep)
+        num_sps = segments.count(self.subproject_dir)
+        if num_sps == 0:
+            if self.subproject == '':
+                return
+            raise InterpreterException('Sandbox violation: Tried to grab file %s from a different subproject.' % segments[-1])
+        if num_sps > 1:
+            raise InterpreterException('Sandbox violation: Tried to grab file %s from a nested subproject.' % segments[-1])
+        sproj_name = segments[segments.index(self.subproject_dir) + 1]
+        if sproj_name != self.subproject:
+            raise InterpreterException('Sandbox violation: Tried to grab file %s from a different subproject.' % segments[-1])
+
     def source_strings_to_files(self, sources):
         results = []
         for s in sources:
@@ -2792,6 +2826,7 @@ different subdirectory.
                               CustomTargetHolder, CustomTargetIndexHolder)):
                 pass
             elif isinstance(s, str):
+                self.validate_within_subproject(self.subdir, s)
                 s = mesonlib.File.from_source_file(self.environment.source_dir, self.subdir, s)
             else:
                 raise InterpreterException('Source item is {!r} instead of '

test cases/failing/63 subproj filegrab/meson.build
@@ -1,0 +1,3 @@
+project('mainproj', 'c')
+
+subproject('a')

test cases/failing/63 subproj filegrab/subprojects/a/meson.build
@@ -1,0 +1,3 @@
+project('a', 'c')
+
+executable('prog', '../../prog.c')

test cases/failing/63 subproj filegrab/prog.c
@@ -1,0 +1,1 @@
+int main(int argc, char **argv) { return 0; }

commit 7f71999010865898a1fe29b39ec94eaae90e86c1

fix all subproject using wrap-file broken
subproject using wrap-file is broken since commit (68bd64c Prevent
projects from directly grabbing files from other subprojects. )

subproject with wrap-file usually have version number after name

- **sproj_name** is `zlib-1.2.8` according to `directory = zlib-1.2.8`
of zlib.wrap
- but **self.subproject** `zlib`

mesonbuild/interpreter.py
@@ -1373,6 +1373,9 @@ class Interpreter(InterpreterBase):
         self.coredata = self.environment.get_coredata()
         self.backend = backend
         self.subproject = subproject
+        # Subproject directory is usually the name of the subproject, but can
+        # be different for dependencies provided by wrap files.
+        self.subproject_directory_name = subdir.split(os.path.sep)[-1]
         self.subproject_dir = subproject_dir
         self.option_file = os.path.join(self.source_root, self.subdir, 'meson_options.txt')
         self.load_root_meson_file()
@@ -2785,7 +2788,6 @@ different subdirectory.
         super().run()
         mlog.log('Build targets in project:', mlog.bold(str(len(self.build.targets))))
 
-
     # Check that the indicated file is within the same subproject
     # as we currently are. This is to stop people doing
     # nasty things like:
@@ -2816,7 +2818,7 @@ different subdirectory.
         if num_sps > 1:
             raise InterpreterException('Sandbox violation: Tried to grab file %s from a nested subproject.' % segments[-1])
         sproj_name = segments[segments.index(self.subproject_dir) + 1]
-        if sproj_name != self.subproject:
+        if sproj_name != self.subproject_directory_name:
             raise InterpreterException('Sandbox violation: Tried to grab file %s from a different subproject.' % segments[-1])
 
     def source_strings_to_files(self, sources):

test cases/common/162 wrap file should not failed/meson.build
@@ -1,0 +1,3 @@
+project('mainproj', 'c')
+
+subproject('zlib')

test cases/common/162 wrap file should not failed/subprojects/packagecache/zlib-1.2.8-8-wrap.zip
@@ -1,0 +1,1 @@
+dummy

test cases/common/162 wrap file should not failed/subprojects/packagecache/zlib-1.2.8.tar.gz
@@ -1,0 +1,1 @@
+dummy

test cases/common/162 wrap file should not failed/subprojects/zlib-1.2.8/meson.build
@@ -1,0 +1,2 @@
+project('shared lib', 'c')
+shared_library('foo', 'foo.c')

test cases/common/162 wrap file should not failed/subprojects/zlib.wrap
@@ -1,0 +1,10 @@
+[wrap-file]
+directory = zlib-1.2.8
+
+source_url = http://zlib.net/fossils/zlib-1.2.8.tar.gz
+source_filename = zlib-1.2.8.tar.gz
+source_hash = 36658cb768a54c1d4dec43c3116c27ed893e88b02ecfcb44f2166f9c0b7f2a0d
+
+patch_url = https://wrapdb.mesonbuild.com/v1/projects/zlib/1.2.8/8/get_zip
+patch_filename = zlib-1.2.8-8-wrap.zip
+patch_hash = 17c52a0e0c59ce926d3959005d5cd8178c6c7e2c9a4a1304279a8320c955ac60