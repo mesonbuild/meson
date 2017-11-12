fa6f01d096fbf3d84d68070d6592cd8d5b761e1b
Jussi Pakkanen <jpakkane@gmail.com>
2017-11-12 23:31:45 +0200

    Use absolute paths for rpaths on OSX.

--- mesonbuild/compilers/c.py
+++ mesonbuild/compilers/c.py
@@ -90,6 +90,8 @@ class CCompiler(Compiler):
 
     # The default behavior is this, override in MSVC
     def build_rpath_args(self, build_dir, from_dir, rpath_paths, build_rpath, install_rpath):
+        if self.id == 'clang' and self.clang_type == compilers.CLANG_OSX:
+            return self.build_osx_rpath_args(build_dir, rpath_paths, build_rpath)
         return self.build_unix_rpath_args(build_dir, from_dir, rpath_paths, build_rpath, install_rpath)
 
     def get_dependency_gen_args(self, outtarget, outfile):
