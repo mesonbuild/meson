632d12f2810b9ee6bb494ad554626fc5abe47835
Benjamin Redelings <benjamin.redelings@gmail.com>
2018-02-07 21:50:15 -0500

    Add the right directory to linker search path.

--- mesonbuild/dependencies/misc.py
+++ mesonbuild/dependencies/misc.py
@@ -463,7 +463,7 @@ class BoostDependency(ExternalDependency):
     def get_link_args(self):
         args = []
         for dir in self.extra_lib_dirs():
-            args += self.compiler.get_linker_search_args(self.libdir)
+            args += self.compiler.get_linker_search_args(dir)
         for lib in self.requested_modules:
             args += self.lib_modules['boost_' + lib]
         return args
