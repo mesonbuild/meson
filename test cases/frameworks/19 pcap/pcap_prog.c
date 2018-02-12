7a618d2817599d1474a5f7a3e71ecfdd1af24d76
Jon Turney <jon.turney@dronecode.org.uk>
2018-02-12 18:26:19 +0000

    Fix test cases/frameworks/19 pcap for OSX

--- test cases/frameworks/19 pcap/pcap_prog.c
+++ test cases/frameworks/19 pcap/pcap_prog.c
@@ -4,6 +4,12 @@ int
 main()
 {
     char errbuf[PCAP_ERRBUF_SIZE];
-    pcap_t *p = pcap_create(NULL, errbuf);
+#ifdef __APPLE__
+    // source = NULL for "any" doesn't work on macOS (linux only?)
+    char *source = "en0";
+#else
+    char *source = NULL;
+#endif
+    pcap_t *p = pcap_create(source, errbuf);
     return p == NULL;
 }
