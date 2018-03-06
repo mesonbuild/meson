c64bae148465d0ee8ac6bf026836024c2ac56d5d
Mathieu Duponchelle <mathieu@centricular.com>
2018-03-07 00:19:53 +0100

    Implement python 2 module
    Factor out a base class from the python 3 module, subclasses
    only need to implement _find_python.

--- mesonbuild/modules/python2.py
+++ mesonbuild/modules/python2.py
@@ -1,4 +1,4 @@
-# Copyright 2016-2017 The Meson development team
+# Copyright 2018 The Meson development team
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -12,14 +12,14 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from .. import mesonlib, dependencies
+from .. import dependencies
 from .python_common import PythonModule
 
 
-class Python3Module(PythonModule):
+class Python2Module(PythonModule):
     def _find_python(self):
-        return dependencies.ExternalProgram('python3', mesonlib.python_command, silent=True)
+        return dependencies.ExternalProgram('python2', silent = True)
 
 
 def initialize():
-    return Python3Module()
+    return Python2Module()
--- run_project_tests.py
+++ run_project_tests.py
@@ -509,6 +509,7 @@ def detect_tests_to_run():
         ('fortran', 'fortran', backend is not Backend.ninja or not shutil.which('gfortran')),
         ('swift', 'swift', backend not in (Backend.ninja, Backend.xcode) or not shutil.which('swiftc')),
         ('python3', 'python3', backend is not Backend.ninja),
+        ('python2', 'python2', backend is not Backend.ninja),
         ('fpga', 'fpga', shutil.which('yosys') is None),
         ('frameworks', 'frameworks', False),
     ]
