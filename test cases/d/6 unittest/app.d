ad2ab56e5f000d8be05e3e6d780e3aab00fddbce
Matthias Klumpp <matthias@tenstral.net>
2018-03-29 15:10:02 +0200

    Add test to check that D feature flags get applied to all files
    This adds regression tests for issue #3337

--- test cases/d/6 unittest/app.d
+++ test cases/d/6 unittest/app.d
@@ -23,9 +23,13 @@ unittest
 {
     writeln ("TEST");
     import core.stdc.stdlib : exit;
+    import second_unit;
 
     assert (getFour () > 2);
     assert (getFour () == 4);
+
+    // this is a regression test for https://github.com/mesonbuild/meson/issues/3337
+    secondModuleTestFunc ();
 
     // we explicitly terminate here to give the unittest program a different exit
     // code than the main application has.
