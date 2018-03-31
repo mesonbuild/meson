16c4a3b4f8bfb353603bca72c20484ecbcb134e6
Alexis Jeandet <alexis.jeandet@member.fsf.org>
2018-03-31 10:38:09 +0200

    Added link to documentation and examples for find_library replacement (#3346)

--- mesonbuild/interpreter.py
+++ mesonbuild/interpreter.py
@@ -2311,7 +2311,10 @@ to directly access options of other subprojects.''')
         return progobj
 
     def func_find_library(self, node, args, kwargs):
-        raise InvalidCode('find_library() is removed, use the corresponding method in a compiler object instead.')
+        raise InvalidCode('find_library() is removed, use meson.get_compiler(\'name\').find_library() instead.\n'
+                          'Look here for documentation: http://mesonbuild.com/Reference-manual.html#compiler-object\n'
+                          'Look here for example: http://mesonbuild.com/howtox.html#add-math-library-lm-portably\n'
+                          )
 
     def _find_cached_dep(self, name, kwargs):
         # Check if we want this as a cross-dep or a native-dep
