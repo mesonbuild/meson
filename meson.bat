ecb3d6328daf38d605b5cb7439be1e21b7609015
pal1000 <liviuprodea@yahoo.com>
2018-04-03 11:32:04 +0300

    Make the script more robust when it comes to finding Python. Can now locate python if: - python is PATH; - python.exe is in the current directory; - python.exe is in parent directory relative to the script but not in PATH (this din't work before).
    Python being in parent directory is tipical and it opens the posibility of running Pypi version of Meson even if Python is not in PATH by just using either relative or absolute path to the script itself to run it.

--- meson.bat
+++ meson.bat
@@ -1,18 +1,28 @@
+@REM Locate Python
 @REM Reset error level before checking if Python is reachable
 @set ERRORLEVEL=0
 @where /q python.exe
-@IF ERRORLEVEL 1 GOTO missingpython
-
-@REM Locate Python and make sure Meson is installed correctly
+@IF ERRORLEVEL 1 GOTO NotInPATHPython
 @FOR /F "tokens=* USEBACKQ" %%F IN (`where python.exe`) DO @SET pythonloc=%%F
-@IF NOT EXIST "%pythonloc:python.exe=Scripts\meson.py%" GOTO nomeson
+@set pythonloc="%pythonloc%"
+@GOTO CheckMeson
+
+:NotInPATHPython
+@set pythonloc="%~dp0
+@if NOT "%pythonloc:~-1%"=="\" pythonloc=%pythonloc%\
+@set pythonloc=%pythonloc%..\python.exe"
+@IF NOT EXIST %pythonloc% GOTO missingpython
+
+:CheckMeson
+@REM Make sure Meson is installed correctly
+@IF NOT EXIST %pythonloc:python.exe=Scripts\meson.py% GOTO nomeson
 
 @REM Launch Meson
-@"%pythonloc%" "%pythonloc:python.exe=Scripts\meson.py%" %*
+@%pythonloc% %pythonloc:python.exe=Scripts\meson.py% %*
 @GOTO exit
 
 :missingpython
-@echo Fatal: Couldm't find Python 3.x
+@echo Fatal: Couldn't find Python 3.x
 @GOTO exit
 
 :nomeson
