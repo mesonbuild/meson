ce9b13afd085e94a76c225d7b05f28664db41043
Nirbheek Chauhan <nirbheek@centricular.com>
2018-04-13 14:22:02 +0530

    custom targets: Don't replace \\ with /
    This is no longer required since we always use `/` as the path
    separator for file arguments on Windows now. The only effect this
    now has is to mangle the string args people pass to custom targets.
    
    Closes https://github.com/mesonbuild/meson/issues/1564

--- test cases/common/48 test args/copyfile.py
+++ test cases/common/48 test args/copyfile.py
@@ -3,4 +3,18 @@
 import sys
 import shutil
 
+# If either of these use `\` as the path separator, it will cause problems with
+# MSYS, MSYS2, and Cygwin tools that expect the path separator to always be
+# `/`. All Native-Windows tools also accept `/` as the path separator, so
+# it's fine to always use that for arguments.
+# See: https://github.com/mesonbuild/meson/issues/1564
+#
+# Note that this applies to both MinGW and MSVC toolchains since people use
+# MSYS tools with both, or use a mixed toolchain environment.
+if '\\' in sys.argv[1]:
+    raise RuntimeError('Found \\ in source arg {!r}'.format(sys.argv[1]))
+
+if '\\' in sys.argv[2]:
+    raise RuntimeError('Found \\ in dest arg {!r}'.format(sys.argv[2]))
+
 shutil.copyfile(sys.argv[1], sys.argv[2])
