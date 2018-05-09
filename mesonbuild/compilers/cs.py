ed701b5cb0b628c632a916defcbdda312cb00197
Nirbheek Chauhan <nirbheek@centricular.com>
2018-05-09 21:27:33 +0530

    Revert "Add macOS linker versioning information"
    This reverts commit fa6ca160548d7e8df9c4c724e6c96f5e004e5316.
    
    Closes https://github.com/mesonbuild/meson/issues/3550

--- mesonbuild/compilers/cs.py
+++ mesonbuild/compilers/cs.py
@@ -41,7 +41,7 @@ class CsCompiler(Compiler):
     def get_link_args(self, fname):
         return ['-r:' + fname]
 
-    def get_soname_args(self, prefix, shlib_name, suffix, path, soversion, version, is_shared_module):
+    def get_soname_args(self, prefix, shlib_name, suffix, path, soversion, is_shared_module):
         return []
 
     def get_werror_args(self):
