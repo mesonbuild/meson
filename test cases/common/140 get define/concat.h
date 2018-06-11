b19b714f374f7bbf9fe049ff7239f25d3ec5b108
Christoph Behle <behlec@gmail.com>
2018-06-11 19:50:48 +0200

    Add tests for concatenate_string_literals

--- test cases/common/140 get define/concat.h
+++ test cases/common/140 get define/concat.h
@@ -5,8 +5,19 @@
 #define TEST_VERSION_MINOR 0
 #define TEST_VERSION_BUGFIX 0
 
-#define TEST_VERSION_STR             \
-  TEST_STRINGIFY(TEST_VERSION_MAJOR) \
-  "." TEST_STRINGIFY(TEST_VERSION_MINOR) "." TEST_STRINGIFY(TEST_VERSION_BUGFIX)
+#define TEST_VERSION_STR                                       \
+    TEST_STRINGIFY(TEST_VERSION_MAJOR)                         \
+    "." TEST_STRINGIFY(TEST_VERSION_MINOR) "." TEST_STRINGIFY( \
+        TEST_VERSION_BUGFIX)
 
-// TEST_VERSION_STR "6" "." "0" "." "0"
+#define TEST_CONCAT_1 \
+    "ab"              \
+    "cd"              \
+    "ef"
+#define TEST_CONCAT_2 1
+#define TEST_CONCAT_3 1 2 3
+#define TEST_CONCAT_4 "ab" 1 "cd"
+#define TEST_CONCAT_5 \
+    "ab\""            \
+    "cd"
+#define TEST_CONCAT_6 "ab\" \"cd"
