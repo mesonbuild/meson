07fc2da58cd2058685b683ea622a62a16daaf2a0
Charles Barto <bartoc@umich.edu>
2018-06-21 15:53:14 -0400

    fix style in nim.py

--- mesonbuild/compilers/nim.py
+++ mesonbuild/compilers/nim.py
@@ -1,4 +1,5 @@
-import os.path, subprocess
+import os.path
+import subprocess
 from ..mesonlib import EnvironmentException, version_compare
 
 from .compilers import (
@@ -8,38 +9,44 @@ from .compilers import (
     CompilerArgs
 )
 
+
 class NimCompiler(Compiler):
+
     def __init__(self, exelist, version):
         self.language = 'nim'
         super().__init__(exelist, version)
         self.id = 'nim'
-    
+
     def sanity_check(self, work_dir, environment):
         source_name = os.path.join(work_dir, 'sanity.nim')
         output_name = os.path.join(work_dir, 'nimtest')
         with open(source_name, 'w') as ofile:
             pass
-        pc = subprocess.Popen(self.exelist + ['c'] + self.get_output_args(output_name) + [source_name], cwd=work_dir)
+        pc = subprocess.Popen(
+            self.exelist + ['c'] + self.get_output_args(output_name) + [source_name], cwd=work_dir)
         pc.wait()
         if pc.returncode != 0:
-            raise EnvironmentException('Nim compiler can not compile programs.')
+            raise EnvironmentException(
+                'Nim compiler can not compile programs.')
         if subprocess.call(output_name) != 0:
-            raise EnvironmentException("Executables created by Nim compiler are not runnable.")
-    
+            raise EnvironmentException(
+                "Executables created by Nim compiler are not runnable.")
+
     def get_output_args(self, target):
         return ['--out:' + target]
 
     def get_outdir_args(self, outdir):
         return ['--nimcache:{}'.format(os.path.join(outdir, 'nimcache'))]
+
     def needs_static_linker(self):
         return False
 
     def name_string(self):
         return ' '.join(self.exelist)
-        
+
     def get_compile_only_args(self):
         return ['-c']
-    
+
     def get_dependency_gen_args(self, outtarget, outfile):
         return []
 
@@ -53,18 +60,13 @@ class NimCompiler(Compiler):
         return nim_buildtype_args[buildtype]
 
     def get_always_args(self):
-        return ['c',]
+        return ['c', ]
 
     def get_warn_args(self, warninglevel):
         return []
 
     def get_compiler_shared_lib_args(self):
         return ['--app:lib']
-    
+
     def get_compiler_static_lib_args(self):
         return ['--app:staticlib']
-        
-        
-
-
-    
