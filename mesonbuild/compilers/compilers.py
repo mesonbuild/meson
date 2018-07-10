d2abba9be2abffe95f374ce9c03c759f39a2535c
Alberto Sartori <alberto.sartori86@gmail.com>
2018-07-10 13:41:51 +0200

    implement get_link_whole_for() for IntelCompiler
    closes #3862

--- mesonbuild/compilers/compilers.py
+++ mesonbuild/compilers/compilers.py
@@ -1561,6 +1561,9 @@ class IntelCompiler:
         else:
             return ['-openmp']
 
+    def get_link_whole_for(self, args):
+        return ['-Wl,--whole-archive'] + args + ['-Wl,--no-whole-archive']
+
 
 class ArmCompiler:
     # Functionality that is common to all ARM family compilers.
