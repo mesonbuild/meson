503b6f88675e479eb881b3a9b8c860ff9fe5a5f7
Nirbheek Chauhan <nirbheek@centricular.com>
2018-08-11 04:50:55 +0530

    setup: Require setuptools and entry_points:
    This gives us a consistent experience and a simpler setup across all
    operating systems. Setuptools is available everywhere these days.
    
    Also remove deprecated wrapper entry points.
    
    This feature was in high demand from people on Windows.

--- setup.py
+++ setup.py
@@ -14,7 +14,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-import os
 import sys
 
 from mesonbuild.coredata import version
@@ -24,37 +23,11 @@ if sys.version_info < (3, 5, 0):
           'Meson requires Python 3.5.0 or greater')
     sys.exit(1)
 
-# We need to support Python installations that have nothing but the basic
-# Python installation. Use setuptools when possible and fall back to
-# plain distutils when setuptools is not available.
-try:
-    from setuptools import setup
-    from setuptools.command.install_scripts import install_scripts as orig
-except ImportError:
-    from distutils.core import setup
-    from distutils.command.install_scripts import install_scripts as orig
+from setuptools import setup
 
-class install_scripts(orig):
-    def run(self):
-        if sys.platform == 'win32':
-            super().run()
-            return
-
-        if not self.skip_build:
-            self.run_command('build_scripts')
-        self.outfiles = []
-        if not self.dry_run:
-            self.mkpath(self.install_dir)
-
-        # We want the files to be installed without a suffix on Unix
-        for infile in self.get_inputs():
-            infile = os.path.basename(infile)
-            in_built = os.path.join(self.build_dir, infile)
-            in_stripped = infile[:-3] if infile.endswith('.py') else infile
-            outfile = os.path.join(self.install_dir, in_stripped)
-            # NOTE: Mode is preserved by default
-            self.copy_file(in_built, outfile)
-            self.outfiles.append(outfile)
+# On windows, will create Scripts/meson.exe and Scripts/meson-script.py
+# Other platforms will create bin/meson
+entries = {'console_scripts': ['meson=mesonbuild.mesonmain:main']}
 
 setup(name='meson',
       version=version,
@@ -71,17 +44,8 @@ setup(name='meson',
                 'mesonbuild.modules',
                 'mesonbuild.scripts',
                 'mesonbuild.wrap'],
-      scripts=['meson.py',
-               'mesonconf.py',
-               'mesontest.py',
-               'mesonintrospect.py',
-               'wraptool.py'],
-      cmdclass={'install_scripts': install_scripts},
-      data_files=[('share/man/man1', ['man/meson.1',
-                                      'man/mesonconf.1',
-                                      'man/mesonintrospect.1',
-                                      'man/mesontest.1',
-                                      'man/wraptool.1']),
+      entry_points=entries,
+      data_files=[('share/man/man1', ['man/meson.1']),
                   ('share/polkit-1/actions', ['data/com.mesonbuild.install.policy'])],
       classifiers=['Development Status :: 5 - Production/Stable',
                    'Environment :: Console',
