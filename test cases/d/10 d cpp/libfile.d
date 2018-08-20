deb7331dabf989594df94c2abb1c698342503ba6
GoaLitiuM <goalitium@kapsi.fi>
2018-08-20 03:47:40 +0300

    Remove dependency to D runtime in mixed language tests
    It is undefined behaviour to call D I/O functions without initializing D runtime first. Simplify the test so it will work in all platforms.

--- test cases/d/10 d cpp/libfile.d
+++ test cases/d/10 d cpp/libfile.d
@@ -1,5 +1,5 @@
-import std.stdio;
+import core.stdc.stdio;
 
 extern (C++) void print_hello(int i) {
-    writefln("Hello. Here is a number printed with D: %d", i);
+    printf("Hello. Here is a number printed with D: %d\n", i);
 }
