0a5ff338012a00f32c3aa9d8773835accc3e4e5b
Jussi Pakkanen <jpakkane@gmail.com>
2018-08-25 11:08:00 +0300

    Update version numbers for release.

--- mesonbuild/coredata.py
+++ mesonbuild/coredata.py
@@ -22,7 +22,7 @@ from .mesonlib import default_libdir, default_libexecdir, default_prefix
 import ast
 import argparse
 
-version = '0.47.1'
+version = '0.47.2'
 backendlist = ['ninja', 'vs', 'vs2010', 'vs2015', 'vs2017', 'xcode']
 
 default_yielding = False
