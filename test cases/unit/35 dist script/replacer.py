bfee6b3289943aaa7b158d0244cbc377538ab8d7
Jan Tojnar <jtojnar@gmail.com>
2018-10-02 06:13:44 +0200

    Make custom dist scripts accept arguments.
    meson.add_dist_script, introduced in #3906, did not accept any arguments
    other than script name. Since all other meson.add_*_script methods
    do accept args, this makes the dist script accept them as well.

--- test cases/unit/35 dist script/replacer.py
+++ test cases/unit/35 dist script/replacer.py
@@ -2,11 +2,15 @@
 
 import os
 import pathlib
+import sys
+
+if len(sys.argv) < 3:
+    sys.exit('usage: replacer.py <pattern> <replacement>')
 
 source_root = pathlib.Path(os.environ['MESON_DIST_ROOT'])
 
 modfile = source_root / 'prog.c'
 
 contents = modfile.read_text()
-contents = contents.replace('"incorrect"', '"correct"')
+contents = contents.replace(sys.argv[1], sys.argv[2])
 modfile.write_text(contents)
