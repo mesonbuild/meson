aba8792b6657e3aa380586764a8b3ec567895a28
sghctoma <sghctoma@gmail.com>
2018-10-23 16:45:32 +0200

    Change default pkgconfig directory on FreeBSD
    FreeBSD uses ${PREFIX}/libdata/pkgconfig as default pkgconf search path
    for non-base libraries. This commit makes that directory the default
    install_dir in the pkgconfig module on FreeBSD.

--- mesonbuild/modules/pkgconfig.py
+++ mesonbuild/modules/pkgconfig.py
@@ -437,7 +437,10 @@ class PkgConfigModule(ExtensionModule):
         pcfile = filebase + '.pc'
         pkgroot = kwargs.get('install_dir', default_install_dir)
         if pkgroot is None:
-            pkgroot = os.path.join(state.environment.coredata.get_builtin_option('libdir'), 'pkgconfig')
+            if mesonlib.is_freebsd():
+                pkgroot = os.path.join(state.environment.coredata.get_builtin_option('prefix'), 'libdata', 'pkgconfig')
+            else:
+                pkgroot = os.path.join(state.environment.coredata.get_builtin_option('libdir'), 'pkgconfig')
         if not isinstance(pkgroot, str):
             raise mesonlib.MesonException('Install_dir must be a string.')
         self.generate_pkgconfig_file(state, deps, subdirs, name, description, url,
