0859442480d4e0f607100a3bd62224156474a6b2
Jussi Pakkanen <jpakkane@gmail.com>
2018-11-10 21:18:03 +0200

    Refactor check to its own function.

--- test cases/common/152 simd/simdtest.h
+++
@@ -1,9 +0,0 @@
-memcpy(four, four_initial, sizeof(four_initial));
-printf("Using %s.\n", type);
-fptr(four);
-for(i=0; i<4; i++) {
-    if(four[i] != expected[i]) {
-        printf("Increment function failed, got %f expected %f.\n", four[i], expected[i]);
-        r=1;
-    }
-}
