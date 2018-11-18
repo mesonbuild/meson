mesonbuild/meson

commit 92487ea33db9d882929d755308853d2ed82abd0d

Add partial_dependency method to dependencies
This adds a new method, partial_dependency to all dependencies. These
sub dependencies are copies of the original dependency, but with one or
more of the attributes replaced with an empty list. This allows creating
a sub dependency that has only cflags or drops link_arguments, for
example.

mesonbuild/dependencies/base.py
@@ -1,4 +1,4 @@
-# Copyright 2013-2017 The Meson development team
+# Copyright 2013-2018 The Meson development team
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -15,6 +15,7 @@
 # This file contains the detection logic for external dependencies.
 # Custom logic for several other packages are in separate files.
 
+import copy
 import os
 import re
 import stat
@@ -146,6 +147,23 @@ class Dependency:
     def get_configtool_variable(self, variable_name):
         raise DependencyException('{!r} is not a config-tool dependency'.format(self.name))
 
+    def get_partial_dependency(self, *, compile_args=False, link_args=False,
+                               links=False, includes=False, sources=False):
+        """Create a new dependency that contains part of the parent dependency.
+
+        The following options can be inherited:
+            links -- all link_with arguemnts
+            includes -- all include_directory and -I/-isystem calls
+            sources -- any source, header, or generated sources
+            compile_args -- any compile args
+            link_args -- any link args
+
+        Additionally the new dependency will have the version parameter of it's
+        parent (if any) and the requested values of any dependencies will be
+        added as well.
+        """
+        RuntimeError('Unreachable code in partial_dependency called')
+
 
 class InternalDependency(Dependency):
     def __init__(self, version, incdirs, compile_args, link_args, libraries, whole_libraries, sources, ext_deps):
@@ -167,6 +185,21 @@ class InternalDependency(Dependency):
     def get_configtool_variable(self, variable_name):
         raise DependencyException('Method "get_configtool_variable()" is '
                                   'invalid for an internal dependency')
+
+    def get_partial_dependency(self, *, compile_args=False, link_args=False,
+                               links=False, includes=False, sources=False):
+        compile_args = self.compile_args.copy() if compile_args else []
+        link_args = self.link_args.copy() if link_args else []
+        libraries = self.libraries.copy() if links else []
+        whole_libraries = self.whole_libraries.copy() if links else []
+        sources = self.sources.copy() if sources else []
+        includes = self.include_directories.copy() if includes else []
+        deps = [d.get_partial_dependency(
+            compile_args=compile_args, link_args=link_args, links=links,
+            includes=includes, sources=sources) for d in self.ext_deps]
+        return InternalDependency(
+            self.version, includes, compile_args, link_args, libraries,
+            whole_libraries, sources, deps)
 
 
 class ExternalDependency(Dependency):
@@ -210,6 +243,18 @@ class ExternalDependency(Dependency):
 
     def get_compiler(self):
         return self.compiler
+
+    def get_partial_dependency(self, *, compile_args=False, link_args=False,
+                               links=False, includes=False, sources=False):
+        new = copy.copy(self)
+        if not compile_args:
+            new.compile_args = []
+        if not link_args:
+            new.link_args = []
+        if not sources:
+            new.sources = []
+
+        return new
 
 
 class ConfigToolDependency(ExternalDependency):
@@ -886,6 +931,15 @@ class ExternalLibrary(ExternalDependency):
            (language == 'vala' and self.language != 'vala'):
             return []
         return self.link_args
+
+    def get_partial_dependency(self, *, compile_args=False, link_args=False,
+                               links=False, includes=False, sources=False):
+        # External library only has link_args, so ignore the rest of the
+        # interface.
+        new = copy.copy(self)
+        if not link_args:
+            new.link_args = []
+        return new
 
 
 class ExtraFrameworkDependency(ExternalDependency):

mesonbuild/interpreter.py
@@ -275,6 +275,7 @@ class DependencyHolder(InterpreterObject, ObjectHolder):
                              'version': self.version_method,
                              'get_pkgconfig_variable': self.pkgconfig_method,
                              'get_configtool_variable': self.configtool_method,
+                             'partial_dependency': self.partial_dependency_method,
                              })
 
     def type_name_method(self, args, kwargs):
@@ -306,12 +307,18 @@ class DependencyHolder(InterpreterObject, ObjectHolder):
             raise InterpreterException('Variable name must be a string.')
         return self.held_object.get_configtool_variable(varname)
 
+    def partial_dependency_method(self, args, kwargs):
+        if args:
+            raise InterpreterException('partial_dependency takes no positional arguments')
+        return DependencyHolder(self.held_object.get_partial_dependency(**kwargs))
+
 class InternalDependencyHolder(InterpreterObject, ObjectHolder):
     def __init__(self, dep):
         InterpreterObject.__init__(self)
         ObjectHolder.__init__(self, dep)
         self.methods.update({'found': self.found_method,
                              'version': self.version_method,
+                             'partial_dependency': self.partial_dependency_method,
                              })
 
     def found_method(self, args, kwargs):
@@ -319,6 +326,11 @@ class InternalDependencyHolder(InterpreterObject, ObjectHolder):
 
     def version_method(self, args, kwargs):
         return self.held_object.get_version()
+
+    def partial_dependency_method(self, args, kwargs):
+        if args:
+            raise InterpreterException('get_partial_dependency takes no positional arguments')
+        return DependencyHolder(self.held_object.get_partial_dependency(**kwargs))
 
 class ExternalProgramHolder(InterpreterObject, ObjectHolder):
     def __init__(self, ep):
@@ -346,7 +358,9 @@ class ExternalLibraryHolder(InterpreterObject, ObjectHolder):
     def __init__(self, el):
         InterpreterObject.__init__(self)
         ObjectHolder.__init__(self, el)
-        self.methods.update({'found': self.found_method})
+        self.methods.update({'found': self.found_method,
+                             'partial_dependency': self.partial_dependency_method,
+                             })
 
     def found(self):
         return self.held_object.found()
@@ -365,6 +379,11 @@ class ExternalLibraryHolder(InterpreterObject, ObjectHolder):
 
     def get_exe_args(self):
         return self.held_object.get_exe_args()
+
+    def partial_dependency_method(self, args, kwargs):
+        if args:
+            raise InterpreterException('partial_dependency takes no positional arguments')
+        return DependencyHolder(self.held_object.get_partial_dependency(**kwargs))
 
 class GeneratorHolder(InterpreterObject, ObjectHolder):
     def __init__(self, interpreter, args, kwargs):

test cases/common/191 partial dependency/declare_dependency/headers/foo.c
@@ -1,0 +1,16 @@
+/* Copyright © 2018 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#error "Included C sources that shouldn't be."

test cases/common/191 partial dependency/declare_dependency/headers/foo.h
@@ -1,0 +1,16 @@
+/* Copyright © 2018 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+int foo(void);

test cases/common/191 partial dependency/declare_dependency/main.c
@@ -1,0 +1,25 @@
+/* Copyright © 2018 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "foo.h"
+
+int main() {
+    int a = foo();
+    if (a == 1) {
+        return 0;
+    } else {
+        return 1;
+    }
+}

test cases/common/191 partial dependency/declare_dependency/meson.build
@@ -1,0 +1,28 @@
+# Copyright © 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+dec_dep = declare_dependency(
+  sources : files('headers/foo.c'),
+  include_directories : include_directories('headers'),
+)
+
+sub_dep = dec_dep.partial_dependency(includes : true)
+
+dec_exe = executable(
+  'declare_dep',
+  files('main.c', 'other.c'),
+  dependencies : sub_dep,
+)
+
+test('Declare Dependency', dec_exe)

test cases/common/191 partial dependency/declare_dependency/other.c
@@ -1,0 +1,20 @@
+/* Copyright © 2018 Intel Corporation
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#include "foo.h"
+
+int foo(void) {
+    return 1;
+}

test cases/common/191 partial dependency/meson.build
@@ -1,0 +1,17 @@
+# Copyright © 2018 Intel Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+project('partial dependency', ['c', 'cpp'])
+
+subdir('declare_dependency')

commit 09f8d86f238c6c036978397b1c064f09a04cf64c

lgtm: Instantiating an exception, but not raising it
Instantiating an exception, but not raising it, has no effect
This alert was introduced in 92487ea 10 months ago

mesonbuild/dependencies/base.py
@@ -174,7 +174,7 @@ class Dependency:
         parent (if any) and the requested values of any dependencies will be
         added as well.
         """
-        RuntimeError('Unreachable code in partial_dependency called')
+        raise RuntimeError('Unreachable code in partial_dependency called')
 
 
 class InternalDependency(Dependency):