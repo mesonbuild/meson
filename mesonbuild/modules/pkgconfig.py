mesonbuild/meson

commit 2cbf7caf5c8d81e40c790d41cc3b87d30b40cc9f

Nirbheek/fix pkgconfig library dedup (#3813)
* Add a test case for bad de-dup of -framework args

https://github.com/mesonbuild/meson/issues/3800

* pkgconfig: Don't naively de-dup all arguments

Honestly don't know what I was smoking. Of course the `Libs:` field in
a pkg-config file can have arguments other than -l and -L

Closes https://github.com/mesonbuild/meson/issues/3800

* pkgconfig module: Fix needlessly aggressive de-dup

mesonbuild/modules/pkgconfig.py
@@ -170,16 +170,18 @@ class DependenciesHelper:
         return ', '.join(result)
 
     def remove_dups(self):
-        def _fn(xs):
+        def _fn(xs, libs=False):
             # Remove duplicates whilst preserving original order
             result = []
             for x in xs:
-                if x not in result:
+                # Don't de-dup unknown strings to avoid messing up arguments like:
+                # ['-framework', 'CoreAudio', '-framework', 'CoreMedia']
+                if x not in result or (libs and (isinstance(x, str) and not x.endswith(('-l', '-L')))):
                     result.append(x)
             return result
-        self.pub_libs = _fn(self.pub_libs)
+        self.pub_libs = _fn(self.pub_libs, True)
         self.pub_reqs = _fn(self.pub_reqs)
-        self.priv_libs = _fn(self.priv_libs)
+        self.priv_libs = _fn(self.priv_libs, True)
         self.priv_reqs = _fn(self.priv_reqs)
         self.cflags = _fn(self.cflags)
 

test cases/unit/33 external, internal library rpath/external library/meson.build
@@ -5,5 +5,5 @@ l = shared_library('faa_pkg', 'faa.c', install: true)
 
 pkg = import('pkgconfig')
 pkg.generate(name: 'faa_pkg',
-             libraries: l,
+             libraries: [l, '-framework', 'CoreFoundation', '-framework', 'CoreMedia'],
              description: 'FAA, a pkg-config test library')

mesonbuild/dependencies/base.py
@@ -589,9 +589,12 @@ class PkgConfigDependency(ExternalDependency):
         if ret != 0:
             raise DependencyException('Could not generate libs for %s:\n\n%s' %
                                       (self.name, out))
-        # These libraries should be safe to de-dup
-        link_args = OrderedSet()
+        link_args = []
+        # Library paths should be safe to de-dup
         libpaths = OrderedSet()
+        # Track -lfoo libraries to avoid duplicate work
+        libs_found = OrderedSet()
+        # Track not-found libraries to know whether to add library paths
         libs_notfound = []
         libtype = 'static' if self.static else 'default'
         # We always look for the file ourselves instead of depending on the
@@ -609,6 +612,9 @@ class PkgConfigDependency(ExternalDependency):
                 libpaths.add(lib[2:])
                 continue
             elif lib.startswith('-l'):
+                # Don't resolve the same -lfoo argument again
+                if lib in libs_found:
+                    continue
                 if self.clib_compiler:
                     args = self.clib_compiler.find_library(lib[2:], self.env,
                                                            list(libpaths), libtype)
@@ -618,10 +624,11 @@ class PkgConfigDependency(ExternalDependency):
                 else:
                     args = None
                 if args:
+                    libs_found.add(lib)
                     # Replace -l arg with full path to library if available
+                    # else, library is provided by the compiler and can't be resolved
                     if not args[0].startswith('-l'):
                         lib = args[0]
-                    # else, library is provided by the compiler and can't be resolved
                 else:
                     # Library wasn't found, maybe we're looking in the wrong
                     # places or the library will be provided with LDFLAGS or
@@ -645,10 +652,12 @@ class PkgConfigDependency(ExternalDependency):
                     raise DependencyException('Got a libtools specific "%s" dependencies'
                                               'but we could not compute the actual shared'
                                               'library path' % lib)
-                lib = shared_lib
                 self.is_libtool = True
-            link_args.add(lib)
-        self.link_args = list(link_args)
+                lib = shared_lib
+                if lib in link_args:
+                    continue
+            link_args.append(lib)
+        self.link_args = link_args
         # Add all -Lbar args if we have -lfoo args in link_args
         if libs_notfound:
             # Order of -L flags doesn't matter with ld, but it might with other

commit 37aae1fd78dde8876042b4af02d46b0b0d5c0431

pkgconfig: Fix flag deduplication
This is a regression introduced by 2cbf7caf5, generated pkg-config files
have many duplicated '-pthread' flags.

mesonbuild/modules/pkgconfig.py
@@ -192,7 +192,11 @@ class DependenciesHelper:
             for x in xs:
                 # Don't de-dup unknown strings to avoid messing up arguments like:
                 # ['-framework', 'CoreAudio', '-framework', 'CoreMedia']
-                if x not in result or (libs and (isinstance(x, str) and not x.endswith(('-l', '-L')))):
+                known_flags = ['-pthread']
+                cannot_dedup = libs and isinstance(x, str) and \
+                    not x.startswith(('-l', '-L')) and \
+                    x not in known_flags
+                if x not in result or cannot_dedup:
                     result.append(x)
             return result
         self.pub_libs = _fn(self.pub_libs, True)

run_unittests.py
@@ -3650,26 +3650,30 @@ class LinuxlikeTests(BasePlatformTests):
         privatedir2 = self.privatedir
 
         os.environ['PKG_CONFIG_LIBDIR'] = os.pathsep.join([privatedir1, privatedir2])
-        cmd = ['pkg-config', 'dependency-test']
+        self._run(['pkg-config', 'dependency-test', '--validate'])
 
-        out = self._run(cmd + ['--print-requires']).strip().split('\n')
-        self.assertEqual(sorted(out), sorted(['libexposed']))
-
-        out = self._run(cmd + ['--print-requires-private']).strip().split('\n')
-        self.assertEqual(sorted(out), sorted(['libfoo >= 1.0']))
-
-        out = self._run(cmd + ['--cflags-only-other']).strip().split()
-        self.check_pkg_flags_are_same(out, ['-pthread', '-DCUSTOM'])
-
-        out = self._run(cmd + ['--libs-only-l', '--libs-only-other']).strip().split()
-        self.check_pkg_flags_are_same(out, ['-pthread', '-lcustom',
-                                            '-llibmain', '-llibexposed'])
-
-        out = self._run(cmd + ['--libs-only-l', '--libs-only-other', '--static']).strip().split()
-        self.check_pkg_flags_are_same(out, ['-pthread', '-lcustom',
-                                            '-llibmain', '-llibexposed',
-                                            '-llibinternal', '-lcustom2',
-                                            '-lfoo'])
+        # pkg-config strips some duplicated flags so we have to parse the
+        # generated file ourself.
+        expected = {
+            'Requires': 'libexposed',
+            'Requires.private': 'libfoo >= 1.0',
+            'Libs': '-L${libdir} -llibmain -pthread -lcustom',
+            'Libs.private': '-lcustom2 -L${libdir} -llibinternal',
+            'Cflags': '-I${includedir} -pthread -DCUSTOM',
+        }
+        if is_osx() or is_haiku():
+            expected['Cflags'] = expected['Cflags'].replace('-pthread ', '')
+        with open(os.path.join(privatedir2, 'dependency-test.pc')) as f:
+            matched_lines = 0
+            for line in f:
+                parts = line.split(':', 1)
+                if parts[0] in expected:
+                    key = parts[0]
+                    val = parts[1].strip()
+                    expected_val = expected[key]
+                    self.assertEqual(expected_val, val)
+                    matched_lines += 1
+            self.assertEqual(len(expected), matched_lines)
 
         cmd = ['pkg-config', 'requires-test']
         out = self._run(cmd + ['--print-requires']).strip().split('\n')
@@ -3678,11 +3682,6 @@ class LinuxlikeTests(BasePlatformTests):
         cmd = ['pkg-config', 'requires-private-test']
         out = self._run(cmd + ['--print-requires-private']).strip().split('\n')
         self.assertEqual(sorted(out), sorted(['libexposed', 'libfoo >= 1.0', 'libhello']))
-
-    def check_pkg_flags_are_same(self, output, expected):
-        if is_osx() or is_haiku():
-            expected = [x for x in expected if x != '-pthread']
-        self.assertEqual(sorted(output), sorted(expected))
 
     def test_pkg_unfound(self):
         testdir = os.path.join(self.unit_test_dir, '23 unfound pkgconfig')

test cases/common/48 pkgconfig-gen/dependencies/meson.build
@@ -28,8 +28,8 @@ custom2_dep = declare_dependency(link_args : ['-lcustom2'], compile_args : ['-DC
 # - Having pc_dep in libraries_private should add it in Requires.private
 # - pc_dep_dup is the same library and same version, should be ignored
 # - notfound_dep is not required so it shouldn't appear in the pc file.
-pkgg.generate(libraries : [main_lib, exposed_lib, threads_dep , custom_dep],
-  libraries_private : [custom_dep, custom2_dep, pc_dep, pc_dep_dup, notfound_dep],
+pkgg.generate(libraries : [main_lib, exposed_lib, threads_dep, threads_dep, custom_dep, custom_dep, '-pthread'],
+  libraries_private : [custom_dep, custom2_dep, custom2_dep, pc_dep, pc_dep_dup, notfound_dep],
   version : '1.0',
   name : 'dependency-test',
   filebase : 'dependency-test',