9c387c56b432690289bcdace66026d73d82def23
Randy <randy408@protonmail.com>
2019-01-15 18:19:16 +0200

    test: Prioritize generated headers even when indirectly included
    This test is for checking whether a generated header is prioritized over
    the source tree header when there might be a naming conflict and is
    included indirectly from a subproject e.g. in the case of SDL2
    where the include directory contains the "main" header SDL.h and a
    dummy config header SDL_config.h, the generated SDL_config.h should
    take priority over source tree header.

---
+++ test cases/common/211 generated header priority/main.c
@@ -0,0 +1,10 @@
+#include <mainheader.h>
+
+#ifndef STUFF_ONLY_THE_GENERATED_HEADER_DEFINES
+    #error Generated headers should be prioritized over source tree headers
+#endif
+
+int main(int argc, char **argv)
+{
+    return 0;
+}
