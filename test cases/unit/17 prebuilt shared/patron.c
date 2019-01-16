6c637b1780d02477b98c642e38f980eece7fbff7
Simon McVittie <smcv@collabora.com>
2019-01-16 18:51:31 +0000

    tests: Don't return an undefined value from main()
    This caused test failures while backporting meson to an old runtime
    environment.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- test cases/unit/17 prebuilt shared/patron.c
+++ test cases/unit/17 prebuilt shared/patron.c
@@ -5,4 +5,5 @@ int main(int argc, char **argv) {
     printf("You are standing outside the Great Library of Alexandria.\n");
     printf("You decide to go inside.\n\n");
     alexandria_visit();
+    return 0;
 }
