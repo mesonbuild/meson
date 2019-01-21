222f346f992f03cef730f85d73a9f9498e912d10
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-01-20 22:53:22 -0500

    correct fortran code

--- test cases/fortran/10 find library/gzip.f90
+++ test cases/fortran/10 find library/gzip.f90
@@ -1,35 +1,32 @@
 module gzip
 
-  use iso_c_binding, only: c_char, c_ptr, c_int
-  implicit none
+use iso_c_binding, only: c_char, c_ptr, c_int
+implicit none
 
-  interface
-     function gzopen(path, mode) bind(C)
-       import c_char, c_ptr
+interface
+type(c_ptr) function gzopen(path, mode) bind(C)
+import c_char, c_ptr
 
-       character(c_char), intent(in) :: path(*), mode(*)
-       type(c_ptr) :: gzopen
-     end function gzopen
-  end interface
+character(kind=c_char), intent(in) :: path(*), mode(*)
+end function gzopen
+end interface
 
-  interface
-     function gzwrite(file, buf, len) bind(C)
-       import c_int, c_ptr
+interface
+integer(c_int) function gzwrite(file, buf, len) bind(C)
+import c_int, c_ptr, c_char
 
-       type(c_ptr), value, intent(in) :: file
-       type(*), intent(in) :: buf
-       integer(c_int), value, intent(in) :: len
-       integer(c_int) :: gzwrite
-     end function gzwrite
-  end interface
+type(c_ptr), value, intent(in) :: file
+character(kind=c_char), intent(in) :: buf
+integer(c_int), value, intent(in) :: len
+end function gzwrite
+end interface
 
-  interface
-     function gzclose(file) bind(C)
-       import c_int, c_ptr
+interface
+integer(c_int) function gzclose(file) bind(C)
+import c_int, c_ptr
 
-       type(c_ptr), value, intent(in) :: file
-       integer(c_int) :: gzclose
-     end function gzclose
-  end interface
+type(c_ptr), value, intent(in) :: file
+end function gzclose
+end interface
 
 end module gzip
