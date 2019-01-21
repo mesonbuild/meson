eda8434af56b7926a186e68a721433668155230b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-01-20 23:16:45 -0500

    modernize

--- test cases/fortran/5 static/static_hello.f90
+++ test cases/fortran/5 static/static_hello.f90
@@ -1,17 +1,17 @@
 module static_hello
-  implicit none
+implicit none
 
-  private
-  public :: static_say_hello
+private
+public :: static_say_hello
 
-  interface static_say_hello
-     module procedure say_hello
-  end interface static_say_hello
+interface static_say_hello
+  module procedure say_hello
+end interface static_say_hello
 
 contains
 
-  subroutine say_hello
-    print *, "Static library called."
-  end subroutine say_hello
+subroutine say_hello
+  print *, "Static library called."
+end subroutine say_hello
 
 end module static_hello
