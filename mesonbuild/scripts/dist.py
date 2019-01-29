2bdb069012ce25abbf89909d7ae5636e86c25753
Dylan Baker <dylan@pnwbakers.com>
2019-01-29 14:41:11 -0800

    dist: Handle git worktrees, which have a .git file instead of dir
    This is the second most straight forward stupid way of handling
    this (with usiing os.path.exists) as the most stupid obvious way. The
    only major advantage is that having .git as something other than a
    file or directory still doesn't register.
    
    Fixes: #3378

--- mesonbuild/scripts/dist.py
+++ mesonbuild/scripts/dist.py
@@ -188,7 +188,8 @@ def run(args):
 
     dist_name = build.project_name + '-' + build.project_version
 
-    if os.path.isdir(os.path.join(src_root, '.git')):
+    _git = os.path.join(src_root, '.git')
+    if os.path.isdir(_git) or os.path.isfile(_git):
         names = create_dist_git(dist_name, src_root, bld_root, dist_sub, build.dist_scripts)
     elif os.path.isdir(os.path.join(src_root, '.hg')):
         names = create_dist_hg(dist_name, src_root, bld_root, dist_sub, build.dist_scripts)
