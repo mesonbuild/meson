08757dcdfd9ff3c9e79f448ce1ab796ca7d21cf0
Corentin Noël <corentin.noel@collabora.com>
2019-02-04 13:49:01 +0100

    vala: Support position-independent executables

--- mesonbuild/compilers/vala.py
+++ mesonbuild/compilers/vala.py
@@ -49,6 +49,12 @@ class ValaCompiler(Compiler):
     def get_pic_args(self):
         return []
 
+    def get_pie_args(self):
+        return []
+
+    def get_pie_link_args(self):
+        return []
+
     def get_always_args(self):
         return ['-C']
 
