974feda44657d33b511f8ec39ff66c5cc6491af3
Andrei Alexeyev <0x416b617269@gmail.com>
2019-02-18 04:19:11 +0200

    runpython: insert script's dir into import path
    Fixes #4947

--- mesonbuild/mesonmain.py
+++ mesonbuild/mesonmain.py
@@ -80,6 +80,7 @@ class CommandLineParser:
     def run_runpython_command(self, options):
         import runpy
         sys.argv[1:] = options.script_args
+        sys.path.insert(0, os.path.dirname(options.script_file))
         runpy.run_path(options.script_file, run_name='__main__')
         return 0
 
