f73b36c2f342ff9bef5ebc9ca49fa14f54b81d85
Daniel Mensinger <daniel@mensinger-ka.de>
2019-02-23 12:23:44 +0100

    cmake: Added test for configure_file

--- test cases/cmake/2 advanced/main.cpp
+++ test cases/cmake/2 advanced/main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
 #include <cmMod.hpp>
+#include "config.h"
+
+#if CONFIG_OPT != 42
+#error "Invalid value of CONFIG_OPT"
+#endif
 
 using namespace std;
 
--- test cases/cmake/2 advanced/subprojects/cmMod/lib/cmMod.cpp
+++ test cases/cmake/2 advanced/subprojects/cmMod/lib/cmMod.cpp
@@ -1,5 +1,10 @@
 #include "cmMod.hpp"
 #include <zlib.h>
+#include "config.h"
+
+#if CONFIG_OPT != 42
+#error "Invalid value of CONFIG_OPT"
+#endif
 
 using namespace std;
 
