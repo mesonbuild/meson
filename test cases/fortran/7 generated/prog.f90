f0ef7d218a5db4597adaa951738c63d980e4635e
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-02-25 02:07:38 -0500

    cleanup fortran example syntax

--- test cases/fortran/7 generated/prog.f90
+++ test cases/fortran/7 generated/prog.f90
@@ -1,9 +1,7 @@
 program prog
-  use mod2
-  implicit none
+use mod2
+implicit none
 
-  if (modval1 + modval2 /= 3) then
-    stop 1
-  end if
+if (modval1 + modval2 /= 3) stop 1
 
 end program prog
