b101e7a7a218917c16aa2db289f275766cba1fc0
jrl64 <32905389+jrl64@users.noreply.github.com>
2019-03-15 09:05:53 -0500

    Change GnuCCompiler to be consistent with ClangCCompiler

--- mesonbuild/compilers/c.py
+++ mesonbuild/compilers/c.py
@@ -1291,7 +1291,8 @@ class GnuCCompiler(GnuCompiler, CCompiler):
         opts = CCompiler.get_options(self)
         c_stds = ['c89', 'c99', 'c11']
         g_stds = ['gnu89', 'gnu99', 'gnu11']
-        if version_compare(self.version, '>=8.0.0'):
+        v = '>=8.0.0'
+        if version_compare(self.version, v):
             c_stds += ['c17', 'c18']
             g_stds += ['gnu17', 'gnu18']
         opts.update({'c_std': coredata.UserComboOption('c_std', 'C language standard to use',
