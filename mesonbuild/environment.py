8fe4a1824a61c2fda0b032948c4f7bf4eb7e5686
Ross Burton <ross.burton@intel.com>
2019-03-18 16:16:56 +0000

    load_configs: generalise the search path
    Instead of hard-coding the fact that load_configs() searches for files under
    meson/native, pass in the subdirectory allowing the cross-file code to use the
    same logic.

--- mesonbuild/environment.py
+++ mesonbuild/environment.py
@@ -408,7 +408,7 @@ class Environment:
 
         if self.coredata.config_files is not None:
             config = MesonConfigFile.from_config_parser(
-                coredata.load_configs(self.coredata.config_files))
+                coredata.load_configs(self.coredata.config_files, 'native'))
             self.binaries.build = BinaryTable(config.get('binaries', {}))
             self.paths.build = Directories(**config.get('paths', {}))
 
