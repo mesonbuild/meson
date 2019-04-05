42a04f7e230d3b8d4f5c46ffccf01ca7dfb94ef2
Michael Hirsch, Ph.D <10931741+scivision@users.noreply.github.com>
2019-04-05 16:48:20 -0400

    BUGFIX: Microsoft MPI is only compatible with Intel Fortran

--- test cases/frameworks/17 mpi/main.f90
+++ test cases/frameworks/17 mpi/main.f90
@@ -1,21 +1,30 @@
-program mpitest
-  implicit none
-  include 'mpif.h'
-  logical :: flag
-  integer :: ier
-  call MPI_Init(ier)
-  if (ier /= 0) then
-    print *, 'Unable to initialize MPI: ', ier
-    stop 1
-  endif
-  call MPI_Initialized(flag, ier)
-  if (ier /= 0) then
-    print *, 'Unable to check MPI initialization state: ', ier
-    stop 1
-  endif
-  call MPI_Finalize(ier)
-  if (ier /= 0) then
-    print *, 'Unable to finalize MPI: ', ier
-    stop 1
-  endif
-end program mpitest
+use, intrinsic :: iso_fortran_env, only: stderr=>error_unit
+use mpi
+
+implicit none
+
+logical :: flag
+integer :: ier
+
+call MPI_Init(ier)
+
+if (ier /= 0) then
+  write(stderr,*) 'Unable to initialize MPI', ier
+  stop 1
+endif
+
+call MPI_Initialized(flag, ier)
+if (ier /= 0) then
+  write(stderr,*) 'Unable to check MPI initialization state: ', ier
+  stop 1
+endif
+
+call MPI_Finalize(ier)
+if (ier /= 0) then
+  write(stderr,*) 'Unable to finalize MPI: ', ier
+  stop 1
+endif
+
+print *, "OK: Fortran MPI"
+
+end program
