54f9ec45104bd01c5679d4d1468ae96dec9e1fba
makise-homura <akemi_homura@kurisa.ch>
2019-04-19 18:02:01 +0300

    Fix concatenation of library dirs

--- mesonbuild/backend/ninjabackend.py
+++ mesonbuild/backend/ninjabackend.py
@@ -2418,7 +2418,7 @@ rule FORTRAN_DEP_HACK%s
         guessed_dependencies = []
         # TODO The get_library_naming requirement currently excludes link targets that use d or fortran as their main linker
         if hasattr(linker, 'get_library_naming'):
-            search_dirs = tuple(search_dirs) + linker.get_library_dirs(self.environment)
+            search_dirs = tuple(search_dirs) + tuple(linker.get_library_dirs(self.environment))
             static_patterns = linker.get_library_naming(self.environment, LibType.STATIC, strict=True)
             shared_patterns = linker.get_library_naming(self.environment, LibType.SHARED, strict=True)
             for libname in libs:
