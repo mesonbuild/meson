c0f61181d8fb57e8a68739a52170ec3dfea36291
Dylan Baker <dylan@pnwbakers.com>
2019-06-21 09:37:35 -0700

    c_function_attributes: remove 'protected' from 'visibility'
    This doesn't exist on macos with the apple compiler, which always causes
    failures.
    
    Fixes #5530

--- mesonbuild/compilers/c_function_attributes.py
+++ mesonbuild/compilers/c_function_attributes.py
@@ -93,8 +93,7 @@ C_FUNC_ATTRIBUTES = {
     'visibility': '''
         int foo_def(void) __attribute__((visibility("default")));
         int foo_hid(void) __attribute__((visibility("hidden")));
-        int foo_int(void) __attribute__((visibility("internal")));
-        int foo_pro(void) __attribute__((visibility("protected")));''',
+        int foo_int(void) __attribute__((visibility("internal")));''',
     'warning':
         'int foo(void) __attribute__((warning("")));',
     'warn_unused_result':
