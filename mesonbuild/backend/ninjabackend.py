77c3e6a4aaed07e626f4bf4deb7eb66e0f03a33d
James Westman <flyingpimonster@flyingpimonster.net>
2019-06-24 12:04:12 -0500

    Fix two errors when cross-compiling with Vala
    - AttributeError: 'ValaCompiler' object has no attribute 'get_program_dirs'
    
      Fixed by adding a `get_program_dirs()` function to the base Compiler
      class, to match `get_library_dirs()`
    
    - KeyError: 'vala_COMPILER'
    
      Fixed by creating the Vala compile rules for all machines, not just
      the build machine.

--- mesonbuild/backend/ninjabackend.py
+++ mesonbuild/backend/ninjabackend.py
@@ -1653,8 +1653,7 @@ https://gcc.gnu.org/bugzilla/show_bug.cgi?id=47485'''))
                 self.generate_cs_compile_rule(compiler)
             return
         if langname == 'vala':
-            if self.environment.machines.matches_build_machine(compiler.for_machine):
-                self.generate_vala_compile_rules(compiler)
+            self.generate_vala_compile_rules(compiler)
             return
         if langname == 'rust':
             self.generate_rust_compile_rules(compiler)
