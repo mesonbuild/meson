e2039da5d99dc8fe4255069ef8ab14459766bf2d
Dylan Baker <dylan@pnwbakers.com>
2019-06-24 14:02:26 -0700

    Split attribute visibility
    * c_function_attributes: remove 'protected' from 'visibility'
    
    This doesn't exist on macos with the apple compiler, which always causes
    failures.
    
    Fixes #5530
    
    * compilers: Add split visibility checks to has_function_attribute
    
    These check for a single visibility at a time, rather than all four at
    once. This allows for finer grained searches, and should make using
    these checks safer across operating systems.

--- mesonbuild/compilers/c_function_attributes.py
+++ mesonbuild/compilers/c_function_attributes.py
@@ -93,8 +93,15 @@ C_FUNC_ATTRIBUTES = {
     'visibility': '''
         int foo_def(void) __attribute__((visibility("default")));
         int foo_hid(void) __attribute__((visibility("hidden")));
-        int foo_int(void) __attribute__((visibility("internal")));
-        int foo_pro(void) __attribute__((visibility("protected")));''',
+        int foo_int(void) __attribute__((visibility("internal")));''',
+    'visibility:default':
+        'int foo(void) __attribute__((visibility("default")));',
+    'visibility:hidden':
+        'int foo(void) __attribute__((visibility("hidden")));',
+    'visibility:internal':
+        'int foo(void) __attribute__((visibility("internal")));',
+    'visibility:protected':
+        'int foo(void) __attribute__((visibility("protected")));',
     'warning':
         'int foo(void) __attribute__((warning("")));',
     'warn_unused_result':
