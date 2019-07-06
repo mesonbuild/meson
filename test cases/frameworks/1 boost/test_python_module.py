74de2a81dc1c2b2acab1d5ea31bbb63f20315463
Georg Jäger <georg.jaeger@ovgu.de>
2019-07-06 14:08:58 +0200

    Adding unit test for detection of Boost.Python and subsequent compilation with the library. The unit test considers both, python 2 and 3.

---
+++ test cases/frameworks/1 boost/test_python_module.py
@@ -0,0 +1,22 @@
+import sys
+sys.path.append(sys.argv[1])
+
+# import compiled python module depending on version of python we are running with
+if sys.version_info[0] == 2:
+    import python2_module as pm
+
+if sys.version_info[0] == 3:
+    import python3_module as pm
+
+
+def run():
+    msg = 'howdy'
+    w = pm.World()
+    w.set(msg)
+
+    assert( msg == w.greet() )
+    version_string = str(sys.version_info[0]) + "." + str(sys.version_info[1])
+    assert( version_string == w.version())
+
+if __name__ == '__main__':
+    run()
