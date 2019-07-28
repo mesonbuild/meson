ae1018809e61e8660f1ab0436e6d54829ba289ca
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-07-28 18:19:42 -0400

    doc

--- test cases/common/13 pch/cpp/prog.cc
+++ test cases/common/13 pch/cpp/prog.cc
@@ -1,4 +1,5 @@
-#include "prog.hh"
+// Note: if using PGI compilers, you will need to add #include "prog.hh"
+// even though you're using precompiled headers.
 void func() {
     std::cout << "This is a function that fails to compile if iostream is not included."
               << std::endl;
