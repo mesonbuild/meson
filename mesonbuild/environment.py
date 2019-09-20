24bd0294372d388160d3b093407d6f0db9aa5481
Ting-Wei Lan <lantw@src.gnome.org>
2019-09-20 23:37:59 +0800

    environment: LLVM 9 is now a stable release

--- mesonbuild/environment.py
+++ mesonbuild/environment.py
@@ -171,6 +171,7 @@ def get_llvm_tool_names(tool: str) -> typing.List[str]:
     # unless it becomes a stable release.
     suffixes = [
         '', # base (no suffix)
+        '-9',   '90',
         '-8',   '80',
         '-7',   '70',
         '-6.0', '60',
@@ -181,7 +182,7 @@ def get_llvm_tool_names(tool: str) -> typing.List[str]:
         '-3.7', '37',
         '-3.6', '36',
         '-3.5', '35',
-        '-9',     # Debian development snapshot
+        '-10',    # Debian development snapshot
         '-devel', # FreeBSD development snapshot
     ]
     names = []
