af2c1e4eb79d9005df344826a2b789e9b3ac3000
Andrei Alexeyev <akari@taisei-project.org>
2019-10-14 18:11:14 +0300

    Revert "Remove BasicLinkerIsCompilerMixin from LDC and DMD"
    This reverts commit 04d6a439457d76b9cf4c4f98fce238219366e241.

--- mesonbuild/compilers/d.py
+++ mesonbuild/compilers/d.py
@@ -635,7 +635,7 @@ class GnuDCompiler(DCompiler, GnuCompiler):
         return parameter_list
 
 
-class LLVMDCompiler(DmdLikeCompilerMixin, LinkerEnvVarsMixin, DCompiler):
+class LLVMDCompiler(DmdLikeCompilerMixin, LinkerEnvVarsMixin, BasicLinkerIsCompilerMixin, DCompiler):
 
     def __init__(self, exelist, version, for_machine: MachineChoice,
                  info: 'MachineInfo', arch, **kwargs):
@@ -674,7 +674,7 @@ class LLVMDCompiler(DmdLikeCompilerMixin, LinkerEnvVarsMixin, DCompiler):
         return ldc_optimization_args[optimization_level]
 
 
-class DmdDCompiler(DmdLikeCompilerMixin, LinkerEnvVarsMixin, DCompiler):
+class DmdDCompiler(DmdLikeCompilerMixin, LinkerEnvVarsMixin, BasicLinkerIsCompilerMixin, DCompiler):
 
     def __init__(self, exelist, version, for_machine: MachineChoice,
                  info: 'MachineInfo', arch, **kwargs):
