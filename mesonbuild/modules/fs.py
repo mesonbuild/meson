4adfd921ae9138dcbc1787e049529d1a923b6a97
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2019-11-08 03:18:42 -0500

    fs: use expanduser

--- mesonbuild/modules/fs.py
+++ mesonbuild/modules/fs.py
@@ -32,7 +32,7 @@ class FSModule(ExtensionModule):
     def _check(self, check: str, state: 'ModuleState', args: typing.Sequence[str]) -> ModuleReturnValue:
         if len(args) != 1:
             MesonException('method takes exactly one argument.')
-        test_file = Path(state.source_root) / state.subdir / args[0]
+        test_file = Path(state.source_root) / state.subdir / Path(args[0]).expanduser()
         return ModuleReturnValue(getattr(test_file, check)(), [])
 
     @stringArgs
