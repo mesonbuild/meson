b33830f99e95596a5afa23d00970443c6d47909f
Xavier Claessens <xclaesse@gmail.com>
2019-11-28 11:26:20 -0500

    Revert "Have set() and set_quoted() of configuration object work with newlines."
    This reverts commit 6ed36e97aeb01dd779115a9710d3a97cdbcb4ccf.

--- test cases/common/14 configure file/dumpprog.c
+++ test cases/common/14 configure file/dumpprog.c
@@ -48,13 +48,5 @@ int main(void) {
         printf("Quoted number defined incorrectly.\n");
         return 1;
     }
-    if(MULTILINE != 3) {
-        printf("Multiline macro defined incorrectly.\n");
-        return 1;
-    }
-    if(strcmp(MULTILINE_STRING, "line1\nline2") != 0) {
-        printf("Quoted multiline macro defined incorrectly.\n");
-        return 1;
-    }
     SHOULD_BE_RETURN 0;
 }
