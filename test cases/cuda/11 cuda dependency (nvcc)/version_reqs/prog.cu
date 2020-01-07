824468c2614c9779af9ad8e2b41936253320b576
Jan Alexander Steffens (heftig) <jan.steffens@gmail.com>
2020-01-07 23:48:18 +0100

    Fix CUDA test without devices.
    Getting the runtime version fails when no devices are present.

--- test cases/cuda/11 cuda dependency (nvcc)/version_reqs/prog.cu
+++ test cases/cuda/11 cuda dependency (nvcc)/version_reqs/prog.cu
@@ -9,20 +9,21 @@ int cuda_devices(void) {
 
 int main(void) {
     std::cout << "Compiled against CUDA version: " << CUDART_VERSION << "\n";
+
     int runtime_version = 0;
-    cudaError_t r = cudaRuntimeGetVersion(&runtime_version);
-    if (r != cudaSuccess) {
-        std::cout << "Couldn't obtain CUDA runtime version (error " << r << "). Exiting.\n";
-        return -1;
+    switch (cudaError_t r = cudaRuntimeGetVersion(&runtime_version)) {
+        case cudaSuccess:
+            std::cout << "CUDA runtime version: " << runtime_version << "\n";
+            break;
+        case cudaErrorNoDevice:
+            std::cout << "No CUDA hardware found. Exiting.\n";
+            return 0;
+        default:
+            std::cout << "Couldn't obtain CUDA runtime version (error " << r << "). Exiting.\n";
+            return -1;
     }
-    std::cout << "CUDA runtime version: " << runtime_version << "\n";
 
     int n = cuda_devices();
-    if (n == 0) {
-        std::cout << "No CUDA hardware found. Exiting.\n";
-        return 0;
-    }
-
     std::cout << "Found " << n << " CUDA devices.\n";
     return 0;
 }
