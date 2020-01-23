4ffd2224de5e639b83ec919cb47a9d596e1d1f90
Jon Turney <jon.turney@dronecode.org.uk>
2020-01-23 15:23:13 +0000

    Split tests out from 'common' which require a native compiler
    Split out tests (and parts of tests) which require a native compiler
    from the 'common' suite to a new suite called 'native', so we can
    selectively avoid running those tests when only a cross-compiler is
    available.
    
    Also move test '211 cmake module' to 'cmake' suite, since it appears
    that the way we use cmake requires a native compiler.

--- run_project_tests.py
+++ run_project_tests.py
@@ -50,7 +50,7 @@ from run_tests import get_backend_commands, get_backend_args_for_dir, Backend
 from run_tests import ensure_backend_detects_changes
 from run_tests import guess_backend
 
-ALL_TESTS = ['cmake', 'common', 'warning-meson', 'failing-meson', 'failing-build', 'failing-test',
+ALL_TESTS = ['cmake', 'common', 'native', 'warning-meson', 'failing-meson', 'failing-build', 'failing-test',
              'keyval', 'platform-osx', 'platform-windows', 'platform-linux',
              'java', 'C#', 'vala',  'rust', 'd', 'objective c', 'objective c++',
              'fortran', 'swift', 'cuda', 'python3', 'python', 'fpga', 'frameworks', 'nasm', 'wasm'
@@ -961,6 +961,7 @@ def detect_tests_to_run(only: T.List[str], use_tmp: bool) -> T.List[T.Tuple[str,
     all_tests = [
         TestCategory('cmake', 'cmake', not shutil.which('cmake') or (os.environ.get('compiler') == 'msvc2015' and under_ci)),
         TestCategory('common', 'common'),
+        TestCategory('native', 'native'),
         TestCategory('warning-meson', 'warning', stdout_mandatory=True),
         TestCategory('failing-meson', 'failing', stdout_mandatory=True),
         TestCategory('failing-build', 'failing build'),
