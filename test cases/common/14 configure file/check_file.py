2cb5dcedf29a41ff1d8d21a122dd16cf114ffd65
Nirbheek Chauhan <nirbheek@centricular.com>
2020-02-11 19:30:22 +0530

    configure_file: Also copy timestamps to avoid useless rebuilds
    If we always copy files over without timestamps, we're forcing
    a (probably unnecessary) rebuild. Also include a test for this.

--- test cases/common/14 configure file/check_file.py
+++ test cases/common/14 configure file/check_file.py
@@ -6,9 +6,15 @@ import sys
 if len(sys.argv) == 2:
     assert(os.path.exists(sys.argv[1]))
 elif len(sys.argv) == 3:
-    f1 = open(sys.argv[1], 'rb').read()
-    f2 = open(sys.argv[2], 'rb').read()
-    if f1 != f2:
+    f1 = sys.argv[1]
+    f2 = sys.argv[2]
+    m1 = os.stat(f1).st_mtime_ns
+    m2 = os.stat(f2).st_mtime_ns
+    # Compare only os.stat()
+    if m1 != m2:
+        raise RuntimeError('mtime of {!r} () != mtime of {!r} ()'.format(f1, m1, f2, m2))
+    import filecmp
+    if not filecmp.cmp(f1, f2):
         raise RuntimeError('{!r} != {!r}'.format(f1, f2))
 else:
     raise AssertionError
