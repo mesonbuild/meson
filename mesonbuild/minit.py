c72de63b9bd08f97c8f74151b9e9e0bdd77763bb
Michael Brockus <55331536+michaelbadcrumble@users.noreply.github.com>
2020-02-24 10:10:56 -0800

    use python set to tuple for checking members

--- mesonbuild/minit.py
+++ mesonbuild/minit.py
@@ -40,8 +40,8 @@ we currently have one meson template at this time.
 from mesonbuild.templates.mesontemplates import create_meson_build
 
 FORTRAN_SUFFIXES = ('.f', '.for', '.F', '.f90', '.F90')
-LANG_SUFFIXES = ('.c', '.cc', '.cpp', '.cs', '.cu', '.d', '.m', '.mm', '.rs', '.java') + FORTRAN_SUFFIXES
-LANG_SUPPORTED = ('c', 'cpp', 'cs', 'cuda', 'd', 'fortran', 'java', 'rust', 'objc', 'objcpp')
+LANG_SUFFIXES = {'.c', '.cc', '.cpp', '.cs', '.cu', '.d', '.m', '.mm', '.rs', '.java'}.add(FORTRAN_SUFFIXES)
+LANG_SUPPORTED = {'c', 'cpp', 'cs', 'cuda', 'd', 'fortran', 'java', 'rust', 'objc', 'objcpp'}
 
 DEFAULT_PROJECT = 'executable'
 DEFAULT_VERSION = '0.1'
