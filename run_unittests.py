344f0680b34cf8e06333a17e10bbba92bffc3f5f
Nirbheek Chauhan <nirbheek@centricular.com>
2020-03-07 00:18:40 +0530

    coredata: Passing an option is supposed to set it, not flip the default
    With the current logic passing `--debug` will actually be parsed as
    `-Ddebug=false`, which is absolutely not what is expected.
    
    There is no case in which the presence of a boolean option in `--foo`
    form will mean 'I want feature foo disabled', regardless of the
    *default* value of that option.
    
    Also includes a test.
    
    Closes https://github.com/mesonbuild/meson/issues/4686

--- run_unittests.py
+++ run_unittests.py
@@ -3826,6 +3826,14 @@ recommended as it is not supported on some platforms''')
         self.assertEqual(opts['debug'], True)
         self.assertEqual(opts['optimization'], '2')
         self.assertEqual(opts['buildtype'], 'debugoptimized')
+        # Setting both buildtype and debug on the command-line should work
+        # Also test that --debug is parsed as -Ddebug=true
+        self.new_builddir()
+        self.init(testdir, extra_args=['-Dbuildtype=debugoptimized', '--debug'])
+        opts = self.get_opts_as_dict()
+        self.assertEqual(opts['debug'], True)
+        self.assertEqual(opts['optimization'], '2')
+        self.assertEqual(opts['buildtype'], 'debugoptimized')
 
     @skipIfNoPkgconfig
     @unittest.skipIf(is_windows(), 'Help needed with fixing this test on windows')
