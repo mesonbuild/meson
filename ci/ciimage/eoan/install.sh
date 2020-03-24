aa3cb4a9bb88c13a07d1518973a4d47eba2e47ed
Daniel Mensinger <daniel@mensinger-ka.de>
2020-03-24 19:35:25 +0100

    ci: Use the new CI image builder for eoan

---
+++ ci/ciimage/eoan/install.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+
+set -e
+
+export DEBIAN_FRONTEND=noninteractive
+export LANG='C.UTF-8'
+export DC=gdc
+
+pkgs=(
+  python3-pytest-xdist
+  python3-pip libxml2-dev libxslt1-dev libyaml-dev libjson-glib-dev
+  wget unzip
+  qt5-default clang
+  pkg-config-arm-linux-gnueabihf
+  qt4-linguist-tools
+  python-dev
+  libomp-dev
+  dub ldc
+  mingw-w64 mingw-w64-tools nim
+  libclang-dev
+  libgcrypt20-dev
+  libgpgme-dev
+  libhdf5-dev
+  libboost-python-dev libboost-regex-dev
+  libblocksruntime-dev
+  libperl-dev
+  liblapack-dev libscalapack-mpi-dev
+)
+
+sed -i '/^#\sdeb-src /s/^#//' "/etc/apt/sources.list"
+apt-get -y update
+apt-get -y upgrade
+apt-get -y install eatmydata
+
+# Base stuff
+eatmydata apt-get -y build-dep meson
+
+# packages
+eatmydata apt-get -y install "${pkgs[@]}"
+eatmydata apt-get -y install --no-install-recommends wine-stable  # Wine is special
+
+eatmydata python3 -m pip install hotdoc codecov jsonschema
+
+# dub stuff
+dub fetch urld
+dub build urld --compiler=gdc
+dub fetch dubtestproject
+dub build dubtestproject:test1 --compiler=ldc2
+dub build dubtestproject:test2 --compiler=ldc2
+
+# cleanup
+apt-get -y clean
+apt-get -y autoclean
