7005e211a2aaf05626752c0c73d2518d8034911b
Jon Turney <jon.turney@dronecode.org.uk>
2020-03-24 23:58:04 +0000

    Report an errror if glib-compile-resources is missing
    Produce a diagnostic if we try to use gnome.compile_resources() when
    glib-compile-resources isn't present, rather than a python backtrace.

--- mesonbuild/modules/gnome.py
+++ mesonbuild/modules/gnome.py
@@ -227,7 +227,10 @@ class GnomeModule(ExtensionModule):
         for source_dir in source_dirs:
             cmd += ['--sourcedir', os.path.join(state.subdir, source_dir)]
 
-        pc, stdout, stderr = Popen_safe(cmd, cwd=state.environment.get_source_dir())
+        try:
+            pc, stdout, stderr = Popen_safe(cmd, cwd=state.environment.get_source_dir())
+        except (FileNotFoundError, PermissionError):
+            raise MesonException('Could not execute glib-compile-resources.')
         if pc.returncode != 0:
             m = 'glib-compile-resources failed to get dependencies for {}:\n{}'
             mlog.warning(m.format(cmd[1], stderr))
