ed982b4ad97c12c736a3ae547776e01d00d636ac
Corey McNeish <c.r.mcneish@gmail.com>
2020-06-07 12:57:57 -0700

    Handle native/cross file templates in a more consistent way
    All tests can now create a `nativefile.ini.in` if they need to use some
    parameter that the testing framework knows about but they can't.

--- run_project_tests.py
+++ run_project_tests.py
@@ -476,6 +476,28 @@ def create_deterministic_builddir(test: TestDef, use_tmpdir: bool) -> str:
     os.mkdir(abs_pathname)
     return abs_pathname
 
+def format_parameter_file(file_basename: str, test: TestDef, test_build_dir: str) -> Path:
+    confdata = ConfigurationData()
+    confdata.values = {'MESON_TEST_ROOT': (str(test.path.absolute()), 'base directory of current test')}
+
+    template = test.path / (file_basename + '.in')
+    destination = Path(test_build_dir) / file_basename
+    mesonlib.do_conf_file(str(template), str(destination), confdata, 'meson')
+
+    return destination
+
+def detect_parameter_files(test: TestDef, test_build_dir: str) -> (Path, Path):
+    nativefile = test.path / 'nativefile.ini'
+    crossfile = test.path / 'crossfile.ini'
+
+    if os.path.exists(test.path / 'nativefile.ini.in'):
+        nativefile = format_parameter_file('nativefile.ini', test, test_build_dir)
+
+    if os.path.exists(test.path / 'crossfile.ini.in'):
+        crossfile = format_parameter_file('crossfile.ini', test, test_build_dir)
+
+    return nativefile, crossfile
+
 def run_test(test: TestDef, extra_args, compiler, backend, flags, commands, should_fail, use_tmp: bool):
     if test.skip:
         return None
@@ -498,8 +520,9 @@ def _run_test(test: TestDef, test_build_dir: str, install_dir: str, extra_args, 
     if 'libdir' not in test.do_not_set_opts:
         gen_args += ['--libdir', 'lib']
     gen_args += [test.path.as_posix(), test_build_dir] + flags + extra_args
-    nativefile = test.path / 'nativefile.ini'
-    crossfile = test.path / 'crossfile.ini'
+
+    nativefile, crossfile = detect_parameter_files(test, test_build_dir)
+
     if nativefile.exists():
         gen_args.extend(['--native-file', nativefile.as_posix()])
     if crossfile.exists():
@@ -606,13 +629,6 @@ def _run_test(test: TestDef, test_build_dir: str, install_dir: str, extra_args, 
 
     return testresult
 
-def set_boost_nativefile_paths(test: TestDef) -> None:
-    script_dir = os.path.abspath(os.path.split(__file__)[0])
-
-    confdata = ConfigurationData()
-    confdata.values = {'ROOT': (script_dir, 'project base directory')}
-
-    mesonlib.do_conf_file(str(test.path / 'nativefile.ini.in'), str(test.path / 'nativefile.ini'), confdata, 'meson')
 
 def gather_tests(testdir: Path, stdout_mandatory: bool) -> T.List[TestDef]:
     tests = [t.name for t in testdir.iterdir() if t.is_dir()]
@@ -624,10 +640,6 @@ def gather_tests(testdir: Path, stdout_mandatory: bool) -> T.List[TestDef]:
         test_def_file = t.path / 'test.json'
         if test_def_file.is_file():
             test_def = json.loads(test_def_file.read_text())
-
-        # Perform setup for boost tests that require absolute paths in nativefile.ini
-        if str(t.path).endswith('32 boost root') or str(t.path).endswith('33 boost split root'):
-            set_boost_nativefile_paths(t)
 
         # Handle additional environment variables
         env = {}  # type: T.Dict[str, str]
