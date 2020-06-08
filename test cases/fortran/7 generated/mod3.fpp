b639d3cefcd59d90bbbcf6ae1e75861596761dea
Michael Hirsch <scivision@users.noreply.github.com>
2020-06-08 00:13:18 -0400

    fortran generated test: add code to reveal issue
    used with Issue #7265

---
+++ test cases/fortran/7 generated/mod3.fpp
@@ -0,0 +1,6 @@
+module mod3
+implicit none
+
+integer, parameter :: modval3 = @THREE@
+
+end module mod3
--- test cases/fortran/7 generated/prog.f90
+++ test cases/fortran/7 generated/prog.f90
@@ -1,7 +1,8 @@
-program prog
-use mod2
+program generated
+use mod2, only : modval1, modval2
+use mod3, only : modval3
 implicit none
 
-if (modval1 + modval2 /= 3) stop 1
+if (modval1 + modval2 + modval3 /= 6) error stop
 
-end program prog
+end program generated
