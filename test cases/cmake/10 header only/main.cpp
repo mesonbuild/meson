6c56478ee1e15477b54d92bf25b7b4b1489bfcdb
Daniel Mensinger <daniel@mensinger-ka.de>
2020-06-12 22:03:50 +0200

    cmake: fix definitions with interface libraries (fixes #7299)

--- test cases/cmake/10 header only/main.cpp
+++ test cases/cmake/10 header only/main.cpp
@@ -3,8 +3,14 @@
 
 using namespace std;
 
+#define EXPECTED "Hello World compDef 42"
+
 int main(void) {
   cmModClass obj("Hello");
   cout << obj.getStr() << endl;
+  if (obj.getStr() != EXPECTED) {
+    cerr << "Expected: '" << EXPECTED << "'" << endl;
+    return 1;
+  }
   return 0;
 }
