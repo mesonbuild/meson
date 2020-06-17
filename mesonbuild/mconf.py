mesonbuild/meson

commit 278c294aa45efc3e8b068bcd7632828ed5c92523

Compiler options per lang
A current rather untyped storage of options is one of the things that
contributes to the options code being so complex. This takes a small
step in synching down by storing the compiler options in dicts per
language.

Future work might be replacing the langauge strings with an enum, and
defaultdict with a custom struct, just like `PerMachine` and
`MachineChoice`.

mesonbuild/mconf.py
@@ -97,9 +97,9 @@ class Conf:
             else:
                 print('{0:{width[0]}} {1:{width[1]}} {3}'.format(*line, width=col_widths))
 
-    def split_options_per_subproject(self, options):
+    def split_options_per_subproject(self, options_iter):
         result = {}
-        for k, o in options.items():
+        for k, o in options_iter:
             subproject = ''
             if ':' in k:
                 subproject, optname = k.split(':')
@@ -211,10 +211,15 @@ class Conf:
                 return 'build.' + k
             return k[:idx + 1] + 'build.' + k[idx + 1:]
 
-        core_options = self.split_options_per_subproject(core_options)
-        host_compiler_options = self.split_options_per_subproject(self.coredata.compiler_options.host)
-        build_compiler_options = self.split_options_per_subproject({insert_build_prefix(k): o for k, o in self.coredata.compiler_options.build.items()})
-        project_options = self.split_options_per_subproject(self.coredata.user_options)
+        core_options = self.split_options_per_subproject(core_options.items())
+        host_compiler_options = self.split_options_per_subproject(
+            self.coredata.flatten_lang_iterator(
+                self.coredata.compiler_options.host.items()))
+        build_compiler_options = self.split_options_per_subproject(
+            self.coredata.flatten_lang_iterator(
+                (insert_build_prefix(k), o)
+                for k, o in self.coredata.compiler_options.build.items()))
+        project_options = self.split_options_per_subproject(self.coredata.user_options.items())
         show_build_options = self.default_values_only or self.build.environment.is_cross_build()
 
         self.add_section('Main project options')

mesonbuild/backend/backends.py
@@ -166,18 +166,21 @@ class Backend:
         return os.path.join(self.environment.get_build_dir(), self.get_target_filename(target))
 
     def get_base_options_for_target(self, target):
-        return OptionOverrideProxy(target.option_overrides,
+        return OptionOverrideProxy(target.option_overrides_base,
                                    self.environment.coredata.builtins,
                                    self.environment.coredata.base_options)
 
     def get_compiler_options_for_target(self, target):
-        return OptionOverrideProxy(
-            target.option_overrides,
-            self.environment.coredata.compiler_options[target.for_machine])
+        comp_reg = self.environment.coredata.compiler_options[target.for_machine]
+        comp_override = target.option_overrides_compiler
+        return {
+            lang: OptionOverrideProxy(comp_override[lang], comp_reg[lang])
+            for lang in set(comp_reg.keys()) | set(comp_override.keys())
+        }
 
     def get_option_for_target(self, option_name, target):
-        if option_name in target.option_overrides:
-            override = target.option_overrides[option_name]
+        if option_name in target.option_overrides_base:
+            override = target.option_overrides_base[option_name]
             return self.environment.coredata.validate_option_value(option_name, override)
         return self.environment.coredata.get_builtin_option(option_name, target.subproject)
 
@@ -581,7 +584,7 @@ class Backend:
         # starting from hard-coded defaults followed by build options and so on.
         commands = CompilerArgs(compiler)
 
-        copt_proxy = self.get_compiler_options_for_target(target)
+        copt_proxy = self.get_compiler_options_for_target(target)[compiler.language]
         # First, the trivial ones that are impossible to override.
         #
         # Add -nostdinc/-nostdinc++ if needed; can't be overridden

mesonbuild/backend/ninjabackend.py
@@ -2560,7 +2560,14 @@ https://gcc.gnu.org/bugzilla/show_bug.cgi?id=47485'''))
         # to be after all internal and external libraries so that unresolved
         # symbols from those can be found here. This is needed when the
         # *_winlibs that we want to link to are static mingw64 libraries.
-        commands += linker.get_option_link_args(self.environment.coredata.compiler_options[target.for_machine])
+        if hasattr(linker, 'get_language'):
+            # The static linker doesn't know what language it is building, so we
+            # don't know what option. Fortunately, it doesn't care to see the
+            # language-specific options either.
+            #
+            # We shouldn't check whether we are making a static library, because
+            # in the LTO case we do use a real compiler here.
+            commands += linker.get_option_link_args(self.environment.coredata.compiler_options[target.for_machine][linker.get_language()])
 
         dep_targets = []
         dep_targets.extend(self.guess_external_link_dependencies(linker, target, commands, internal))

mesonbuild/backend/vs2010backend.py
@@ -891,8 +891,10 @@ class Vs2010Backend(backends.Backend):
         # generate_single_compile() and generate_basic_compiler_args()
         for l, comp in target.compilers.items():
             if l in file_args:
-                file_args[l] += compilers.get_base_compile_args(self.get_base_options_for_target(target), comp)
-                file_args[l] += comp.get_option_compile_args(self.environment.coredata.compiler_options[target.for_machine])
+                file_args[l] += compilers.get_base_compile_args(
+                        self.get_base_options_for_target(target), comp)
+                file_args[l] += comp.get_option_compile_args(
+                        self.environment.coredata.compiler_options[target.for_machine][comp.language])
 
         # Add compile args added using add_project_arguments()
         for l, args in self.build.projects_args[target.for_machine].get(target.subproject, {}).items():
@@ -905,10 +907,11 @@ class Vs2010Backend(backends.Backend):
                 file_args[l] += args
         # Compile args added from the env or cross file: CFLAGS/CXXFLAGS, etc. We want these
         # to override all the defaults, but not the per-target compile args.
-        for key, opt in self.environment.coredata.compiler_options[target.for_machine].items():
-            l, suffix = key.split('_', 1)
-            if suffix == 'args' and l in file_args:
-                file_args[l] += opt.value
+        for l in file_args.keys():
+            opts = self.environment.coredata.compiler_options[target.for_machine][l]
+            k = 'args'
+            if k in opts:
+                file_args[l] += opts[k].value
         for args in file_args.values():
             # This is where Visual Studio will insert target_args, target_defines,
             # etc, which are added later from external deps (see below).
@@ -1115,7 +1118,8 @@ class Vs2010Backend(backends.Backend):
         # to be after all internal and external libraries so that unresolved
         # symbols from those can be found here. This is needed when the
         # *_winlibs that we want to link to are static mingw64 libraries.
-        extra_link_args += compiler.get_option_link_args(self.environment.coredata.compiler_options[compiler.for_machine])
+        extra_link_args += compiler.get_option_link_args(
+                self.environment.coredata.compiler_options[compiler.for_machine][comp.language])
         (additional_libpaths, additional_links, extra_link_args) = self.split_link_args(extra_link_args.to_native())
 
         # Add more libraries to be linked if needed

mesonbuild/build.py
@@ -13,7 +13,7 @@
 # limitations under the License.
 
 import copy, os, re
-from collections import OrderedDict
+from collections import OrderedDict, defaultdict
 import itertools, pathlib
 import hashlib
 import pickle
@@ -28,7 +28,10 @@ from .mesonlib import (
     extract_as_list, typeslistify, stringlistify, classify_unity_sources,
     get_filenames_templates_dict, substitute_values, has_path_sep, unholder
 )
-from .compilers import Compiler, is_object, clink_langs, sort_clink, lang_suffixes, is_known_suffix
+from .compilers import (
+    Compiler, all_languages, is_object, clink_langs, sort_clink, lang_suffixes,
+    is_known_suffix
+)
 from .linkers import StaticLinker
 from .interpreterbase import FeatureNew
 
@@ -358,7 +361,8 @@ a hard error in the future.'''.format(name))
         self.for_machine = for_machine
         self.install = False
         self.build_always_stale = False
-        self.option_overrides = {}
+        self.option_overrides_base = {}
+        self.option_overrides_compiler = defaultdict(dict)
         if not hasattr(self, 'typename'):
             raise RuntimeError('Target type is not set for target class "{}". This is a bug'.format(type(self).__name__))
 
@@ -448,7 +452,15 @@ a hard error in the future.'''.format(name))
             # set, use the value of 'install' if it's enabled.
             self.build_by_default = True
 
-        self.option_overrides = self.parse_overrides(kwargs)
+        option_overrides = self.parse_overrides(kwargs)
+
+        for k, v in option_overrides.items():
+            if '_' in k:
+               lang, k2 = k.split('_', 1)
+               if lang in all_languages:
+                   self.option_overrides_compiler[lang][k2] = v
+                   continue
+            self.option_overrides_base[k] = v
 
     def parse_overrides(self, kwargs) -> dict:
         result = {}

mesonbuild/compilers/c.py
@@ -107,25 +107,32 @@ class ClangCCompiler(ClangCompiler, CCompiler):
         if version_compare(self.version, self._C18_VERSION):
             c_stds += ['c18']
             g_stds += ['gnu18']
-        opts.update({'c_std': coredata.UserComboOption('C language standard to use',
-                                                       ['none'] + c_stds + g_stds,
-                                                       'none')})
+        opts.update({
+            'std': coredata.UserComboOption(
+                'C language standard to use',
+                ['none'] + c_stds + g_stds,
+                'none',
+            ),
+        })
         if self.info.is_windows() or self.info.is_cygwin():
             opts.update({
-                'c_winlibs': coredata.UserArrayOption('Standard Win libraries to link against',
-                                                      gnu_winlibs), })
+                'winlibs': coredata.UserArrayOption(
+                    'Standard Win libraries to link against',
+                    gnu_winlibs,
+                ),
+            })
         return opts
 
     def get_option_compile_args(self, options):
         args = []
-        std = options['c_std']
+        std = options['std']
         if std.value != 'none':
             args.append('-std=' + std.value)
         return args
 
     def get_option_link_args(self, options):
         if self.info.is_windows() or self.info.is_cygwin():
-            return options['c_winlibs'].value[:]
+            return options['winlibs'].value[:]
         return []
 
 
@@ -166,15 +173,18 @@ class ArmclangCCompiler(ArmclangCompiler, CCompiler):
 
     def get_options(self):
         opts = CCompiler.get_options(self)
-        opts.update({'c_std': coredata.UserComboOption('C language standard to use',
-                                                       ['none', 'c90', 'c99', 'c11',
-                                                        'gnu90', 'gnu99', 'gnu11'],
-                                                       'none')})
+        opts.update({
+            'std': coredata.UserComboOption(
+                'C language standard to use',
+                ['none', 'c90', 'c99', 'c11', 'gnu90', 'gnu99', 'gnu11'],
+                'none',
+            ),
+        })
         return opts
 
     def get_option_compile_args(self, options):
         args = []
-        std = options['c_std']
+        std = options['std']
         if std.value != 'none':
             args.append('-std=' + std.value)
         return args
@@ -204,25 +214,32 @@ class GnuCCompiler(GnuCompiler, CCompiler):
         if version_compare(self.version, v):
             c_stds += ['c17', 'c18']
             g_stds += ['gnu17', 'gnu18']
-        opts.update({'c_std': coredata.UserComboOption('C language standard to use',
-                                                       ['none'] + c_stds + g_stds,
-                                                       'none')})
+        opts.update({
+            'std': coredata.UserComboOption(
+                'C language standard to use',
+                ['none'] + c_stds + g_stds,
+                'none',
+            ),
+        })
         if self.info.is_windows() or self.info.is_cygwin():
             opts.update({
-                'c_winlibs': coredata.UserArrayOption('Standard Win libraries to link against',
-                                                      gnu_winlibs), })
+                'winlibs': coredata.UserArrayOption(
+                    'Standard Win libraries to link against',
+                    gnu_winlibs,
+                ),
+            })
         return opts
 
     def get_option_compile_args(self, options):
         args = []
-        std = options['c_std']
+        std = options['std']
         if std.value != 'none':
             args.append('-std=' + std.value)
         return args
 
     def get_option_link_args(self, options):
         if self.info.is_windows() or self.info.is_cygwin():
-            return options['c_winlibs'].value[:]
+            return options['winlibs'].value[:]
         return []
 
     def get_pch_use_args(self, pch_dir, header):
@@ -248,11 +265,17 @@ class ElbrusCCompiler(GnuCCompiler, ElbrusCompiler):
     # It does support some various ISO standards and c/gnu 90, 9x, 1x in addition to those which GNU CC supports.
     def get_options(self):
         opts = CCompiler.get_options(self)
-        opts.update({'c_std': coredata.UserComboOption('C language standard to use',
-                                                       ['none', 'c89', 'c90', 'c9x', 'c99', 'c1x', 'c11',
-                                                        'gnu89', 'gnu90', 'gnu9x', 'gnu99', 'gnu1x', 'gnu11',
-                                                        'iso9899:2011', 'iso9899:1990', 'iso9899:199409', 'iso9899:1999'],
-                                                       'none')})
+        opts.update({
+            'std': coredata.UserComboOption(
+                'C language standard to use',
+                [
+                    'none', 'c89', 'c90', 'c9x', 'c99', 'c1x', 'c11',
+                    'gnu89', 'gnu90', 'gnu9x', 'gnu99', 'gnu1x', 'gnu11',
+                    'iso9899:2011', 'iso9899:1990', 'iso9899:199409', 'iso9899:1999',
+                ],
+                'none',
+            ),
+        })
         return opts
 
     # Elbrus C compiler does not have lchmod, but there is only linker warning, not compiler error.
@@ -285,14 +308,18 @@ class IntelCCompiler(IntelGnuLikeCompiler, CCompiler):
         g_stds = ['gnu89', 'gnu99']
         if version_compare(self.version, '>=16.0.0'):
             c_stds += ['c11']
-        opts.update({'c_std': coredata.UserComboOption('C language standard to use',
-                                                       ['none'] + c_stds + g_stds,
-                                                       'none')})
+        opts.update({
+            'std': coredata.UserComboOption(
+                'C language standard to use',
+                ['none'] + c_stds + g_stds,
+                'none',
+            ),
+        })
         return opts
 
     def get_option_compile_args(self, options):
         args = []
-        std = options['c_std']
+        std = options['std']
         if std.value != 'none':
             args.append('-std=' + std.value)
         return args
@@ -304,12 +331,16 @@ class VisualStudioLikeCCompilerMixin:
 
     def get_options(self):
         opts = super().get_options()
-        opts.update({'c_winlibs': coredata.UserArrayOption('Windows libs to link against.',
-                                                           msvc_winlibs)})
+        opts.update({
+            'winlibs': coredata.UserArrayOption(
+                'Windows libs to link against.',
+                msvc_winlibs,
+            ),
+        })
         return opts
 
     def get_option_link_args(self, options):
-        return options['c_winlibs'].value[:]
+        return options['winlibs'].value[:]
 
 
 class VisualStudioCCompiler(MSVCCompiler, VisualStudioLikeCCompilerMixin, CCompiler):
@@ -343,14 +374,18 @@ class IntelClCCompiler(IntelVisualStudioLikeCompiler, VisualStudioLikeCCompilerM
     def get_options(self):
         opts = super().get_options()
         c_stds = ['none', 'c89', 'c99', 'c11']
-        opts.update({'c_std': coredata.UserComboOption('C language standard to use',
-                                                       c_stds,
-                                                       'none')})
+        opts.update({
+            'std': coredata.UserComboOption(
+                'C language standard to use',
+                c_stds,
+                'none',
+            ),
+        })
         return opts
 
     def get_option_compile_args(self, options):
         args = []
-        std = options['c_std']
+        std = options['std']
         if std.value == 'c89':
             mlog.warning("ICL doesn't explicitly implement c89, setting the standard to 'none', which is close.", once=True)
         elif std.value != 'none':
@@ -367,14 +402,18 @@ class ArmCCompiler(ArmCompiler, CCompiler):
 
     def get_options(self):
         opts = CCompiler.get_options(self)
-        opts.update({'c_std': coredata.UserComboOption('C language standard to use',
-                                                       ['none', 'c90', 'c99'],
-                                                       'none')})
+        opts.update({
+            'std': coredata.UserComboOption(
+                'C language standard to use',
+                ['none', 'c90', 'c99'],
+                'none',
+            ),
+        })
         return opts
 
     def get_option_compile_args(self, options):
         args = []
-        std = options['c_std']
+        std = options['std']
         if std.value != 'none':
             args.append('--' + std.value)
         return args
@@ -393,9 +432,13 @@ class CcrxCCompiler(CcrxCompiler, CCompiler):
 
     def get_options(self):
         opts = CCompiler.get_options(self)
-        opts.update({'c_std': coredata.UserComboOption('C language standard to use',
-                                                       ['none', 'c89', 'c99'],
-                                                       'none')})
+        opts.update({
+            'std': coredata.UserComboOption(
+                'C language standard to use',
+                ['none', 'c89', 'c99'],
+                'none',
+            ),
+        })
         return opts
 
     def get_no_stdinc_args(self):
@@ -403,7 +446,7 @@ class CcrxCCompiler(CcrxCompiler, CCompiler):
 
     def get_option_compile_args(self, options):
         args = []
-        std = options['c_std']
+        std = options['std']
         if std.value == 'c89':
             args.append('-lang=c')
         elif std.value == 'c99':

mesonbuild/compilers/compilers.py
@@ -1272,10 +1272,10 @@ def get_global_options(lang: str,
     """Retreive options that apply to all compilers for a given language."""
     description = 'Extra arguments passed to the {}'.format(lang)
     opts = {
-        lang + '_args': coredata.UserArrayOption(
+        'args': coredata.UserArrayOption(
             description + ' compiler',
             [], split_args=True, user_input=True, allow_dups=True),
-        lang + '_link_args': coredata.UserArrayOption(
+        'link_args': coredata.UserArrayOption(
             description + ' linker',
             [], split_args=True, user_input=True, allow_dups=True),
     }
@@ -1288,12 +1288,13 @@ def get_global_options(lang: str,
         comp.INVOKES_LINKER)
 
     for k, o in opts.items():
-        if k in properties:
+        user_k = lang + '_' + k
+        if user_k in properties:
             # Get from configuration files.
-            o.set_value(properties[k])
-        elif k == lang + '_args':
+            o.set_value(properties[user_k])
+        elif k == 'args':
             o.set_value(compile_args)
-        elif k == lang + '_link_args':
+        elif k == 'link_args':
             o.set_value(link_args)
 
     return opts

commit 8f200405a563dfad6e438ac9a8a7b7c376f298f7

mconf: Fix regression when printing all options
This is a regression introduced by #5489

mesonbuild/mconf.py
@@ -97,9 +97,9 @@ class Conf:
             else:
                 print('{0:{width[0]}} {1:{width[1]}} {3}'.format(*line, width=col_widths))
 
-    def split_options_per_subproject(self, options_iter):
+    def split_options_per_subproject(self, options):
         result = {}
-        for k, o in options_iter:
+        for k, o in options.items():
             subproject = ''
             if ':' in k:
                 subproject, optname = k.split(':')
@@ -211,15 +211,15 @@ class Conf:
                 return 'build.' + k
             return k[:idx + 1] + 'build.' + k[idx + 1:]
 
-        core_options = self.split_options_per_subproject(core_options.items())
+        core_options = self.split_options_per_subproject(core_options)
         host_compiler_options = self.split_options_per_subproject(
-            self.coredata.flatten_lang_iterator(
-                self.coredata.compiler_options.host.items()))
+            dict(self.coredata.flatten_lang_iterator(
+                self.coredata.compiler_options.host.items())))
         build_compiler_options = self.split_options_per_subproject(
-            self.coredata.flatten_lang_iterator(
+            dict(self.coredata.flatten_lang_iterator(
                 (insert_build_prefix(k), o)
-                for k, o in self.coredata.compiler_options.build.items()))
-        project_options = self.split_options_per_subproject(self.coredata.user_options.items())
+                for k, o in self.coredata.compiler_options.build.items())))
+        project_options = self.split_options_per_subproject(self.coredata.user_options)
         show_build_options = self.default_values_only or self.build.environment.is_cross_build()
 
         self.add_section('Main project options')