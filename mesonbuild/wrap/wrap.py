e1094df77e16835861b160ea5d9ed35756a678f8
Xavier Claessens <xavier.claessens@collabora.com>
2020-07-03 14:00:04 -0400

    wrap: Add back filename member in PackageDefinition
    It is still used by msubprojects.py and cause issues when updating
    wrapdb.

--- mesonbuild/wrap/wrap.py
+++ mesonbuild/wrap/wrap.py
@@ -103,6 +103,7 @@ class WrapNotFoundException(WrapException):
 
 class PackageDefinition:
     def __init__(self, fname: str):
+        self.filename = fname
         self.type = None
         self.values = {} # type: T.Dict[str, str]
         self.provided_deps = {} # type: T.Dict[str, T.Optional[str]]
