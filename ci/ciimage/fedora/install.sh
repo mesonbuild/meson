136b3971624994d20615a002fef18042d0b0a8ca
Nirbheek Chauhan <nirbheek@centricular.com>
2020-08-25 21:34:02 +0530

    ci: Need to update all image rebuild scripts
    Since we're changing the OpenSuse one, we also need to update the rest
    since all the images will be rebuilt now.

--- ci/ciimage/fedora/install.sh
+++ ci/ciimage/fedora/install.sh
@@ -3,11 +3,11 @@
 set -e
 
 pkgs=(
-  python python-setuptools python-wheel python-pip python-pytest-xdist pygobject3 python3-devel python2-devel
-  ninja-build make git autoconf automake patch python3-Cython python2-Cython python3-jsonschema
+  python python-setuptools python-wheel python-pip python-pytest-xdist pygobject python3-devel python2-devel
+  ninja-build make git autoconf automake patch python3-Cython python3-jsonschema
   elfutils gcc gcc-c++ gcc-fortran gcc-objc gcc-objc++ vala rust bison flex ldc libasan libasan-static
   mono-core boost-devel gtkmm30 gtest-devel gmock-devel protobuf-devel wxGTK3-devel gobject-introspection
-  boost-python3-devel boost-python2-devel
+  boost-python3-devel
   itstool gtk3-devel java-latest-openjdk-devel gtk-doc llvm-devel clang-devel SDL2-devel graphviz-devel zlib zlib-devel zlib-static
   #hdf5-openmpi-devel hdf5-devel netcdf-openmpi-devel netcdf-devel netcdf-fortran-openmpi-devel netcdf-fortran-devel scalapack-openmpi-devel
   doxygen vulkan-devel vulkan-validation-layers-devel openssh mercurial gtk-sharp2-devel libpcap-devel gpgme-devel
