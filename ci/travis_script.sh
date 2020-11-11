3aef33a99a718d8fed36539c109a14c984e0923b
Dylan Baker <dylan@pnwbakers.com>
2020-11-10 16:03:49 -0800

    travis: Drop osx support
    We're now running this through github actions, no need for travis.

--- ci/travis_script.sh
+++ ci/travis_script.sh
@@ -4,16 +4,15 @@ set -e
 
 msg() { echo -e "\x1b[1;32mINFO:  \x1b[37m$*\x1b[0m"; }
 
-if [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
-  # TODO enable coverage
-  #curl -s https://codecov.io/bash > upload.sh
-  #chmod +x upload.sh
+# TODO enable coverage
+#curl -s https://codecov.io/bash > upload.sh
+#chmod +x upload.sh
 
-  # We need to copy the current checkout inside the Docker container,
-  # because it has the MR id to be tested checked out.
+# We need to copy the current checkout inside the Docker container,
+# because it has the MR id to be tested checked out.
 
-  msg "Generating runner:"
-  cat <<EOF | tee run.sh
+msg "Generating runner:"
+cat <<EOF | tee run.sh
 #!/bin/bash
 
 set -e
@@ -50,23 +49,9 @@ ADD . /root
 
 EOF
 
-  msg "Building the docker image..."
-  docker build -t test_img .
+msg "Building the docker image..."
+docker build -t test_img .
 
-  msg "Start running tests"
-  #ci_env=`bash <(curl -s https://codecov.io/env)`
-  docker run --security-opt seccomp:unconfined test_img /root/run.sh
-
-elif [[ "$TRAVIS_OS_NAME" == "osx"   ]]; then
-  # Ensure that llvm is added after $PATH, otherwise the clang from that llvm install will be used instead of the native apple clang.
-  export SDKROOT=$(xcodebuild -version -sdk macosx Path)
-  export CPPFLAGS=-I/usr/local/include LDFLAGS=-L/usr/local/lib
-  export OBJC=$CC
-  export OBJCXX=$CXX
-  export PATH=$HOME/tools:/usr/local/opt/qt/bin:$PATH:$(brew --prefix llvm)/bin
-  if test "$MESON_RSP_THRESHOLD" != ""
-  then
-    export MESON_RSP_THRESHOLD=$MESON_RSP_THRESHOLD
-  fi
-  ./run_tests.py $RUN_TESTS_ARGS --backend=ninja -- $MESON_ARGS
-fi
+msg "Start running tests"
+#ci_env=`bash <(curl -s https://codecov.io/env)`
+docker run --security-opt seccomp:unconfined test_img /root/run.sh
