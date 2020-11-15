7ba4c570c11517084ce1ce75ebc5c71286ec2786
e820 <l.lehning@icloud.com>
2020-11-15 08:42:13 +0100

    Minor improvements according to PR feedback

--- mesonbuild/compilers/zig.py
+++ mesonbuild/compilers/zig.py
@@ -1,4 +1,4 @@
-# Copyright 2012-2017 The Meson development team
+# Copyright 2012-2020 The Meson development team
 
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -18,7 +18,8 @@ import textwrap
 import typing as T
 
 from .compilers import Compiler
-from ..mesonlib import MachineChoice, EnvironmentException
+from .mixins.islinker import BasicLinkerIsCompilerMixin
+from ..mesonlib import MachineChoice, EnvironmentException, MesonException
 
 if T.TYPE_CHECKING:
     from ..envconfig import MachineInfo
@@ -35,7 +36,7 @@ zig_optimization_args = {
 }  # type: T.Dict[str, T.List[str]]
 
 
-class ZigCompiler(Compiler):
+class ZigCompiler(BasicLinkerIsCompilerMixin, Compiler):
     language = 'zig'
 
     def __init__(self, exelist: T.List[str], version: str, for_machine: MachineChoice, info: 'MachineInfo',
@@ -48,10 +49,23 @@ class ZigCompiler(Compiler):
         return zig_optimization_args[optimization_level]
 
     def get_output_args(self, outputname: str) -> T.List[str]:
-        return ['-femit-bin=%s' % outputname]
+        return [f'-femit-bin={outputname}']
 
     def get_pic_args(self) -> T.List[str]:
         return ['-fPIC']
+
+    def get_colorout_args(self, colortype: str) -> T.List[str]:
+        if colortype == 'auto':
+            return ['--color', 'auto']
+        elif colortype == 'always':
+            return ['--color', 'on']
+        elif colortype == 'never':
+            return ['--color', 'off']
+        else:
+            raise MesonException(f'Invalid color type for zig {colortype}')
+
+    def get_std_shared_lib_link_args(self) -> T.List[str]:
+        return ['-dynamic']
 
     def needs_static_linker(self) -> bool:
         return False
@@ -96,4 +110,4 @@ class ZigCompiler(Compiler):
         pe = subprocess.Popen(cmdlist, stdout=subprocess.DEVNULL, stderr=subprocess.DEVNULL)
         pe.wait()
         if pe.returncode != 0:
-            raise EnvironmentException('Executables created by Zig compiler %s are not runnable.' % self.name_string())
+            raise EnvironmentException(f'Executables created by Zig compiler {self.name_string()} are not runnable.')
