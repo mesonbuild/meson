mesonbuild/meson

commit cce172432be30233e9876264c259342d2a2006ad

Use run-clang-tidy when available.

mesonbuild/scripts/clangtidy.py
@@ -14,23 +14,37 @@
 
 import pathlib
 import subprocess
+import shutil
 from concurrent.futures import ThreadPoolExecutor
 
 from ..compilers import lang_suffixes
 
-def clangformat(srcdir_name, builddir_name):
+def manual_clangformat(srcdir_name, builddir_name):
     srcdir = pathlib.Path(srcdir_name)
     suffixes = set(lang_suffixes['c']).union(set(lang_suffixes['cpp']))
     suffixes.add('h')
     futures = []
+    returncode = 0
     with ThreadPoolExecutor() as e:
         for f in (x for suff in suffixes for x in srcdir.glob('**/*.' + suff)):
             strf = str(f)
             if strf.startswith(builddir_name):
                 continue
-            futures.append(e.submit(subprocess.check_call, ['clang-tidy', '-p', builddir_name, strf]))
-        [x.result() for x in futures]
-    return 0
+            futures.append(e.submit(subprocess.run, ['clang-tidy', '-p', builddir_name, strf]))
+        [max(returncode, x.result().returncode) for x in futures]
+    return returncode
+
+def clangformat(srcdir_name, builddir_name):
+    run_clang_tidy = None
+    for rct in ('run-clang-tidy', 'run-clang-tidy.py'):
+        if shutil.which(rct):
+            run_clang_tidy = rct
+            break
+    if run_clang_tidy:
+        return subprocess.run([run_clang_tidy, '-p', builddir_name]).returncode
+    else:
+        print('Could not find run-clang-tidy, running checks manually.')
+        manual_clangformat(srcdir_name, builddir_name)
 
 def run(args):
     srcdir_name = args[0]

commit c3fa9b2f5c5c191db42769907d25d99d61375874

Fix clang-tidy return value reporting (Part Ⅱ)
It turns out my first attempt to fix this in 00d5ef3191e5 ("Fix
clang-tidy return value reporting (#7949)") is not sufficient: The
local variable returncode is never updated and stays at 0. This fixes
the returncode calculation.

Fixes: cce172432be3 ("Use run-clang-tidy when available.")

mesonbuild/scripts/clangtidy.py
@@ -36,7 +36,7 @@ def manual_clangtidy(srcdir_name: str, builddir_name: str) -> int:
             if strf.startswith(builddir_name):
                 continue
             futures.append(e.submit(subprocess.run, ['clang-tidy', '-p', builddir_name, strf]))
-        [max(returncode, x.result().returncode) for x in futures]
+        returncode = max([x.result().returncode for x in futures])
     return returncode
 
 def clangtidy(srcdir_name: str, builddir_name: str) -> int: