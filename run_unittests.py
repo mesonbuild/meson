3df064484a09ef9a1ece98ea74e9c87855994d63
Jussi Pakkanen <jpakkane@gmail.com>
2020-11-21 23:01:27 +0200

    Add unit test.

--- run_unittests.py
+++ run_unittests.py
@@ -5821,6 +5821,16 @@ class WindowsTests(BasePlatformTests):
         self.init(testdir, extra_args=['-Db_vscrt=mtd'])
         sanitycheck_vscrt('/MTd')
 
+    def test_modules(self):
+        if self.backend is not Backend.ninja:
+            raise unittest.SkipTest('C++ modules only work with the Ninja backend (not {}).'.format(self.backend.name))
+        if 'VSCMD_VER' not in os.environ:
+            raise unittest.SkipTest('C++ modules is only supported with Visual Studio.')
+        if version_compare(os.environ['VSCMD_VER'], '<16.9.0'):
+            raise unittest.SkipTest('C++ modules are only supported with VS 2019 Preview or newer.')
+        self.init(os.path.join(self.unit_test_dir, '87 cpp modules'))
+        self.build()
+
 
 @unittest.skipUnless(is_osx(), "requires Darwin")
 class DarwinTests(BasePlatformTests):
