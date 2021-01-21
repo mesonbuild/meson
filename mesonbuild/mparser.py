d3cdc15c9020411651d2c4925048e767657e2804
Dylan Baker <dylan@pnwbakers.com>
2021-01-21 13:47:32 -0800

    mparser: Use an EmptyNode instead of None, which is invalid

--- mesonbuild/mparser.py
+++ mesonbuild/mparser.py
@@ -446,8 +446,8 @@ class IfNode(BaseNode):
 class IfClauseNode(BaseNode):
     def __init__(self, linenode: BaseNode):
         super().__init__(linenode.lineno, linenode.colno, linenode.filename)
-        self.ifs = []          # type: T.List[IfNode]
-        self.elseblock = None  # type: T.Union[EmptyNode, CodeBlockNode]
+        self.ifs: T.List[IfNode] = []
+        self.elseblock: T.Union[EmptyNode, CodeBlockNode] = EmptyNode(-1, -1, '')
 
 class UMinusNode(BaseNode):
     def __init__(self, current_location: Token, value: BaseNode):
