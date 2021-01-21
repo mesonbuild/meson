f34b8af3fbdbf3e3d19b5655b736cb7d1c6ba1e6
Dylan Baker <dylan@pnwbakers.com>
2021-01-21 10:49:01 -0800

    tests/rust: Add a generated library to generated main test
    Which is broken, of course.

--- test cases/rust/11 generated main/gen.py
+++ test cases/rust/11 generated main/gen.py
@@ -6,10 +6,14 @@ import argparse
 def main() -> None:
     parser = argparse.ArgumentParser()
     parser.add_argument('out')
+    parser.add_argument('--mode', choices=['main', 'lib'], default='main')
     args = parser.parse_args()
 
     with open(args.out, 'w') as f:
-        f.write('fn main() { println!("I prefer tarnish, actually.") }')
+        if args.mode == 'main':
+            f.write('fn main() { println!("I prefer tarnish, actually.") }')
+        elif args.mode == 'lib':
+            f.write('pub fn libfun() { println!("I prefer tarnish, actually.") }')
 
 
 if __name__ == "__main__":
