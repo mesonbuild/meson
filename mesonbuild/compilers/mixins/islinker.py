6c1467db272c4700ae79f74c3541f6ccc4814138
Dylan Baker <dylan@pnwbakers.com>
2021-02-16 12:01:36 -0800

    compilers: Only insert -flto-jobs in clang's link arguments
    Clang has a hand `-Wunused-command-line-argument` switch, which when
    turned to an error, gets very grump about `-flto-jobs=0` being set in
    the compiler arguments (although `-flto=` belongs there). We'll refactor
    a bit to put that only in the link arguments.
    
    GCC doesn't have this probably because, a) it doesn't have an equivalent
    warning, and b) it uses `-flto=<$numthreads.
    
    Fixes: #8347

--- mesonbuild/compilers/mixins/islinker.py
+++ mesonbuild/compilers/mixins/islinker.py
@@ -48,7 +48,7 @@ class BasicLinkerIsCompilerMixin(Compiler):
     def sanitizer_link_args(self, value: str) -> T.List[str]:
         return []
 
-    def get_lto_link_args(self) -> T.List[str]:
+    def get_lto_link_args(self, *, threads: int = 0, mode: str = 'default') -> T.List[str]:
         return []
 
     def can_linker_accept_rsp(self) -> bool:
