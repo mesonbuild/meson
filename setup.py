3c205fc1b5a281e8d2312282ec18b57fdacedf5a
Eli Schwartz <eschwartz@archlinux.org>
2021-03-09 15:41:47 -0500

    setuptools: move stuff to declarative cfg if possible
    We're down to just declaring the data files in python now.
    
    setup.cfg can, uniquely, retrieve version info by trying to parse the
    AST for simple assignments (which we use) instead of importing the
    entire module.

--- setup.py
+++ setup.py
@@ -20,23 +20,12 @@ if sys.version_info < (3, 6):
     raise SystemExit('ERROR: Tried to install Meson with an unsupported Python version: \n{}'
                      '\nMeson requires Python 3.6.0 or greater'.format(sys.version))
 
-from mesonbuild.coredata import version
-from setuptools import setup, find_packages
+from setuptools import setup
 
-# On windows, will create Scripts/meson.exe and Scripts/meson-script.py
-# Other platforms will create bin/meson
-entries = {'console_scripts': ['meson=mesonbuild.mesonmain:main']}
 data_files = []
 if sys.platform != 'win32':
     # Only useful on UNIX-like systems
     data_files = [('share/man/man1', ['man/meson.1']),
                   ('share/polkit-1/actions', ['data/com.mesonbuild.install.policy'])]
 
-setup(name='meson',
-      version=version,
-      packages=find_packages(
-          include=['mesonbuild', 'mesonbuild.*'],
-          exclude=['*.data']
-      ),
-      entry_points=entries,
-      data_files=data_files,)
+setup(data_files=data_files,)
