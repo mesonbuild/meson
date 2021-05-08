076def12026a53dae2525189432f84453da4c126
Volker Weißmann <volker.weissmann@gmx.de>
2021-05-08 19:41:27 +0200

    simple bugfix

--- mesonbuild/compilers/mixins/arm.py
+++ mesonbuild/compilers/mixins/arm.py
@@ -85,6 +85,7 @@ class ArmCompiler(Compiler):
                           '3': default_warn_args + []}  # type: T.Dict[str, T.List[str]]
         # Assembly
         self.can_compile_suffixes.add('s')
+        self.can_compile_suffixes.add('S')
 
     def get_pic_args(self) -> T.List[str]:
         # FIXME: Add /ropi, /rwpi, /fpic etc. qualifiers to --apcs
@@ -152,6 +153,7 @@ class ArmclangCompiler(Compiler):
              'b_ndebug', 'b_staticpic', 'b_colorout']}
         # Assembly
         self.can_compile_suffixes.add('s')
+        self.can_compile_suffixes.add('S')
 
     def get_pic_args(self) -> T.List[str]:
         # PIC support is not enabled by default for ARM,
