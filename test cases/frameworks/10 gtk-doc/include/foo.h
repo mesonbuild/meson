4e312c19e693a69b0650ce6c8a8903163c959996
Xavier Claessens <xavier.claessens@collabora.com>
2021-05-11 09:18:47 -0400

    gnome: Fix gtkdoc generation
    install_scripts used to replace @BUILD_ROOT@ and @SOURCE_ROOT@ but it
    was not documented and got removed in Meson 0.58.0. gnome.gtkdoc() was
    relying on that behaviour, but it has always been broken in the case the
    source or build directory contains spaces.
    
    Fix this by changing get_include_args() to substitue paths directly
    which will then get escaped correctly.
    
    Add a unit test that builds GObject documentation which is where this
    issue has been spotted.
    
    Fixes: #8744

--- test cases/frameworks/10 gtk-doc/include/foo.h
+++ test cases/frameworks/10 gtk-doc/include/foo.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <glib-object.h>
+
 /**
  * FooIndecision:
  * @FOO_MAYBE:     Something maybe
@@ -13,3 +15,19 @@ typedef enum {
     FOO_POSSIBLY,
 } FooIndecision;
 
+/**
+ * FooObjClass:
+ *
+ * The class
+ */
+
+/**
+ * FooObj:
+ *
+ * The instance
+ */
+
+#define FOO_TYPE_OBJ foo_obj_get_type()
+G_DECLARE_FINAL_TYPE(FooObj, foo_obj, FOO, OBJ, GObject)
+
+int foo_do_something(FooObj *self);
