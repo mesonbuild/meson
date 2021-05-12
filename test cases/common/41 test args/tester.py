96e8e019303abe5626e737867e1249ab6eb53dff
Xavier Claessens <xavier.claessens@collabora.com>
2021-05-12 16:16:41 -0400

    interpreter: flatten environment() initial values
    Turns out listify() flattens by default, but stringlistify() cannot
    flatten... How do I realize this only now?
    
    Fixes: #8727

--- test cases/common/41 test args/tester.py
+++ test cases/common/41 test args/tester.py
@@ -1,6 +1,10 @@
 #!/usr/bin/env python3
 
 import sys
+import os
+
+assert os.environ['MESONTESTING'] == 'picklerror'
+assert os.environ['TEST_LIST_FLATTENING'] == '1'
 
 with open(sys.argv[1]) as f:
     if f.read() != 'contents\n':
