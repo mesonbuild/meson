157f6f7bb8b1b08a67efbf3f67f4be7323191401
Naveen M K <naveen@syrusdark.website>
2021-05-28 22:35:36 +0530

    Remove `Microsoft.VisualStudio.Workload.WDExpress`

--- mesonbuild/mesonmain.py
+++ mesonbuild/mesonmain.py
@@ -73,7 +73,6 @@ def setup_vsenv():
             '-prerelease',
             '-requiresAny',
             '-requires', 'Microsoft.VisualStudio.Component.VC.Tools.x86.x64',
-            '-requires', 'Microsoft.VisualStudio.Workload.WDExpress',
             '-products', '*',
             '-format',
             'json'
