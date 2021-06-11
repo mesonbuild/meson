202e345dfba7ff3a39e4fb560b98889182dcc506
Daniel Mensinger <daniel@mensinger-ka.de>
2021-06-11 16:01:14 +0200

    interpreter: Refactor interpreter.compiler to use ObjectHolder

--- mesonbuild/compilers/__init__.py
+++ mesonbuild/compilers/__init__.py
@@ -15,6 +15,7 @@
 # Public symbols for compilers sub-package when using 'from . import compilers'
 __all__ = [
     'Compiler',
+    'RunResult',
 
     'all_languages',
     'base_options',
@@ -112,6 +113,7 @@ __all__ = [
 # Bring symbols from each module into compilers sub-package namespace
 from .compilers import (
     Compiler,
+    RunResult,
     all_languages,
     base_options,
     clib_langs,
