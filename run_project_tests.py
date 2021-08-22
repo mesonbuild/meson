dbce1cfed689537b38bb7548fababa62b0961f23
Mat Cross <mathewc@nag.co.uk>
2021-08-22 10:35:49 +0100

    Presence of nagfor signifies that Fortran tests can be run.

--- run_project_tests.py
+++ run_project_tests.py
@@ -1050,6 +1050,7 @@ def detect_tests_to_run(only: T.Dict[str, T.List[str]], use_tmp: bool) -> T.List
     skip_fortran = not(shutil.which('gfortran') or
                        shutil.which('flang') or
                        shutil.which('pgfortran') or
+                       shutil.which('nagfor') or
                        shutil.which('ifort'))
 
     class TestCategory:
