2cb8c34240c1279cbcc126e2b9a85363b5b7f1a1
Moroz Oleg <o.moroz@1440.space>
2021-09-03 09:53:11 +0300

    fix for issue https://github.com/mesonbuild/meson/issues/6314

--- mesonbuild/backend/vs2010backend.py
+++ mesonbuild/backend/vs2010backend.py
@@ -1099,6 +1099,11 @@ class Vs2010Backend(backends.Backend):
         else:
             ET.SubElement(clconf, 'FavorSizeOrSpeed').text = 'Speed'
         # Note: SuppressStartupBanner is /NOLOGO and is 'true' by default
+        if self.name in('vs2017', 'vs2019'):
+            if 'cpp' in file_args:
+                optargs = [x for x in file_args['cpp'] if x.startswith('/std:c++')]
+                if len(optargs)>0:
+                    ET.SubElement(clconf, 'LanguageStandard').text = optargs[0].replace("/std:c++","stdcpp")'
         pch_sources = {}
         if self.environment.coredata.options.get(OptionKey('b_pch')):
             for lang in ['c', 'cpp']:
