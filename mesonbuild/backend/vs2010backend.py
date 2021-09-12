387e84656845cfcc3c966f6cf754de65d601eccd
Олег Мороз <oleg.moroz@mcc.vniiem.ru>
2021-09-12 10:11:01 +0300

    Fixes .vcxproj for vs2017 vs2019
    fixes #6314
    
    in case of backend is vs2017 or vs2019 place LanguageStandard tag with stdcpp version and LanguageStandard_C tag with stdc version in .vcxproj file

--- mesonbuild/backend/vs2010backend.py
+++ mesonbuild/backend/vs2010backend.py
@@ -1119,6 +1119,15 @@ class Vs2010Backend(backends.Backend):
         else:
             ET.SubElement(clconf, 'FavorSizeOrSpeed').text = 'Speed'
         # Note: SuppressStartupBanner is /NOLOGO and is 'true' by default
+        if self.name in ('vs2017', 'vs2019'):
+            if 'cpp' in file_args:
+                optargs = [x for x in file_args['cpp'] if x.startswith('/std:c++')]
+                if optargs:
+                    ET.SubElement(clconf, 'LanguageStandard').text = optargs[0].replace("/std:c++","stdcpp")
+            if 'c' in file_args:
+                optargs = [x for x in file_args['c'] if x.startswith('/std:c')]
+                if optargs:
+                    ET.SubElement(clconf, 'LanguageStandard_C').text = optargs[0].replace("/std:c","stdc")
         pch_sources = {}
         if self.environment.coredata.options.get(OptionKey('b_pch')):
             for lang in ['c', 'cpp']:
