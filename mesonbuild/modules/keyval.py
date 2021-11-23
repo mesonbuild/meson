717d03af1caf35ea73356ef8135f26f3aea98277
Dylan Baker <dylan@pnwbakers.com>
2021-11-23 13:15:28 -0800

    modules/keyval: cleanup imports

--- mesonbuild/modules/keyval.py
+++ mesonbuild/modules/keyval.py
@@ -12,13 +12,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from . import ExtensionModule
-
-from .. import mesonlib
-from ..mesonlib import typeslistify
-from ..interpreterbase import FeatureNew, noKwargs, InvalidCode
-
 import os
+
+from . import ExtensionModule
+from .. import mesonlib
+from ..interpreterbase import FeatureNew, noKwargs, InvalidCode
+from ..mesonlib import typeslistify
 
 class KeyvalModule(ExtensionModule):
 
