mesonbuild/meson

commit dbb109703e0b1f91965868c3ab193eff6e45eb22

convert more mlog.deprecation into FeatureDeprecated

mesonbuild/modules/gnome.py
@@ -990,7 +990,8 @@ class GnomeModule(ExtensionModule):
             scan_command += ['--sources-top-dirs', os.path.join(state.environment.get_build_dir(), self.interpreter.subproject_dir, state.subproject)]
 
         if '--warn-error' in scan_command:
-            mlog.deprecation('Passing --warn-error is deprecated in favor of "fatal_warnings" keyword argument since v0.55')
+            FeatureDeprecated.single_use('gnome.generate_gir argument --warn-error', '0.55.0',
+                                         state.subproject, 'Use "fatal_warnings" keyword argument')
         if kwargs['fatal_warnings']:
             scan_command.append('--warn-error')
 

mesonbuild/build.py
@@ -40,7 +40,7 @@ from .compilers import (
     is_known_suffix, detect_static_linker, detect_compiler_for
 )
 from .linkers import StaticLinker
-from .interpreterbase import FeatureNew
+from .interpreterbase import FeatureNew, FeatureDeprecated
 
 if T.TYPE_CHECKING:
     from ._typing import ImmutableListProtocol, ImmutableSetProtocol
@@ -1421,7 +1421,8 @@ You probably should put it in link_with instead.''')
             if os.path.dirname(pchlist[0]) != os.path.dirname(pchlist[1]):
                 raise InvalidArguments('PCH files must be stored in the same folder.')
 
-            mlog.warning('PCH source files are deprecated, only a single header file should be used.')
+            FeatureDeprecated.single_use('PCH source files', '0.50.0', self.subproject,
+                                         'Only a single header file should be used.')
         elif len(pchlist) > 2:
             raise InvalidArguments('PCH definition may have a maximum of 2 files.')
         for f in pchlist:

commit cedea374df47ce0901de062989ace2e474e3b7b7

modules/gnome: import missing FeatureDeprecated for generate_gir
Commit dbb109703e0b1f91965868c3ab193eff6e45eb22 broke generate_gir
by using FeatureDeprecated without importing it.

mesonbuild/modules/gnome.py
@@ -33,7 +33,7 @@ from ..build import CustomTarget, CustomTargetIndex, GeneratedList, InvalidArgum
 from ..dependencies import Dependency, PkgConfigDependency, InternalDependency
 from ..interpreter.type_checking import DEPEND_FILES_KW, INSTALL_KW, NoneType, in_set_validator
 from ..interpreterbase import noPosargs, noKwargs, permittedKwargs, FeatureNew, FeatureDeprecatedKwargs
-from ..interpreterbase import typed_kwargs, KwargInfo, ContainerTypeInfo
+from ..interpreterbase import typed_kwargs, KwargInfo, ContainerTypeInfo, FeatureDeprecated
 from ..interpreterbase.decorators import typed_pos_args
 from ..mesonlib import (
     MachineChoice, MesonException, OrderedSet, Popen_safe, join_args,