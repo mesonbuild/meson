8267e47d93d27072fc52f43506c6e62e256546f0
Michele Dionisio <michele.dionisio@gmail.com>
2022-01-30 16:12:48 +0100

    partial rollback on clang-tidy that not work as clang-format
    partial rollback of 364d951b347ab932d9a78cb6846d1288925cf32c
    because run-clang-format work in a different way anche check only
    what is build and not all source code

--- mesonbuild/scripts/clangtidy.py
+++ mesonbuild/scripts/clangtidy.py
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import shutil
+import re
+import os
 import argparse
 import subprocess
 from pathlib import Path
@@ -21,6 +24,19 @@ import typing as T
 
 def run_clang_tidy(fname: Path, builddir: Path) -> subprocess.CompletedProcess:
     return subprocess.run(['clang-tidy', '-p', str(builddir), str(fname)])
+    
+def clangtidy(srcdir: Path, builddir: Path) -> int:
+    run_clang_tidy_exist = None
+    for rct in ('run-clang-tidy', 'run-clang-tidy.py'):
+        if shutil.which(rct):
+            run_clang_tidy_exist = rct
+            break
+    if run_clang_tidy_exist:
+        builddir_name =  str(builddir)
+        return subprocess.run([run_clang_tidy_exist, '-p', builddir_name, '^(?!' + re.escape(builddir_name + os.path.sep) + ').*$']).returncode
+    else:
+        print('Could not find run-clang-tidy, running checks manually.')
+        return run_tool('clang-tidy', srcdir, builddir, run_clang_tidy, builddir)
 
 def run(args: T.List[str]) -> int:
     parser = argparse.ArgumentParser()
@@ -30,5 +46,6 @@ def run(args: T.List[str]) -> int:
 
     srcdir = Path(options.sourcedir)
     builddir = Path(options.builddir)
+    
+    return clangtidy(srcdir, builddir)
 
-    return run_tool('clang-tidy', srcdir, builddir, run_clang_tidy, builddir)
