e2e74ee371df0cdc940901e7ea68a35970f54033
Dylan Baker <dylan@pnwbakers.com>
2022-03-07 10:16:27 -0800

    mesonlib: Add a from_node initializer to MesonException
    This is useful when creating an exception with a node option, with less
    typing.

--- mesonbuild/mesonlib/universal.py
+++ mesonbuild/mesonlib/universal.py
@@ -41,6 +41,7 @@ if T.TYPE_CHECKING:
     from ..build import ConfigurationData
     from ..coredata import KeyedOptionDictType, UserOption
     from ..compilers.compilers import Compiler
+    from ..mparser import BaseNode
 
 FileOrString = T.Union['File', str]
 
@@ -170,6 +171,15 @@ class MesonException(Exception):
         self.file = file
         self.lineno = lineno
         self.colno = colno
+
+    @classmethod
+    def from_node(cls, *args: object, node: BaseNode) -> MesonException:
+        """Create a MesonException with location data from a BaseNode
+
+        :param node: A BaseNode to set location data from
+        :return: A Meson Exception instance
+        """
+        return cls(*args, file=node.filename, lineno=node.lineno, colno=node.colno)
 
 
 class MesonBugException(MesonException):
