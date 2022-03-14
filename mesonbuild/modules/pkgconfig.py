b587580551b4bc45feb578194c8401680da78558
Dylan Baker <dylan@pnwbakers.com>
2022-03-14 11:03:15 -0700

    modules/pkgconfig: combine two branches
    There is an `if InternalDependency` followed by `elif Dependency` branch
    that are identical, except for one call in the InternalDependency
    branch. These can be combined to reduce code duplication.

--- mesonbuild/modules/pkgconfig.py
+++ mesonbuild/modules/pkgconfig.py
@@ -190,18 +190,15 @@ class DependenciesHelper:
                 if obj.found():
                     processed_reqs.append(obj.name)
                     self.add_version_reqs(obj.name, obj.version_reqs)
-            elif isinstance(obj, dependencies.InternalDependency):
-                if obj.found():
-                    if obj.objects:
-                        raise mesonlib.MesonException('.pc file cannot refer to individual object files.')
-                    processed_libs += obj.get_link_args()
-                    processed_cflags += obj.get_compile_args()
-                    self._add_lib_dependencies(obj.libraries, obj.whole_libraries, obj.ext_deps, public, private_external_deps=True)
-                    self._add_uninstalled_incdirs(obj.get_include_dirs())
             elif isinstance(obj, dependencies.Dependency):
                 if obj.found():
                     processed_libs += obj.get_link_args()
                     processed_cflags += obj.get_compile_args()
+                    if isinstance(obj, dependencies.InternalDependency):
+                        if obj.objects:
+                            raise mesonlib.MesonException('.pc file cannot refer to individual object files.')
+                        self._add_lib_dependencies(obj.libraries, obj.whole_libraries, obj.ext_deps, public, private_external_deps=True)
+                        self._add_uninstalled_incdirs(obj.get_include_dirs())
             elif isinstance(obj, build.SharedLibrary) and obj.shared_library_only:
                 # Do not pull dependencies for shared libraries because they are
                 # only required for static linking. Adding private requires has
