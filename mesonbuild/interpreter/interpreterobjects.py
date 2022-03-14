d29647c907c042bec65f7dd39c9d71ad3dc563f1
Dylan Baker <dylan@pnwbakers.com>
2022-03-14 12:14:20 -0700

    interpreterobjects: warn for partial_dependency includes for non-system
    Due to the implementation of combining compiler arguments and include
    arguments only InternalDependencies worked correctly with
    `partial_dependency(includes : true)`, and we should warn about this.

--- mesonbuild/interpreter/interpreterobjects.py
+++ mesonbuild/interpreter/interpreterobjects.py
@@ -28,6 +28,8 @@ from ..mesonlib import HoldableObject, OptionKey, listify, Popen_safe
 
 import typing as T
 
+from mesonbuild import dependencies
+
 if T.TYPE_CHECKING:
     from . import kwargs
     from ..cmake.interpreter import CMakeInterpreter
@@ -528,6 +530,9 @@ class DependencyHolder(ObjectHolder[Dependency]):
     @noPosargs
     @typed_kwargs('dependency.partial_dependency', *_PARTIAL_DEP_KWARGS)
     def partial_dependency_method(self, args: T.List[TYPE_nvar], kwargs: 'kwargs.DependencyMethodPartialDependency') -> Dependency:
+        if kwargs['includes'] and not isinstance(self.held_object, dependencies.InternalDependency):
+            FeatureNew.single_use('dependency.partial_depencency with "includes" for exteranl dependencies', '1.5.0', self.subproject,
+                                  'use "compile_args: meson.version().version_compare(\'< 1.5.0\')"', self.current_node)
         pdep = self.held_object.get_partial_dependency(**kwargs)
         return pdep
 
