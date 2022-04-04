6a2e130eae94881868c95efdd9df4feae7db0b7d
Jonathan Lopez <jonathanglopez@gmail.com>
2022-04-03 23:21:30 -0400

    Create subprojects directory in meson project root if subprojects does not exist

--- mesonbuild/wrap/wraptool.py
+++ mesonbuild/wrap/wraptool.py
@@ -110,12 +110,13 @@ def get_project_root() -> pathlib.Path:
 
 def install(options: 'argparse.Namespace') -> None:
     name = options.name
-    if not os.path.isdir('subprojects'):
-        raise SystemExit('Subprojects dir not found. Run this script in your source root directory.')
-    if os.path.isdir(os.path.join('subprojects', name)):
-        raise SystemExit('Subproject directory for this project already exists.')
-    wrapfile = os.path.join('subprojects', name + '.wrap')
-    if os.path.exists(wrapfile):
+    subprojects = get_project_root() / "subprojects"
+    if not subprojects.is_dir():
+        subprojects.mkdir()
+    if (subprojects / name).is_dir():
+        raise SystemExit(f"Subproject directory for {name} already exists.")
+    wrapfile = subprojects / f"{name}.wrap"
+    if wrapfile.exists():
         raise SystemExit('Wrap file already exists.')
     (version, revision) = get_latest_version(name, options.allow_insecure)
     url = open_wrapdburl(f'https://wrapdb.mesonbuild.com/v2/{name}_{version}-{revision}/{name}.wrap', options.allow_insecure, True)
