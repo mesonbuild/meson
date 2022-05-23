e559ea0b7c96f2436d82ed9f94fe78d7c2aaef1d
Paolo Bonzini <pbonzini@redhat.com>
2022-05-23 14:48:19 +0200

    add testcase for previous commit

--- test cases/common/216 custom target input extracted objects/check_object.py
+++ test cases/common/216 custom target input extracted objects/check_object.py
@@ -3,11 +3,15 @@
 import sys, os
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
-        print(sys.argv[0], 'object', 'output')
+    if len(sys.argv) < 4:
+        print(sys.argv[0], 'n output objects...')
         sys.exit(1)
-    elif os.path.exists(sys.argv[1]):
-        with open(sys.argv[2], 'wb') as out:
-            pass
-    else:
+    if len(sys.argv) != int(sys.argv[1]) + 3:
+        print(f'expected {sys.argv[1]} objects, got {len(sys.argv) - 3}')
         sys.exit(1)
+    for i in sys.argv[3:]:
+        print('testing', i)
+        if not os.path.exists(i):
+            sys.exit(1)
+    with open(sys.argv[2], 'wb') as out:
+        pass
