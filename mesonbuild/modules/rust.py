9e4d950451200038d9b6de5a96dd10d6e59d0418
Dylan Baker <dylan@pnwbakers.com>
2022-06-03 09:56:31 -0700

    modules/rust: Fix passing non-executables to test

--- mesonbuild/modules/rust.py
+++ mesonbuild/modules/rust.py
@@ -144,9 +144,52 @@ class RustModule(ExtensionModule):
         tkwargs['args'] = extra_args + ['--test', '--format', 'pretty']
         tkwargs['protocol'] = 'rust'
 
-        new_target = copy.copy(base_target)
-        new_target.name = name
-        new_target.install = False
+        # If we have an Executable we can take a fast path
+        if isinstance(base_target, Executable):
+            new_target = copy.deepcopy(base_target)
+            new_target.name = name
+            new_target.subdir = state.subdir
+            new_target.install = False
+        else:
+            new_target = Executable(
+                name,
+                state.subdir,
+                state.subproject,
+                base_target.for_machine,
+                state.environment,
+                base_target.sources,
+                base_target.structured_sources,
+                base_target.objects,
+                base_target.compilers,
+                build_by_default=base_target.build_by_default,
+                build_always_stale=base_target.build_always_stale,
+                install=False,
+                extra_files=base_target.extra_files,
+                # Option overrides intentionally not set here
+                build_rpath=base_target.build_rpath,
+                d_debug=base_target.d_features['debug'],
+                d_import_dirs=base_target.d_features['import_dirs'],
+                d_module_versions=base_target.d_features['versions'],
+                d_unittest=base_target.d_features['unittest'],
+                dependencies=list(base_target._added_deps),
+                gnu_symbol_visibility=base_target.gnu_symbol_visibility,
+                implicit_include_directories=base_target.implicit_include_directories,
+                include_directories=base_target.include_dirs,
+                # language args intentionally not set here
+                link_args=base_target.link_args,
+                link_depends=base_target.link_depends,
+                link_language=base_target.link_language,
+                link_whole=base_target.link_whole,
+                name_prefix=base_target.prefix,
+                name_suffix=base_target.suffix,
+                # pch intentionally not set here
+                vala_gir=base_target.vala_gir,
+                vala_header=base_target.vala_header,
+                vala_vapi=base_target.vala_vapi,
+            )
+            new_target.options = copy.deepcopy(base_target.options)
+            new_target.extra_args = copy.deepcopy(base_target.extra_args)
+            new_target.pch = copy.deepcopy(base_target.pch)
         new_target.extra_args['rust'].append('--test')
         new_target.add_deps(dependencies)
 
