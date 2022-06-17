mesonbuild/meson

commit 7c757dff71c031ae02ecc358a69a213a5db4051a

holders: Fix the remaining code to respect the holder changes

mesonbuild/build.py
@@ -40,7 +40,7 @@ from .compilers import (
     is_known_suffix
 )
 from .linkers import StaticLinker
-from .interpreterbase import FeatureNew, TYPE_nkwargs, TYPE_nvar
+from .interpreterbase import FeatureNew
 
 if T.TYPE_CHECKING:
     from ._typing import ImmutableListProtocol, ImmutableSetProtocol

mesonbuild/ast/interpreter.py
@@ -76,6 +76,9 @@ class MockRunTarget(MesonInterpreterObject):
 ADD_SOURCE = 0
 REMOVE_SOURCE = 1
 
+_T = T.TypeVar('_T')
+_V = T.TypeVar('_V')
+
 class AstInterpreter(InterpreterBase):
     def __init__(self, source_root: str, subdir: str, subproject: str, visitors: T.Optional[T.List[AstVisitor]] = None):
         super().__init__(source_root, subdir, subproject)
@@ -140,6 +143,12 @@ class AstInterpreter(InterpreterBase):
                            'summary': self.func_do_nothing,
                            'range': self.func_do_nothing,
                            })
+
+    def _unholder_args(self, args: _T, kwargs: _V) -> T.Tuple[_T, _V]:
+        return args, kwargs
+
+    def _holderify(self, res: _T) -> _T:
+        return res
 
     def func_do_nothing(self, node: BaseNode, args: T.List[TYPE_nvar], kwargs: T.Dict[str, TYPE_nvar]) -> bool:
         return True

mesonbuild/interpreter/compiler.py
@@ -1,7 +1,6 @@
 import functools
 
-from .interpreterobjects import (IncludeDirsHolder, ExternalLibraryHolder,
-                                 extract_required_kwarg, extract_search_dirs)
+from .interpreterobjects import (extract_required_kwarg, extract_search_dirs)
 
 from .. import mesonlib
 from .. import mlog
@@ -14,12 +13,12 @@ import typing as T
 import os
 
 if T.TYPE_CHECKING:
-    from ..environment import Environment
+    from ..interpreter import Interpreter
     from ..compilers import Compiler, RunResult
 
 class TryRunResultHolder(ObjectHolder['RunResult']):
-    def __init__(self, res: 'RunResult'):
-        super().__init__(res)
+    def __init__(self, res: 'RunResult', interpreter: 'Interpreter'):
+        super().__init__(res, interpreter)
         self.methods.update({'returncode': self.returncode_method,
                              'compiled': self.compiled_method,
                              'stdout': self.stdout_method,
@@ -65,9 +64,9 @@ find_library_permitted_kwargs = {
 find_library_permitted_kwargs |= {'header_' + k for k in header_permitted_kwargs}
 
 class CompilerHolder(ObjectHolder['Compiler']):
-    def __init__(self, compiler: 'Compiler', env: 'Environment', subproject: str):
-        super().__init__(compiler, subproject=subproject)
-        self.environment = env
+    def __init__(self, compiler: 'Compiler', interpreter: 'Interpreter'):
+        super().__init__(compiler, interpreter)
+        self.environment = self.env
         self.methods.update({'compiles': self.compiles_method,
                              'links': self.links_method,
                              'get_id': self.get_id_method,
@@ -144,9 +143,10 @@ class CompilerHolder(ObjectHolder['Compiler']):
         args = []
         incdirs = mesonlib.extract_as_list(kwargs, 'include_directories')
         for i in incdirs:
-            if not isinstance(i, IncludeDirsHolder):
+            from ..build import IncludeDirs
+            if not isinstance(i, IncludeDirs):
                 raise InterpreterException('Include directories argument must be an include_directories object.')
-            for idir in i.held_object.to_string_list(self.environment.get_source_dir()):
+            for idir in i.to_string_list(self.environment.get_source_dir()):
                 args += self.compiler.get_include_args(idir, False)
         if not nobuiltins:
             opts = self.environment.coredata.options
@@ -162,7 +162,7 @@ class CompilerHolder(ObjectHolder['Compiler']):
             final_deps = []
             while deps:
                 next_deps = []
-                for d in mesonlib.unholder(mesonlib.listify(deps)):
+                for d in mesonlib.listify(deps):
                     if not isinstance(d, dependencies.Dependency) or d.is_built():
                         raise InterpreterException('Dependencies must be external dependencies')
                     final_deps.append(d)
@@ -223,7 +223,7 @@ class CompilerHolder(ObjectHolder['Compiler']):
             else:
                 h = mlog.red('NO (%d)' % result.returncode)
             mlog.log('Checking if', mlog.bold(testname, True), msg, 'runs:', h)
-        return TryRunResultHolder(result)
+        return result
 
     @noPosargs
     @permittedKwargs({})
@@ -614,7 +614,7 @@ class CompilerHolder(ObjectHolder['Compiler']):
                                            self.environment,
                                            self.compiler.language,
                                            silent=True)
-        return ExternalLibraryHolder(lib, self.subproject)
+        return lib
 
     @FeatureNewKwargs('compiler.find_library', '0.51.0', ['static'])
     @FeatureNewKwargs('compiler.find_library', '0.50.0', ['has_headers'])
@@ -659,7 +659,7 @@ class CompilerHolder(ObjectHolder['Compiler']):
                                                libtype, libname))
         lib = dependencies.ExternalLibrary(libname, linkargs, self.environment,
                                            self.compiler.language)
-        return ExternalLibraryHolder(lib, self.subproject)
+        return lib
 
     @permittedKwargs({})
     def has_argument_method(self, args: T.Sequence[str], kwargs) -> bool:

mesonbuild/interpreter/kwargs.py
@@ -4,15 +4,14 @@
 
 """Keyword Argument type annotations."""
 
+from mesonbuild import coredata
 import typing as T
 
 from typing_extensions import TypedDict, Literal
 
+from .. import build
 from ..mesonlib import MachineChoice, File
-from .interpreterobjects import (
-    BuildTargetHolder, CustomTargetHolder, EnvironmentVariablesHolder,
-    FeatureOptionHolder, TargetHolder
-)
+from .interpreterobjects import EnvironmentVariablesObject
 
 
 class FuncAddProjectArgs(TypedDict):
@@ -34,13 +33,13 @@ class BaseTest(TypedDict):
 
     """Shared base for the Rust module."""
 
-    args: T.List[T.Union[str, File, TargetHolder]]
+    args: T.List[T.Union[str, File, build.Target]]
     should_fail: bool
     timeout: int
     workdir: T.Optional[str]
-    depends: T.List[T.Union[CustomTargetHolder, BuildTargetHolder]]
+    depends: T.List[T.Union[build.CustomTarget, build.BuildTarget]]
     priority: int
-    env: T.Union[EnvironmentVariablesHolder, T.List[str], T.Dict[str, str], str]
+    env: T.Union[EnvironmentVariablesObject, T.List[str], T.Dict[str, str], str]
     suite: T.List[str]
 
 
@@ -70,7 +69,7 @@ class ExtractRequired(TypedDict):
     a boolean or a feature option should inherit it's arguments from this class.
     """
 
-    required: T.Union[bool, 'FeatureOptionHolder']
+    required: T.Union[bool, coredata.UserFeatureOption]
 
 
 class FuncGenerator(TypedDict):
@@ -81,7 +80,7 @@ class FuncGenerator(TypedDict):
     output: T.List[str]
     depfile: bool
     capture:  bool
-    depends: T.List[T.Union['BuildTargetHolder', 'CustomTargetHolder']]
+    depends: T.List[T.Union[build.BuildTarget, build.CustomTarget]]
 
 
 class GeneratorProcess(TypedDict):
@@ -90,3 +89,16 @@ class GeneratorProcess(TypedDict):
 
     preserve_path_from: T.Optional[str]
     extra_args: T.List[str]
+
+class DependencyMethodPartialDependency(TypedDict):
+
+    """ Keyword Arguments for the dep.partial_dependency methods """
+
+    compile_args: bool
+    link_args: bool
+    links: bool
+    includes: bool
+    sources: bool
+
+class BuildTargeMethodExtractAllObjects(TypedDict):
+    recursive: bool

mesonbuild/interpreter/mesonmain.py
@@ -5,16 +5,15 @@ from .. import dependencies
 from .. import build
 from .. import mlog
 
-from ..mesonlib import unholder, MachineChoice, OptionKey
+from ..mesonlib import MachineChoice, OptionKey
 from ..programs import OverrideProgram, ExternalProgram
 from ..interpreterbase import (MesonInterpreterObject, FeatureNewKwargs, FeatureNew, FeatureDeprecated,
                                typed_pos_args, permittedKwargs, noArgsFlattening, noPosargs, noKwargs,
                                MesonVersionString, InterpreterException)
 
-from .compiler import CompilerHolder
 from .interpreterobjects import (ExecutableHolder, ExternalProgramHolder,
                                  CustomTargetHolder, CustomTargetIndexHolder,
-                                 EnvironmentVariablesHolder)
+                                 EnvironmentVariablesObject)
 
 import typing as T
 
@@ -57,11 +56,11 @@ class MesonMain(MesonInterpreterObject):
                              'add_devenv': self.add_devenv_method,
                              })
 
-    def _find_source_script(self, prog: T.Union[str, mesonlib.File, ExecutableHolder], args):
+    def _find_source_script(self, prog: T.Union[str, mesonlib.File, build.Executable, ExternalProgram], args):
 
-        if isinstance(prog, (ExecutableHolder, ExternalProgramHolder)):
-            return self.interpreter.backend.get_executable_serialisation([unholder(prog)] + args)
-        found = self.interpreter.func_find_program({}, prog, {}).held_object
+        if isinstance(prog, (build.Executable, ExternalProgram)):
+            return self.interpreter.backend.get_executable_serialisation([prog] + args)
+        found = self.interpreter.func_find_program({}, prog, {})
         es = self.interpreter.backend.get_executable_serialisation([found] + args)
         es.subproject = self.interpreter.subproject
         return es
@@ -254,7 +253,7 @@ class MesonMain(MesonInterpreterObject):
         for_machine = self.interpreter.machine_from_native_kwarg(kwargs)
         clist = self.interpreter.coredata.compilers[for_machine]
         if cname in clist:
-            return CompilerHolder(clist[cname], self.build.environment, self.interpreter.subproject)
+            return clist[cname]
         raise InterpreterException(f'Tried to access compiler for language "{cname}", not specified for {for_machine.get_lower_case_name()} machine.')
 
     @noPosargs
@@ -375,9 +374,9 @@ class MesonMain(MesonInterpreterObject):
 
     @FeatureNew('add_devenv', '0.58.0')
     @noKwargs
-    @typed_pos_args('add_devenv', (str, list, dict, EnvironmentVariablesHolder))
-    def add_devenv_method(self, args: T.Union[str, list, dict, EnvironmentVariablesHolder], kwargs: T.Dict[str, T.Any]) -> None:
+    @typed_pos_args('add_devenv', (str, list, dict, EnvironmentVariablesObject))
+    def add_devenv_method(self, args: T.Union[str, list, dict, EnvironmentVariablesObject], kwargs: T.Dict[str, T.Any]) -> None:
         env = args[0]
         if isinstance(env, (str, list, dict)):
-            env = EnvironmentVariablesHolder(env)
-        self.build.devenv.append(env.held_object)
+            env = EnvironmentVariablesObject(env)
+        self.build.devenv.append(env.vars)

mesonbuild/modules/python.py
@@ -50,6 +50,7 @@ class PythonDependency(SystemDependency):
         self.variables = python_holder.variables
         self.paths = python_holder.paths
         self.link_libpython = python_holder.link_libpython
+        self.info: T.Optional[T.Dict[str, str]] = None
         if mesonlib.version_compare(self.version, '>= 3.0'):
             self.major_version = 3
         else:
@@ -278,12 +279,20 @@ print (json.dumps ({
 }))
 '''
 
+class PythonExternalProgram(ExternalProgram):
+    def __init__(self, name: str, command: T.Optional[T.List[str]] = None, ext_prog: T.Optional[ExternalProgram] = None):
+        if ext_prog is None:
+            super().__init__(name, command=command, silent=True)
+        else:
+            self.name = ext_prog.name
+            self.command = ext_prog.command
+            self.path = ext_prog.path
+        self.info: T.Dict[str, str] = {}
 
 class PythonInstallation(ExternalProgramHolder):
-    def __init__(self, interpreter, python, info):
-        ExternalProgramHolder.__init__(self, python, interpreter.subproject)
-        self.interpreter = interpreter
-        self.subproject = self.interpreter.subproject
+    def __init__(self, python, interpreter):
+        ExternalProgramHolder.__init__(self, python, interpreter)
+        info = python.info
         prefix = self.interpreter.environment.coredata.get_option(mesonlib.OptionKey('prefix'))
         self.variables = info['variables']
         self.paths = info['paths']
@@ -325,11 +334,10 @@ class PythonInstallation(ExternalProgramHolder):
         # behavior. See https://github.com/mesonbuild/meson/issues/4117
         if not self.link_libpython:
             new_deps = []
-            for holder in mesonlib.extract_as_list(kwargs, 'dependencies'):
-                dep = holder.held_object
+            for dep in mesonlib.extract_as_list(kwargs, 'dependencies'):
                 if isinstance(dep, PythonDependency):
-                    holder = self.interpreter.holderify(dep.get_partial_dependency(compile_args=True))
-                new_deps.append(holder)
+                    dep = dep.get_partial_dependency(compile_args=True)
+                new_deps.append(dep)
             kwargs['dependencies'] = new_deps
 
         suffix = self.variables.get('EXT_SUFFIX') or self.variables.get('SO') or self.variables.get('.so')
@@ -360,7 +368,7 @@ class PythonInstallation(ExternalProgramHolder):
             dep = PythonDependency(self, self.interpreter.environment, kwargs)
             if required and not dep.found():
                 raise mesonlib.MesonException('Python dependency not found')
-        return self.interpreter.holderify(dep)
+        return dep
 
     @permittedKwargs(['pure', 'subdir'])
     def install_sources_method(self, args, kwargs):
@@ -377,7 +385,7 @@ class PythonInstallation(ExternalProgramHolder):
         else:
             kwargs['install_dir'] = os.path.join(self.platlib_install_path, subdir)
 
-        return self.interpreter.holderify(self.interpreter.func_install_data(None, args, kwargs))
+        return self.interpreter.func_install_data(None, args, kwargs)
 
     @noPosargs
     @permittedKwargs(['pure', 'subdir'])
@@ -519,25 +527,26 @@ class PythonModule(ExtensionModule):
 
         if disabled:
             mlog.log('Program', name_or_path or 'python', 'found:', mlog.red('NO'), '(disabled by:', mlog.bold(feature), ')')
-            return ExternalProgramHolder(NonExistingExternalProgram(), state.subproject)
+            return NonExistingExternalProgram()
 
         if not name_or_path:
-            python = ExternalProgram('python3', mesonlib.python_command, silent=True)
+            python = PythonExternalProgram('python3', mesonlib.python_command)
         else:
-            python = ExternalProgram.from_entry('python3', name_or_path)
+            tmp_python = ExternalProgram.from_entry('python3', name_or_path)
+            python = PythonExternalProgram('python3', ext_prog=tmp_python)
 
             if not python.found() and mesonlib.is_windows():
                 pythonpath = self._get_win_pythonpath(name_or_path)
                 if pythonpath is not None:
                     name_or_path = pythonpath
-                    python = ExternalProgram(name_or_path, silent=True)
+                    python = PythonExternalProgram(name_or_path)
 
             # Last ditch effort, python2 or python3 can be named python
             # on various platforms, let's not give up just yet, if an executable
             # named python is available and has a compatible version, let's use
             # it
             if not python.found() and name_or_path in ['python2', 'python3']:
-                python = ExternalProgram('python', silent=True)
+                python = PythonExternalProgram('python')
 
         if python.found() and want_modules:
             for mod in want_modules:
@@ -566,11 +575,11 @@ class PythonModule(ExtensionModule):
         if not python.found():
             if required:
                 raise mesonlib.MesonException('{} not found'.format(name_or_path or 'python'))
-            res = ExternalProgramHolder(NonExistingExternalProgram(), state.subproject)
+            return NonExistingExternalProgram()
         elif missing_modules:
             if required:
                 raise mesonlib.MesonException('{} is missing modules: {}'.format(name_or_path or 'python', ', '.join(missing_modules)))
-            res = ExternalProgramHolder(NonExistingExternalProgram(), state.subproject)
+            return NonExistingExternalProgram()
         else:
             # Sanity check, we expect to have something that at least quacks in tune
             try:
@@ -586,14 +595,17 @@ class PythonModule(ExtensionModule):
                 mlog.debug(stderr)
 
             if isinstance(info, dict) and 'version' in info and self._check_version(name_or_path, info['version']):
-                res = PythonInstallation(self.interpreter, python, info)
+                python.info = info
+                return python
             else:
-                res = ExternalProgramHolder(NonExistingExternalProgram(), state.subproject)
                 if required:
                     raise mesonlib.MesonException(f'{python} is not a valid python or it is missing setuptools')
+                return NonExistingExternalProgram()
 
-        return res
+        raise mesonlib.MesonBugException('Unreachable code was reached (PythonModule.find_installation).')
 
 
 def initialize(*args, **kwargs):
-    return PythonModule(*args, **kwargs)
+    mod = PythonModule(*args, **kwargs)
+    mod.interpreter.append_holder_map(PythonExternalProgram, PythonInstallation)
+    return mod

commit b87067dc8ba5ade1a36f353c89b43b018e8c0cc4

fix regression that broke proper warnings about passing subproject as a dep
Regression in commit 7c757dff71c031ae02ecc358a69a213a5db4051a.
SubprojectHolder is no longer an ObjectHolder and says so via a TODO:
this means that we have to fiddle with held_object. Yay.

mesonbuild/build.py
@@ -1373,6 +1373,9 @@ You probably should put it in link_with instead.''')
                 # This is a bit of a hack. We do not want Build to know anything
                 # about the interpreter so we can't import it and use isinstance.
                 # This should be reliable enough.
+                if hasattr(dep, 'held_object'):
+                    # FIXME: subproject is not a real ObjectHolder so we have to do this by hand
+                    dep = dep.held_object
                 if hasattr(dep, 'project_args_frozen') or hasattr(dep, 'global_args_frozen'):
                     raise InvalidArguments('Tried to use subproject object as a dependency.\n'
                                            'You probably wanted to use a dependency declared in it instead.\n'