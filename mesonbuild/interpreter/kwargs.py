8be277f613be4e04c34f49e1868702f6b15f98e5
Dylan Baker <dylan@pnwbakers.com>
2022-06-17 14:05:25 -0700

    interpreter: add type annotations to func_declare_dependency

--- mesonbuild/interpreter/kwargs.py
+++ mesonbuild/interpreter/kwargs.py
@@ -12,6 +12,7 @@ from typing_extensions import TypedDict, Literal, Protocol
 from .. import build
 from .. import coredata
 from ..compilers import Compiler
+from ..dependencies.base import Dependency
 from ..mesonlib import EnvironmentVariables, MachineChoice, File, FileMode, FileOrString, OptionKey
 from ..modules.cmake import CMakeSubprojectOptions
 from ..programs import ExternalProgram
@@ -360,3 +361,20 @@ class Jar(_BaseBuildTarget):
 
     main_class: str
     java_resources: T.Optional[build.StructuredSources]
+
+
+class FuncDeclareDependency(TypedDict):
+
+    compile_args: T.List[str]
+    d_import_dirs: T.List[T.Union[build.IncludeDirs, str]]
+    d_module_versions: T.List[T.Union[str, int]]
+    dependencies: T.List[Dependency]
+    extra_files: T.List[FileOrString]
+    include_directories: T.List[T.Union[build.IncludeDirs, str]]
+    link_args: T.List[str]
+    link_whole: T.List[T.Union[build.StaticLibrary, build.CustomTarget, build.CustomTargetIndex]]
+    link_with: T.List[build.LibTypes]
+    objects: T.List[build.ExtractedObjects]
+    sources: T.List[T.Union[FileOrString, build.GeneratedTypes]]
+    variables: T.Dict[str, str]
+    version: T.Optional[str]
