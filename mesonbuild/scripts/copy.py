2d6a5bb9ac3289e63b14794bb878aed6bd40019d
Dylan Baker <dylan@pnwbakers.com>
2022-08-26 10:19:38 -0700

    scripts/copy: use argparse
    This is ground work for more complex handling

--- mesonbuild/scripts/copy.py
+++ mesonbuild/scripts/copy.py
@@ -6,13 +6,28 @@
 This is easier than trying to detect whether to use copy, cp, or something else.
 """
 
+from __future__ import annotations
+
+import argparse
 import shutil
 import typing as T
 
+if T.TYPE_CHECKING:
+    from typing_extensions import Protocol
 
-def run(args: T.List[str]) -> int:
+    class Args(Protocol):
+        source: str
+        dest: str
+
+
+def run(raw_args: T.List[str]) -> int:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('source')
+    parser.add_argument('dest')
+    args: Args = parser.parse_args(raw_args)
+
     try:
-        shutil.copy2(args[0], args[1])
+        shutil.copy2(args.source, args.dest)
     except Exception:
         return 1
     return 0
