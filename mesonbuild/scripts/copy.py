0610fc1bcd3984e86689ec16bd94fb903a17f8b3
Dylan Baker <dylan@pnwbakers.com>
2022-08-26 10:27:20 -0700

    scripts/copy: add support for setting the file mode in the build dir
    This can be done by setting the `--file-mode` argument, and passing a
    numeric value suitable for passing to `os.chmod`.
    
    This will be used to allow `fs.copyfile` to set the mode of a file in
    the build directory, which is sometimes useful.

--- mesonbuild/scripts/copy.py
+++ mesonbuild/scripts/copy.py
@@ -9,6 +9,7 @@ This is easier than trying to detect whether to use copy, cp, or something else.
 from __future__ import annotations
 
 import argparse
+import os
 import shutil
 import typing as T
 
@@ -18,16 +19,20 @@ if T.TYPE_CHECKING:
     class Args(Protocol):
         source: str
         dest: str
+        file_mode: int
 
 
 def run(raw_args: T.List[str]) -> int:
     parser = argparse.ArgumentParser()
     parser.add_argument('source')
     parser.add_argument('dest')
+    parser.add_argument('--file-mode', action='store', help='permission bits as suitable to pass to `os.chmod`', type=int)
     args: Args = parser.parse_args(raw_args)
 
     try:
         shutil.copy2(args.source, args.dest)
+        if args.file_mode:
+            os.chmod(args.dest, args.file_mode)
     except Exception:
         return 1
     return 0
