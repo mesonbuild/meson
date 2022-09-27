0ab1a86068b37310ef9d5ba6662b2cd968912ab9
JCWasmx86 <JCWasmx86@t-online.de>
2022-09-27 21:03:48 +0200

    mfmt: Implement parsing of files

--- mesonbuild/mfmt.py
+++ mesonbuild/mfmt.py
@@ -1,10 +1,20 @@
 import argparse
-from . import coredata, mlog
+from . import coredata, mlog, mparser
+from . import mesonlib
 
 def add_arguments(parser: argparse.ArgumentParser) -> None:
     coredata.register_builtin_arguments(parser)
-    parser.add_argument('FILE', nargs=argparse.REMAINDER,
+    parser.add_argument('files', nargs=argparse.REMAINDER,
                         help='files to format')
 
 def run(options: argparse.Namespace) -> int:
-    pass
+    for filename in options.files:
+        with open(filename, encoding='utf-8') as f:
+            code = f.read()
+        assert isinstance(code, str)
+        try:
+            codeblock = mparser.Parser(code, filename).parse()
+        except mesonlib.MesonException as me:
+            me.file = filename
+            raise me
+        print(codeblock)
