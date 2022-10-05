mesonbuild/meson

commit 53a187ba2a50d5de6c88f0697a3ea30c0999c29d

modules/gnome: use typed_pos_args for gdbus_codegen

mesonbuild/modules/gnome.py
@@ -1273,11 +1273,10 @@ class GnomeModule(ExtensionModule):
     @FeatureNewKwargs('build target', '0.47.0', ['extra_args', 'autocleanup'])
     @permittedKwargs({'interface_prefix', 'namespace', 'extra_args', 'autocleanup', 'object_manager', 'build_by_default',
                       'annotations', 'docbook', 'install_header', 'install_dir', 'sources'})
-    def gdbus_codegen(self, state, args, kwargs):
-        if len(args) not in (1, 2):
-            raise MesonException('gdbus_codegen takes at most two arguments, name and xml file.')
+    @typed_pos_args('gnome.gdbus_codegen', str, optargs=[str])
+    def gdbus_codegen(self, state: 'ModuleState', args: T.Tuple[str, T.Optional[str]], kwargs) -> ModuleReturnValue:
         namebase = args[0]
-        xml_files = args[1:]
+        xml_files: T.List[str] = [args[1]] if args[1] else []
         cmd = [state.find_program('gdbus-codegen')]
         extra_args = mesonlib.stringlistify(kwargs.pop('extra_args', []))
         cmd += extra_args

commit 9dbae67dc8dd8713f0c03d3ba4748aba363d0660

gnome: allow custom targets as gdbus-codegen inputs
Custom targets as sources to `gnome.gdbus_codegen` worked until version 0.60
of Meson, but broke in 0.61 because of the conversion to typed_pos_args
and typed_kwargs.  Reinstate this by adding custom targets to the
decorators and annotations.

While generators also used to work, they are a bit tricky because
gdbus_codegen desugars to two custom_targets and therefore the generator
is invoked twice.  This should not be a problem, but be explicit and
leave that to a separate commit to highlight the problem.

Fixes: 53a187ba2 ("modules/gnome: use typed_pos_args for gdbus_codegen", 2021-11-01)
Fixes: ef52e6093 ("modules/gnome: use typed_kwargs for gdbus_codegen", 2021-11-08)

mesonbuild/modules/gnome.py
@@ -1570,11 +1570,11 @@ class GnomeModule(ExtensionModule):
     def gtkdoc_html_dir(self, state: 'ModuleState', args: T.Tuple[str], kwargs: 'TYPE_kwargs') -> str:
         return os.path.join('share/gtk-doc/html', args[0])
 
-    @typed_pos_args('gnome.gdbus_codegen', str, optargs=[(str, mesonlib.File)])
+    @typed_pos_args('gnome.gdbus_codegen', str, optargs=[(str, mesonlib.File, build.CustomTarget, build.CustomTargetIndex)])
     @typed_kwargs(
         'gnome.gdbus_codegen',
         _BUILD_BY_DEFAULT.evolve(since='0.40.0'),
-        KwargInfo('sources', ContainerTypeInfo(list, (str, mesonlib.File)), since='0.46.0', default=[], listify=True),
+        KwargInfo('sources', ContainerTypeInfo(list, (str, mesonlib.File, build.CustomTarget, build.CustomTargetIndex)), since='0.46.0', default=[], listify=True),
         KwargInfo('extra_args', ContainerTypeInfo(list, str), since='0.47.0', default=[], listify=True),
         KwargInfo('interface_prefix', (str, NoneType)),
         KwargInfo('namespace', (str, NoneType)),
@@ -1592,10 +1592,10 @@ class GnomeModule(ExtensionModule):
             validator=in_set_validator({'all', 'none', 'objects'})),
         INSTALL_DIR_KW.evolve(since='0.46.0')
     )
-    def gdbus_codegen(self, state: 'ModuleState', args: T.Tuple[str, T.Optional['FileOrString']],
+    def gdbus_codegen(self, state: 'ModuleState', args: T.Tuple[str, T.Optional[T.Union['FileOrString', build.GeneratedTypes]]],
                       kwargs: 'GdbusCodegen') -> ModuleReturnValue:
         namebase = args[0]
-        xml_files: T.List['FileOrString'] = [args[1]] if args[1] else []
+        xml_files: T.List[T.Union['FileOrString', build.GeneratedTypes]] = [args[1]] if args[1] else []
         cmd: T.List[T.Union['ExternalProgram', str]] = [state.find_program('gdbus-codegen')]
         cmd.extend(kwargs['extra_args'])
 

test cases/frameworks/7 gnome/gdbus/meson.build
@@ -58,6 +58,27 @@ else
   includes = include_directories('..')
 endif
 
+# check that custom targets work
+gdbus_xml_ct = custom_target('built xml sources for gdbus',
+  output: 'com.example.SampleCustomTarget.xml',
+  input: 'data/com.example.Sample.xml',
+  command : [copyfile, '@INPUT@', '@OUTPUT@'])
+
+gdbus_src_ct = gnome.gdbus_codegen(
+  'generated-gdbus-customtarget-src',
+  gdbus_xml_ct,
+  interface_prefix : 'com.example.',
+  namespace : 'Sample',
+  annotations : [],
+)
+gdbus_src_cti = gnome.gdbus_codegen(
+  'generated-gdbus-customtargetindex-src',
+  gdbus_xml_ct[0],
+  interface_prefix : 'com.example.',
+  namespace : 'Sample',
+  annotations : [],
+)
+
 gdbus_exe = executable('gdbus-test', 'gdbusprog.c',
   gdbus_src,
   include_directories : includes,

test cases/frameworks/7 gnome/meson.build
@@ -1,5 +1,7 @@
 project('gobject-introspection', 'c')
 
+copyfile = find_program('copyfile.py')
+
 glib = dependency('glib-2.0', required: false)
 if not glib.found()
   error('MESON_SKIP_TEST glib not found.')

test cases/frameworks/7 gnome/resources/meson.build
@@ -1,8 +1,6 @@
 # There are two tests here, because the 2nd one depends on a version of
 # GLib (2.51.1) that is very recent at the time of writing.
 
-copyfile = find_program('copyfile.py')
-
 simple_gresource = configure_file(
   input : 'simple.gresource.xml',
   output : 'simple-gen.gresource.xml',