00a4452a9be4d1ff20068548c1002b9b671666ce
Xavier Claessens <xavier.claessens@collabora.com>
2022-10-14 15:32:50 -0400

    Add MASM compiler
    ml and armasm are Microsoft's Macro Assembler, part of MSVC.

--- mesonbuild/compilers/detect.py
+++ mesonbuild/compilers/detect.py
@@ -107,6 +107,7 @@ def compiler_from_language(env: 'Environment', lang: str, for_machine: MachineCh
         'swift': detect_swift_compiler,
         'cython': detect_cython_compiler,
         'nasm': detect_nasm_compiler,
+        'masm': detect_masm_compiler,
     }
     return lang_map[lang](env, for_machine) if lang in lang_map else None
 
@@ -1173,6 +1174,47 @@ def detect_nasm_compiler(env: 'Environment', for_machine: MachineChoice) -> Comp
     _handle_exceptions(popen_exceptions, compilers)
     raise EnvironmentException('Unreachable code (exception to make mypy happy)')
 
+def detect_masm_compiler(env: 'Environment', for_machine: MachineChoice) -> Compiler:
+    # We need a C compiler to properly detect the machine info and linker
+    is_cross = env.is_cross_build(for_machine)
+    cc = detect_c_compiler(env, for_machine)
+    if not is_cross:
+        from ..environment import detect_machine_info
+        info = detect_machine_info({'c': cc})
+    else:
+        info = env.machines[for_machine]
+
+    from .asm import MasmCompiler, MasmARMCompiler
+    comp_class: T.Type[Compiler]
+    if info.cpu_family == 'x86':
+        comp = ['ml']
+        comp_class = MasmCompiler
+        arg = '/?'
+    elif info.cpu_family == 'x86_64':
+        comp = ['ml64']
+        comp_class = MasmCompiler
+        arg = '/?'
+    elif info.cpu_family == 'arm':
+        comp = ['armasm']
+        comp_class = MasmARMCompiler
+        arg = '-h'
+    elif info.cpu_family == 'aarch64':
+        comp = ['armasm64']
+        comp_class = MasmARMCompiler
+        arg = '-h'
+    else:
+        raise EnvironmentException(f'Platform {info.cpu_family} not supported by MASM')
+
+    popen_exceptions: T.Dict[str, Exception] = {}
+    try:
+        output = Popen_safe(comp + [arg])[2]
+        version = search_version(output)
+        env.coredata.add_lang_args(comp_class.language, comp_class, for_machine, env)
+        return comp_class(comp, version, for_machine, info, cc.linker, is_cross=is_cross)
+    except OSError as e:
+        popen_exceptions[' '.join(comp + [arg])] = e
+    _handle_exceptions(popen_exceptions, [comp])
+    raise EnvironmentException('Unreachable code (exception to make mypy happy)')
 
 # GNU/Clang defines and version
 # =============================
