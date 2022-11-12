82929301aa9334ea17207961175de6dd39e83a13
JCWasmx86 <JCWasmx86@t-online.de>
2022-11-12 19:56:26 +0100

    Add idempotency tests

--- unittests/formattertests.py
+++ unittests/formattertests.py
@@ -27,18 +27,21 @@ class FormatterTests(unittest.TestCase):
         for i, line in enumerate(formatter.lines):
             self.assertEqual(old_lines[i], line)
         assert(len(formatter.comments) == 0)
+        self.assert_idempotency(formatter.lines)
 
     def test_indentation(self):
         (formatter, old_lines) = FormatterTests.fill_formatter('indentation.test')
         for i, line in enumerate(formatter.lines):
             self.assertEqual(old_lines[i], line)
         assert(len(formatter.comments) == 0)
+        self.assert_idempotency(formatter.lines)
 
     def test_generics(self):
         (formatter, old_lines) = FormatterTests.fill_formatter('generics.test')
         for i, line in enumerate(formatter.lines):
             self.assertEqual(old_lines[i], line)
         assert(len(formatter.comments) == 0)
+        self.assert_idempotency(formatter.lines)
 
     def test_space_array(self):
         config = {}
@@ -46,6 +49,8 @@ class FormatterTests(unittest.TestCase):
         (formatter, old_lines) = FormatterTests.fill_formatter('space_array.test', config)
         for i, line in enumerate(formatter.lines):
             self.assertEqual(old_lines[i], line)
+        assert(len(formatter.comments) == 0)
+        self.assert_idempotency(formatter.lines, config)
 
     def test_wide_colon(self):
         config = {}
@@ -53,6 +58,27 @@ class FormatterTests(unittest.TestCase):
         (formatter, old_lines) = FormatterTests.fill_formatter('wide_colon.test', config)
         for i, line in enumerate(formatter.lines):
             self.assertEqual(old_lines[i], line)
+        assert(len(formatter.comments) == 0)
+        self.assert_idempotency(formatter.lines, config)
+
+    def assert_idempotency(self, new_lines, extra_config = {}):
+        config = {}
+        config['max_line_len'] = 80
+        config['indent_by'] = '    '
+        config['space_array'] = False
+        config['kwa_ml'] = False
+        config['wide_colon'] = False
+        config['no_single_comma_function'] = False
+        config.update(extra_config)
+        parser = mesonbuild.mparser.Parser('\n'.join(new_lines), '-')
+        codeblock = parser.parse()
+        comments = parser.comments()
+        formatter = mesonbuild.ast.AstFormatter(comments, new_lines, config)
+        codeblock.accept(formatter)
+        formatter.end()
+        assert(len(formatter.comments) == 0)
+        for i, line in enumerate(formatter.lines):
+            self.assertEqual(new_lines[i], line)
 
     @staticmethod
     def fill_formatter(file: str, extra_config={}) -> (mesonbuild.ast.AstFormatter, T.List[str]):
