90fb9a39d9cd93a767973f3b250973e93a93afdf
JCWasmx86 <JCWasmx86@t-online.de>
2022-11-15 15:12:42 +0100

    Add support for recursing.
    This reduces the amount needed for e.g. formatting mesa
    from 75 seconds to 1.5 (229 files, ~11kLOC)

--- mesonbuild/mfmt.py
+++ mesonbuild/mfmt.py
@@ -1,8 +1,10 @@
 import argparse
+import os
 import sys
 from . import coredata, mparser
 from . import mesonlib
 from .ast import AstFormatter
+from pathlib import Path
 
 def add_arguments(parser: argparse.ArgumentParser) -> None:
     coredata.register_builtin_arguments(parser)
@@ -10,21 +12,9 @@ def add_arguments(parser: argparse.ArgumentParser) -> None:
     parser.add_argument('-o', '--output', help='Output file')
     parser.add_argument('-i', '--inplace', action='store_true', help='Edit the file inplace')
     parser.add_argument('-c', '--config', help='Specify config file')
+    parser.add_argument('-R', '--recurse', action='store_true', help='Recursively format meson files in a given directory')
 
-def run(options: argparse.Namespace) -> int:
-    if options.file == '-':
-        code = sys.stdin.read()
-    else:
-        with open(options.file, encoding='utf-8') as f:
-            code = f.read()
-    assert isinstance(code, str)
-    try:
-        parser = mparser.Parser(code, options.file)
-        codeblock = parser.parse()
-        comments = parser.comments()
-    except mesonlib.MesonException as me:
-        me.file = options.file
-        raise me
+def parse_fmt_config(file: str):
     config = {}
     config['max_line_len'] = 80
     config['indent_by'] = '    '
@@ -32,7 +22,7 @@ def run(options: argparse.Namespace) -> int:
     config['kwa_ml'] = False
     config['wide_colon'] = False
     config['no_single_comma_function'] = False
-    if options.config is not None:
+    if file is not None:
         with open(options.config, encoding='utf-8') as f:
             for line in f.readlines():
                 ls = line.lstrip()
@@ -54,17 +44,51 @@ def run(options: argparse.Namespace) -> int:
                     config['indent_by'] = value.replace('\n', '')
                 else:
                     print("Unknown key", key, file=sys.stderr)
+    return config
 
+def format_code(options: argparse.Namespace, file: str, output: str, code: str) -> int:
+    try:
+        parser = mparser.Parser(code, file)
+        codeblock = parser.parse()
+        comments = parser.comments()
+    except mesonlib.MesonException as me:
+        me.file = file
+        raise me
+    config = parse_fmt_config(options.config)
     formatter = AstFormatter(comments, code.splitlines(), config)
     codeblock.accept(formatter)
     formatter.end()
     if not options.inplace:
-        output = sys.stdout if options.output is None else open(options.output, 'w', encoding='utf8')
+        real_output = sys.stdout if output is None else open(output, 'w', encoding='utf8')
     else:
-        output = open(options.file, 'w', encoding='utf8')
+        real_output = open(file, 'w', encoding='utf8')
     for line in formatter.lines:
-        print(line, end='\n', file=output)
+        print(line, end='\n', file=real_output)
     if len(formatter.comments) != 0:
         print('Unable to readd', len(formatter.comments), 'comments', file=sys.stderr)
         for c in formatter.comments:
             print(c.text, file=sys.stderr)
+    return 0
+
+def run(options: argparse.Namespace) -> int:
+    if options.file == '-':
+        code = sys.stdin.read()
+        return format_code(options, '-', options.output, code)
+    else:
+        if os.path.isdir(options.file):
+            if not options.recurse:
+                print('A directory was passed, but no -R/--recurse was specified', file=sys.stderr)
+                return -1
+            else:
+                # Always do inplace editing
+                options.inplace = True
+                for path in Path(options.file).rglob('meson.build'):
+                    full_path = str(path)
+                    with open(full_path, encoding='utf-8') as f:
+                        code = f.read()
+                    format_code(options, full_path, full_path, code)
+                return 0
+        else:
+            with open(options.file, encoding='utf-8') as f:
+                code = f.read()
+                return format_code(options, options.file, options.output, code)
