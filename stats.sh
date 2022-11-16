7cf756e3a324f347e116a1daca9a989a09c5d9a9
JCWasmx86 <JCWasmx86@t-online.de>
2022-11-16 09:56:39 +0100

    stats.sh: Add confirmation prompt and tests until when it converges to one formatting

--- stats.sh
+++ stats.sh
@@ -16,8 +16,27 @@ check_it() {
         cd ../meson || exit
         percentage=$(echo "print(str(round(($n_lost_comments/$n_comments) * 100, 3)) + '%')" | python /dev/stdin)
         echo "$1 has $n_comments, lost $n_lost_comments during formatting ($percentage)"
+        rounds=0
+        while :; do
+                python meson.py fmt_unstable ../$1 -R >/dev/null 2>&1 || exit
+                cd ../$1 || exit
+                if [[ $(git diff --stat) != '' ]]; then
+                        git commit -a -m "FOO" --no-verify >/dev/null 2>&1 || exit
+                        ((rounds++))
+                else
+                        break
+                fi
+                cd ../meson || exit
+        done
+        echo "$1 needed $rounds formatting passes to be stable"
+        git reset --hard HEAD~$rounds >/dev/null 2>&1 || exit
+        cd ../meson || exit
 }
-
+if [ -z "$YES_THIS_WILL_OVERWRITE_MY_CHANGES_TO_THESE_PROJECTS" ]; then
+        echo "No confirmation given. This uses git reset --hard and may destroy your changes,"
+        echo "if any of those projects has uncommitted changes"
+        exit 1
+fi
 check_it systemd
 check_it mesa
 check_it gnome-builder
