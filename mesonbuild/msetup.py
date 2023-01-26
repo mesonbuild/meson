a071607de0ef767c540188b08a811cc11ccff69f
Dylan Baker <dylan@pnwbakers.com>
2023-01-26 11:35:13 -0800

    msetup: stop calling interpreter methods
    This is just a really silly indirection to get at data we already have
    in the environment, without having to call through the interpreter and
    its validation.

--- mesonbuild/msetup.py
+++ mesonbuild/msetup.py
@@ -219,18 +219,15 @@ class MesonApp:
         # even to write `T.Callable[[*mlog.TV_Loggable], None]`
         logger_fun = T.cast('T.Callable[[mlog.TV_Loggable, mlog.TV_Loggable], None]',
                             (mlog.log if env.is_cross_build() else mlog.debug))
-        build_machine = intr.builtin['build_machine']
-        host_machine = intr.builtin['host_machine']
-        target_machine = intr.builtin['target_machine']
-        assert isinstance(build_machine, interpreter.MachineHolder)
-        assert isinstance(host_machine, interpreter.MachineHolder)
-        assert isinstance(target_machine, interpreter.MachineHolder)
-        logger_fun('Build machine cpu family:', mlog.bold(build_machine.cpu_family_method([], {})))
-        logger_fun('Build machine cpu:', mlog.bold(build_machine.cpu_method([], {})))
-        mlog.log('Host machine cpu family:', mlog.bold(host_machine.cpu_family_method([], {})))
-        mlog.log('Host machine cpu:', mlog.bold(host_machine.cpu_method([], {})))
-        logger_fun('Target machine cpu family:', mlog.bold(target_machine.cpu_family_method([], {})))
-        logger_fun('Target machine cpu:', mlog.bold(target_machine.cpu_method([], {})))
+        build_machine = env.machines.build
+        host_machine = env.machines.host
+        target_machine = env.machines.target
+        logger_fun('Build machine cpu family:', mlog.bold(build_machine.cpu_family))
+        logger_fun('Build machine cpu:', mlog.bold(build_machine.cpu))
+        mlog.log('Host machine cpu family:', mlog.bold(host_machine.cpu_family))
+        mlog.log('Host machine cpu:', mlog.bold(host_machine.cpu))
+        logger_fun('Target machine cpu family:', mlog.bold(target_machine.cpu_family))
+        logger_fun('Target machine cpu:', mlog.bold(target_machine.cpu))
         try:
             if self.options.profile:
                 fname = os.path.join(self.build_dir, 'meson-private', 'profile-interpreter.log')
