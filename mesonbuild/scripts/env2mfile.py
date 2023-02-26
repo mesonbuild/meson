2aa4d6e54e1cabe86a0a12c9fd9f8205337629fa
Jussi Pakkanen <jpakkane@gmail.com>
2023-02-26 17:13:47 +0200

    Handle a cross compilation setup that only has a C compiler.

--- mesonbuild/scripts/env2mfile.py
+++ mesonbuild/scripts/env2mfile.py
@@ -145,7 +145,10 @@ def deb_detect_cmake(infos: MachineInfo, data: T.Dict[str, str]) -> None:
     system_processor_map = {'arm': 'armv7l', 'mips64el': 'mips64', 'powerpc64le': 'ppc64le'}
 
     infos.cmake["CMAKE_C_COMPILER"] = infos.compilers['c']
-    infos.cmake["CMAKE_CXX_COMPILER"] = infos.compilers['cpp']
+    try:
+        infos.cmake["CMAKE_CXX_COMPILER"] = infos.compilers['cpp']
+    except KeyError:
+        pass
     infos.cmake["CMAKE_SYSTEM_NAME"] = system_name_map[data['DEB_HOST_ARCH_OS']]
     infos.cmake["CMAKE_SYSTEM_PROCESSOR"] = system_processor_map.get(data['DEB_HOST_GNU_CPU'],
                                                                      data['DEB_HOST_GNU_CPU'])
