6a1427401c76db73081e478c4ff49fcc75420de6
Eli Schwartz <eschwartz@archlinux.org>
2023-03-06 21:37:29 -0500

    tests: add a python test for bytecode compilation
    Some tweaks are added to the test case so that it supports python2 as
    well.

--- test cases/python/2 extmodule/ext/tachyon_module.c
+++ test cases/python/2 extmodule/ext/tachyon_module.c
@@ -1,5 +1,5 @@
 /*
-  Copyright 2016 The Meson development team
+  Copyright 2018 The Meson development team
 
   Licensed under the Apache License, Version 2.0 (the "License");
   you may not use this file except in compliance with the License.
@@ -27,7 +27,11 @@ static PyObject* phaserize(PyObject *self, PyObject *args) {
         return NULL;
 
     result = strcmp(message, "shoot") ? 0 : 1;
+#if PY_VERSION_HEX < 0x03000000
+    return PyInt_FromLong(result);
+#else
     return PyLong_FromLong(result);
+#endif
 }
 
 static PyMethodDef TachyonMethods[] = {
@@ -36,9 +40,14 @@ static PyMethodDef TachyonMethods[] = {
     {NULL, NULL, 0, NULL}
 };
 
+#if PY_VERSION_HEX < 0x03000000
+PyMODINIT_FUNC inittachyon(void) {
+    Py_InitModule("tachyon", TachyonMethods);
+}
+#else
 static struct PyModuleDef tachyonmodule = {
    PyModuleDef_HEAD_INIT,
-   MESON_MODULENAME,
+   "tachyon",
    NULL,
    -1,
    TachyonMethods
@@ -47,3 +56,4 @@ static struct PyModuleDef tachyonmodule = {
 PyMODINIT_FUNC PyInit_tachyon(void) {
     return PyModule_Create(&tachyonmodule);
 }
+#endif
