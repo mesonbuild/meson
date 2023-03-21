ed95beffd0956658f6de31fe1307332972ecd6ef
Xavier Claessens <xavier.claessens@collabora.com>
2023-03-21 09:58:49 -0400

    env2mfile: Not all compilers have env for flags

--- mesonbuild/scripts/env2mfile.py
+++ mesonbuild/scripts/env2mfile.py
@@ -263,7 +263,9 @@ def write_machine_file(infos: MachineInfo, ofilename: str, write_system_info: bo
 
 def detect_language_args_from_envvars(langname: str, envvar_suffix: str = '') -> T.Tuple[T.List[str], T.List[str]]:
     ldflags = tuple(shlex.split(os.environ.get('LDFLAGS' + envvar_suffix, '')))
-    compile_args = shlex.split(os.environ.get(compilers.CFLAGS_MAPPING[langname] + envvar_suffix, ''))
+    compile_args = []
+    if langname in compilers.CFLAGS_MAPPING:
+        compile_args = shlex.split(os.environ.get(compilers.CFLAGS_MAPPING[langname] + envvar_suffix, ''))
     if langname in compilers.LANGUAGES_USING_CPPFLAGS:
         cppflags = tuple(shlex.split(os.environ.get('CPPFLAGS' + envvar_suffix, '')))
         lang_compile_args = list(cppflags) + compile_args
