697b42f022e6c514798b64e70894c72c8ad2b1a8
Jussi Pakkanen <jpakkane@gmail.com>
2023-03-28 13:10:50 +0300

    Bump version number for rc1.

--- mesonbuild/coredata.py
+++ mesonbuild/coredata.py
@@ -1,4 +1,4 @@
-# Copyright 2012-2022 The Meson development team
+# Copyright 2012-2023 The Meson development team
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
@@ -59,7 +59,7 @@ if T.TYPE_CHECKING:
 #
 # Pip requires that RCs are named like this: '0.1.0.rc1'
 # But the corresponding Git tag needs to be '0.1.0rc1'
-version = '1.0.99'
+version = '1.1.0.rc1'
 
 backendlist = ['ninja', 'vs', 'vs2010', 'vs2012', 'vs2013', 'vs2015', 'vs2017', 'vs2019', 'vs2022', 'xcode', 'none']
 
