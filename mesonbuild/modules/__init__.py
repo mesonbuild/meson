21a5ea8620843dbea761c336b673d79a5d0d24b7
Xavier Claessens <xavier.claessens@collabora.com>
2023-05-24 09:39:01 -0400

    find_program: Fallback if version mismatch
    Fixes: #11797

--- mesonbuild/modules/__init__.py
+++ mesonbuild/modules/__init__.py
@@ -26,6 +26,7 @@ from ..programs import ExternalProgram
 
 if T.TYPE_CHECKING:
     from ..interpreter import Interpreter
+    from ..interpreter.interpreter import ProgramVersionFunc
     from ..interpreter.interpreterobjects import MachineHolder
     from ..interpreterbase import TYPE_var, TYPE_kwargs
     from ..programs import OverrideProgram
@@ -86,7 +87,7 @@ class ModuleState:
 
     def find_program(self, prog: T.Union[mesonlib.FileOrString, T.List[mesonlib.FileOrString]],
                      required: bool = True,
-                     version_func: T.Optional[T.Callable[[T.Union[ExternalProgram, build.Executable, OverrideProgram]], str]] = None,
+                     version_func: T.Optional[ProgramVersionFunc] = None,
                      wanted: T.Optional[str] = None, silent: bool = False,
                      for_machine: MachineChoice = MachineChoice.HOST) -> T.Union[ExternalProgram, build.Executable, OverrideProgram]:
         if not isinstance(prog, list):
