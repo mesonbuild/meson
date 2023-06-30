bdc7c891635cdeab82482daa71903cb49c04682f
Dylan Baker <dylan@pnwbakers.com>
2023-06-30 10:54:02 -0700

    minit: fix docstring
    Which must come before `from __future__`

--- mesonbuild/minit.py
+++ mesonbuild/minit.py
@@ -11,9 +11,10 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-from __future__ import annotations
 
 """Code that creates simple startup projects."""
+
+from __future__ import annotations
 
 from pathlib import Path
 from enum import Enum
