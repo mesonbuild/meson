mesonbuild/meson

commit 2be074b1d445fcd30535bcf7518f8ce0738bcbf3

ninjabackend: Use rsp_file_syntax method
This also makes us of the new enum value in the backend, for better type
saftey.

mesonbuild/backend/ninjabackend.py
@@ -32,14 +32,11 @@ from .. import compilers
 from ..arglist import CompilerArgs
 from ..compilers import (
     Compiler, CCompiler,
-    DmdDCompiler,
     FortranCompiler,
-    LLVMDCompiler,
     PGICCompiler,
-    VisualStudioCsCompiler,
     VisualStudioLikeCompiler,
 )
-from ..linkers import ArLinker, VisualStudioLinker
+from ..linkers import ArLinker, RSPFileSyntax
 from ..mesonlib import (
     File, LibType, MachineChoice, MesonException, OrderedSet, PerMachine,
     ProgressBar, quote_arg, unholder,
@@ -51,6 +48,8 @@ from ..interpreter import Interpreter
 
 if T.TYPE_CHECKING:
     from ..linkers import StaticLinker
+    from ..compilers.cs import CsCompiler
+
 
 FORTRAN_INCLUDE_PAT = r"^\s*#?include\s*['\"](\w+\.\w+)['\"]"
 FORTRAN_MODULE_PAT = r"^\s*\bmodule\b\s+(\w+)\s*(?:!+.*)*$"
@@ -90,12 +89,6 @@ else:
     quote_func = quote_arg
     execute_wrapper = []
     rmfile_prefix = ['rm', '-f', '{}', '&&']
-
-
-def dlang_cl_rsp(compiler: Compiler) -> bool:
-    '''Return whether the D compiler accepts cl style RSP quote'''
-    return (isinstance(compiler, DmdDCompiler) or
-        mesonlib.is_windows() and isinstance(compiler, LLVMDCompiler))
 
 
 def get_rsp_threshold():
@@ -183,7 +176,7 @@ class NinjaComment:
 class NinjaRule:
     def __init__(self, rule, command, args, description,
                  rspable = False, deps = None, depfile = None, extra = None,
-                 rspfile_quote_style = 'gcc'):
+                 rspfile_quote_style: RSPFileSyntax = RSPFileSyntax.GCC):
 
         def strToCommandArg(c):
             if isinstance(c, NinjaCommandArg):
@@ -217,7 +210,7 @@ class NinjaRule:
         self.rspable = rspable  # if a rspfile can be used
         self.refcount = 0
         self.rsprefcount = 0
-        self.rspfile_quote_style = rspfile_quote_style  # rspfile quoting style is 'gcc' or 'cl'
+        self.rspfile_quote_style = rspfile_quote_style
 
         if self.depfile == '$DEPFILE':
             self.depfile += '_UNQUOTED'
@@ -235,7 +228,7 @@ class NinjaRule:
         return ninja_quote(qf(str(x)))
 
     def write(self, outfile):
-        if self.rspfile_quote_style == 'cl':
+        if self.rspfile_quote_style is RSPFileSyntax.MSVC:
             rspfile_quote_func = cmd_quote
         else:
             rspfile_quote_func = gcc_rsp_quote
@@ -395,7 +388,7 @@ class NinjaBuildElement:
         outfile.write(line)
 
         if use_rspfile:
-            if self.rule.rspfile_quote_style == 'cl':
+            if self.rule.rspfile_quote_style is RSPFileSyntax.MSVC:
                 qf = cmd_quote
             else:
                 qf = gcc_rsp_quote
@@ -1872,7 +1865,7 @@ int dummy;
                 pool = None
             self.add_rule(NinjaRule(rule, cmdlist, args, description,
                                     rspable=static_linker.can_linker_accept_rsp(),
-                                    rspfile_quote_style='cl' if isinstance(static_linker, VisualStudioLinker) else 'gcc',
+                                    rspfile_quote_style=static_linker.rsp_file_syntax(),
                                     extra=pool))
 
     def generate_dynamic_link_rules(self):
@@ -1895,8 +1888,7 @@ int dummy;
                     pool = None
                 self.add_rule(NinjaRule(rule, command, args, description,
                                         rspable=compiler.can_linker_accept_rsp(),
-                                        rspfile_quote_style='cl' if (compiler.get_argument_syntax() == 'msvc' or
-                                                                     dlang_cl_rsp(compiler)) else 'gcc',
+                                        rspfile_quote_style=compiler.rsp_file_syntax(),
                                         extra=pool))
 
         args = self.environment.get_build_command() + \
@@ -1918,14 +1910,14 @@ int dummy;
         description = 'Compiling Java object $in'
         self.add_rule(NinjaRule(rule, command, [], description))
 
-    def generate_cs_compile_rule(self, compiler):
+    def generate_cs_compile_rule(self, compiler: 'CsCompiler') -> None:
         rule = self.compiler_to_rule_name(compiler)
         command = compiler.get_exelist()
         args = ['$ARGS', '$in']
         description = 'Compiling C Sharp target $out'
         self.add_rule(NinjaRule(rule, command, args, description,
                                 rspable=mesonlib.is_windows(),
-                                rspfile_quote_style='cl' if isinstance(compiler, VisualStudioCsCompiler) else 'gcc'))
+                                rspfile_quote_style=compiler.rsp_file_syntax()))
 
     def generate_vala_compile_rules(self, compiler):
         rule = self.compiler_to_rule_name(compiler)
@@ -2019,8 +2011,7 @@ https://gcc.gnu.org/bugzilla/show_bug.cgi?id=47485'''))
             depfile = '$DEPFILE'
         self.add_rule(NinjaRule(rule, command, args, description,
                                 rspable=compiler.can_linker_accept_rsp(),
-                                rspfile_quote_style='cl' if (compiler.get_argument_syntax() == 'msvc' or
-                                                             dlang_cl_rsp(compiler)) else 'gcc',
+                                rspfile_quote_style=compiler.rsp_file_syntax(),
                                 deps=deps, depfile=depfile))
 
     def generate_pch_rule_for(self, langname, compiler):

mesonbuild/compilers/d.py
@@ -22,6 +22,7 @@ from ..mesonlib import (
 )
 
 from ..arglist import CompilerArgs
+from ..linkers import RSPFileSyntax
 from .compilers import (
     d_dmd_buildtype_args,
     d_gdc_buildtype_args,
@@ -36,7 +37,7 @@ if T.TYPE_CHECKING:
     from ..dependencies import Dependency, ExternalProgram
     from ..envconfig import MachineInfo
     from ..environment import Environment
-    from ..linkers import DynamicLinker, RSPFileSyntax
+    from ..linkers import DynamicLinker
 else:
     CompilerMixinBase = object
 
@@ -780,7 +781,7 @@ class LLVMDCompiler(DmdLikeCompilerMixin, DCompiler):
     def get_disable_assert_args(self) -> T.List[str]:
         return ['--release']
 
-    def rsp_file_syntax(self) -> 'RSPFileSyntax':
+    def rsp_file_syntax(self) -> RSPFileSyntax:
         # We use `mesonlib.is_windows` here because we want to konw what the
         # build machine is, not the host machine. This really means whe whould
         # have the Environment not the MachineInfo in the compiler.
@@ -867,5 +868,5 @@ class DmdDCompiler(DmdLikeCompilerMixin, DCompiler):
     def get_disable_assert_args(self) -> T.List[str]:
         return ['-release']
 
-    def rsp_file_syntax(self) -> 'RSPFileSyntax':
+    def rsp_file_syntax(self) -> RSPFileSyntax:
         return RSPFileSyntax.MSVC

mesonbuild/environment.py
@@ -2061,7 +2061,7 @@ class Environment:
             if 'DMD32 D Compiler' in out or 'DMD64 D Compiler' in out:
                 return DLinker(linker, compiler.arch)
             if 'LDC - the LLVM D compiler' in out:
-                return DLinker(linker, compiler.arch)
+                return DLinker(linker, compiler.arch, rsp_syntax=compiler.rsp_file_syntax())
             if 'GDC' in out and ' based on D ' in out:
                 return DLinker(linker, compiler.arch)
             if err.startswith('Renesas') and ('rlink' in linker or 'rlink.exe' in linker):

commit 21d3fd0720f0d53a5a3ead4531d57e0224589544

linkers: Fix RSP syntax for linkers invoked with clang
Fixes: #8981
Fixes: 2be074b1d445fcd30535bcf7518f8ce0738bcbf3

mesonbuild/backend/ninjabackend.py
@@ -2389,7 +2389,7 @@ class NinjaBackend(backends.Backend):
                 else:
                     pool = None
 
-                options = self._rsp_options(compiler)
+                options = self._rsp_options(compiler.linker if getattr(compiler.linker, 'direct', False) else compiler)
                 self.add_rule(NinjaRule(rule, command, args, description, **options, extra=pool))
             if self.environment.machines[for_machine].is_aix():
                 rule = 'AIX_LINKER{}'.format(self.get_rule_suffix(for_machine))

mesonbuild/compilers/asm.py
@@ -177,7 +177,8 @@ class MasmCompiler(Compiler):
     def get_compile_only_args(self) -> T.List[str]:
         return ['/c']
 
-    def get_argument_syntax(self) -> str:
+    @staticmethod
+    def get_argument_syntax() -> str:
         return 'msvc'
 
     def needs_static_linker(self) -> bool:

mesonbuild/compilers/compilers.py
@@ -1001,7 +1001,8 @@ class Compiler(HoldableObject, metaclass=abc.ABCMeta):
     def get_pie_link_args(self) -> T.List[str]:
         return self.linker.get_pie_args()
 
-    def get_argument_syntax(self) -> str:
+    @staticmethod
+    def get_argument_syntax() -> str:
         """Returns the argument family type.
 
         Compilers fall into families if they try to emulate the command line
@@ -1339,14 +1340,6 @@ class Compiler(HoldableObject, metaclass=abc.ABCMeta):
     def get_prelink_args(self, prelink_name: str, obj_list: T.List[str]) -> T.List[str]:
         raise EnvironmentException(f'{self.id} does not know how to do prelinking.')
 
-    def rsp_file_syntax(self) -> 'RSPFileSyntax':
-        """The format of the RSP file that this compiler supports.
-
-        If `self.can_linker_accept_rsp()` returns True, then this needs to
-        be implemented
-        """
-        return self.linker.rsp_file_syntax()
-
     def get_debug_args(self, is_debug: bool) -> T.List[str]:
         """Arguments required for a debug build."""
         return []
@@ -1362,6 +1355,11 @@ class Compiler(HoldableObject, metaclass=abc.ABCMeta):
         """Get compiler's preprocessor.
         """
         raise EnvironmentException(f'{self.get_id()} does not support preprocessor')
+
+    @staticmethod
+    def rsp_file_syntax() -> 'RSPFileSyntax':
+        from ..linkers import RSPFileSyntax
+        return RSPFileSyntax.GCC
 
 def get_global_options(lang: str,
                        comp: T.Type[Compiler],

mesonbuild/compilers/d.py
@@ -939,7 +939,7 @@ class LLVMDCompiler(DmdLikeCompilerMixin, DCompiler):
             return ['--release']
         return []
 
-    def rsp_file_syntax(self) -> RSPFileSyntax:
+    def rsp_file_syntax(self) -> 'RSPFileSyntax':
         # We use `mesonlib.is_windows` here because we want to know what the
         # build machine is, not the host machine. This really means we would
         # have the Environment not the MachineInfo in the compiler.
@@ -1029,5 +1029,5 @@ class DmdDCompiler(DmdLikeCompilerMixin, DCompiler):
             return ['-release']
         return []
 
-    def rsp_file_syntax(self) -> RSPFileSyntax:
+    def rsp_file_syntax(self) -> 'RSPFileSyntax':
         return RSPFileSyntax.MSVC

mesonbuild/compilers/mixins/gnu.py
@@ -442,7 +442,8 @@ class GnuLikeCompiler(Compiler, metaclass=abc.ABCMeta):
         # For other targets, discard the .def file.
         return []
 
-    def get_argument_syntax(self) -> str:
+    @staticmethod
+    def get_argument_syntax() -> str:
         return 'gcc'
 
     def get_profile_generate_args(self) -> T.List[str]:

mesonbuild/compilers/mixins/visualstudio.py
@@ -24,6 +24,7 @@ import typing as T
 from ... import arglist
 from ... import mesonlib
 from ... import mlog
+from ...linkers import RSPFileSyntax
 from mesonbuild.compilers.compilers import CompileCheckMode
 
 if T.TYPE_CHECKING:
@@ -394,7 +395,8 @@ class VisualStudioLikeCompiler(Compiler, metaclass=abc.ABCMeta):
         # false without compiling anything
         return name in {'dllimport', 'dllexport'}, False
 
-    def get_argument_syntax(self) -> str:
+    @staticmethod
+    def get_argument_syntax() -> str:
         return 'msvc'
 
     def symbols_have_underscore_prefix(self, env: 'Environment') -> bool:
@@ -414,6 +416,9 @@ class VisualStudioLikeCompiler(Compiler, metaclass=abc.ABCMeta):
         # As a last resort, try search in a compiled binary
         return self._symbols_have_underscore_prefix_searchbin(env)
 
+    @staticmethod
+    def rsp_file_syntax() -> 'RSPFileSyntax':
+        return RSPFileSyntax.MSVC
 
 class MSVCCompiler(VisualStudioLikeCompiler):
 

mesonbuild/linkers/base.py
@@ -46,5 +46,5 @@ class ArLikeLinker:
     def get_output_args(self, target: str) -> T.List[str]:
         return [target]
 
-    def rsp_file_syntax(self) -> RSPFileSyntax:
+    def rsp_file_syntax(self) -> 'RSPFileSyntax':
         return RSPFileSyntax.GCC

mesonbuild/linkers/detect.py
@@ -79,7 +79,10 @@ def guess_win_linker(env: 'Environment', compiler: T.List[str], comp_class: T.Ty
         elif not invoked_directly:
             return linkers.ClangClDynamicLinker(
                 for_machine, override, exelist=compiler, prefix=comp_class.LINKER_PREFIX,
-                version=search_version(o), direct=False, machine=None)
+                version=search_version(o), direct=False, machine=None,
+                rsp_syntax=linkers.RSPFileSyntax.MSVC
+                if (invoked_directly or comp_class.get_argument_syntax() == 'msvc')
+                else linkers.RSPFileSyntax.GCC)
 
     if value is not None and invoked_directly:
         compiler = value
@@ -90,7 +93,10 @@ def guess_win_linker(env: 'Environment', compiler: T.List[str], comp_class: T.Ty
         return linkers.ClangClDynamicLinker(
             for_machine, [],
             prefix=comp_class.LINKER_PREFIX if use_linker_prefix else [],
-            exelist=compiler, version=search_version(o), direct=invoked_directly)
+            exelist=compiler, version=search_version(o), direct=invoked_directly,
+            rsp_syntax=linkers.RSPFileSyntax.MSVC
+            if (invoked_directly or comp_class.get_argument_syntax() == 'msvc')
+            else linkers.RSPFileSyntax.GCC)
     elif 'OPTLINK' in o:
         # Optlink's stdout *may* begin with a \r character.
         return linkers.OptlinkDynamicLinker(compiler, for_machine, version=search_version(o))
@@ -105,7 +111,10 @@ def guess_win_linker(env: 'Environment', compiler: T.List[str], comp_class: T.Ty
         return linkers.MSVCDynamicLinker(
             for_machine, [], machine=target, exelist=compiler,
             prefix=comp_class.LINKER_PREFIX if use_linker_prefix else [],
-            version=search_version(out), direct=invoked_directly)
+            version=search_version(out), direct=invoked_directly,
+            rsp_syntax=linkers.RSPFileSyntax.MSVC
+            if (invoked_directly or comp_class.get_argument_syntax() == 'msvc')
+            else linkers.RSPFileSyntax.GCC)
     elif 'GNU coreutils' in o:
         import shutil
         fullpath = shutil.which(compiler[0])

mesonbuild/linkers/linkers.py
@@ -99,7 +99,7 @@ class StaticLinker:
     def get_linker_always_args(self) -> T.List[str]:
         return []
 
-    def rsp_file_syntax(self) -> RSPFileSyntax:
+    def rsp_file_syntax(self) -> 'RSPFileSyntax':
         """The format of the RSP file that this compiler supports.
 
         If `self.can_linker_accept_rsp()` returns True, then this needs to
@@ -164,7 +164,7 @@ class DynamicLinker(metaclass=abc.ABCMeta):
         # avoid issues with quoting and max argument length
         return mesonlib.is_windows()
 
-    def rsp_file_syntax(self) -> RSPFileSyntax:
+    def rsp_file_syntax(self) -> 'RSPFileSyntax':
         """The format of the RSP file that this compiler supports.
 
         If `self.can_linker_accept_rsp()` returns True, then this needs to
@@ -341,7 +341,7 @@ class VisualStudioLikeLinker(StaticLinkerBase):
         from ..compilers.c import VisualStudioCCompiler
         return VisualStudioCCompiler.native_args_to_unix(args)
 
-    def rsp_file_syntax(self) -> RSPFileSyntax:
+    def rsp_file_syntax(self) -> 'RSPFileSyntax':
         return RSPFileSyntax.MSVC
 
 
@@ -429,7 +429,7 @@ class DLinker(StaticLinker):
             return ['-m32']
         return []
 
-    def rsp_file_syntax(self) -> RSPFileSyntax:
+    def rsp_file_syntax(self) -> 'RSPFileSyntax':
         return self.__rsp_syntax
 
 
@@ -514,7 +514,7 @@ class MetrowerksStaticLinker(StaticLinker):
     def get_output_args(self, target: str) -> T.List[str]:
         return ['-o', target]
 
-    def rsp_file_syntax(self) -> RSPFileSyntax:
+    def rsp_file_syntax(self) -> 'RSPFileSyntax':
         return RSPFileSyntax.GCC
 
 
@@ -1242,11 +1242,13 @@ class VisualStudioLikeLinkerMixin(DynamicLinkerBase):
 
     def __init__(self, exelist: T.List[str], for_machine: mesonlib.MachineChoice,
                  prefix_arg: T.Union[str, T.List[str]], always_args: T.List[str], *,
-                 version: str = 'unknown version', direct: bool = True, machine: str = 'x86'):
+                 version: str = 'unknown version', direct: bool = True, machine: str = 'x86',
+                 rsp_syntax: RSPFileSyntax = RSPFileSyntax.MSVC):
         # There's no way I can find to make mypy understand what's going on here
         super().__init__(exelist, for_machine, prefix_arg, always_args, version=version)
         self.machine = machine
         self.direct = direct
+        self.rsp_syntax = rsp_syntax
 
     def get_buildtype_args(self, buildtype: str) -> T.List[str]:
         return mesonlib.listify([self._apply_prefix(a) for a in self._BUILDTYPE_ARGS[buildtype]])
@@ -1293,8 +1295,11 @@ class VisualStudioLikeLinkerMixin(DynamicLinkerBase):
         """The command to generate the import library."""
         return self._apply_prefix(['/IMPLIB:' + implibname])
 
-    def rsp_file_syntax(self) -> RSPFileSyntax:
-        return RSPFileSyntax.MSVC
+    def can_linker_accept_rsp(self) -> bool:
+        return True
+
+    def rsp_file_syntax(self) -> 'RSPFileSyntax':
+        return self.rsp_syntax
 
 
 class MSVCDynamicLinker(VisualStudioLikeLinkerMixin, DynamicLinker):
@@ -1307,9 +1312,10 @@ class MSVCDynamicLinker(VisualStudioLikeLinkerMixin, DynamicLinker):
                  exelist: T.Optional[T.List[str]] = None,
                  prefix: T.Union[str, T.List[str]] = '',
                  machine: str = 'x86', version: str = 'unknown version',
-                 direct: bool = True):
+                 direct: bool = True, rsp_syntax: RSPFileSyntax = RSPFileSyntax.MSVC):
         super().__init__(exelist or ['link.exe'], for_machine,
-                         prefix, always_args, machine=machine, version=version, direct=direct)
+                         prefix, always_args, machine=machine, version=version, direct=direct,
+                         rsp_syntax=rsp_syntax)
 
     def get_always_args(self) -> T.List[str]:
         return self._apply_prefix(['/nologo', '/release']) + super().get_always_args()
@@ -1328,9 +1334,10 @@ class ClangClDynamicLinker(VisualStudioLikeLinkerMixin, DynamicLinker):
                  exelist: T.Optional[T.List[str]] = None,
                  prefix: T.Union[str, T.List[str]] = '',
                  machine: str = 'x86', version: str = 'unknown version',
-                 direct: bool = True):
+                 direct: bool = True, rsp_syntax: RSPFileSyntax = RSPFileSyntax.MSVC):
         super().__init__(exelist or ['lld-link.exe'], for_machine,
-                         prefix, always_args, machine=machine, version=version, direct=direct)
+                         prefix, always_args, machine=machine, version=version, direct=direct,
+                         rsp_syntax=rsp_syntax)
 
     def get_output_args(self, outputname: str) -> T.List[str]:
         # If we're being driven indirectly by clang just skip /MACHINE
@@ -1600,7 +1607,7 @@ class MetrowerksLinker(DynamicLinker):
     def invoked_by_compiler(self) -> bool:
         return False
 
-    def rsp_file_syntax(self) -> RSPFileSyntax:
+    def rsp_file_syntax(self) -> 'RSPFileSyntax':
         return RSPFileSyntax.GCC
 
 