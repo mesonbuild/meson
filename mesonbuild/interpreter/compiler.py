025aea1dab4bcf9aafdd72acf48476d999a729b9
Xavier Claessens <xavier.claessens@collabora.com>
2023-09-03 12:27:47 -0400

    Compiler checks must use per-subproject options
    Fixes: #12202

--- mesonbuild/interpreter/compiler.py
+++ mesonbuild/interpreter/compiler.py
@@ -261,7 +261,7 @@ class CompilerHolder(ObjectHolder['Compiler']):
             for idir in i.to_string_list(self.environment.get_source_dir(), self.environment.get_build_dir()):
                 args.extend(self.compiler.get_include_args(idir, False))
         if not kwargs['no_builtin_args']:
-            opts = self.environment.coredata.options
+            opts = coredata.OptionsView(self.environment.coredata.options, self.subproject)
             args += self.compiler.get_option_compile_args(opts)
             if mode is CompileCheckMode.LINK:
                 args.extend(self.compiler.get_option_link_args(opts))
