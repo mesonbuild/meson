10f5af941ea1867d992c8ff5c22944bf88f76449
Dylan Baker <dylan@pnwbakers.com>
2023-09-05 12:52:32 -0700

    build: add annotation for Build.subprojects
    Which maps subproject name to version

--- mesonbuild/build.py
+++ mesonbuild/build.py
@@ -353,7 +353,7 @@ class Build:
         self.data: T.List[Data] = []
         self.symlinks: T.List[SymlinkData] = []
         self.static_linker: PerMachine[StaticLinker] = PerMachine(None, None)
-        self.subprojects = {}
+        self.subprojects: T.Dict[str, str] = {}
         self.subproject_dir = ''
         self.install_scripts: T.List['ExecutableSerialisation'] = []
         self.postconf_scripts: T.List['ExecutableSerialisation'] = []
