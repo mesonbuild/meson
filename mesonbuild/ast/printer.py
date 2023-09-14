0bc51d5221f437b6e182063157e10e3d89d9326b
Charles Brunet <charles.brunet@optelgroup.com>
2023-09-14 15:17:26 -0400

    simplify RawPrinter using FullAstVisitor

--- mesonbuild/ast/printer.py
+++ mesonbuild/ast/printer.py
@@ -6,9 +6,8 @@
 from __future__ import annotations
 
 from .. import mparser
-from .visitor import AstVisitor
+from .visitor import AstVisitor, FullAstVisitor
 
-from itertools import zip_longest
 import re
 import typing as T
 
@@ -239,223 +238,61 @@ class AstPrinter(AstVisitor):
         else:
             self.result = re.sub(r', $', '', self.result)
 
-class RawPrinter(AstVisitor):
+class RawPrinter(FullAstVisitor):
 
     def __init__(self) -> None:
         self.result = ''
 
     def visit_default_func(self, node: mparser.BaseNode) -> None:
-        # XXX: this seems like it could never actually be reached...
-        self.result += node.value  # type: ignore[attr-defined]
-        if node.whitespaces:
-            node.whitespaces.accept(self)
+        self.enter_node(node)
+        assert hasattr(node, 'value')
+        self.result += node.value
+        self.exit_node(node)
 
-    def visit_unary_operator(self, node: mparser.UnaryOperatorNode) -> None:
-        node.operator.accept(self)
-        node.value.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_binary_operator(self, node: mparser.BinaryOperatorNode) -> None:
-        node.left.accept(self)
-        node.operator.accept(self)
-        node.right.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
+    def visit_EmptyNode(self, node: mparser.EmptyNode) -> None:
+        self.enter_node(node)
+        self.exit_node(node)
 
     def visit_BooleanNode(self, node: mparser.BooleanNode) -> None:
+        self.enter_node(node)
         self.result += 'true' if node.value else 'false'
-        if node.whitespaces:
-            node.whitespaces.accept(self)
+        self.exit_node(node)
 
     def visit_NumberNode(self, node: mparser.NumberNode) -> None:
+        self.enter_node(node)
         self.result += node.raw_value
-        if node.whitespaces:
-            node.whitespaces.accept(self)
+        self.exit_node(node)
 
     def visit_StringNode(self, node: mparser.StringNode) -> None:
+        self.enter_node(node)
         self.result += f"'{node.raw_value}'"
-        if node.whitespaces:
-            node.whitespaces.accept(self)
+        self.exit_node(node)
 
     def visit_MultilineStringNode(self, node: mparser.MultilineStringNode) -> None:
+        self.enter_node(node)
         self.result += f"'''{node.value}'''"
-        if node.whitespaces:
-            node.whitespaces.accept(self)
+        self.exit_node(node)
 
     def visit_FormatStringNode(self, node: mparser.FormatStringNode) -> None:
-        self.result += 'f'
-        self.visit_StringNode(node)
+        self.enter_node(node)
+        self.result += f"f'{node.raw_value}'"
+        self.exit_node(node)
 
     def visit_MultilineFormatStringNode(self, node: mparser.MultilineFormatStringNode) -> None:
-        self.result += 'f'
-        self.visit_MultilineStringNode(node)
+        self.enter_node(node)
+        self.result += f"f'''{node.value}'''"
+        self.exit_node(node)
 
     def visit_ContinueNode(self, node: mparser.ContinueNode) -> None:
+        self.enter_node(node)
         self.result += 'continue'
-        if node.whitespaces:
-            node.whitespaces.accept(self)
+        self.exit_node(node)
 
     def visit_BreakNode(self, node: mparser.BreakNode) -> None:
+        self.enter_node(node)
         self.result += 'break'
-        if node.whitespaces:
-            node.whitespaces.accept(self)
+        self.exit_node(node)
 
-    def visit_ArrayNode(self, node: mparser.ArrayNode) -> None:
-        node.lbracket.accept(self)
-        node.args.accept(self)
-        node.rbracket.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_DictNode(self, node: mparser.DictNode) -> None:
-        node.lcurl.accept(self)
-        node.args.accept(self)
-        node.rcurl.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_ParenthesizedNode(self, node: mparser.ParenthesizedNode) -> None:
-        node.lpar.accept(self)
-        node.inner.accept(self)
-        node.rpar.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_OrNode(self, node: mparser.OrNode) -> None:
-        self.visit_binary_operator(node)
-
-    def visit_AndNode(self, node: mparser.AndNode) -> None:
-        self.visit_binary_operator(node)
-
-    def visit_ComparisonNode(self, node: mparser.ComparisonNode) -> None:
-        self.visit_binary_operator(node)
-
-    def visit_ArithmeticNode(self, node: mparser.ArithmeticNode) -> None:
-        self.visit_binary_operator(node)
-
-    def visit_NotNode(self, node: mparser.NotNode) -> None:
-        self.visit_unary_operator(node)
-
-    def visit_CodeBlockNode(self, node: mparser.CodeBlockNode) -> None:
-        if node.pre_whitespaces:
-            node.pre_whitespaces.accept(self)
-        for i in node.lines:
-            i.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_IndexNode(self, node: mparser.IndexNode) -> None:
-        node.iobject.accept(self)
-        node.lbracket.accept(self)
-        node.index.accept(self)
-        node.rbracket.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_MethodNode(self, node: mparser.MethodNode) -> None:
-        node.source_object.accept(self)
-        node.dot.accept(self)
-        node.name.accept(self)
-        node.lpar.accept(self)
-        node.args.accept(self)
-        node.rpar.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_FunctionNode(self, node: mparser.FunctionNode) -> None:
-        node.func_name.accept(self)
-        node.lpar.accept(self)
-        node.args.accept(self)
-        node.rpar.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_AssignmentNode(self, node: mparser.AssignmentNode) -> None:
-        node.var_name.accept(self)
-        node.operator.accept(self)
-        node.value.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_PlusAssignmentNode(self, node: mparser.PlusAssignmentNode) -> None:
-        node.var_name.accept(self)
-        node.operator.accept(self)
-        node.value.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_ForeachClauseNode(self, node: mparser.ForeachClauseNode) -> None:
-        node.foreach_.accept(self)
-        for varname, comma in zip_longest(node.varnames, node.commas):
-            varname.accept(self)
-            if comma is not None:
-                comma.accept(self)
-        node.colon.accept(self)
-        node.items.accept(self)
-        node.block.accept(self)
-        node.endforeach.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_IfClauseNode(self, node: mparser.IfClauseNode) -> None:
-        for i in node.ifs:
-            i.accept(self)
-        if not isinstance(node.elseblock, mparser.EmptyNode):
-            node.elseblock.accept(self)
-        node.endif.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_UMinusNode(self, node: mparser.UMinusNode) -> None:
-        self.visit_unary_operator(node)
-
-    def visit_IfNode(self, node: mparser.IfNode) -> None:
-        node.if_.accept(self)
-        node.condition.accept(self)
-        node.block.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_ElseNode(self, node: mparser.ElseNode) -> None:
-        node.else_.accept(self)
-        node.block.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_TernaryNode(self, node: mparser.TernaryNode) -> None:
-        node.condition.accept(self)
-        node.questionmark.accept(self)
-        node.trueblock.accept(self)
-        node.colon.accept(self)
-        node.falseblock.accept(self)
-        if node.whitespaces:
-            node.whitespaces.accept(self)
-
-    def visit_ArgumentNode(self, node: mparser.ArgumentNode) -> None:
-        commas_iter = iter(node.commas)
-
-        for arg in node.arguments:
-            arg.accept(self)
-            try:
-                comma = next(commas_iter)
-                comma.accept(self)
-            except StopIteration:
-                pass
-
-        assert len(node.colons) == len(node.kwargs)
-        for (key, val), colon in zip(node.kwargs.items(), node.colons):
-            key.accept(self)
-            colon.accept(self)
-            val.accept(self)
-            try:
-                comma = next(commas_iter)
-                comma.accept(self)
-            except StopIteration:
-                pass
-
-        if node.whitespaces:
-            node.whitespaces.accept(self)
 
 class AstJSONPrinter(AstVisitor):
     def __init__(self) -> None:
