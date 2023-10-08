0214f816271a3925ed47f45b5d888a325713c188
Alexandre Janniaux <ajanni@videolabs.io>
2023-10-08 15:13:13 +0200

    mlog: add context after the line
    Longer source reference will be expanded below the diagnostic message:
    
            meson.build:6:3: ERROR: message(...) returns void and is not valid in condition.
            meson.build:6:3:  6 | if message('''foo
            meson.build:6:3:    |    ^^^^^^^^^^^^^^
            meson.build:6:3:    |     Move the call to message before the condition
            meson.build:6:3:  ...located before...
            meson.build:6:3:  7 |   bar
            meson.build:6:3:  8 |   baz''')
    
    There can be errors with lineno > len(line) + 1. See test in
    test cases/failing/121 missing compiler/.

--- mesonbuild/mlog.py
+++ mesonbuild/mlog.py
@@ -367,6 +367,7 @@ class _Logger:
         node_content: T.List[T.Union[AnsiDecorator, str]] = []
         node_marker: T.List[T.Union[AnsiDecorator, str]] = []
         node_resolve: T.List[T.Union[AnsiDecorator, str]] = []
+        node_after: T.List[T.List[T.Union[AnsiDecorator, str]]] = []
         if all(getattr(e, a, None) is not None for a in ['file', 'lineno', 'colno']):
             # Mypy doesn't follow hasattr, and it's pretty easy to visually inspect
             # that this is correct, so we'll just ignore it.
@@ -391,7 +392,9 @@ class _Logger:
             # Missing expected source lines are sourced past the end of the file
             # See test `failing-meson/118 missing compiler` for an example.
             if 1 <= lineno <= len(lines):
-                line_content = lines[lineno - 1]
+                source_lines = lines[lineno - 1:end_lineno]
+                line_content = source_lines[0]
+
                 lineno_size = len(str(end_lineno))
                 lineno_str = str(lineno).rjust(lineno_size)
                 empty_lineno = (lineno_size + 1) * ' '
@@ -409,8 +412,15 @@ class _Logger:
                     node_resolve.append(empty_lineno + ' | ' + colno * ' ')
                     node_resolve.append(yellow(f'{error_resolve}'))
 
+                if lineno != end_lineno:
+                    node_after.extend([['...located before...']])
+                    node_after.extend([
+                        [f' {str(i + lineno + 1).rjust(lineno_size)} | ' + line]
+                        for i, line in enumerate(source_lines[1:])
+                    ])
+
         with self.force_logging():
-            for args_list in [args, node_content, node_marker, node_resolve]:
+            for args_list in [args, node_content, node_marker, node_resolve, *node_after]:
                 if args_list:
                     self.log(*(args_prefix + args_list), is_error=True)
 
