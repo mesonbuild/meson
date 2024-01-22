8494a8561bc2f08b9178ab1fdec89f49021e7fa2
Dylan Baker <dylan@pnwbakers.com>
2024-01-22 10:48:17 -0800

    interpreter: move subproject to LocalState
    Unfortunately we need to keep the `.subproject` attribute to continue to
    implement an interface. I have implemented this as a method, which
    simplifies this, as it simply looks up the state value. I have left the
    uses of .subproject as they are numerous and we can't remove the
    attribute anyway.

--- mesonbuild/interpreterbase/state.py
+++ mesonbuild/interpreterbase/state.py
@@ -5,12 +5,17 @@
 
 from __future__ import annotations
 import dataclasses
+import typing as T
 
+if T.TYPE_CHECKING:
+    from . import SubProject
 
 @dataclasses.dataclass
 class LocalState:
 
     """State that is local to a single subproject."""
+
+    subproject: SubProject
 
 
 @dataclasses.dataclass
