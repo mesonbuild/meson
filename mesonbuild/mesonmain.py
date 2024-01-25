f1b7f52ab76034a4c884250adb256d0918c42afe
Eli Schwartz <eschwartz93@gmail.com>
2024-01-24 22:16:49 -0500

    When configuring fails in Github Actions, print folded logs
    A common, and challenging, issue in CI runners is debugging issues when
    you know the information you want to check, but it's in the log file
    which you don't have because remote CI machines.
    
    There are various edge cases where this is especially hard to solve,
    such as inside of `pip install` where the build directory with the log
    file is automatically cleaned up. But it's never really *easy* when you
    don't expect it, and the best case scenario is your iteration time gets
    cut in half as you hurriedly go add some `cat`s to your CI scripts.
    
    Meson can, at least sometimes, detect platforms where text can be
    emitted inside of "folds", which are auto-collapsed and don't obscure
    the general output, but when clicked will expand the logfile contents.
    Hook this up.
    
    We start off with a Github Actions implementation. We had some internal
    code used by our own project tests runner, which can be utilized.
    
    Also permit forcing it via an environment variable, in case
    autodetection fails and you just want to force *something*, especially
    when meson is called a couple layers deep inside some other tool.
    
    (cherry picked from commit 2b80d4cca174916a4b6d0c47c54ad56795741bb6)

--- mesonbuild/mesonmain.py
+++ mesonbuild/mesonmain.py
@@ -27,6 +27,9 @@ def errorhandler(e: Exception, command: str) -> int:
         logfile = mlog.shutdown()
         if logfile is not None:
             mlog.log("\nA full log can be found at", mlog.bold(logfile))
+            contents = mlog.ci_fold_file(logfile, f'CI platform detected, click here for {os.path.basename(logfile)} contents.')
+            if contents:
+                print(contents)
         if os.environ.get('MESON_FORCE_BACKTRACE'):
             raise e
         return 1
