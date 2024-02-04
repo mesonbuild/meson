91caf7aa56ba622926f7056d4a42083a75c24e5a
U2FsdGVkX1 <U2FsdGVkX1@gmail.com>
2024-02-04 14:07:54 +0800

    Fix ninja cannot find the library when libraries contain symlinks.

--- mesonbuild/compilers/mixins/gnu.py
+++ mesonbuild/compilers/mixins/gnu.py
@@ -469,16 +469,13 @@ class GnuLikeCompiler(Compiler, metaclass=abc.ABCMeta):
             # paths under /lib would be considered not a "system path",
             # which is wrong and breaks things. Store everything, just to be sure.
             pobj = pathlib.Path(p)
-            unresolved = pobj.as_posix()
             if pobj.exists():
+                resolved = pobj.resolve().as_posix()
+                if resolved not in result:
+                    result.append(resolved)
+                unresolved = pobj.as_posix()
                 if unresolved not in result:
                     result.append(unresolved)
-                try:
-                    resolved = pathlib.Path(p).resolve().as_posix()
-                    if resolved not in result:
-                        result.append(resolved)
-                except FileNotFoundError:
-                    pass
         return result
 
     def get_compiler_dirs(self, env: 'Environment', name: str) -> T.List[str]:
