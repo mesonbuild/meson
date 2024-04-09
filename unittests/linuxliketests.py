aa862f52e0a8ec4569948398c8c66f63479e369c
Jussi Pakkanen <jpakkane@gmail.com>
2024-04-09 21:43:33 +0300

    Can specify per-subproject options from the command line.

--- unittests/linuxliketests.py
+++ unittests/linuxliketests.py
@@ -1867,3 +1867,45 @@ class LinuxlikeTests(BasePlatformTests):
     def test_top_options_in_sp(self):
         testdir = os.path.join(self.unit_test_dir, '124 pkgsubproj')
         self.init(testdir)
+
+    def check_has_flag(self, compdb, src, argument):
+        for i in compdb:
+            if src in i['file']:
+                self.assertIn(argument, i['command'])
+                return
+        self.assertTrue(False, f'Source {src} not found in compdb')
+
+    def test_persp_options(self):
+        testdir = os.path.join(self.unit_test_dir, '123 persp options')
+        self.init(testdir, extra_args='-Doptimization=1')
+        compdb = self.get_compdb()
+        mainsrc = 'toplevel.c'
+        sub1src = 'sub1.c'
+        sub2src = 'sub2.c'
+        self.check_has_flag(compdb, mainsrc, '-O1')
+        self.check_has_flag(compdb, sub1src, '-O1')
+        self.check_has_flag(compdb, sub2src, '-O1')
+
+        # Set subproject option to O2
+        compdb = self.get_compdb()
+        self.check_has_flag(compdb, mainsrc, '-O1')
+        self.check_has_flag(compdb, sub1src, '-O1')
+        self.check_has_flag(compdb, sub2src, '-O2')
+
+        # Set top level option to O3
+        compdb = self.get_compdb()
+        self.check_has_flag(compdb, mainsrc, '-O3')
+        self.check_has_flag(compdb, sub1src, '-O1')
+        self.check_has_flag(compdb, sub2src, '-O2')
+
+        # UnsetSet subproject
+        compdb = self.get_compdb()
+        self.check_has_flag(compdb, mainsrc, '-O3')
+        self.check_has_flag(compdb, sub1src, '-O1')
+        self.check_has_flag(compdb, sub2src, '-O1')
+
+        # Set global value
+        compdb = self.get_compdb()
+        self.check_has_flag(compdb, mainsrc, '-O3')
+        self.check_has_flag(compdb, sub1src, '-O2')
+        self.check_has_flag(compdb, sub2src, '-O2')
