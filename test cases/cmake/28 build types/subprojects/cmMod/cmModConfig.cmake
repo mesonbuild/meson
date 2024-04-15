d546868dd9d234a956348e05980d81974a5e0a0d
Momtchil Momtchev <momtchil@momtchev.com>
2024-04-15 15:37:18 +0200

    test MAP_IMPORTED_<CONFIG> handling

--- test cases/cmake/28 build types/subprojects/cmMod/cmModConfig.cmake
+++ test cases/cmake/28 build types/subprojects/cmMod/cmModConfig.cmake
@@ -1,9 +1,11 @@
 # Check if meson correctly sets the build type and resolves the correct entry
 add_library(cmMod::cmModLib++ STATIC IMPORTED)
 
+set_property(TARGET cmMod::cmModLib++ PROPERTY MAP_IMPORTED_CONFIG_RELEASE IMPORTED_SPECIAL)
+set_property(TARGET cmMod::cmModLib++ PROPERTY MAP_IMPORTED_CONFIG_DEBUG IMPORTED_SPECIAL)
+
 # Do not take this
 set_property(TARGET cmMod::cmModLib++ PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/invalid.a)
 
-# Take one of these
-set_property(TARGET cmMod::cmModLib++ PROPERTY IMPORTED_LOCATION_RELEASE "${CMAKE_BINARY_DIR}/../libcmModLib_internal.a")
-set_property(TARGET cmMod::cmModLib++ PROPERTY IMPORTED_LOCATION_DEBUG "${CMAKE_BINARY_DIR}/../libcmModLib_internal.a")
+# Take this
+set_property(TARGET cmMod::cmModLib++ PROPERTY IMPORTED_LOCATION_IMPORTED_SPECIAL "${CMAKE_BINARY_DIR}/../libcmModLib_internal.a")
