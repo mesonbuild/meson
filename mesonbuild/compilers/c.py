d244e50f774de33f0e2b7235f5f05494caefda69
Jussi Pakkanen <jpakkane@gmail.com>
2024-04-21 01:26:38 +0300

    Convert more usages to per-target methods.

--- mesonbuild/compilers/c.py
+++ mesonbuild/compilers/c.py
@@ -263,10 +263,13 @@ class GnuCCompiler(GnuCStds, GnuCompiler, CCompiler):
                 gnu_winlibs)
         return opts
 
-    def get_option_compile_args(self, target: 'BuildTarget', env: 'Environment') -> T.List[str]:
+    def get_option_compile_args(self, target: 'BuildTarget', env: 'Environment', subproject=None) -> T.List[str]:
         args = []
         key = self.form_compileropt_key('std')
-        std = env.coredata.get_option_for_target(target, key)
+        if target:
+            std = env.coredata.get_option_for_target(target, key)
+        else:
+            std = env.coredata.get_option_for_subproject(key, subproject)
         if std != 'none':
             args.append('-std=' + std)
         return args
