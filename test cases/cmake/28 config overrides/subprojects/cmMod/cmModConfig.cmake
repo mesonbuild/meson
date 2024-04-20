81f961338025a4e617518cea395a73f45ef685a9
Momtchil Momtchev <momtchil@momtchev.com>
2024-04-20 16:20:06 +0200

    support `$CONFIG` generator expression and config overrides (#3)
    * support CMake $<CONFIG> generator expressions
    
    * fix the C-like syntax
    
    * add an explicit cas to avoid the mypy error
    
    * reimplement reading of config-declined properties
    
    * fix mypy types
    
    * import only when checking to avoid a cycle
    
    * test MAP_IMPORTED_<CONFIG> handling
    
    * refactor to address static code analysis tool remarks
    
    * simplify the makefile
    
    * construct the name of the static library
    
    * use only listed configs when IMPORTED_CONFIGURATIONS is defined
    
    * set explicitly the package config directory
    
    * construct the library name when it has been explicitly modified (meson on Windows)
    
    * set explicitly the library name with MSVC
    
    ---------
    
    Co-authored-by: Momtchil Momtchev <momtchil@motmchev.com>

---
+++ test cases/cmake/28 config overrides/subprojects/cmMod/cmModConfig.cmake
@@ -0,0 +1,28 @@
+# Check if meson correctly sets the build type and resolves the correct entry
+add_library(cmMod::cmModLib++ STATIC IMPORTED)
+
+# Construct the name of the library that was built
+# (cmake in meson does not support TARGET_FILE generator expression unless the target is imported)
+get_target_property(CMMOD_LIB_DIR cmModLib_internal BINARY_DIR)
+get_target_property(CMMOD_LIB_NAME cmModLib_internal NAME)
+
+# meson Windows static library naming scheme
+if(MSVC)
+  set(CMMOD_LIB_PREFIX lib)
+  set(CMMOD_LIB_SUFFIX .a)
+else()
+  set(CMMOD_LIB_PREFIX ${CMAKE_STATIC_LIBRARY_PREFIX})
+  set(CMMOD_LIB_SUFFIX ${CMAKE_STATIC_LIBRARY_SUFFIX})
+endif()
+
+set(CMMOD_STATIC_LIB "${CMMOD_LIB_DIR}/../${CMMOD_LIB_PREFIX}${CMMOD_LIB_NAME}${CMMOD_LIB_SUFFIX}")
+
+# Map both RELEASE and DEBUG to IMPORTED_SPECIAL
+set_property(TARGET cmMod::cmModLib++ PROPERTY MAP_IMPORTED_CONFIG_RELEASE IMPORTED_SPECIAL)
+set_property(TARGET cmMod::cmModLib++ PROPERTY MAP_IMPORTED_CONFIG_DEBUG IMPORTED_SPECIAL)
+
+# Do not take this
+set_property(TARGET cmMod::cmModLib++ PROPERTY IMPORTED_LOCATION ${CMAKE_BINARY_DIR}/invalid.a)
+
+# Take this
+set_property(TARGET cmMod::cmModLib++ PROPERTY IMPORTED_LOCATION_IMPORTED_SPECIAL ${CMMOD_STATIC_LIB})
