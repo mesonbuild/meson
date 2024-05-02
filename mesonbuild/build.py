f800b3193c3f01125952a71e14ef0fc22a63a537
Tristan Partin <tristan@partin.io>
2024-05-02 00:01:25 -0500

    Use correct subdir when generating processed file path
    We need the subdir of where the output file will actually be created,
    not the current subdir of the interpreter.
    
    Fixes: #13168
    (cherry picked from commit 23eb7ba700cd6c53d5491cc4f99c532629efa1e2)

--- mesonbuild/build.py
+++ mesonbuild/build.py
@@ -1818,13 +1818,9 @@ class Generator(HoldableObject):
             env=env if env is not None else EnvironmentVariables())
 
         for e in files:
-            if isinstance(e, CustomTarget):
-                output.depends.add(e)
-            if isinstance(e, CustomTargetIndex):
-                output.depends.add(e.target)
             if isinstance(e, (CustomTarget, CustomTargetIndex)):
                 output.depends.add(e)
-                fs = [File.from_built_file(state.subdir, f) for f in e.get_outputs()]
+                fs = [File.from_built_file(e.get_subdir(), f) for f in e.get_outputs()]
             elif isinstance(e, GeneratedList):
                 if preserve_path_from:
                     raise InvalidArguments("generator.process: 'preserve_path_from' is not allowed if one input is a 'generated_list'.")
