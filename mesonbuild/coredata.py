924b345b192b0d651bf9167b80f207ff47aec261
Jussi Pakkanen <jpakkane@gmail.com>
2024-05-15 23:40:54 +0300

    Pass original strings to setter method.

--- mesonbuild/coredata.py
+++ mesonbuild/coredata.py
@@ -726,6 +726,20 @@ class CoreData:
     def can_set_per_sb(self, keystr):
         return True
 
+    def set_options_from_configure_strings(self, D_args) -> bool:
+        dirty = False
+        for entry in D_args:
+            if '=' not in entry:
+                raise MesonException(f'A -D argument must be of form "name=value" instead of {entry}')
+            key, val = entry.split('=', 1)
+            if key in self.sp_option_overrides:
+                self.sp_option_overrides[key] = val
+                dirty = True
+            else:
+                # FIXME call the default setter, possibly after creating an OptionKey first.
+                pass
+        return dirty
+
     def create_sp_options(self, A) -> bool:
         if A is None:
             return False
