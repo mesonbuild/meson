870780b83c225c06ab08bb97ed613fa82c25b27e
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-05-18 11:58:33 -0600

    Update cpptemplates.py

--- mesonbuild/templates/cpptemplates.py
+++ mesonbuild/templates/cpptemplates.py
@@ -1,37 +1,34 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright 2019 The Meson development team
-
-from __future__ import annotations
-
+from pathlib import Path
 from mesonbuild.templates.sampleimpl import FileHeaderImpl
 
-
-hello_cpp_template = '''#include <iostream>
+hello_cpp_template = """
+#include <iostream>
 
 #define PROJECT_NAME "{project_name}"
 
 int main(int argc, char **argv) {{
-    if(argc != 1) {{
-        std::cout << argv[0] <<  "takes no arguments.\\n";
+    if (argc != 1) {{
+        std::cout << argv[0] << "takes no arguments.\\n";
         return 1;
     }}
     std::cout << "This is project " << PROJECT_NAME << ".\\n";
     return 0;
 }}
-'''
+"""
 
-hello_cpp_meson_template = '''project('{project_name}', 'cpp',
-  version : '{version}',
-  default_options : ['warning_level=3',
-                     'cpp_std=c++14'])
+hello_cpp_meson_template = f"""
+project('{project_name}', 'cpp',
+  version: '{version}',
+  default_options: ['warning_level=3', 'cpp_std=c++14'])
 
 exe = executable('{exe_name}', '{source_name}',
-  install : true)
+  install: true)
 
 test('basic', exe)
-'''
+"""
 
-lib_hpp_template = '''#pragma once
+lib_hpp_template = """
+#pragma once
 #if defined _WIN32 || defined __CYGWIN__
   #ifdef BUILDING_{utoken}
     #define {utoken}_PUBLIC __declspec(dllexport)
@@ -40,9 +37,9 @@ lib_hpp_template = '''#pragma once
   #endif
 #else
   #ifdef BUILDING_{utoken}
-      #define {utoken}_PUBLIC __attribute__ ((visibility ("default")))
+    #define {utoken}_PUBLIC __attribute__ ((visibility ("default")))
   #else
-      #define {utoken}_PUBLIC
+    #define {utoken}_PUBLIC
   #endif
 #endif
 
@@ -55,16 +52,15 @@ public:
   int get_number() const;
 
 private:
-
   int number;
 
 }};
 
 }}
+"""
 
-'''
-
-lib_cpp_template = '''#include <{header_file}>
+lib_cpp_template = """
+#include <{header_file}>
 
 namespace {namespace} {{
 
@@ -73,65 +69,70 @@ namespace {namespace} {{
 }}
 
 int {class_name}::get_number() const {{
-  return number;
+    return number;
 }}
 
 }}
-'''
+"""
 
-lib_cpp_test_template = '''#include <{header_file}>
+lib_cpp_test_template = """
+#include <{header_file}>
 #include <iostream>
 
 int main(int argc, char **argv) {{
-    if(argc != 1) {{
+    if (argc != 1) {{
         std::cout << argv[0] << " takes no arguments.\\n";
         return 1;
     }}
     {namespace}::{class_name} c;
     return c.get_number() != 6;
 }}
-'''
+"""
 
-lib_cpp_meson_template = '''project('{project_name}', 'cpp',
-  version : '{version}',
-  default_options : ['warning_level=3', 'cpp_std=c++14'])
+lib_cpp_meson_template = f"""
+project('{project_name}', 'cpp',
+  version: '{version}',
+  default_options: ['warning_level=3', 'cpp_std=c++14'])
 
 # These arguments are only used to build the shared library
 # not the executables that use the library.
 lib_args = ['-DBUILDING_{utoken}']
 
 shlib = shared_library('{lib_name}', '{source_file}',
-  install : true,
-  cpp_args : lib_args,
-  gnu_symbol_visibility : 'hidden',
+  install: true,
+  cpp_args: lib_args,
+  gnu_symbol_visibility: 'hidden',
 )
 
 test_exe = executable('{test_exe_name}', '{test_source_file}',
-  link_with : shlib)
+  link_with: shlib)
 test('{test_name}', test_exe)
 
 # Make this library usable as a Meson subproject.
 {ltoken}_dep = declare_dependency(
   include_directories: include_directories('.'),
-  link_with : shlib)
+  link_with: shlib
+)
 
 # Make this library usable from the system's
 # package manager.
-install_headers('{header_file}', subdir : '{header_dir}')
+install_headers('{header_file}', subdir: '{header_dir}')
 
 pkg_mod = import('pkgconfig')
 pkg_mod.generate(
-  name : '{project_name}',
-  filebase : '{ltoken}',
-  description : 'Meson sample project.',
-  subdirs : '{header_dir}',
-  libraries : shlib,
-  version : '{version}',
+  name: '{project_name}',
+  filebase: '{ltoken}',
+  description: 'Meson sample project.',
+  subdirs: '{header_dir}',
+  libraries: shlib,
+  version: '{version}'
 )
-'''
-
+"""
 
 class CppProject(FileHeaderImpl):
+    """
+    Implementation for C++ project.
+    """
 
     source_ext = 'cpp'
     header_ext = 'hpp'
