a725fabfd690161f301688b597b61f5a316d0fa0
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-05-18 11:51:55 -0600

    Update sampleimpl.py

--- mesonbuild/templates/sampleimpl.py
+++ mesonbuild/templates/sampleimpl.py
@@ -1,19 +1,19 @@
-# SPDX-License-Identifier: Apache-2.0
-# Copyright 2019 The Meson development team
-
-from __future__ import annotations
-
 import abc
 import re
-import typing as T
-
-if T.TYPE_CHECKING:
-    from ..minit import Arguments
-
+from typing import Dict, Union
 
 class SampleImpl(metaclass=abc.ABCMeta):
+    """
+    Base class for Sample implementation.
+    """
 
-    def __init__(self, args: Arguments):
+    def __init__(self, args):
+        """
+        Initialize SampleImpl instance.
+        
+        Args:
+            args: Arguments object.
+        """
         self.name = args.name
         self.version = args.version
         self.lowercase_token = re.sub(r'[^a-z0-9]', '_', self.name.lower())
@@ -21,43 +21,77 @@ class SampleImpl(metaclass=abc.ABCMeta):
         self.capitalized_token = self.lowercase_token.capitalize()
 
     @abc.abstractmethod
-    def create_executable(self) -> None:
+    def create_executable(self):
+        """
+        Abstract method to create executable.
+        """
         pass
 
     @abc.abstractmethod
-    def create_library(self) -> None:
+    def create_library(self):
+        """
+        Abstract method to create library.
+        """
         pass
 
-    @abc.abstractproperty
-    def exe_template(self) -> str:
+    @property
+    @abc.abstractmethod
+    def exe_template(self):
+        """
+        Abstract property for executable template.
+        """
         pass
 
-    @abc.abstractproperty
-    def exe_meson_template(self) -> str:
+    @property
+    @abc.abstractmethod
+    def exe_meson_template(self):
+        """
+        Abstract property for executable Meson template.
+        """
         pass
 
-    @abc.abstractproperty
-    def lib_template(self) -> str:
+    @property
+    @abc.abstractmethod
+    def lib_template(self):
+        """
+        Abstract property for library template.
+        """
         pass
 
-    @abc.abstractproperty
-    def lib_test_template(self) -> str:
+    @property
+    @abc.abstractmethod
+    def lib_test_template(self):
+        """
+        Abstract property for library test template.
+        """
         pass
 
-    @abc.abstractproperty
-    def lib_meson_template(self) -> str:
+    @property
+    @abc.abstractmethod
+    def lib_meson_template(self):
+        """
+        Abstract property for library Meson template.
+        """
         pass
 
-    @abc.abstractproperty
-    def source_ext(self) -> str:
+    @property
+    @abc.abstractmethod
+    def source_ext(self):
+        """
+        Abstract property for source file extension.
+        """
         pass
 
 
 class ClassImpl(SampleImpl):
+    """
+    Implementation for class-based languages like Java and C#.
+    """
 
-    """For Class based languages, like Java and C#"""
-
-    def create_executable(self) -> None:
+    def create_executable(self):
+        """
+        Create executable.
+        """
         source_name = f'{self.capitalized_token}.{self.source_ext}'
         with open(source_name, 'w', encoding='utf-8') as f:
             f.write(self.exe_template.format(project_name=self.name,
@@ -68,21 +102,13 @@ class ClassImpl(SampleImpl):
                                                    source_name=source_name,
                                                    version=self.version))
 
-    def create_library(self) -> None:
+    def create_library(self):
+        """
+        Create library.
+        """
         lib_name = f'{self.capitalized_token}.{self.source_ext}'
         test_name = f'{self.capitalized_token}_test.{self.source_ext}'
-        kwargs = {'utoken': self.uppercase_token,
-                  'ltoken': self.lowercase_token,
-                  'class_test': f'{self.capitalized_token}_test',
-                  'class_name': self.capitalized_token,
-                  'source_file': lib_name,
-                  'test_source_file': test_name,
-                  'test_exe_name': f'{self.lowercase_token}_test',
-                  'project_name': self.name,
-                  'lib_name': self.lowercase_token,
-                  'test_name': self.lowercase_token,
-                  'version': self.version,
-                  }
+        kwargs = self._lib_kwargs()
         with open(lib_name, 'w', encoding='utf-8') as f:
             f.write(self.lib_template.format(**kwargs))
         with open(test_name, 'w', encoding='utf-8') as f:
@@ -90,12 +116,37 @@ class ClassImpl(SampleImpl):
         with open('meson.build', 'w', encoding='utf-8') as f:
             f.write(self.lib_meson_template.format(**kwargs))
 
+    def _lib_kwargs(self):
+        """
+        Get language-specific keyword arguments.
+        
+        Returns:
+            Dict[str, Union[str, int]]: Dictionary of key-value pairs.
+        """
+        return {
+            'utoken': self.uppercase_token,
+            'ltoken': self.lowercase_token,
+            'class_test': f'{self.capitalized_token}_test',
+            'class_name': self.capitalized_token,
+            'source_file': f'{self.capitalized_token}.{self.source_ext}',
+            'test_source_file': f'{self.capitalized_token}_test.{self.source_ext}',
+            'test_exe_name': f'{self.lowercase_token}_test',
+            'project_name': self.name,
+            'lib_name': self.lowercase_token,
+            'test_name': self.lowercase_token,
+            'version': self.version,
+        }
+
 
 class FileImpl(SampleImpl):
+    """
+    Implementation for file-based languages without headers.
+    """
 
-    """File based languages without headers"""
-
-    def create_executable(self) -> None:
+    def create_executable(self):
+        """
+        Create executable.
+        """
         source_name = f'{self.lowercase_token}.{self.source_ext}'
         with open(source_name, 'w', encoding='utf-8') as f:
             f.write(self.exe_template.format(project_name=self.name))
@@ -105,31 +156,13 @@ class FileImpl(SampleImpl):
                                                    source_name=source_name,
                                                    version=self.version))
 
-    def lib_kwargs(self) -> T.Dict[str, str]:
-        """Get Language specific keyword arguments
-
-        :return: A dictionary of key: values to fill in the templates
+    def create_library(self):
         """
-        return {
-            'utoken': self.uppercase_token,
-            'ltoken': self.lowercase_token,
-            'header_dir': self.lowercase_token,
-            'class_name': self.capitalized_token,
-            'function_name': f'{self.lowercase_token[0:3]}_func',
-            'namespace': self.lowercase_token,
-            'source_file': f'{self.lowercase_token}.{self.source_ext}',
-            'test_source_file': f'{self.lowercase_token}_test.{self.source_ext}',
-            'test_exe_name': f'{self.lowercase_token}_test',
-            'project_name': self.name,
-            'lib_name': self.lowercase_token,
-            'test_name': self.lowercase_token,
-            'version': self.version,
-        }
-
-    def create_library(self) -> None:
+        Create library.
+        """
         lib_name = f'{self.lowercase_token}.{self.source_ext}'
         test_name = f'{self.lowercase_token}_test.{self.source_ext}'
-        kwargs = self.lib_kwargs()
+        kwargs = self._lib_kwargs()
         with open(lib_name, 'w', encoding='utf-8') as f:
             f.write(self.lib_template.format(**kwargs))
         with open(test_name, 'w', encoding='utf-8') as f:
@@ -137,24 +170,44 @@ class FileImpl(SampleImpl):
         with open('meson.build', 'w', encoding='utf-8') as f:
             f.write(self.lib_meson_template.format(**kwargs))
 
-
-class FileHeaderImpl(FileImpl):
-
-    @abc.abstractproperty
-    def header_ext(self) -> str:
-        pass
-
-    @abc.abstractproperty
-    def lib_header_template(self) -> str:
-        pass
-
-    def lib_kwargs(self) -> T.Dict[str, str]:
-        kwargs = super().lib_kwargs()
+    def _lib_kwargs(self):
+        """
+        Get language-specific keyword arguments.
+        
+        Returns:
+            Dict[str, Union[str, int]]: Dictionary of key-value pairs.
+        """
+        kwargs = super()._lib_kwargs()
         kwargs['header_file'] = f'{self.lowercase_token}.{self.header_ext}'
         return kwargs
 
-    def create_library(self) -> None:
+
+class FileHeaderImpl(FileImpl):
+    """
+    Implementation for file-based languages with headers.
+    """
+
+    @property
+    @abc.abstractmethod
+    def header_ext(self):
+        """
+        Abstract property for header file extension.
+        """
+        pass
+
+    @property
+    @abc.abstractmethod
+    def lib_header_template(self):
+        """
+        Abstract property for library header template.
+        """
+        pass
+
+    def create_library(self):
+        """
+        Create library.
+        """
         super().create_library()
-        kwargs = self.lib_kwargs()
+        kwargs = self._lib_kwargs()
         with open(kwargs['header_file'], 'w', encoding='utf-8') as f:
             f.write(self.lib_header_template.format_map(kwargs))
