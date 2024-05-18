436ca61f2806afa3d8c965d23eb6e61aa321fc44
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-05-18 12:16:26 -0600

    Update samplefactory.py

--- mesonbuild/templates/samplefactory.py
+++ mesonbuild/templates/samplefactory.py
@@ -5,6 +5,7 @@ from __future__ import annotations
 
 import typing as T
 
+# Import the templates for different programming languages
 from mesonbuild.templates.valatemplates import ValaProject
 from mesonbuild.templates.fortrantemplates import FortranProject
 from mesonbuild.templates.objcpptemplates import ObjCppProject
@@ -17,11 +18,12 @@ from mesonbuild.templates.cpptemplates import CppProject
 from mesonbuild.templates.cstemplates import CSharpProject
 from mesonbuild.templates.ctemplates import CProject
 
+# Import the Arguments and SampleImpl types
 if T.TYPE_CHECKING:
     from ..minit import Arguments
     from .sampleimpl import ClassImpl, FileHeaderImpl, FileImpl, SampleImpl
 
-
+# Mapping of programming languages to corresponding template classes
 _IMPL: T.Mapping[str, T.Union[T.Type[ClassImpl], T.Type[FileHeaderImpl], T.Type[FileImpl]]] = {
     'c': CProject,
     'cpp': CppProject,
@@ -38,4 +40,17 @@ _IMPL: T.Mapping[str, T.Union[T.Type[ClassImpl], T.Type[FileHeaderImpl], T.Type[
 
 
 def sample_generator(options: Arguments) -> SampleImpl:
-    return _IMPL[options.language](options)
+    """
+    Generate a sample implementation based on the provided options.
+
+    Args:
+        options (Arguments): The options specifying the project type and language.
+
+    Returns:
+        SampleImpl: An instance of a sample implementation based on the provided options.
+    """
+    # Retrieve the appropriate template class based on the language specified in the options
+    template_class = _IMPL[options.language]
+
+    # Instantiate the template class with the provided options and return the instance
+    return template_class(options)
