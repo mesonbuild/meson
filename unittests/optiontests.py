17142e5bbc50da6650c963502a0e5fabf52ca4d2
Jussi Pakkanen <jpakkane@gmail.com>
2024-05-24 21:30:04 +0300

    Write more tests.

--- unittests/optiontests.py
+++ unittests/optiontests.py
@@ -7,7 +7,7 @@ import unittest
 
 
 class OptionTests(unittest.TestCase):
-    
+
     def test_basic(self):
         os = OptionStore()
         name = 'someoption'
@@ -15,3 +15,47 @@ class OptionTests(unittest.TestCase):
         vo = UserStringOption(name, 'An option of some sort', default_value)
         os.add_system_option(name, vo)
         self.assertEqual(os.get_value_for(name), default_value)
+
+    def test_reset(self):
+        os = OptionStore()
+        name = 'someoption'
+        original_value = 'original'
+        reset_value = 'reset'
+        vo = UserStringOption(name, 'An option set twice', original_value)
+        os.add_system_option(name, vo)
+        self.assertEqual(os.get_value_for(name), original_value)
+        self.assertEqual(os.num_options(), 1)
+        vo2 = UserStringOption(name, 'An option set twice', reset_value)
+        os.add_system_option(name, vo2)
+        self.assertEqual(os.get_value_for(name), original_value)
+        self.assertEqual(os.num_options(), 1)
+
+    def test_project_nonyielding(self):
+        os = OptionStore()
+        name = 'someoption'
+        top_value = 'top'
+        sub_value = 'sub'
+        vo = UserStringOption(name, 'A top level option', top_value)
+        os.add_project_option(name, '', False, vo)
+        self.assertEqual(os.get_value_for(name, ''), top_value)
+        self.assertEqual(os.num_options(), 1)
+        vo2 = UserStringOption(name, 'A subproject option', sub_value)
+        os.add_project_option(name, 'sub', False, vo2)
+        self.assertEqual(os.get_value_for(name), top_value)
+        self.assertEqual(os.get_value_for(name, 'sub'), sub_value)
+        self.assertEqual(os.num_options(), 2)
+
+    def test_project_yielding(self):
+        os = OptionStore()
+        name = 'someoption'
+        top_value = 'top'
+        sub_value = 'sub'
+        vo = UserStringOption(name, 'A top level option', top_value)
+        os.add_project_option(name, '', False, vo)
+        self.assertEqual(os.get_value_for(name, ''), top_value)
+        self.assertEqual(os.num_options(), 1)
+        vo2 = UserStringOption(name, 'A subproject option', sub_value)
+        os.add_project_option(name, 'sub', True, vo2)
+        self.assertEqual(os.get_value_for(name), top_value)
+        self.assertEqual(os.get_value_for(name, 'sub'), top_value)
+        self.assertEqual(os.num_options(), 2)
