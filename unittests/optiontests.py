51b73bdb9dec0ca1d812b14699830dbb7efa1c31
Jussi Pakkanen <jpakkane@gmail.com>
2024-05-24 22:57:30 +0300

    Implement augments in OptionStore.

--- unittests/optiontests.py
+++ unittests/optiontests.py
@@ -9,53 +9,101 @@ import unittest
 class OptionTests(unittest.TestCase):
 
     def test_basic(self):
-        os = OptionStore()
+        optstore = OptionStore()
+        name = 'someoption'
+        default_value = 'somevalue'
+        new_value = 'new_value'
+        vo = UserStringOption(name, 'An option of some sort', default_value)
+        optstore.add_system_option(name, vo)
+        self.assertEqual(optstore.get_value_for(name), default_value)
+        optstore.set_option(name, '', new_value)
+        self.assertEqual(optstore.get_value_for(name), new_value)
+
+    def test_subproject_for_system(self):
+        optstore = OptionStore()
         name = 'someoption'
         default_value = 'somevalue'
         vo = UserStringOption(name, 'An option of some sort', default_value)
-        os.add_system_option(name, vo)
-        self.assertEqual(os.get_value_for(name), default_value)
+        optstore.add_system_option(name, vo)
+        self.assertEqual(optstore.get_value_for(name, 'somesubproject'), default_value)
 
     def test_reset(self):
-        os = OptionStore()
+        optstore = OptionStore()
         name = 'someoption'
         original_value = 'original'
         reset_value = 'reset'
         vo = UserStringOption(name, 'An option set twice', original_value)
-        os.add_system_option(name, vo)
-        self.assertEqual(os.get_value_for(name), original_value)
-        self.assertEqual(os.num_options(), 1)
+        optstore.add_system_option(name, vo)
+        self.assertEqual(optstore.get_value_for(name), original_value)
+        self.assertEqual(optstore.num_options(), 1)
         vo2 = UserStringOption(name, 'An option set twice', reset_value)
-        os.add_system_option(name, vo2)
-        self.assertEqual(os.get_value_for(name), original_value)
-        self.assertEqual(os.num_options(), 1)
+        optstore.add_system_option(name, vo2)
+        self.assertEqual(optstore.get_value_for(name), original_value)
+        self.assertEqual(optstore.num_options(), 1)
 
     def test_project_nonyielding(self):
-        os = OptionStore()
+        optstore = OptionStore()
         name = 'someoption'
         top_value = 'top'
         sub_value = 'sub'
-        vo = UserStringOption(name, 'A top level option', top_value)
-        os.add_project_option(name, '', False, vo)
-        self.assertEqual(os.get_value_for(name, ''), top_value)
-        self.assertEqual(os.num_options(), 1)
+        vo = UserStringOption(name, 'A top level option', top_value, False)
+        optstore.add_project_option(name, '', vo)
+        self.assertEqual(optstore.get_value_for(name, ''), top_value, False)
+        self.assertEqual(optstore.num_options(), 1)
         vo2 = UserStringOption(name, 'A subproject option', sub_value)
-        os.add_project_option(name, 'sub', False, vo2)
-        self.assertEqual(os.get_value_for(name), top_value)
-        self.assertEqual(os.get_value_for(name, 'sub'), sub_value)
-        self.assertEqual(os.num_options(), 2)
+        optstore.add_project_option(name, 'sub', vo2)
+        self.assertEqual(optstore.get_value_for(name), top_value)
+        self.assertEqual(optstore.get_value_for(name, 'sub'), sub_value)
+        self.assertEqual(optstore.num_options(), 2)
 
     def test_project_yielding(self):
-        os = OptionStore()
+        optstore = OptionStore()
         name = 'someoption'
         top_value = 'top'
         sub_value = 'sub'
         vo = UserStringOption(name, 'A top level option', top_value)
-        os.add_project_option(name, '', False, vo)
-        self.assertEqual(os.get_value_for(name, ''), top_value)
-        self.assertEqual(os.num_options(), 1)
-        vo2 = UserStringOption(name, 'A subproject option', sub_value)
-        os.add_project_option(name, 'sub', True, vo2)
-        self.assertEqual(os.get_value_for(name), top_value)
-        self.assertEqual(os.get_value_for(name, 'sub'), top_value)
-        self.assertEqual(os.num_options(), 2)
+        optstore.add_project_option(name, '', vo)
+        self.assertEqual(optstore.get_value_for(name, ''), top_value)
+        self.assertEqual(optstore.num_options(), 1)
+        vo2 = UserStringOption(name, 'A subproject option', sub_value, True)
+        optstore.add_project_option(name, 'sub', vo2)
+        self.assertEqual(optstore.get_value_for(name), top_value)
+        self.assertEqual(optstore.get_value_for(name, 'sub'), top_value)
+        self.assertEqual(optstore.num_options(), 2)
+
+    def test_augments(self):
+        optstore = OptionStore()
+        name = 'cpp_std'
+        sub_name = 'sub'
+        sub2_name = 'sub2'
+        top_value = 'c++11'
+        aug_value = 'c++23'
+
+        co = UserComboOption(name,
+                             'C++ language standard to use',
+                             ['c++98', 'c++11', 'c++14', 'c++17', 'c++20', 'c++23'],
+                             top_value)
+        optstore.add_system_option(name, co)
+        self.assertEqual(optstore.get_value_for(name), top_value)
+        self.assertEqual(optstore.get_value_for(name, sub_name), top_value)
+        self.assertEqual(optstore.get_value_for(name, sub2_name), top_value)
+
+        # First augment a subproject
+        optstore.set_from_configure_command([], [f'{sub_name}:{name}={aug_value}'], [])
+        self.assertEqual(optstore.get_value_for(name), top_value)
+        self.assertEqual(optstore.get_value_for(name, sub_name), aug_value)
+        self.assertEqual(optstore.get_value_for(name, sub2_name), top_value)
+        optstore.set_from_configure_command([], [], [f'{sub_name}:{name}'])
+        self.assertEqual(optstore.get_value_for(name), top_value)
+        self.assertEqual(optstore.get_value_for(name, sub_name), top_value)
+        self.assertEqual(optstore.get_value_for(name, sub2_name), top_value)
+
+        # And now augment the top level option
+        optstore.set_from_configure_command([], [f':{name}={aug_value}'], [])
+        self.assertEqual(optstore.get_value_for(name), aug_value)
+        self.assertEqual(optstore.get_value_for(name, sub_name), top_value)
+        self.assertEqual(optstore.get_value_for(name, sub2_name), top_value)
+        optstore.set_from_configure_command([], [], [f':{name}'])
+        self.assertEqual(optstore.get_value_for(name), top_value)
+        self.assertEqual(optstore.get_value_for(name, sub_name), top_value)
+        self.assertEqual(optstore.get_value_for(name, sub2_name), top_value)
