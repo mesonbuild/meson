4ad792e158b6059eb847dd0562aff9bd7029981f
Sam James <sam@gentoo.org>
2024-06-03 09:43:20 +0100

    compilers: detect: fix pre-processor scraping by defining language
    _get_gnu_compiler_defines and _get_clang_compiler_defines were broken
    by not defining the language they used.
    
    Neither GCC nor Clang infer the language based on the driver name which means
    `self.defines` isn't populated correctly in compilers/cpp.py.
    
    e.g.
    ```
     $ echo "" | g++ -E -dM - | grep -i cplus
    
     $ echo "" | g++ -x c++ -E -dM - | grep -i cplus
     #define __cplusplus 201703L
    ```
    
    Fix that by passing '-cpp -x LANGUAGE' as a first pass. If it fails, try
    again without '-cpp -x LANGUAGE' as before, as its portability isn't
    certain. We do '-cpp' because during testing, I found Fortran needs this,
    although per below, I had to drop Fortran in the end and leave it to the
    fallback (existing) path.
    
    Without this change, a63739d394dd77314270f5a46f79171a8c544e77 is only
    partially effective. It works if the system has injected Clang options
    via /etc/clang configuration files, but not by e.g. patching the driver
    (or for GCC there too).
    
    Unfortunately, we have to wimp out for Fortran and fallback to the
    old method because you need the language standard (e.g. -x f95).

--- mesonbuild/compilers/mixins/clang.py
+++ mesonbuild/compilers/mixins/clang.py
@@ -36,6 +36,13 @@ clang_optimization_args: T.Dict[str, T.List[str]] = {
     's': ['-Oz'],
 }
 
+_LANG_MAP = {
+    'c': 'c',
+    'cpp': 'c++',
+    'objc': 'objective-c',
+    'objcpp': 'objective-c++',
+}
+
 class ClangCompiler(GnuLikeCompiler):
 
     id = 'clang'
