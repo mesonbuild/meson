37c8dc7dd87a688030fca11a616543b7ee33cf2d
Florian "sp1rit"​ <sp1rit@disroot.org>
2024-07-11 11:55:44 +0200

    ciimage/arch: don't force java to be 1.8
    Parent introduced unit test (9 jni) that depends on at least java 9,
    thus causing the test to be skipped.

--- ci/ciimage/arch/install.sh
+++ ci/ciimage/arch/install.sh
@@ -11,7 +11,7 @@ pkgs=(
   ninja make git sudo fakeroot autoconf automake patch
   libelf gcc gcc-fortran gcc-objc vala rust byacc flex cython go dlang-dmd
   mono boost boost-libs qt5-base gtkmm3 gtest gmock protobuf gobject-introspection
-  itstool glib2-devel gtk3 java-environment=8 gtk-doc llvm clang sdl2 graphviz
+  itstool glib2-devel gtk3 java-environment gtk-doc llvm clang sdl2 graphviz
   doxygen vulkan-headers vulkan-icd-loader vulkan-validation-layers openssh mercurial gtk-sharp-3 qt5-tools
   libwmf cmake netcdf-fortran openmpi nasm gnustep-base gettext
   python-lxml hotdoc rust-bindgen qt6-base qt6-tools qt6-declarative wayland wayland-protocols
