67b238d6161d79674dc513cafed5236eb255dddd
Dylan Baker <dylan@pnwbakers.com>
2024-07-17 09:26:50 -0700

    unittests: fix git dist test on Windows with Python < 3.8
    pathlib.Path objects are not supported in subprocess calls on Windows
    before Python 3.8, so we must convert to a string.

--- test cases/unit/35 dist script/subprojects/sub/dist-script.py
+++ test cases/unit/35 dist script/subprojects/sub/dist-script.py
@@ -12,7 +12,7 @@ source_root = pathlib.Path(os.environ['MESON_PROJECT_DIST_ROOT'])
 mesonrewrite = shlex.split(os.environ['MESONREWRITE'])
 rewrite_cmd = ['kwargs', 'set', 'project', '/', 'version', 'release']
 
-subprocess.run([*mesonrewrite, '-s', source_root, *rewrite_cmd], check=True)
+subprocess.run([*mesonrewrite, '-s', str(source_root.absolute()), *rewrite_cmd], check=True)
 
 modfile = source_root / 'prog.c'
 with modfile.open('w') as f:
