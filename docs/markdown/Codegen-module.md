3d7d3533813c8cf416fd45cc5c7ed84f2b108cca
Dylan Baker <dylan@pnwbakers.com>
2024-07-23 09:40:12 -0700

    modules/codegen: Add wrapper for lex
    This module is a bit of a dumping ground for code generators,
    particularly ones that are important and non-trivial to wrap, either due
    to multiple implementations, major command line changes, or complex
    outputs (such as those that may output a directory structure).
    
    The initially provided method is for lex. It provides a simple wrapper
    that handles win_flex, reflex, flex, and generic lex.

---
+++ docs/markdown/Codegen-module.md
@@ -0,0 +1,64 @@
+---
+short-description: Common Code Generators Module
+authors:
+    - name: Dylan Baker
+      email: dylan@pnwbakers.com
+      years: [2024, 2025]
+...
+
+# Codegen Module
+
+*(New in 1.9.0)*
+
+This module provides wrappers around common code generators, such as flex/lex and bison/yacc. This purpose of this is to make it easier and more pleasant to use *common* code generators in a mesonic way.
+
+## Functions
+
+### lex()
+
+```meson
+lex_gen = codegen.lex(implementations : ['flex', 'reflex'], flex_version : ['>= 2.6', '< 3'], reflex_version : '!= 1.4.2')
+```
+
+This function provides fine grained control over what implementation(s) and version(s) of lex are acceptable for a given project (These are set per-subproject). It returns a [new object](#lexgenerator), which can be used to generate code.
+
+It accepts the following keyword arguments:
+
+ - `implementations`: a string array of acceptable implementations to use. May include: `lex`, `flex`, `reflex`, or `win_flex`.
+ - `lex_version`: a string array of version constraints to apply to the `lex` binary
+ - `flex_version`: a string array of version constraints to apply to the `flex` binary
+ - `reflex_version`: a string array of version constraints to apply to the `relex` binary
+ - `win_flex_version`: a string array of version constraints to apply to the `win_flex` binary
+
+
+## Returned Objects
+
+### LexGenerator
+
+#### generate
+
+```meson
+lex = codegen.lex()
+lex.generate('lexer.l')
+```
+
+This function wraps flex, lex, reflex (but not RE/flex), and win_flex (on Windows). When using win_flex it will automatically add the `--wincompat` argument.
+
+This requires an input file, which may be a string, File, or generated source. It additionally takes the following options keyword arguments:
+
+- `args`: An array of extra arguments to pass the lexer
+- `plainname`: If set to true then `@PLAINNAME@` will be used as the source base, otherwise `@BASENAME@`.
+- `source`: the name of the source output. If this is unset Meson will use `{base}.{ext}` with an extension of `cpp` if the input has an extension of `.ll`, or `c` otherwise, with base being determined by the `plainname` argument.
+- `header`: The optional output name for a header file. If this is unset no header is added
+- `table`: The optional output name for a table file. If this is unset no table will be generated
+
+The outputs will be in the form `source [header] [table]`, which means those can be accessed by indexing the output of the `lex` call:
+
+```meson
+lex = codegen.lex()
+l1 = lex.generate('lexer.l', header : '@BASENAME@.h', table : '@BASENAME@.tab.h')
+headers = [l1[1], l1[2]]  # [header, table]
+
+l2 = lex.generate('lexer.l', table : '@BASENAME@.tab.h')
+table = l2[1]
+```
---
+++ mesonbuild/modules/codegen.py
@@ -0,0 +1,242 @@
+# SPDX-License-Identifier: Apache-2.0
+# Copyright © 2024-2025 Intel Corporation
+
+from __future__ import annotations
+import dataclasses
+import os
+import typing as T
+
+from . import ExtensionModule, ModuleInfo
+from ..build import CustomTarget, CustomTargetIndex, GeneratedList
+from ..compilers.compilers import lang_suffixes
+from ..interpreter.type_checking import NoneType
+from ..interpreterbase import (
+    ContainerTypeInfo, ObjectHolder, KwargInfo, typed_pos_args, typed_kwargs,
+    noPosargs
+)
+from ..mesonlib import File, MesonException, Popen_safe
+from ..programs import ExternalProgram
+from ..utils.core import HoldableObject
+from .. import mlog
+
+if T.TYPE_CHECKING:
+    from typing_extensions import Literal, TypeAlias, TypedDict
+
+    from . import ModuleState
+    from ..build import Executable
+    from ..interpreter import Interpreter
+    from ..programs import OverrideProgram
+    from .._typing import ImmutableListProtocol
+
+    Program: TypeAlias = T.Union[Executable, ExternalProgram, OverrideProgram]
+    LexImpls = Literal['lex', 'flex', 'reflex', 'win_flex']
+
+    class LexGenerateKwargs(TypedDict):
+
+        args: T.List[str]
+        source: T.Optional[str]
+        header: T.Optional[str]
+        table: T.Optional[str]
+        plainname: bool
+
+    class FindLexKwargs(TypedDict):
+
+        lex_version: T.List[str]
+        flex_version: T.List[str]
+        reflex_version: T.List[str]
+        win_flex_version: T.List[str]
+        implementations: T.List[LexImpls]
+
+
+def is_subset_validator(choices: T.Set[str]) -> T.Callable[[T.List[str]], T.Optional[str]]:
+
+    def inner(check: T.List[str]) -> T.Optional[str]:
+        if not set(check).issubset(choices):
+            invalid = ', '.join(sorted(set(check).difference(choices)))
+            valid = ', '.join(sorted(choices))
+            return f"valid members are '{valid}', not '{invalid}'"
+        return None
+
+    return inner
+
+
+@dataclasses.dataclass
+class _CodeGenerator(HoldableObject):
+
+    program: Program
+    arguments: ImmutableListProtocol[str] = dataclasses.field(default_factory=list)
+
+    def command(self) -> T.List[T.Union[Program, str]]:
+        return (T.cast('T.List[T.Union[Program, str]]', [self.program]) +
+                T.cast('T.List[T.Union[Program, str]]', self.arguments))
+
+
+@dataclasses.dataclass
+class LexGenerator(_CodeGenerator):
+    pass
+
+
+class LexHolder(ObjectHolder[LexGenerator]):
+
+    def __init__(self, obj: LexGenerator, interpreter: Interpreter):
+        super().__init__(obj, interpreter)
+        self.methods.update({
+            'generate': self.generate_method,
+        })
+
+    @typed_pos_args('codegen.lex.generate', (str, File, GeneratedList, CustomTarget, CustomTargetIndex))
+    @typed_kwargs(
+        'codegen.lex.generate',
+        KwargInfo('args', ContainerTypeInfo(list, str), default=[], listify=True),
+        KwargInfo('source', (str, NoneType)),
+        KwargInfo('header', (str, NoneType)),
+        KwargInfo('table', (str, NoneType)),
+        KwargInfo('plainname', bool, default=False),
+    )
+    def generate_method(self, args: T.Tuple[T.Union[str, File, GeneratedList, CustomTarget, CustomTargetIndex]], kwargs: LexGenerateKwargs) -> CustomTarget:
+        input = self.interpreter.source_strings_to_files([args[0]])[0]
+        if isinstance(input, File):
+            is_cpp = input.endswith(".ll")
+            name = os.path.splitext(input.fname)[0]
+        else:
+            gen_input = input.get_outputs()
+            if len(gen_input) != 1:
+                raise MesonException('codegen.lex.generate: generated type inputs must have exactly one output, index into them to select the correct input')
+            is_cpp = gen_input[0].endswith('.ll')
+            name = os.path.splitext(gen_input[0])[0]
+        name = os.path.basename(name)
+
+        # If an explicit source was given, use that to determine whether the
+        # user expects this to be a C or C++ source.
+        if kwargs['source'] is not None:
+            ext = kwargs['source'].rsplit('.', 1)[1]
+            is_cpp = ext in lang_suffixes['cpp']
+
+        # Flex uses FlexLexer.h for C++ code
+        if is_cpp and self.held_object.program.name in {'flex', 'win_flex'}:
+            try:
+                found = self.interpreter.environment.coredata.compilers.host['cpp'].has_header('FlexLexer.h', '', self.interpreter.environment)
+                if not found[0]:
+                    raise MesonException('Could not find FlexLexer.h, which is required for Flex with C++')
+            except KeyError:
+                raise MesonException("Could not find a C++ compiler to search for FlexLexer.h")
+
+        if kwargs['source'] is None:
+            outputs = ['@{}@.{}'.format(
+                'PLAINNAME' if kwargs['plainname'] else 'BASENAME',
+                'cpp' if is_cpp else 'c')]
+        else:
+            outputs = [kwargs['source']]
+
+        command = self.held_object.command()
+        if kwargs['header'] is not None:
+            outputs.append(kwargs['header'])
+            command.append(f'--header-file=@OUTPUT{len(outputs) - 1}@')
+        if kwargs['table'] is not None:
+            outputs.append(kwargs['table'])
+            command.append(f'--tables-file=@OUTPUT{len(outputs) - 1}@')
+        command.extend(kwargs['args'])
+        # Flex, at least, seems to require that input be the last argument given
+        command.append('@INPUT@')
+
+        target = CustomTarget(
+            f'codegen-lex-{name}',
+            self.interpreter.subdir,
+            self.interpreter.subproject,
+            self.interpreter.environment,
+            command,
+            [input],
+            outputs,
+            backend=self.interpreter.backend,
+            description=f'Generating lexer {{}} with {self.held_object.program.name}',
+        )
+        self.interpreter.add_target(target.name, target)
+
+        return target
+
+
+class CodeGenModule(ExtensionModule):
+
+    """Module with helpers for codegen wrappers."""
+
+    INFO = ModuleInfo('codegen', '1.9.0', unstable=True)
+
+    def __init__(self, interpreter: Interpreter) -> None:
+        super().__init__(interpreter)
+        self.methods.update({
+            'lex': self.lex_method,
+        })
+
+    def __find_lex(self, state: ModuleState,
+                   lex_version: T.Optional[T.List[str]] = None,
+                   flex_version: T.Optional[T.List[str]] = None,
+                   reflex_version: T.Optional[T.List[str]] = None,
+                   win_flex_version: T.Optional[T.List[str]] = None,
+                   implementations: T.Optional[T.List[LexImpls]] = None) -> LexGenerator:
+        names: T.List[LexImpls] = []
+        if implementations:
+            names = implementations
+        else:
+            assert state.environment.machines.host is not None, 'for mypy'
+            if state.environment.machines.host.system == 'windows':
+                names.append('win_flex')
+            names.extend(['flex', 'reflex', 'lex'])
+
+        versions: T.Mapping[str, T.List[str]] = {
+            'lex': lex_version or [],
+            'flex': flex_version or [],
+            'reflex': reflex_version or [],
+            'win_flex': win_flex_version or [],
+        }
+
+        for name in names:
+            bin = state.find_program(name, wanted=versions[name], required=name == names[-1])
+            if bin.found():
+                # If you're building reflex as a subproject, we consider that you
+                # know what you're doing.
+                if name == 'reflex' and isinstance(bin, ExternalProgram):
+                    # there are potentially 3 programs called "reflex":
+                    # 1. https://invisible-island.net/reflex/, an alternate fork
+                    #    of the original flex, this is supported
+                    # 2. https://www.genivia.com/doc/reflex/html/, an
+                    #    alternative implementation for generating C++ scanners.
+                    #    Not supported
+                    # 3. https://github.com/cespare/reflex, which is not a lex
+                    #    implementation at all, but a file watcher
+                    _, out, err = Popen_safe(bin.get_command() + ['--version'])
+                    if 'unknown flag: --version' in err:
+                        mlog.debug('Skipping cespare/reflex, which is not a lexer and is not supported')
+                        continue
+                    if 'Written by Robert van Engelen' in out:
+                        mlog.debug('Skipping RE/flex, which is not compatible with POSIX lex.')
+                        continue
+                break
+
+        args: T.List[str] = []
+        if bin.name == 'win_flex':
+            args.append('--wincompat')
+        args.extend(['-o', '@OUTPUT0@'])
+        return LexGenerator(bin, T.cast('ImmutableListProtocol[str]', args))
+
+    @noPosargs
+    @typed_kwargs(
+        'codegen.lex',
+        KwargInfo('lex_version', ContainerTypeInfo(list, str), default=[], listify=True),
+        KwargInfo('flex_version', ContainerTypeInfo(list, str), default=[], listify=True),
+        KwargInfo('reflex_version', ContainerTypeInfo(list, str), default=[], listify=True),
+        KwargInfo('win_flex_version', ContainerTypeInfo(list, str), default=[], listify=True),
+        KwargInfo(
+            'implementations',
+            ContainerTypeInfo(list, str),
+            default=[],
+            listify=True,
+            validator=is_subset_validator({'lex', 'flex', 'reflex', 'win_flex'})
+        ),
+    )
+    def lex_method(self, state: ModuleState, args: T.Tuple, kwargs: FindLexKwargs) -> LexGenerator:
+        return self.__find_lex(state, **kwargs)
+
+
+def initialize(interpreter: Interpreter) -> CodeGenModule:
+    interpreter.append_holder_map(LexGenerator, LexHolder)
+    return CodeGenModule(interpreter)
