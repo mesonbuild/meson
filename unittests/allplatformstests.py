80530553a93b89662f2cb810554dca10786e08d4
Dylan Baker <dylan@pnwbakers.com>
2024-09-12 09:12:44 -0700

    unit tests: make the test_c_cpp_stds test more robust
    Check clang-cl as well as cl, and clang as well as gcc.

--- unittests/allplatformstests.py
+++ unittests/allplatformstests.py
@@ -4999,7 +4999,7 @@ class AllPlatformTests(BasePlatformTests):
             olddata = newdata
             oldmtime = newmtime
 
-    def test_c_cpp_stds(self):
+    def test_c_cpp_stds(self) -> None:
         testdir = os.path.join(self.unit_test_dir, '115 c cpp stds')
         self.init(testdir)
         # Invalid values should fail whatever compiler we have
@@ -5011,7 +5011,7 @@ class AllPlatformTests(BasePlatformTests):
             self.setconf('-Dc_std=c++11')
         env = get_fake_env()
         cc = detect_c_compiler(env, MachineChoice.HOST)
-        if cc.get_id() == 'msvc':
+        if cc.get_id() in {'msvc', 'clang-cl'}:
             # default_option should have selected those
             self.assertEqual(self.getconf('c_std'), 'c89')
             self.assertEqual(self.getconf('cpp_std'), 'vc++11')
@@ -5024,7 +5024,7 @@ class AllPlatformTests(BasePlatformTests):
             # The first supported std should be selected
             self.setconf('-Dcpp_std=gnu++11,vc++11,c++11')
             self.assertEqual(self.getconf('cpp_std'), 'vc++11')
-        elif cc.get_id() == 'gcc':
+        elif cc.get_id() in {'gcc', 'clang'}:
             # default_option should have selected those
             self.assertEqual(self.getconf('c_std'), 'gnu89')
             self.assertEqual(self.getconf('cpp_std'), 'gnu++98')
