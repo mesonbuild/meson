efa85c5b645624e17ac733e209bda4d118ad8cfb
Dylan Baker <dylan@pnwbakers.com>
2024-10-01 09:58:16 -0700

    minstall: fix bug link

--- mesonbuild/minstall.py
+++ mesonbuild/minstall.py
@@ -152,7 +152,7 @@ def set_chown(path: str, user: T.Union[str, int, None] = None,
 
     if sys.version_info >= (3, 13):
         # pylint: disable=unexpected-keyword-arg
-        # cannot handle sys.version_info, https://github.com/pylint-dev/pylint/issues/9138
+        # cannot handle sys.version_info, https://github.com/pylint-dev/pylint/issues/9622
         shutil.chown(path, user, group, dir_fd=dir_fd, follow_symlinks=follow_symlinks)
     else:
         real_os_chown = os.chown
