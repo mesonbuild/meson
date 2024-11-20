e720e7ce65bf36e684c942dd549478186020eb6f
Paolo Bonzini <pbonzini@redhat.com>
2024-11-20 14:42:29 +0100

    ninjabackend: generate command line for rust doctests

--- mesonbuild/backend/ninjabackend.py
+++ mesonbuild/backend/ninjabackend.py
@@ -1963,14 +1963,14 @@ class NinjaBackend(backends.Backend):
 
         return orderdeps, main_rust_file
 
-    def get_rust_compiler_args(self, target: build.BuildTarget, rustc: Compiler,
+    def get_rust_compiler_args(self, target: build.BuildTarget, rustc: Compiler, src_crate_type: str,
                                depfile: T.Optional[str] = None) -> T.List[str]:
         base_proxy = target.get_options()
         # Compiler args for compiling this target
         args = compilers.get_base_compile_args(base_proxy, rustc, self.environment)
 
         target_name = self.get_target_filename(target)
-        args.extend(['--crate-type', target.rust_crate_type])
+        args.extend(['--crate-type', src_crate_type])
 
         # If we're dynamically linking, add those arguments
         if target.rust_crate_type in {'bin', 'dylib'}:
@@ -2125,7 +2125,7 @@ class NinjaBackend(backends.Backend):
         args = rustc.compiler_args()
 
         depfile = os.path.join(self.get_target_private_dir(target), target.name + '.d')
-        args += self.get_rust_compiler_args(target, rustc, depfile)
+        args += self.get_rust_compiler_args(target, rustc, target.rust_crate_type, depfile)
 
         deps, project_deps, deps_args = self.get_rust_compiler_deps_and_args(target, rustc)
         args += deps_args
@@ -2154,6 +2154,17 @@ class NinjaBackend(backends.Backend):
         if isinstance(target, build.SharedLibrary):
             self.generate_shsym(target)
         self.create_target_source_introspection(target, rustc, args, [main_rust_file], [])
+
+        if target.doctests:
+            from ..compilers.rust import RustCompiler
+            assert isinstance(rustc, RustCompiler)
+            assert target.doctests.target is not None
+            rustdoc = rustc.get_rustdoc(self.environment)
+            args = rustdoc.get_exe_args()
+            args += self.get_rust_compiler_args(target.doctests.target, rustdoc, target.rust_crate_type)
+            _, _, deps_args = self.get_rust_compiler_deps_and_args(target.doctests.target, rustdoc)
+            args += deps_args
+            target.doctests.cmd_args[0:0] = args.to_native() + [main_rust_file]
 
     @staticmethod
     def get_rule_suffix(for_machine: MachineChoice) -> str:
