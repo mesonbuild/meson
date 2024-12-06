796c0a4e8f85f5ea9bd6983976ed670072d7fbfc
Dylan Baker <dylan@pnwbakers.com>
2024-12-06 12:17:26 -0800

    interpreter: port cmake_args to typed_kwargs

--- mesonbuild/interpreter/kwargs.py
+++ mesonbuild/interpreter/kwargs.py
@@ -488,3 +488,4 @@ class FuncDependency(TypedDict):
 
     default_options: T.Dict[OptionKey, T.Union[str, int, bool, T.List[str]]]
     allow_fallback: T.Optional[bool]
+    cmake_args: T.List[str]
