67a71ddb37a7b24576c6ad36b72d8cafe95132d5
Dylan Baker <dylan@pnwbakers.com>
2024-12-06 13:01:51 -0800

    interpreter: port dependency static to typed_kwargs
    Static must be optional, because although the default is static=false,
    we special case that static : false is different than static being not
    set in a number of cases.

--- mesonbuild/interpreter/type_checking.py
+++ mesonbuild/interpreter/type_checking.py
@@ -872,6 +872,7 @@ DEPENDENCY_KWS: T.List[KwargInfo] = [
     KwargInfo('not_found_message', str, default='', since='0.50.0'),
     KwargInfo('optional_modules', ContainerTypeInfo(list, str), listify=True, default=[]),
     KwargInfo('private_headers', bool, default=False),
+    KwargInfo('static', (bool, NoneType)),
     NATIVE_KW,
     REQUIRED_KW,
 ]
