e44f05e9f62425c93b9eb6a6f3034f62793c1d6f
solomoncyj <solomoncyj@hotmail.com>
2024-12-12 00:05:47 +0800

    Update buildreq.py

--- data/buildreq.py
+++ data/buildreq.py
@@ -1,34 +1,35 @@
-    import subprocess 
-    import json 
-    deps_json = \
-        json.loads(subprocess.run([ \
-            "%{__meson}", \
-            "introspect", \
-            "--dependencies", \
-            "meson.build"], \
-        capture_output=True).stdout \
-    deps = dict(zip( \
-        [x['name'] for x in deps_json], \
-        [x['version'] for x in deps_json])) 
-    deps.pop('', None) 
-    for lib, versions in deps.items() : 
-            # Mapping for special cases 
-        pkg_map = { 
-            'qt6': 'qmake',
-            'KF6WindowSystem': 'cmake',
-            'gtk+-3.0': 'pkgconfig',
-            'dbus-1': 'pkgconfig',
-            'ncursesw': 'pkgconfig',
-            'harfbuzz': 'pkgconfig',
-            'fribidi': 'pkgconfig',
-            # Add more mappings as needed
-        }
+import subprocess 
+import json 
+import sys
+deps_json = \
+    json.loads(subprocess.run([ \
+        sys.argv[0], \
+        "introspect", \
+        "--dependencies", \
+        "meson.build"], \
+    capture_output=True).stdout \
+deps = dict(zip( \
+    [x['name'] for x in deps_json], \
+    [x['version'] for x in deps_json])) 
+deps.pop('', None) 
+for lib, versions in deps.items() : 
+     # Mapping for special cases 
+    pkg_map = { 
+        'qt6': 'qmake',
+        'KF6WindowSystem': 'cmake',
+        'gtk+-3.0': 'pkgconfig',
+        'dbus-1': 'pkgconfig',
+        'ncursesw': 'pkgconfig',
+        'harfbuzz': 'pkgconfig',
+        'fribidi': 'pkgconfig',
+        # Add more mappings as needed
+    }
         
-        # Determine the prefix
-        prefix = pkg_map.get(lib, 'pkgconfig')
+    # Determine the prefix
+    prefix = pkg_map.get(lib, 'pkgconfig')
         
-        # Prepare version constraint
-        version_str = ' ' + ' '.join(versions) if versions else ''
+     # Prepare version constraint
+    version_str = ' ' + ' '.join(versions) if versions else ''
         
-        # Generate BuildRequires line
-        print(f"BuildRequires: {prefix}({lib}){version_str}") 
+    # Generate BuildRequires line
+    print(f"BuildRequires: {prefix}({lib}){version_str}") 
