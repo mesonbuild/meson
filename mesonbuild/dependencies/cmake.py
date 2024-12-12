0727c05ff268c0614ebd6cc6f0d037c50dc346c1
Dylan Baker <dylan@pnwbakers.com>
2024-12-12 15:32:55 -0800

    dependencies: make cmake_args type safe

--- mesonbuild/dependencies/cmake.py
+++ mesonbuild/dependencies/cmake.py
@@ -116,7 +116,7 @@ class CMakeDependency(ExternalDependency):
         # Setup the trace parser
         self.traceparser = CMakeTraceParser(self.cmakebin.version(), self._get_build_dir(), self.env)
 
-        cm_args = stringlistify(extract_as_list(kwargs, 'cmake_args'))
+        cm_args = T.cast('T.List[str]', kwargs.get('cmake_args', []))
         cm_args = check_cmake_args(cm_args)
         if CMakeDependency.class_cmakeinfo[self.for_machine] is None:
             CMakeDependency.class_cmakeinfo[self.for_machine] = self._get_cmake_info(cm_args)
