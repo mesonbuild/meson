69c69bf3b2735d63c9e4dcf7eef13dffafb396e2
Nick <0xb000@gmail.com>
2024-12-17 01:10:06 +0200

    Fix lint nit: not x in y -> x not in y

--- mesonbuild/wrap/wrap.py
+++ mesonbuild/wrap/wrap.py
@@ -100,7 +100,7 @@ def expand_wrapdburl(urlstr: str, allow_insecure: bool = False) -> urllib.parse.
     else:
         if not is_wrapdb_subdomain(url.hostname):
             raise WrapException(f'{urlstr} is not a whitelisted WrapDB URL')
-        if has_ssl and not allow_insecure and not url.scheme in {'https', 'ftps'}:
+        if has_ssl and not allow_insecure and url.scheme not in {'https', 'ftps'}:
             raise WrapException(f'WrapDB did not have expected SSL url, instead got {urllib.parse.urlunparse(url)}')
     return url
 
