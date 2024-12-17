e9e582020898e52d17b4c48afe65589bc1dee065
Rolf Eike Beer <eb@emlix.com>
2024-12-17 14:41:17 +0100

    print out which command needs an exe_wrapper
    It's not the first time I run into an issue with an intentionally missing
    exe_wrapper during cross compilation. In pretty much all the cases the project
    I tried to build already had code available to not need one in the first place.
    Print out what command was actually the culprit to make debugging this easier.

--- mesonbuild/backend/backends.py
+++ mesonbuild/backend/backends.py
@@ -574,7 +574,7 @@ class Backend:
         is_cross_built = not self.environment.machines.matches_build_machine(exe_for_machine)
         if is_cross_built and self.environment.need_exe_wrapper():
             if not self.environment.has_exe_wrapper():
-                msg = 'An exe_wrapper is needed but was not found. Please define one ' \
+                msg = 'An exe_wrapper is needed for ' + exe_cmd[0] + ' but was not found. Please define one ' \
                       'in cross file and check the command and/or add it to PATH.'
                 raise MesonException(msg)
             exe_wrapper = self.environment.get_exe_wrapper()
