d197fef5ee0907e5ed16d02d21bcef26cc8f4b74
Katalin Rebhan <me@dblsaiko.net>
2025-01-02 21:35:49 +0100

    Add MachineInfo.is_macos() function

--- mesonbuild/envconfig.py
+++ mesonbuild/envconfig.py
@@ -338,6 +338,12 @@ class MachineInfo(HoldableObject):
         """
         return self.system in {'darwin', 'ios', 'tvos', 'visionos', 'watchos'}
 
+    def is_macos(self) -> bool:
+        """
+        Machine is macOS?
+        """
+        return self.system == 'darwin' and self.subsystem == 'macos'
+
     def is_android(self) -> bool:
         """
         Machine is Android?
