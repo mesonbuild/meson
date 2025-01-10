84b1762c8450aa336dcb7a38e15722ac50d168fe
Dylan Baker <dylan@pnwbakers.com>
2025-01-10 11:41:27 -0800

    build: use a TypedDict for SharedModule kwargs

--- mesonbuild/build.py
+++ mesonbuild/build.py
@@ -49,6 +49,7 @@ if T.TYPE_CHECKING:
     from .mesonlib import ExecutableSerialisation, FileMode, FileOrString
     from .modules import ModuleState
     from .mparser import BaseNode
+    from .interpreter.kwargs import RustAbi
 
     GeneratedTypes = T.Union['CustomTarget', 'CustomTargetIndex', 'GeneratedList']
     LibTypes = T.Union['SharedLibrary', 'StaticLibrary', 'CustomTarget', 'CustomTargetIndex']
@@ -114,6 +115,11 @@ if T.TYPE_CHECKING:
         export_dynamic: bool
         pie: bool
         vs_module_defs: T.Union[str, File, CustomTarget, CustomTargetIndex]
+
+    class SharedModuleKeywordArguments(BuildTargetKeywordArguments, total=False):
+
+        vs_module_defs: T.Union[str, File, CustomTarget, CustomTargetIndex]
+        rust_abi: T.Optional[RustAbi]
 
 DEFAULT_STATIC_LIBRARY_NAMES: T.Mapping[str, T.Tuple[str, str]] = {
     'unix': ('lib', 'a'),
