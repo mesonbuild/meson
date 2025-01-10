902fb8efa2f30027f972779b31ef1a974ad19606
Dylan Baker <dylan@pnwbakers.com>
2025-01-10 14:27:39 -0800

    interpreter: convert BuildTarget install to bool in KwargInfo
    This just does what would happen later, but with the potential to fail
    in obscure way.

--- mesonbuild/interpreter/type_checking.py
+++ mesonbuild/interpreter/type_checking.py
@@ -578,16 +578,20 @@ def _target_install_feature_validator(val: object) -> T.Iterable[FeatureCheckBas
                             'This was never intended to work, and is essentially the same as using `install: true` regardless of value.')
 
 
+def _target_install_convertor(val: object) -> bool:
+    return bool(val)
+
+
 # Applies to all build_target like classes
 _ALL_TARGET_KWS: T.List[KwargInfo] = [
     OVERRIDE_OPTIONS_KW,
     KwargInfo('build_by_default', bool, default=True, since='0.38.0'),
     KwargInfo('extra_files', ContainerTypeInfo(list, (str, File)), default=[], listify=True),
-    # Accursed. We allow this for backwards compat and warn in the interpreter.
     KwargInfo(
         'install',
         object,
         default=False,
+        convertor=_target_install_convertor,
         feature_validator=_target_install_feature_validator,
     ),
     INSTALL_MODE_KW,
