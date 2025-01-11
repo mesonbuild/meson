09327177ed5c7487503c4da9b9092142d6bdc7dc
Dylan Baker <dylan@pnwbakers.com>
2025-01-10 16:04:50 -0800

    build: remove validation for BuildTarget(build_by_default: )
    I've left the workaround for the install/build_by_default interaction
    for later, that's a more complicated issue to prove, since the
    Interpreter does that automatically, so we need to audit non-Interpreter
    interactions

--- mesonbuild/build.py
+++ mesonbuild/build.py
@@ -715,8 +715,6 @@ class Target(HoldableObject, metaclass=abc.ABCMeta):
     def process_kwargs_base(self, kwargs: BuildTargetKeywordArguments) -> None:
         if 'build_by_default' in kwargs:
             self.build_by_default = kwargs['build_by_default']
-            if not isinstance(self.build_by_default, bool):
-                raise InvalidArguments('build_by_default must be a boolean value.')
 
         if not self.build_by_default and kwargs.get('install', False):
             # For backward compatibility, if build_by_default is not explicitly
