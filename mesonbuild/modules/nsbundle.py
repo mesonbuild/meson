b378908b6f68f60b29b7080bf65dc3ff3c1ce24c
Katalin Rebhan <me@dblsaiko.net>
2025-01-11 22:56:16 +0100

    Construct bundle targets via nsbundle module

--- mesonbuild/modules/nsbundle.py
+++ mesonbuild/modules/nsbundle.py
@@ -5,10 +5,15 @@ from __future__ import annotations
 
 import typing as T
 
-from . import NewExtensionModule, ModuleInfo
+from . import NewExtensionModule, ModuleInfo, ModuleReturnValue
+from mesonbuild.build import AppBundle, FrameworkBundle
+from mesonbuild.interpreter.type_checking import NSAPP_KWS, NSFRAMEWORK_KWS, SOURCES_VARARGS
+from mesonbuild.interpreterbase.decorators import FeatureNew, typed_kwargs, typed_pos_args
 
 if T.TYPE_CHECKING:
-    from mesonbuild.interpreter import Interpreter
+    from . import ModuleState
+    from mesonbuild.interpreter.type_checking import SourcesVarargsType
+    from mesonbuild.interpreter import Interpreter, kwargs as kwtypes
 
 
 def initialize(*args: T.Any, **kwargs: T.Any) -> NSBundleModule:
@@ -21,4 +26,22 @@ class NSBundleModule(NewExtensionModule):
     def __init__(self, interpreter: Interpreter):
         super().__init__()
         self.methods.update({
+            'application': self.application,
+            'framework': self.framework,
         })
+
+    @FeatureNew('nsbundle.application', '1.7.99')
+    @typed_pos_args('nsbundle.application', (str,), varargs=SOURCES_VARARGS)
+    @typed_kwargs('nsbundle.application', *NSAPP_KWS, allow_unknown=True)
+    def application(self, state: ModuleState, args: T.Tuple[str, SourcesVarargsType], kwargs: kwtypes.AppBundle
+                    ) -> ModuleReturnValue:
+        tgt = state.create_build_target(AppBundle, args, kwargs)
+        return ModuleReturnValue(tgt, [tgt])
+
+    @FeatureNew('nsbundle.framework', '1.7.99')
+    @typed_pos_args('nsbundle.framework', (str,), varargs=SOURCES_VARARGS)
+    @typed_kwargs('nsbundle.framework', *NSFRAMEWORK_KWS, allow_unknown=True)
+    def framework(self, state: ModuleState, args: T.Tuple[str, SourcesVarargsType], kwargs: kwtypes.FrameworkBundle
+                  ) -> ModuleReturnValue:
+        tgt = state.create_build_target(FrameworkBundle, args, kwargs)
+        return ModuleReturnValue(tgt, [tgt])
