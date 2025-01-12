d5f29e68c3472814e5574c343f49f3f6c6e405e1
Marco Rebhan <me@dblsaiko.net>
2025-01-12 20:56:35 +0100

    Allow targets to declare outputs as directories

--- mesonbuild/build.py
+++ mesonbuild/build.py
@@ -685,6 +685,9 @@ class Target(HoldableObject, metaclass=abc.ABCMeta):
     def get_outputs(self) -> T.List[str]:
         return []
 
+    def can_output_be_directory(self, output: str) -> bool:
+        return False
+
     def should_install(self) -> bool:
         return False
 
@@ -2759,6 +2762,9 @@ class CustomTarget(Target, CustomTargetBase, CommandBase):
 
     def get_outputs(self) -> T.List[str]:
         return self.outputs
+
+    def can_output_be_directory(self, output: str) -> bool:
+        return True
 
     def get_filename(self) -> str:
         return self.outputs[0]
