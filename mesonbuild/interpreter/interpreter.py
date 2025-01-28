mesonbuild/meson

commit d51e20288853f376e9244671604176913f2e4e79

fix edge case in testcase expect_error() if error is printed using mlog
It catches the exception message itself, but for multi-line exceptions
it may be worth print an error() as well as raising, to communicate
multiple bits of information.

When using the VS backend, this means that we get an actual `ERROR: ...`
printed during a successful run, which then breaks msbuild as msbuild
parses stdout of successful commands, regexes them for the word "error:"
and interprets that as... an error. So a meson project tests example
that uses testcase expect_error() and then successfully configures and
builds, fails to successfully `meson --internal regenerate`.

Sneak around this by doing our own pattern replace to evade msbuild.
There is probably a way to tell msbuild to stop doing this, but that
would require me understanding the vs backend well enough to patch the
xml it generates. No thanks...

mesonbuild/interpreter/interpreter.py
@@ -4,8 +4,7 @@
 
 from __future__ import annotations
 
-import hashlib
-import traceback
+import hashlib, io, sys, traceback
 
 from .. import mparser
 from .. import environment
@@ -1474,10 +1473,18 @@ class Interpreter(InterpreterBase, HoldableObject):
         class ExpectErrorObject(ContextManagerObject):
             def __init__(self, msg: str, how: str, subproject: str) -> None:
                 super().__init__(subproject)
+                self.old_stdout = sys.stdout
+                sys.stdout = self.new_stdout = io.StringIO()
                 self.msg = msg
                 self.how = how
 
             def __exit__(self, exc_type, exc_val, exc_tb):
+                sys.stdout = self.old_stdout
+                for l in self.new_stdout.getvalue().splitlines():
+                    if 'ERROR:' in l:
+                        print(l.replace('ERROR', 'ERROR (msbuild proof)'))
+                    else:
+                        print(l)
                 if exc_val is None:
                     raise InterpreterException('Expecting an error but code block succeeded')
                 if isinstance(exc_val, mesonlib.MesonException):

commit 0b505fcab826cc372e2c8b368f68ff366c1bab84

testcase expect_error(): add missing line from rebase
It was supposed to preserve the color for debugging meson project tests!

Fixes: d51e20288853f376e9244671604176913f2e4e79

mesonbuild/interpreter/interpreter.py
@@ -1481,6 +1481,7 @@ class Interpreter(InterpreterBase, HoldableObject):
                 super().__init__(subproject)
                 self.old_stdout = sys.stdout
                 sys.stdout = self.new_stdout = io.StringIO()
+                sys.stdout.colorize_console = getattr(self.old_stdout, 'colorize_console', None)
                 self.msg = msg
                 self.how = how
 