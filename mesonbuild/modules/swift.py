7050f6fa8d9845c6d222719d715e49aa8636894a
Marco Rebhan <me@dblsaiko.net>
2025-02-11 18:12:03 +0100

    Add swift.generate_cpp_header

--- mesonbuild/modules/swift.py
+++ mesonbuild/modules/swift.py
@@ -5,9 +5,13 @@ from __future__ import annotations
 
 import typing as T
 
-from . import NewExtensionModule, ModuleInfo
+from . import NewExtensionModule, ModuleInfo, ModuleReturnValue
+from mesonbuild import build
+from mesonbuild.compilers.swift import SwiftCompiler
+from mesonbuild.interpreterbase.decorators import typed_kwargs, typed_pos_args
 
 if T.TYPE_CHECKING:
+    from . import ModuleState
     from mesonbuild.interpreter import Interpreter
 
 
@@ -20,5 +24,36 @@ class SwiftModule(NewExtensionModule):
 
     def __init__(self, interpreter: Interpreter):
         super().__init__()
+        self.interpreter = interpreter
+        self.swiftc: T.Optional[SwiftCompiler]
+
+        try:
+            swiftc = interpreter.compilers.host['swift']
+            assert isinstance(swiftc, SwiftCompiler), 'swiftc is not an instance of SwiftCompiler'
+            self.swiftc = swiftc
+        except KeyError:
+            self.swiftc = None
+
         self.methods.update({
+            'generate_cpp_header': self.generate_cpp_header,
         })
+
+    @typed_pos_args('swift.generate_cpp_header', build.BuildTarget)
+    @typed_kwargs('swift.generate_cpp_header')
+    def generate_cpp_header(self, state: ModuleState, args: T.Tuple[build.BuildTarget], kwargs) -> ModuleReturnValue:
+        (module,) = args
+
+        header_name = f'{module.name}-Swift.h'
+        command = [
+            *self.swiftc.get_exelist(),
+            '@INPUT@',
+            *self.swiftc.get_module_args(module.name),
+            *self.swiftc.get_cxx_interoperability_args(self.interpreter.compilers.host),
+            '-emit-clang-header-path', '@OUTPUT@',
+        ]
+
+        tgt = build.CustomTarget(header_name, state.subdir, state.subproject, state.environment, command,
+                                 module.get_sources() + module.get_generated_sources(), [header_name],
+                                 backend=state.backend)
+
+        return ModuleReturnValue(tgt, [tgt])
