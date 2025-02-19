6658799b0fa6dd50dd1bdb782a38214d76732002
Katalin Rebhan <me@dblsaiko.net>
2025-02-19 01:59:36 +0100

    Fix crash linking Swift target with custom target

--- mesonbuild/backend/ninjabackend.py
+++ mesonbuild/backend/ninjabackend.py
@@ -2287,11 +2287,11 @@ class NinjaBackend(backends.Backend):
     def compiler_to_pch_rule_name(cls, compiler: Compiler) -> str:
         return cls.get_compiler_rule_name(compiler.get_language(), compiler.for_machine, 'PCH')
 
-    def swift_module_file_name(self, target) -> str:
+    def swift_module_file_name(self, target: build.BuildTarget) -> str:
         return os.path.join(self.get_target_private_dir(target),
                             target.swift_module_name + '.swiftmodule')
 
-    def determine_swift_dep_modules(self, target) -> T.List[str]:
+    def determine_swift_dep_modules(self, target: build.BuildTarget) -> T.List[str]:
         result = []
         for l in target.link_targets:
             if self.is_swift_target(l):
