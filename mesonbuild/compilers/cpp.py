a42a274bf641cb1ac230ca3626087461d1bd6f0d
Marco Rebhan <me@dblsaiko.net>
2025-02-19 04:59:52 +0100

    Verify that C++ can import Swift headers

--- mesonbuild/compilers/cpp.py
+++ mesonbuild/compilers/cpp.py
@@ -9,7 +9,7 @@ import typing as T
 
 from .. import options
 from .. import mlog
-from ..mesonlib import MesonException, version_compare
+from ..mesonlib import MesonBugException, MesonException, version_compare
 
 from .compilers import (
     gnu_winlibs,
@@ -70,6 +70,8 @@ class CPPCompiler(CLikeCompiler, Compiler):
                  info: 'MachineInfo',
                  linker: T.Optional['DynamicLinker'] = None,
                  full_version: T.Optional[str] = None):
+        self._works_with_swift: T.Optional[bool] = None
+
         # If a child ObjCPP class has already set it, don't set it ourselves
         Compiler.__init__(self, ccache, exelist, version, for_machine, info,
                           is_cross=is_cross, linker=linker,
@@ -89,6 +91,12 @@ class CPPCompiler(CLikeCompiler, Compiler):
     def sanity_check(self, work_dir: str, environment: 'Environment') -> None:
         code = 'class breakCCompiler;int main(void) { return 0; }\n'
         return self._sanity_check_impl(work_dir, environment, 'sanitycheckcpp.cc', code)
+
+    def works_with_swift(self) -> bool:
+        if self._works_with_swift is None:
+            raise MesonBugException('Called CPPCompiler.works_with_swift() but compatibility was never checked')
+
+        return self._works_with_swift
 
     def get_compiler_check_args(self, mode: CompileCheckMode) -> T.List[str]:
         # -fpermissive allows non-conforming code to compile which is necessary
