f4fb89449eb3b884b3eaa65269f5d28a40ef3e9f
Sam James <sam@gentoo.org>
2025-03-12 04:40:34 +0000

    compilers: gnu: clean up get_lto_link_args
    ... by calling self.get_lto_compile_args, as we expect the same arguments
    to be passed at both compile-time and link-time, and we're doing the exact
    same procedure to determine -flto=XXX.
    
    Bug: https://github.com/mesonbuild/meson/issues/9536

--- mesonbuild/compilers/mixins/gnu.py
+++ mesonbuild/compilers/mixins/gnu.py
@@ -639,17 +639,7 @@ class GnuCompiler(GnuLikeCompiler):
     def get_lto_link_args(self, *, threads: int = 0, mode: str = 'default',
                           thinlto_cache_dir: T.Optional[str] = None) -> T.List[str]:
         args: T.List[str] = []
-        if threads == 0:
-            if self._has_lto_auto_support:
-                args.append('-flto=auto')
-            else:
-                # This matches gcc's behavior of using the number of cpus, but
-                # obeying meson's MESON_NUM_PROCESSES convention.
-                args.append(f'-flto={mesonlib.determine_worker_count()}')
-        elif threads > 0:
-            args.append(f'-flto={threads}')
-        else:
-            args.extend(super().get_lto_compile_args(threads=threads))
+        args.extend(self.get_lto_compile_args(threads=threads))
         return args
 
     def get_profile_use_args(self) -> T.List[str]:
