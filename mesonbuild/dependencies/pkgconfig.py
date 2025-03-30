f84c7a086af71b805e7355a45ee266cfa305f311
Paul Caprioli <paul@hpkfft.com>
2025-03-29 17:11:36 -0700

    dependencies/pkgconfig: Canonicalize libpaths
    Prefix library paths specified by -L are converted to canonical paths
    to improve the library runpath stored in the binary output.
    For example, if `pkg-config --libs add` returns the following:
    
        -L/usr/local/lib/pkgconfig/../../lib -ladd
    
    we convert the path so that it is as if it had returned:
    
        -L/usr/local/lib -ladd

--- mesonbuild/dependencies/pkgconfig.py
+++ mesonbuild/dependencies/pkgconfig.py
@@ -1,5 +1,5 @@
 # SPDX-License-Identifier: Apache-2.0
-# Copyright 2013-2021 The Meson development team
+# Copyright 2013-2025 The Meson development team
 
 from __future__ import annotations
 
@@ -423,11 +423,11 @@ class PkgConfigDependency(ExternalDependency):
                 if not path_has_root(path):
                     # Resolve the path as a compiler in the build directory would
                     path = os.path.join(self.env.get_build_dir(), path)
-                prefix_libpaths.add(path)
+                prefix_libpaths.add(Path(path).resolve().as_posix())
         # Library paths are not always ordered in a meaningful way
         #
         # Instead of relying on pkg-config or pkgconf to provide -L flags in a
-        # specific order, we reorder library paths ourselves, according to th
+        # specific order, we reorder library paths ourselves, according to the
         # order specified in PKG_CONFIG_PATH. See:
         # https://github.com/mesonbuild/meson/issues/4271
         #
