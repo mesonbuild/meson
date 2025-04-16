80f26fea5ef2ff1dbd57a16f5a04d45bd2908aba
Eli Schwartz <eschwartz93@gmail.com>
2025-04-16 00:07:51 -0400

    setup.py: crudely label wheels using setuptools_scm if building from git

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 # Copyright 2016 The Meson development team
 
 
-import sys
+import os, sys
 
 if sys.version_info < (3, 7):
     raise SystemExit('ERROR: Tried to install Meson with an unsupported Python version: \n{}'
@@ -11,10 +11,24 @@ if sys.version_info < (3, 7):
 
 from setuptools import setup
 
+scm_args = {}
+HERE = os.path.dirname(__file__)
+if os.path.exists(os.path.join(HERE, '.git')):
+    try:
+        import setuptools_scm
+    except ModuleNotFoundError:
+        pass
+    else:
+        sys.path.insert(0, HERE)
+        from mesonbuild import coredata
+
+        scheme = 'guess-next-dev' if 'rc' in coredata.version else 'release-branch-semver'
+        scm_args = {'use_scm_version': {'version_scheme': scheme}}
+
 data_files = []
 if sys.platform != 'win32':
     # Only useful on UNIX-like systems
     data_files = [('share/man/man1', ['man/meson.1']),
                   ('share/polkit-1/actions', ['data/com.mesonbuild.install.policy'])]
 
-setup(data_files=data_files,)
+setup(data_files=data_files,**scm_args)
