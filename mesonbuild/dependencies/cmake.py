mesonbuild/meson

commit 92c517ea69e0578515d15061e7ec32f518733acf

output PACKAGE_NOT_FOUND_MESSAGE as warning when CMake package is not found

mesonbuild/dependencies/cmake.py
@@ -414,7 +414,16 @@ class CMakeDependency(ExternalDependency):
         # Whether the package is found or not is always stored in PACKAGE_FOUND
         self.is_found = self.traceparser.var_to_bool('PACKAGE_FOUND')
         if not self.is_found:
-            return
+            not_found_message = self.traceparser.get_cmake_var('PACKAGE_NOT_FOUND_MESSAGE')
+            if len(not_found_message) > 0:
+                mlog.warning(
+                    'CMake reported that the package {} was not found with the following reason:\n'
+                    '{}'.format(name, not_found_message[0]))
+            else:
+                mlog.warning(
+                    'CMake reported that the package {} was not found, '
+                    'even though Meson\'s preliminary check succeeded.'.format(name))
+            raise self._gen_exception('PACKAGE_FOUND is false')
 
         # Try to detect the version
         vers_raw = self.traceparser.get_cmake_var('PACKAGE_VERSION')

mesonbuild/dependencies/data/CMakeLists.txt
@@ -100,3 +100,9 @@ if(${_packageName}_FOUND  OR  ${PACKAGE_NAME}_FOUND)
   set(PACKAGE_DEFINITIONS  "${${definitions}}")
   set(PACKAGE_LIBRARIES    "${${libs}}")
 endif()
+
+if(${_packageName}_NOT_FOUND_MESSAGE)
+  set(PACKAGE_NOT_FOUND_MESSAGE "${${_packageName}_NOT_FOUND_MESSAGE}")
+elseif(${PACKAGE_NAME}_NOT_FOUND_MESSAGE)
+  set(PACKAGE_NOT_FOUND_MESSAGE "${${PACKAGE_NAME}_NOT_FOUND_MESSAGE}")
+endif()

commit dacb23b7a200d8214d3395b995a1d1e9e65c286d

Log non-fatally if CMake doesn't find a package
If CMake fails to find a package, we should log it in meson-log.txt, but
shouldn't bother the user unless there are details to report.  In
addition, we were calling mlog.warning() without fatal=False, so if msetup
was called with --fatal-meson-warnings we'd fail the entire setup in this
case, even for optional dependencies.  Log a notice with fatal=False if
CMake reported an error, and a debug message otherwise.

Notably, the "even though Meson's preliminary check succeeded" case can
occur when a dependency is missing but its Find*.cmake is shipped with
CMake itself.

Fixes: 92c517ea69 ("output PACKAGE_NOT_FOUND_MESSAGE as warning when CMake package is not found")

mesonbuild/dependencies/cmake.py
@@ -416,11 +416,11 @@ class CMakeDependency(ExternalDependency):
         if not self.is_found:
             not_found_message = self.traceparser.get_cmake_var('PACKAGE_NOT_FOUND_MESSAGE')
             if len(not_found_message) > 0:
-                mlog.warning(
+                mlog.notice(
                     'CMake reported that the package {} was not found with the following reason:\n'
-                    '{}'.format(name, not_found_message[0]))
+                    '{}'.format(name, not_found_message[0]), fatal=False)
             else:
-                mlog.warning(
+                mlog.debug(
                     'CMake reported that the package {} was not found, '
                     'even though Meson\'s preliminary check succeeded.'.format(name))
             raise self._gen_exception('PACKAGE_FOUND is false')