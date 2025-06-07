c8432df30edea96557db871d67c731537654afbf
Jan200101 <sentrycraft123@gmail.com>
2025-06-07 10:37:02 +0200

    replace regex implementation for cmake with a parser
    cmake does preprocessing in a linear pass through the entire file
    this allows recursive variable lookups which cannot be supported
    by a basic regex implementation.

--- test cases/common/14 configure file/prog7.c
+++ test cases/common/14 configure file/prog7.c
@@ -3,8 +3,12 @@
 
 int main(void) {
     return strcmp(MESSAGE1, "foo")
-        || strcmp(MESSAGE2, "${var1}")
-        || strcmp(MESSAGE3, "\\foo")
-        || strcmp(MESSAGE4, "\\${var1}")
-        || strcmp(MESSAGE5, "\\ ${ ${ \\${ \\${");
+        || strcmp(MESSAGE2, "\foo")
+        || strcmp(MESSAGE3, "\\\\foo")
+        || strcmp(MESSAGE4, "\\\\\foo")
+        || strcmp(MESSAGE5, "foo")
+        || strcmp(MESSAGE6, "\\foo")
+        || strcmp(MESSAGE7, "\\\\foo")
+        || strcmp(MESSAGE8, "@var1\@")
+        || 0;
 }
