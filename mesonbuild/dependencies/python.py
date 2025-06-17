158b101eb544c3a8ef9360869c3e63a93ca2cc3e
Luca Bacci <luca.bacci982@gmail.com>
2025-06-17 10:03:40 +0200

    Python dependency: Implement get_runtime_paths
    Fixes https://github.com/mesonbuild/meson/issues/14712

--- mesonbuild/dependencies/python.py
+++ mesonbuild/dependencies/python.py
@@ -388,6 +388,12 @@ class PythonSystemDependency(SystemDependency, _PythonDependencyBase):
     def log_tried() -> str:
         return 'sysconfig'
 
+    def get_runtime_paths(self) -> T.Optional[T.List[str]]:
+        bindir = self.variables.get('BINDIR')
+        if bindir:
+            return [bindir]
+        return None
+
 def python_factory(env: 'Environment', for_machine: 'MachineChoice',
                    kwargs: T.Dict[str, T.Any],
                    installation: T.Optional['BasicPythonExternalProgram'] = None) -> T.List['DependencyGenerator']:
