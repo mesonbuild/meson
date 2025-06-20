68d29ef7f62df5481aa4de8eaf421f25243f4cb2
Andrew Lister <andrew.lister@stfc.ac.uk>
2025-06-20 15:33:59 +0100

    compilers-fortran: Fix preprocessing when fortran uses concat operator

--- test cases/fortran/23 preprocess/main.f90
+++ test cases/fortran/23 preprocess/main.f90
@@ -1,4 +1,14 @@
 #define MYDEF program
 MYDEF foo
-    write (*,*) 'Hello, world!'
+    character(20) :: str
+#ifdef CORRECT
+        str = 'Hello, ' // 'world!'
+#else
+        str = 'Preprocessing error!'
+#endif
+    if (str /= 'Hello, world!') then
+        print *, 'Preprocessing failed.'
+        error stop 1
+    end if
+    stop 0
 end MYDEF foo
