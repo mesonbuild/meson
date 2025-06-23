07884e366ba2ac376c8a6f7ea31804cafba910cd
Benjamin Gilbert <bgilbert@backtick.net>
2025-06-23 15:09:54 -0700

    rewriter: don't unpack the same values five times
    Fixes: e81acbd606 ("Use a single coredata dictionary for options")

--- mesonbuild/rewriter.py
+++ mesonbuild/rewriter.py
@@ -496,10 +496,6 @@ class Rewriter:
         cdata = self.interpreter.coredata
         options = {
             **{str(k): v for k, v in cdata.optstore.items()},
-            **{str(k): v for k, v in cdata.optstore.items()},
-            **{str(k): v for k, v in cdata.optstore.items()},
-            **{str(k): v for k, v in cdata.optstore.items()},
-            **{str(k): v for k, v in cdata.optstore.items()},
         }
 
         for key, val in sorted(cmd['options'].items()):
