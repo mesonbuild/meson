cdd6a3de23e6d00e9ee774c5479326dffa79195f
Sam James <sam@gentoo.org>
2025-07-01 01:51:59 +0100

    ci: gentoo: handle llvm pkgmove
    Bug: https://bugs.gentoo.org/910572

--- ci/ciimage/gentoo/install.sh
+++ ci/ciimage/gentoo/install.sh
@@ -38,7 +38,7 @@ pkgs_stable=(
   dev-cpp/gtest
   sci-libs/hdf5
   dev-qt/linguist-tools
-  sys-devel/llvm
+  llvm-core/llvm
   dev-qt/qttools
 
   # misc
@@ -66,7 +66,7 @@ pkgs_stable=(
   #gnustep-base/gnustep-base
   #media-gfx/graphviz
   #sci-libs/netcdf-fortran
-  #sys-devel/clang
+  #llvm-core/clang
   #x11-libs/gtk+:3
 )
 pkgs_latest=(
