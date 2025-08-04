mesonbuild/meson

commit e49f2f7283e1d9f18e2f5f54647c07287cd70339

build: allow picking 'rust' as a link_language
rustc only needs to be a linker if there are any Rust-ABI dependencies.
Skip it whenever linking to a C-ABI dependency.  This makes it possible
for Meson to pick 'rust' whenever it sees Rust sources, but not whenever
it sees Rust used by a dependency with "rust_abi: 'c'".

Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/build.py
@@ -885,6 +885,10 @@ class BuildTarget(Target):
                 if isinstance(t, (CustomTarget, CustomTargetIndex)):
                     continue # We can't know anything about these.
                 for name, compiler in t.compilers.items():
+                    if name == 'rust':
+                        # Rust is always linked through a C-ABI target, so do not add
+                        # the compiler here
+                        continue
                     if name in link_langs and name not in self.compilers:
                         self.compilers[name] = compiler
 
@@ -1600,6 +1604,9 @@ class BuildTarget(Target):
             if isinstance(link_target, (CustomTarget, CustomTargetIndex)):
                 continue
             for language in link_target.compilers:
+                if language == 'rust' and not link_target.uses_rust_abi():
+                    # All Rust dependencies must go through a C-ABI dependency, so ignore it
+                    continue
                 if language not in langs:
                     langs.append(language)
 

mesonbuild/compilers/compilers.py
@@ -84,7 +84,7 @@ clib_langs = ('objcpp', 'cpp', 'objc', 'c', 'nasm', 'fortran')
 # List of languages that can be linked with C code directly by the linker
 # used in build.py:process_compilers() and build.py:get_dynamic_linker()
 # This must be sorted, see sort_clink().
-clink_langs = ('d', 'cuda') + clib_langs
+clink_langs = ('rust', 'd', 'cuda') + clib_langs
 
 SUFFIX_TO_LANG = dict(itertools.chain(*(
     [(suffix, lang) for suffix in v] for lang, v in lang_suffixes.items())))

commit 164b8cd5ea47f49bbcb1aa8a537b849119a5eddd

build: fix introspection interpreter issue when project selects Rust
The introspection interpreter sometimes produces targets with no source
files; BuildTarget will then pick a random compiler, according to the
order in clink_langs.

According to the comment in sort_clink, clink_langs are supposed
to list languages from *lowest* to highest priority.  However,
process_compilers_late() process clink_langs in straight order and
returns the first language; which is the one with lowest priority.

This became visible with the addition of Rust to clink_langs,
because Rust has limitation on the names of library targets, but
the bug existed before.

Fixes: e49f2f7283e1d9f18e2f5f54647c07287cd70339
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/build.py
@@ -900,7 +900,7 @@ class BuildTarget(Target):
             # No source files or parent targets, target consists of only object
             # files of unknown origin. Just add the first clink compiler
             # that we have and hope that it can link these objects
-            for lang in link_langs:
+            for lang in reversed(link_langs):
                 if lang in self.all_compilers:
                     self.compilers[lang] = self.all_compilers[lang]
                     break

test cases/rust/20 rust and cpp/meson.build
@@ -8,7 +8,7 @@ project(
   meson_version : '>= 1.2.0',
 )
 
-cpplib = static_library('cpp', 'lib.cpp')
+cpplib = static_library('cpp-lib', 'lib.cpp')
 exe = executable('main', 'main.rs', link_with : cpplib)
 
 test('main', exe)

unittests/allplatformstests.py
@@ -3343,6 +3343,11 @@ class AllPlatformTests(BasePlatformTests):
         testdir = os.path.join(self.unit_test_dir, '58 introspect buildoptions')
         self._run(self.mconf_command + [testdir])
 
+    @skip_if_not_language('rust')
+    def test_meson_configure_srcdir(self):
+        testdir = os.path.join(self.rust_test_dir, '20 rust and cpp')
+        self._run(self.mconf_command + [testdir])
+
     def test_introspect_buildoptions_cross_only(self):
         testdir = os.path.join(self.unit_test_dir, '82 cross only introspect')
         testfile = os.path.join(testdir, 'meson.build')