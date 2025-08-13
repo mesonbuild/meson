f7fad7c82fddc750410c54cfe6bb9eefa5870946
Paolo Bonzini <pbonzini@redhat.com>
2025-08-13 16:43:35 +0200

    cargo: create cargo.Interpreter on the fly
    Do not create a single cargo.Interpreter for the whole execution, instead
    create one as soon as a Cargo.lock file is read and only make it last until
    that Cargo.lock has been processed.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- mesonbuild/environment.py
+++ mesonbuild/environment.py
@@ -47,7 +47,6 @@ if T.TYPE_CHECKING:
     from .compilers.mixins.visualstudio import VisualStudioLikeCompiler
     from .options import OptionDict, ElementaryOptionValues
     from .wrap.wrap import Resolver
-    from . import cargo
 
     CompilersDict = T.Dict[str, Compiler]
 
@@ -727,8 +726,6 @@ class Environment:
         self.default_cmake = ['cmake']
         self.default_pkgconfig = ['pkg-config']
         self.wrap_resolver: T.Optional['Resolver'] = None
-        # Store a global state of Cargo dependencies
-        self.cargo: T.Optional[cargo.Interpreter] = None
 
     def mfilestr2key(self, machine_file_string: str, section: T.Optional[str], section_subproject: T.Optional[str], machine: MachineChoice) -> OptionKey:
         key = OptionKey.from_string(machine_file_string)
