mesonbuild/meson

commit 37f7572ce15494654128184777b16ece38308b9a

options: parse -D and -U arguments directly into a Dict[OptionKey, Optional[str]]
As a side effect, this deduplicates -D and -U arguments passed
to meson configure, taking into account the relative ordering
of -D and -U options.

Fixes: #14754
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/coredata.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 import copy
 
 from . import mlog, options
+import argparse
 import pickle, os, uuid
 import sys
 from functools import lru_cache
@@ -32,7 +33,6 @@ import shlex
 import typing as T
 
 if T.TYPE_CHECKING:
-    import argparse
     from typing_extensions import Protocol
 
     from . import dependencies
@@ -49,13 +49,11 @@ if T.TYPE_CHECKING:
         """Representation of command line options from Meson setup, configure,
         and dist.
 
-        :param projectoptions: The raw list of command line options given
         :param cmd_line_options: command line options parsed into an OptionKey:
             str mapping
         """
 
-        cmd_line_options: T.Dict[OptionKey, str]
-        projectoptions: T.List[str]
+        cmd_line_options: T.Dict[OptionKey, T.Optional[str]]
         cross_file: T.List[str]
         native_file: T.List[str]
 
@@ -413,11 +411,7 @@ class CoreData:
         return value
 
     def set_from_configure_command(self, options: SharedCMDOptions) -> bool:
-        unset_opts = getattr(options, 'unset_opts', [])
-        all_D = options.projectoptions[:]
-        for key, valstr in options.cmd_line_options.items():
-            all_D.append(f'{key!s}={valstr}')
-        return self.optstore.set_from_configure_command(all_D, unset_opts)
+        return self.optstore.set_from_configure_command(options.cmd_line_options)
 
     def set_option(self, key: OptionKey, value, first_invocation: bool = False) -> bool:
         dirty = False
@@ -688,25 +682,60 @@ def save(obj: CoreData, build_dir: str) -> str:
     return filename
 
 
+class KeyNoneAction(argparse.Action):
+    """
+    Custom argparse Action that stores values in a dictionary as keys with value None.
+    """
+
+    def __init__(self, option_strings, dest, nargs=None, **kwargs: object) -> None:
+        assert nargs is None or nargs == 1
+        super().__init__(option_strings, dest, nargs=1, **kwargs)
+
+    def __call__(self, parser: argparse.ArgumentParser, namespace: argparse.Namespace,
+                 arg: T.List[str], option_string: str = None) -> None:
+        current_dict = getattr(namespace, self.dest)
+        if current_dict is None:
+            current_dict = {}
+            setattr(namespace, self.dest, current_dict)
+
+        key = OptionKey.from_string(arg[0])
+        current_dict[key] = None
+
+
+class KeyValueAction(argparse.Action):
+    """
+    Custom argparse Action that parses KEY=VAL arguments and stores them in a dictionary.
+    """
+
+    def __init__(self, option_strings, dest, nargs=None, **kwargs: object) -> None:
+        assert nargs is None or nargs == 1
+        super().__init__(option_strings, dest, nargs=1, **kwargs)
+
+    def __call__(self, parser: argparse.ArgumentParser, namespace: argparse.Namespace,
+                 arg: T.List[str], option_string: str = None) -> None:
+        current_dict = getattr(namespace, self.dest)
+        if current_dict is None:
+            current_dict = {}
+            setattr(namespace, self.dest, current_dict)
+
+        try:
+            keystr, value = arg[0].split('=', 1)
+            key = OptionKey.from_string(keystr)
+            current_dict[key] = value
+        except ValueError:
+            parser.error(f'The argument for option {option_string!r} must be in OPTION=VALUE format.')
+
+
 def register_builtin_arguments(parser: argparse.ArgumentParser) -> None:
     for n, b in options.BUILTIN_OPTIONS.items():
         options.option_to_argparse(b, n, parser, '')
     for n, b in options.BUILTIN_OPTIONS_PER_MACHINE.items():
         options.option_to_argparse(b, n, parser, ' (just for host machine)')
         options.option_to_argparse(b, n.as_build(), parser, ' (just for build machine)')
-    parser.add_argument('-D', action='append', dest='projectoptions', default=[], metavar="option",
+    parser.add_argument('-D', action=KeyValueAction, dest='cmd_line_options', default={}, metavar="option=value",
                         help='Set the value of an option, can be used several times to set multiple options.')
 
 def parse_cmd_line_options(args: SharedCMDOptions) -> None:
-    args.cmd_line_options = {}
-    for o in args.projectoptions:
-        try:
-            keystr, value = o.split('=', 1)
-        except ValueError:
-            raise MesonException(f'Option {o!r} must have a value separated by equals sign.')
-        key = OptionKey.from_string(keystr)
-        args.cmd_line_options[key] = value
-
     # Merge builtin options set with --option into the dict.
     for key in chain(
             options.BUILTIN_OPTIONS.keys(),

mesonbuild/mconf.py
@@ -33,7 +33,6 @@ if T.TYPE_CHECKING:
         builddir: str
         clearcache: bool
         pager: bool
-        unset_opts: T.List[str]
 
     # cannot be TV_Loggable, because non-ansidecorators do direct string concat
     LOGLINE = T.Union[str, mlog.AnsiDecorator]
@@ -47,7 +46,7 @@ def add_arguments(parser: 'argparse.ArgumentParser') -> None:
                         help='Clear cached state (e.g. found dependencies)')
     parser.add_argument('--no-pager', action='store_false', dest='pager',
                         help='Do not redirect output to a pager')
-    parser.add_argument('-U', action='append', dest='unset_opts', default=[],
+    parser.add_argument('-U', action=coredata.KeyNoneAction, dest='cmd_line_options', default={},
                         help='Remove a subproject option.')
 
 def stringify(val: T.Any) -> str:
@@ -355,11 +354,7 @@ class Conf:
             mlog.log('\nThere are no option augments.')
 
 def has_option_flags(options: CMDOptions) -> bool:
-    if options.cmd_line_options:
-        return True
-    if options.unset_opts:
-        return True
-    return False
+    return bool(options.cmd_line_options)
 
 def is_print_only(options: CMDOptions) -> bool:
     if has_option_flags(options):

mesonbuild/msetup.py
@@ -27,7 +27,6 @@ if T.TYPE_CHECKING:
         builddir: str
         sourcedir: str
         pager: bool
-        unset_opts: T.List[str]
 
 git_ignore_file = '''# This file is autogenerated by Meson. If you change or delete it, it won't be recreated.
 *

mesonbuild/options.py
@@ -1111,14 +1111,13 @@ class OptionStore:
         else:
             raise MesonException(f'Unknown option: "{o}".')
 
-    def set_from_configure_command(self, D_args: T.List[str], U_args: T.List[str]) -> bool:
+    def set_from_configure_command(self, D_args: T.Dict[OptionKey, T.Optional[str]]) -> bool:
         dirty = False
-        D_args = [] if D_args is None else D_args
-        U_args = [] if U_args is None else U_args
-        for key, valstr in self.parse_D_arguments(D_args):
-            dirty |= self.set_user_option(key, valstr)
-        for keystr in U_args:
-            key = OptionKey.from_string(keystr)
+        for key, valstr in D_args.items():
+            if valstr is not None:
+                dirty |= self.set_user_option(key, valstr)
+                continue
+
             if key in self.augments:
                 del self.augments[key]
                 dirty = True
@@ -1241,14 +1240,6 @@ class OptionStore:
 
     def is_module_option(self, key: OptionKey) -> bool:
         return key in self.module_options
-
-    def parse_D_arguments(self, D: T.List[str]) -> T.List[T.Tuple[OptionKey, str]]:
-        options = []
-        for setval in D:
-            keystr, valstr = setval.split('=', 1)
-            key = OptionKey.from_string(keystr)
-            options.append((key, valstr))
-        return options
 
     def prefix_split_options(self, coll: OptionDict) -> T.Tuple[T.Optional[str], OptionDict]:
         prefix = None

unittests/optiontests.py
@@ -195,25 +195,25 @@ class OptionTests(unittest.TestCase):
 
         # First augment a subproject
         with self.subTest('set subproject override'):
-            optstore.set_from_configure_command([f'{sub_name}:{name}={aug_value}'], [])
+            optstore.set_from_configure_command({OptionKey.from_string(f'{sub_name}:{name}'): aug_value})
             self.assertEqual(optstore.get_value_for(name), top_value)
             self.assertEqual(optstore.get_value_for(name, sub_name), aug_value)
             self.assertEqual(optstore.get_value_for(name, sub2_name), top_value)
 
         with self.subTest('unset subproject override'):
-            optstore.set_from_configure_command([], [f'{sub_name}:{name}'])
+            optstore.set_from_configure_command({OptionKey.from_string(f'{sub_name}:{name}'): None})
             self.assertEqual(optstore.get_value_for(name), top_value)
             self.assertEqual(optstore.get_value_for(name, sub_name), top_value)
             self.assertEqual(optstore.get_value_for(name, sub2_name), top_value)
 
         # And now augment the top level option
-        optstore.set_from_configure_command([f':{name}={aug_value}'], [])
+        optstore.set_from_configure_command({OptionKey.from_string(f':{name}'): aug_value})
         self.assertEqual(optstore.get_value_for(name, None), top_value)
         self.assertEqual(optstore.get_value_for(name, ''), aug_value)
         self.assertEqual(optstore.get_value_for(name, sub_name), top_value)
         self.assertEqual(optstore.get_value_for(name, sub2_name), top_value)
 
-        optstore.set_from_configure_command([], [f':{name}'])
+        optstore.set_from_configure_command({OptionKey.from_string(f':{name}'): None})
         self.assertEqual(optstore.get_value_for(name), top_value)
         self.assertEqual(optstore.get_value_for(name, sub_name), top_value)
         self.assertEqual(optstore.get_value_for(name, sub2_name), top_value)
@@ -233,8 +233,8 @@ class OptionTests(unittest.TestCase):
                              choices=['c++98', 'c++11', 'c++14', 'c++17', 'c++20', 'c++23'],
                              )
         optstore.add_system_option(name, co)
-        optstore.set_from_configure_command([f'{sub_name}:{name}={aug_value}'], [])
-        optstore.set_from_configure_command([f'{sub_name}:{name}={set_value}'], [])
+        optstore.set_from_configure_command({OptionKey.from_string(f'{sub_name}:{name}'): aug_value})
+        optstore.set_from_configure_command({OptionKey.from_string(f'{sub_name}:{name}'): set_value})
         self.assertEqual(optstore.get_value_for(name), top_value)
         self.assertEqual(optstore.get_value_for(name, sub_name), set_value)
 
@@ -256,7 +256,7 @@ class OptionTests(unittest.TestCase):
 
     def test_reconfigure_b_nonexistent(self):
         optstore = OptionStore(False)
-        optstore.set_from_configure_command(['b_ndebug=true'], [])
+        optstore.set_from_configure_command({OptionKey('b_ndebug'): True})
 
     def test_subproject_proj_opt_with_same_name(self):
         name = 'tests'

commit 2c6ff5c28a61677d758aa78e40ce883cbc17efe8

coredata: do not write None to cmd_line.txt
Fixes: #14955
Fixes: 37f7572ce15494654128184777b16ece38308b9a
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/coredata.py
@@ -641,7 +641,13 @@ def update_cmd_line_file(build_dir: str, options: SharedCMDOptions) -> None:
     filename = get_cmd_line_file(build_dir)
     config = CmdLineFileParser()
     config.read(filename)
-    config['options'].update({str(k): str(v) for k, v in options.cmd_line_options.items()})
+    for k, v in options.cmd_line_options.items():
+        keystr = str(k)
+        if v is not None:
+            config['options'][keystr] = str(v)
+        elif keystr in config['options']:
+            del config['options'][keystr]
+
     with open(filename, 'w', encoding='utf-8') as f:
         config.write(f)
 