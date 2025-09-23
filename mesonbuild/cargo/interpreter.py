23e036aa8a33e22a3b2209cf96e55ec5fc955ef9
Paolo Bonzini <pbonzini@redhat.com>
2025-09-23 14:19:04 +0200

    cargo: use same procedural macro crates for both host and build
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- mesonbuild/cargo/interpreter.py
+++ mesonbuild/cargo/interpreter.py
@@ -663,8 +663,10 @@ class Interpreter:
         depname = _dependency_name(pkg.manifest.package.name, pkg.manifest.package.api, depname_suffix)
 
         lib: mparser.BaseNode
+        machines: T.Iterable[MachineChoice]
         if crate_type == 'proc-macro':
             lib = build.method('proc_macro', build.identifier('rust'), posargs, kwargs)
+            machines = MachineChoice
         else:
             if crate_type in {'lib', 'rlib', 'staticlib'}:
                 target_type = 'static_library'
@@ -676,11 +678,12 @@ class Interpreter:
             if crate_type in {'staticlib', 'cdylib'}:
                 kwargs['rust_abi'] = build.string('c')
             lib = build.function(target_type, posargs, kwargs)
+            machines = [machine]
 
         # lib = xxx_library()
         # dep = declare_dependency()
         # meson.override_dependency()
-        return [
+        result: T.List[mparser.BaseNode] = [
             build.assign(lib, 'lib'),
             build.assign(
                 build.function(
@@ -695,16 +698,19 @@ class Interpreter:
                 ),
                 'dep'
             ),
-            build.method(
+        ]
+
+        for m in machines:
+            result.append(build.method(
                 'override_dependency',
                 build.identifier('meson'),
                 [
                     build.string(depname),
                     build.identifier('dep'),
                 ],
-                {'native': build.bool(machine == MachineChoice.BUILD)},
-            ),
-        ]
+                {'native': build.bool(m == MachineChoice.BUILD)},
+            ))
+        return result
 
 
 def _parse_git_url(url: str, branch: T.Optional[str] = None) -> T.Tuple[str, str, str]:
