f2b5c1c9d0f8f3b4afa5a7a599864e3311970a50
Paolo Bonzini <pbonzini@redhat.com>
2025-09-24 18:27:45 +0200

    cargo: vary dependency names per machine
    override_dependency does not support overriding the same dependency twice,
    for both the build and the host machine.  To work around this, use a
    separate dependency name.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- mesonbuild/cargo/interpreter.py
+++ mesonbuild/cargo/interpreter.py
@@ -35,8 +35,9 @@ if T.TYPE_CHECKING:
 
     from typing_extensions import Literal
 
-def _dependency_name(package_name: str, api: str, suffix: str = '-rs') -> str:
+def _dependency_name(package_name: str, api: str, suffix: str = '-rs', machine: T.Optional[MachineChoice] = None) -> str:
     basename = package_name[:-len(suffix)] if suffix and package_name.endswith(suffix) else package_name
+    suffix += '-build' if machine == MachineChoice.BUILD else ''
     return f'{basename}-{api}{suffix}'
 
 
@@ -561,6 +562,8 @@ class Interpreter:
         kw = {
             'version': build.array([build.string(s) for s in version_]),
             'native': build.bool(machine == MachineChoice.BUILD),
+            # note the subproject name does not use the machine
+            'fallback': build.string(_dependency_name(dep.package, api)),
         }
         # Lookup for this dependency with the features we want in default_options kwarg.
         #
@@ -578,7 +581,7 @@ class Interpreter:
             build.assign(
                 build.function(
                     'dependency',
-                    [build.string(_dependency_name(dep.package, api))],
+                    [build.string(_dependency_name(dep.package, api, machine=machine))],
                     kw,
                 ),
                 _dependency_varname(dep.package, machine),
@@ -725,7 +728,6 @@ class Interpreter:
         }
 
         depname_suffix = '' if lib_type == 'c' else '-rs'
-        depname = _dependency_name(pkg.manifest.package.name, pkg.manifest.package.api, depname_suffix)
 
         lib: mparser.BaseNode
         machines: T.Iterable[MachineChoice]
@@ -770,6 +772,8 @@ class Interpreter:
         ]
 
         for m in machines:
+            depname = _dependency_name(pkg.manifest.package.name, pkg.manifest.package.api,
+                                       depname_suffix, m)
             result.append(build.method(
                 'override_dependency',
                 build.identifier('meson'),
