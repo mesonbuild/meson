mesonbuild/meson

commit 018d224821f91c1d352e71b59e35e6f2fe64b119

ninjabackend: unify conversion from path to -L/-l
When Meson cannot use -Clink-arg=, it has to split the path into a -L
option (to add to the search directory) and a -l option (to actually
link).  Furthermore, if rustc does not support verbatim, Meson needs to
undo the addition of "lib" and the extension.

Do both tasks in the same place and with the same logic, instead of
treating internal and external libraries differently.

Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/backend/ninjabackend.py
@@ -2038,18 +2038,24 @@ class NinjaBackend(backends.Backend):
             except (KeyError, AttributeError):
                 pass
 
-        if mesonlib.version_compare(rustc.version, '>= 1.67.0'):
-            verbatim = '+verbatim'
-        else:
-            verbatim = ''
+        has_verbatim = mesonlib.version_compare(rustc.version, '>= 1.67.0')
 
         def _link_library(libname: str, static: bool, bundle: bool = False) -> None:
             type_ = 'static' if static else 'dylib'
             modifiers = []
+            # Except with -Clink-arg, search is limited to the -L search paths
+            dir_, libname = os.path.split(libname)
+            linkdirs.add(dir_)
             if not bundle and static:
                 modifiers.append('-bundle')
-            if verbatim:
-                modifiers.append(verbatim)
+            if has_verbatim:
+                modifiers.append('+verbatim')
+            else:
+                # undo the effects of -l without verbatim
+                libname, ext = os.path.splitext(libname)
+                if libname.startswith('lib'):
+                    libname = libname[3:]
+
             if modifiers:
                 type_ += ':' + ','.join(modifiers)
             args.append(f'-l{type_}={libname}')
@@ -2062,11 +2068,11 @@ class NinjaBackend(backends.Backend):
         external_deps = target.external_deps.copy()
         target_deps = target.get_dependencies()
         for d in target_deps:
-            linkdirs.add(d.subdir)
             deps.append(self.get_dependency_filename(d))
             if isinstance(d, build.StaticLibrary):
                 external_deps.extend(d.external_deps)
             if d.uses_rust_abi():
+                linkdirs.add(d.subdir)
                 if d not in itertools.chain(target.link_targets, target.link_whole_targets):
                     # Indirect Rust ABI dependency, we only need its path in linkdirs.
                     continue
@@ -2094,8 +2100,7 @@ class NinjaBackend(backends.Backend):
             link_whole = d in target.link_whole_targets
             if isinstance(target, build.StaticLibrary) or (isinstance(target, build.Executable) and rustc.get_crt_static()):
                 static = isinstance(d, build.StaticLibrary)
-                libname = os.path.basename(lib) if verbatim else d.name
-                _link_library(libname, static, bundle=link_whole)
+                _link_library(lib, static, bundle=link_whole)
             elif link_whole:
                 link_whole_args = rustc.linker.get_link_whole_for([lib])
                 args += [f'-Clink-arg={a}' for a in link_whole_args]
@@ -2111,17 +2116,13 @@ class NinjaBackend(backends.Backend):
                     args.append(a)
                     continue
                 elif is_library(a):
-                    dir_, lib = os.path.split(a)
-                    linkdirs.add(dir_)
-
                     if isinstance(target, build.StaticLibrary):
-                        if not verbatim:
-                            lib, ext = os.path.splitext(lib)
-                            if lib.startswith('lib'):
-                                lib = lib[3:]
                         static = a.endswith(('.a', '.lib'))
-                        _link_library(lib, static)
+                        _link_library(a, static)
                         continue
+
+                    dir_, _ = os.path.split(lib)
+                    linkdirs.add(dir_)
 
                 args.append(f'-Clink-arg={a}')
 

commit d433a12238afba653f03397101e1931a67c13fe7

rust: work around unintuitive behavior of -l:-bundle
When a Rust target depends on an intermediate Rust library (.rlib) which
itself has a dependency on a native static library, Meson's generated
rustc command line failed to collect and propagate the search paths for
these indirect native dependencies, leading to a linker error:

error: linking with `cc` failed: exit status: 1
  = note: rust-lld: error: unable to find library -l:libf.a
          collect2: error: ld returned 1 exit status

The root cause is that the intermediate .rlib's metadata correctly
stores the requirement to link the native library (via the -l flag),
but it does not store the library's search path (the -L flag); the
responsibility for providing the search path is therefore deferred
to the final link command.

Ensures that search paths for all transitive dependencies are collected,
allowing the final linker invocation to find and correctly link the
required native libraries.

Reported-by: Martin Kletzander <nert.pinx@gmail.com>
Fixes: 018d22482 ("ninjabackend: unify conversion from path to -L/-l", 2025-10-08)
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/backend/ninjabackend.py
@@ -2079,11 +2079,17 @@ class NinjaBackend(backends.Backend):
         external_deps = target.external_deps.copy()
         target_deps = target.get_dependencies()
         for d in target_deps:
+            # rlibs only store -l flags, not -L; help out rustc and always
+            # add the -L flag, in case it's needed to find non-bundled
+            # dependencies of an rlib.  At this point we don't have
+            # information on whether this is a direct dependency (which
+            # might use -Clink-arg= below) or an indirect one, so always
+            # add to linkdirs.
+            linkdirs.add(d.subdir)
             deps.append(self.get_dependency_filename(d))
             if isinstance(d, build.StaticLibrary):
                 external_deps.extend(d.external_deps)
             if d.uses_rust_abi():
-                linkdirs.add(d.subdir)
                 if d not in itertools.chain(target.link_targets, target.link_whole_targets):
                     # Indirect Rust ABI dependency, we only need its path in linkdirs.
                     continue

test cases/rust/29 rlib link subdir/cdep/f.c
@@ -1,0 +1,1 @@
+int f() { return 42; }

test cases/rust/29 rlib link subdir/cdep/meson.build
@@ -1,0 +1,1 @@
+cdep = static_library('f', 'f.c')

test cases/rust/29 rlib link subdir/f-sys.rs
@@ -1,0 +1,3 @@
+extern "C" {
+    pub fn f() -> ::std::os::raw::c_int;
+}

test cases/rust/29 rlib link subdir/main.rs
@@ -1,0 +1,1 @@
+fn main() { assert_eq!(unsafe { ::f_sys::f() }, 42); }

test cases/rust/29 rlib link subdir/meson.build
@@ -1,0 +1,21 @@
+project(
+  'rlib-link-subdir',
+  ['c', 'rust'],
+  default_options: ['rust_std=2021']
+)
+
+subdir('cdep')
+f_sys = static_library(
+  'f_sys',
+  'f-sys.rs',
+  link_with: cdep,
+  rust_abi: 'rust',
+)
+
+exe = executable(
+  'main',
+  'main.rs',
+  link_with: f_sys,
+)
+
+test('basic', exe)