komh/meson-os2

Extension of the executable is `exe' on OS/2

mesonbuild/backend/backends.py
@@ -550,7 +550,7 @@ class Backend:
         else:
             if exe_cmd[0].endswith('.jar'):
                 exe_cmd = ['java', '-jar'] + exe_cmd
-            elif exe_cmd[0].endswith('.exe') and not (mesonlib.is_windows() or mesonlib.is_cygwin() or mesonlib.is_wsl()):
+            elif exe_cmd[0].endswith('.exe') and not (mesonlib.is_windows() or mesonlib.is_cygwin() or mesonlib.is_wsl() or mesonlib.is_os2()):
                 exe_cmd = ['mono'] + exe_cmd
             exe_wrapper = None
 

mesonbuild/meson

mesonbuild/backend/backends.py
@@ -589,7 +589,7 @@ class Backend:
         else:
             if exe_cmd[0].endswith('.jar'):
                 exe_cmd = ['java', '-jar'] + exe_cmd
-            elif exe_cmd[0].endswith('.exe') and not (mesonlib.is_windows() or mesonlib.is_cygwin() or mesonlib.is_wsl()):
+            elif exe_cmd[0].endswith('.exe') and not (mesonlib.is_windows() or mesonlib.is_cygwin() or mesonlib.is_wsl() or machine.is_os2()):
                 exe_cmd = ['mono'] + exe_cmd
             exe_wrapper = None
 