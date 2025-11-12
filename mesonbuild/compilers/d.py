631508c55de26cb882dba71394a31df600738077
Dylan Baker <dylan@pnwbakers.com>
2025-11-12 09:56:02 -0800

    compilers: Remove Environment parameter from Compiler.get_assert_args

--- mesonbuild/compilers/d.py
+++ mesonbuild/compilers/d.py
@@ -695,7 +695,7 @@ class GnuDCompiler(GnuCompiler, DCompiler):
             return args
         return args + ['-shared-libphobos']
 
-    def get_assert_args(self, disable: bool, env: 'Environment') -> T.List[str]:
+    def get_assert_args(self, disable: bool) -> T.List[str]:
         if disable:
             return ['-frelease']
         return []
@@ -764,7 +764,7 @@ class LLVMDCompiler(DmdLikeCompilerMixin, DCompiler):
             return args
         return args + ['-link-defaultlib-shared']
 
-    def get_assert_args(self, disable: bool, env: 'Environment') -> T.List[str]:
+    def get_assert_args(self, disable: bool) -> T.List[str]:
         if disable:
             return ['--release']
         return []
@@ -848,7 +848,7 @@ class DmdDCompiler(DmdLikeCompilerMixin, DCompiler):
             return args
         return args + ['-defaultlib=phobos2', '-debuglib=phobos2']
 
-    def get_assert_args(self, disable: bool, env: 'Environment') -> T.List[str]:
+    def get_assert_args(self, disable: bool) -> T.List[str]:
         if disable:
             return ['-release']
         return []
