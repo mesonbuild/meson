302fece46310dc848eadad1db7b3a61df602fd80
Jussi Pakkanen <jussi.pakkanen@mailbox.org>
2025-11-13 12:09:15 +0200

    Support Visual Studio 2026.

--- mesonbuild/options.py
+++ mesonbuild/options.py
@@ -60,8 +60,8 @@ DEFAULT_YIELDING = False
 # Can't bind this near the class method it seems, sadly.
 _T = T.TypeVar('_T')
 
-backendlist = ['ninja', 'vs', 'vs2010', 'vs2012', 'vs2013', 'vs2015', 'vs2017', 'vs2019', 'vs2022', 'xcode', 'none']
-genvslitelist = ['vs2022']
+backendlist = ['ninja', 'vs', 'vs2010', 'vs2012', 'vs2013', 'vs2015', 'vs2017', 'vs2019', 'vs2022', 'vs2026', 'xcode', 'none']
+genvslitelist = ['vs2022', 'vs2026']
 buildtypelist = ['plain', 'debug', 'debugoptimized', 'release', 'minsize', 'custom']
 
 # This is copied from coredata. There is no way to share this, because this
