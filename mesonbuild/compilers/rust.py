dd0260738a5582bad754b111961fdb4fe9adfaad
Dylan Baker <dylan@pnwbakers.com>
2025-11-13 11:36:04 -0800

    compilers: Remove Environment parameter from RustCompiler.get_nightly

--- mesonbuild/compilers/rust.py
+++ mesonbuild/compilers/rust.py
@@ -210,11 +210,11 @@ class RustCompiler(Compiler):
     def get_crt_static(self) -> bool:
         return 'target_feature="crt-static"' in self.get_cfgs()
 
-    def get_nightly(self, target: T.Optional[BuildTarget], env: Environment) -> bool:
+    def get_nightly(self, target: T.Optional[BuildTarget]) -> bool:
         if not target:
             return self.allow_nightly
         key = self.form_compileropt_key('nightly')
-        nightly_opt = env.coredata.get_option_for_target(target, key)
+        nightly_opt = self.environment.coredata.get_option_for_target(target, key)
         if nightly_opt == 'enabled' and not self.is_nightly:
             raise EnvironmentException(f'Rust compiler {self.name_string()} is not a nightly compiler as required by the "nightly" option.')
         return nightly_opt != 'disabled' and self.is_nightly
