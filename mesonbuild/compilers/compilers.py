27e872a756664f5584187d7c33d1fec7c22b6cbf
Dylan Baker <dylan@pnwbakers.com>
2025-11-13 09:27:25 -0800

    compilers: Remove Environment parameter from Compiler.check_header

--- mesonbuild/compilers/compilers.py
+++ mesonbuild/compilers/compilers.py
@@ -641,7 +641,7 @@ class Compiler(HoldableObject, metaclass=abc.ABCMeta):
     def get_option_link_args(self, target: 'BuildTarget', env: 'Environment', subproject: T.Optional[str] = None) -> T.List[str]:
         return self.linker.get_option_link_args(target, env, subproject)
 
-    def check_header(self, hname: str, prefix: str, env: 'Environment', *,
+    def check_header(self, hname: str, prefix: str, *,
                      extra_args: T.Union[None, T.List[str], T.Callable[[CompileCheckMode], T.List[str]]] = None,
                      dependencies: T.Optional[T.List['Dependency']] = None) -> T.Tuple[bool, bool]:
         """Check that header is usable.
