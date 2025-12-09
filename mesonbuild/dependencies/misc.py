mesonbuild/meson

commit f070670e77fe36ece1a89ce00c13419de354b9ce

dependencies: add custom atomic dependency
Almost exactly the same as how the dl dependency works. On certain
systems (like BSDs that use clang), stdatomic is provided by compiler-rt
and doesn't need a separate library explictly linked. On a typical
GNU/LINUX system, atomic is a separate library that must be explictly
found and linked against. So just add a builtin and system method for
these two use cases.

mesonbuild/dependencies/misc.py
@@ -51,6 +51,27 @@ def netcdf_factory(env: 'Environment',
 packages['netcdf'] = netcdf_factory
 
 
+class AtomicBuiltinDependency(BuiltinDependency):
+    def __init__(self, name: str, env: Environment, kwargs: T.Dict[str, T.Any]):
+        super().__init__(name, env, kwargs)
+        self.feature_since = ('1.7.0', "consider checking for `atomic_flag_clear` with and without `find_library('atomic')`")
+
+        if self.clib_compiler.has_function('atomic_flag_clear', '#include <stdatomic.h>', env)[0]:
+            self.is_found = True
+
+
+class AtomicSystemDependency(SystemDependency):
+    def __init__(self, name: str, env: Environment, kwargs: T.Dict[str, T.Any]):
+        super().__init__(name, env, kwargs)
+        self.feature_since = ('1.7.0', "consider checking for `atomic_flag_clear` with and without `find_library('atomic')`")
+
+        h = self.clib_compiler.has_header('stdatomic.h', '', env)
+        self.link_args = self.clib_compiler.find_library('atomic', env, [], self.libtype)
+
+        if h[0] and self.link_args:
+            self.is_found = True
+
+
 class DlBuiltinDependency(BuiltinDependency):
     def __init__(self, name: str, env: 'Environment', kwargs: T.Dict[str, T.Any]):
         super().__init__(name, env, kwargs)
@@ -563,6 +584,13 @@ def shaderc_factory(env: 'Environment',
     return candidates
 packages['shaderc'] = shaderc_factory
 
+
+packages['atomic'] = atomic_factory = DependencyFactory(
+    'atomic',
+    [DependencyMethods.SYSTEM, DependencyMethods.BUILTIN],
+    system_class=AtomicSystemDependency,
+    builtin_class=AtomicBuiltinDependency,
+)
 
 packages['cups'] = cups_factory = DependencyFactory(
     'cups',

mesonbuild/dependencies/__init__.py
@@ -220,6 +220,7 @@ packages.defaults.update({
     'shaderc': 'misc',
     'iconv': 'misc',
     'intl': 'misc',
+    'atomic': 'misc',
     'dl': 'misc',
     'openssl': 'misc',
     'libcrypto': 'misc',

commit ccca7fefda601ead88e12d85e8a46b9038754ad1

dependencies: support custom stdatomic lookup for MSVC
Closes: https://github.com/mesonbuild/meson/issues/14233
Fixes: f070670e77fe36ece1a89ce00c13419de354b9ce

mesonbuild/dependencies/misc.py
@@ -61,6 +61,22 @@ class AtomicBuiltinDependency(BuiltinDependency):
 
         if self.clib_compiler.has_function('atomic_flag_clear', '#include <stdatomic.h>')[0]:
             self.is_found = True
+        elif self.clib_compiler.get_id() == 'msvc':
+            feature_flag = '/experimental:c11atomics'
+            std_args = self.clib_compiler.get_option_std_args(None, None)
+            # atomic_flag_clear forwards to __c11_atomic_store in msvc,
+            # which behaves like __builtin_* in gcc, and we're unable to detect it with .has_function().
+            if self.clib_compiler.has_function(
+                "_Atomic_thread_fence", "#include <stdatomic.h>", extra_args=std_args
+            )[0]:
+                self.is_found = True
+            elif self.clib_compiler.has_function(
+                "_Atomic_thread_fence",
+                "#include <stdatomic.h>",
+                extra_args=std_args + [feature_flag],
+            )[0]:
+                self.compile_args = [feature_flag]
+                self.is_found = True
 
 
 class AtomicSystemDependency(SystemDependency):