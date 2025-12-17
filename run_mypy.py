48767f6dcd5376397cdaf486bb03d0a7b1ee3130
Paolo Bonzini <pbonzini@redhat.com>
2025-12-17 11:35:09 +0100

    add typing for munstable_coredata.py

--- run_mypy.py
+++ run_mypy.py
@@ -54,6 +54,7 @@ modules = [
     'mesonbuild/mparser.py',
     'mesonbuild/msetup.py',
     'mesonbuild/mtest.py',
+    'mesonbuild/munstable_coredata.py',
     'mesonbuild/optinterpreter.py',
     'mesonbuild/options.py',
     'mesonbuild/programs.py',
