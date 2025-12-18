5ccfcbf584ca70c67eccb7337d948c059ffa1518
Paolo Bonzini <pbonzini@redhat.com>
2025-12-18 11:05:26 +0100

    build: switch stdlibs to PerMachineDefaultable
    stdlibs is a similar kind of cache to depenency_overrides, share it
    between build and host when not cross compiling.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- mesonbuild/build.py
+++ mesonbuild/build.py
@@ -362,15 +362,17 @@ class Build:
         self.install_dirs: T.List[InstallDir] = []
         self.dep_manifest_name: T.Optional[str] = None
         self.dep_manifest: T.Dict[str, DepManifest] = {}
-        self.stdlibs = PerMachine({}, {})
         self.test_setups: T.Dict[str, TestSetup] = {}
         self.test_setup_default_name = None
         self.searched_programs: T.Set[str] = set() # The list of all programs that have been searched for.
 
         # If we are doing a cross build we need two caches, if we're doing a
         # build == host compilation the both caches should point to the same place.
+        self.stdlibs = PerMachineDefaultable.default(
+            environment.is_cross_build(), {}, {})
         self.dependency_overrides: PerMachine[T.Dict[T.Tuple, DependencyOverride]] = PerMachineDefaultable.default(
             environment.is_cross_build(), {}, {})
+
         self.devenv: T.List[EnvironmentVariables] = []
         self.modules: T.Set[str] = set()
         """Used to track which modules are enabled in all subprojects.
