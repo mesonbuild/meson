b376eff54eb1f242280a6b908bb65977697c6bb8
Paolo Bonzini <pbonzini@redhat.com>
2026-01-19 22:09:21 +0100

    build, interpreter: make project-related fields per machine
    This is a first step towards making it possible to allow subprojects to
    be built for both the build and host machine.  Even though the concept
    of a build-machine subproject doesn't exist yet, prepare for it by
    making the list of (sub)projects per machine, and storing the machine
    in the interpreter's subproject stack.
    
    Extracted from a patch by Dylan Baker <dylan@pnwbakers.com>
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- mesonbuild/interpreter/interpreterobjects.py
+++ mesonbuild/interpreter/interpreterobjects.py
@@ -26,7 +26,7 @@ from ..interpreterbase import (
 from ..interpreter.type_checking import NoneType, ENV_KW, ENV_SEPARATOR_KW, PKGCONFIG_DEFINE_KW
 from ..dependencies import Dependency, ExternalLibrary, InternalDependency
 from ..programs import ExternalProgram, Program
-from ..mesonlib import HoldableObject, listify
+from ..mesonlib import HoldableObject, listify, MachineChoice
 
 import typing as T
 
@@ -901,7 +901,7 @@ class SubprojectHolder(MesonInterpreterObject):
                  warnings: int = 0,
                  disabled_feature: T.Optional[str] = None,
                  exception: T.Optional[Exception] = None,
-                 callstack: T.Optional[T.List[str]] = None) -> None:
+                 callstack: T.Optional[T.List[T.Tuple[str, MachineChoice]]] = None) -> None:
         super().__init__()
         self.held_object = subinterpreter
         self.warnings = warnings
