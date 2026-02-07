mesonbuild/meson

commit 4a9075b863c7a5002e51a129a4efa7281aafa0b2

compilers: Error out on unknown files in compiler checks for MSVC
If you use an unknown file type as the source file for a compiler
check with MSVC, it will emit a warning but then do nothing and return
a success code. Truly bonkers behaviour:

```
cl : Command line warning D9021 : no action performed
  -----------
  stderr:
  cl : Command line warning D9024 : unrecognized source file type 'pixman-0.46.4/a64-neon-test.S', object file assumed
  cl : Command line warning D9027 : source file 'pixman-0.46.4/a64-neon-test.S' ignored
 -----------
 Checking if "NEON A64 Intrinsic Support" compiles: YES
```

Detect that case and fail the compiler check:
https://learn.microsoft.com/en-us/cpp/error-messages/tool-errors/command-line-warning-d9024

mesonbuild/compilers/compilers.py
@@ -69,6 +69,10 @@ lang_suffixes: T.Mapping[str, T.Tuple[str, ...]] = {
     'masm': ('masm',),
     'linearasm': ('sa',),
 }
+# Some compilers only recognize files with specific suffixes.
+compiler_suffixes: T.Mapping[str, T.Tuple[str, ...]] = {
+    'msvc': ('c', 'cxx', 'cpp', 'obj', 'lib', 'def'),
+}
 all_languages = lang_suffixes.keys()
 c_cpp_suffixes = {'h'}
 cpp_suffixes = set(lang_suffixes['cpp']) | c_cpp_suffixes
@@ -849,6 +853,16 @@ class Compiler(HoldableObject, metaclass=abc.ABCMeta):
             else:
                 srcname = code.fname
                 code_debug = f'Source file: {srcname}'
+
+            compiler_id = self.get_id()
+            suffixes = compiler_suffixes.get(compiler_id)
+            if suffixes:
+                suffix = os.path.splitext(srcname)[1]
+                if suffix and suffix[1:] not in suffixes:
+                    err = f'Unrecognized filename suffix for "{srcname}", will be ignored by {compiler_id}'
+                    mlog.error(err)
+                    yield CompileResult('', err, [], 1, srcname)
+                    return
 
             # Construct the compiler command-line
             commands = self.compiler_args()

commit 5dda39e5fef9d07d8bb043d57472072659ad0145

compilers: Fix compiler checks on .cc files for MSVC
MSVC accepts .cc files.  Do not refuse to run compiler checks on source
files with a .cc extension.

Fixes: 4a9075b863c7 ("compilers: Error out on unknown files in compiler checks for MSVC")

mesonbuild/compilers/compilers.py
@@ -82,7 +82,7 @@ lang_suffixes: T.Mapping[Language, T.Tuple[str, ...]] = {
 }
 # Some compilers only recognize files with specific suffixes.
 compiler_suffixes: T.Mapping[str, T.Tuple[str, ...]] = {
-    'msvc': ('c', 'cxx', 'cpp', 'obj', 'lib', 'def'),
+    'msvc': ('c', 'cc', 'cxx', 'cpp', 'obj', 'lib', 'def'),
 }
 all_languages: mesonlib.OrderedSet[Language] = mesonlib.OrderedSet(sorted(lang_suffixes))
 c_cpp_suffixes = {'h'}