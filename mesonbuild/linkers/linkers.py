mesonbuild/meson

commit b1b22987101a97aa5e7afabd73f6ac5a3ac7aa65

compilers: forward gen_vs_module_defs_args to linker
This adds support for DEF files to rust and removes
some hasattr duck typing.

Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/linkers/linkers.py
@@ -66,6 +66,9 @@ class StaticLinker:
     def get_coverage_link_args(self) -> T.List[str]:
         return []
 
+    def gen_vs_module_defs_args(self) -> T.List[str]:
+        return []
+
     def build_rpath_args(self, build_dir: str, from_dir: str, target: BuildTarget,
                          extra_paths: T.Optional[T.List[str]] = None
                          ) -> T.Tuple[T.List[str], T.Set[bytes]]:

mesonbuild/backend/ninjabackend.py
@@ -3525,7 +3525,7 @@ https://gcc.gnu.org/bugzilla/show_bug.cgi?id=47485'''))
                 commands += linker.gen_import_library_args(self.get_import_filename(target))
             if target.pie:
                 commands += linker.get_pie_link_args()
-            if target.vs_module_defs and hasattr(linker, 'gen_vs_module_defs_args'):
+            if target.vs_module_defs:
                 commands += linker.gen_vs_module_defs_args(target.vs_module_defs.rel_to_builddir(self.build_to_src))
         elif isinstance(target, build.SharedLibrary):
             if isinstance(target, build.SharedModule):
@@ -3539,8 +3539,7 @@ https://gcc.gnu.org/bugzilla/show_bug.cgi?id=47485'''))
                 commands += linker.get_soname_args(
                     target.prefix, target.name, target.suffix,
                     target.soversion, target.darwin_versions)
-            # This is only visited when building for Windows using either GCC or Visual Studio
-            if target.vs_module_defs and hasattr(linker, 'gen_vs_module_defs_args'):
+            if target.vs_module_defs:
                 commands += linker.gen_vs_module_defs_args(target.vs_module_defs.rel_to_builddir(self.build_to_src))
             # This is only visited when building for Windows using either GCC or Visual Studio
             if target.import_filename:

mesonbuild/compilers/compilers.py
@@ -1098,6 +1098,9 @@ class Compiler(HoldableObject, metaclass=abc.ABCMeta):
     def get_coverage_link_args(self) -> T.List[str]:
         return self.linker.get_coverage_args()
 
+    def gen_vs_module_defs_args(self, defsfile: str) -> T.List[str]:
+        return self.linker.gen_vs_module_defs_args(defsfile)
+
     def get_assert_args(self, disable: bool) -> T.List[str]:
         """Get arguments to enable or disable assertion.
 

mesonbuild/compilers/mixins/clang.py
@@ -172,13 +172,6 @@ class ClangCompiler(GnuLikeCompiler):
             # Shouldn't work, but it'll be checked explicitly in the OpenMP dependency.
             return []
 
-    def gen_vs_module_defs_args(self, defsfile: str) -> T.List[str]:
-        if isinstance(self.linker, VisualStudioLikeLinkerMixin):
-            # With MSVC, DLLs only export symbols that are explicitly exported,
-            # so if a module defs file is specified, we use that to export symbols
-            return ['-Wl,/DEF:' + defsfile]
-        return super().gen_vs_module_defs_args(defsfile)
-
     @classmethod
     def use_linker_args(cls, linker: str, version: str) -> T.List[str]:
         # Clang additionally can use a linker specified as a path, which GCC

mesonbuild/compilers/mixins/gnu.py
@@ -409,16 +409,6 @@ class GnuLikeCompiler(Compiler, metaclass=abc.ABCMeta):
             vistype = 'hidden'
         return gnu_symbol_visibility_args[vistype]
 
-    def gen_vs_module_defs_args(self, defsfile: str) -> T.List[str]:
-        if not isinstance(defsfile, str):
-            raise RuntimeError('Module definitions file should be str')
-        # On Windows targets, .def files may be specified on the linker command
-        # line like an object file.
-        if self.info.is_windows() or self.info.is_cygwin():
-            return [defsfile]
-        # For other targets, discard the .def file.
-        return []
-
     @staticmethod
     def get_argument_syntax() -> str:
         return 'gcc'

mesonbuild/compilers/mixins/visualstudio.py
@@ -192,13 +192,6 @@ class VisualStudioLikeCompiler(Compiler, metaclass=abc.ABCMeta):
     def get_pic_args(self) -> T.List[str]:
         return [] # PIC is handled by the loader on Windows
 
-    def gen_vs_module_defs_args(self, defsfile: str) -> T.List[str]:
-        if not isinstance(defsfile, str):
-            raise RuntimeError('Module definitions file should be str')
-        # With MSVC, DLLs only export symbols that are explicitly exported,
-        # so if a module defs file is specified, we use that to export symbols
-        return ['/DEF:' + defsfile]
-
     def gen_pch_args(self, header: str, source: str, pchname: str) -> T.Tuple[str, T.List[str]]:
         objname = os.path.splitext(source)[0] + '.obj'
         return objname, ['/Yc' + header, '/Fp' + pchname, '/Fo' + objname]

mesonbuild/compilers/rust.py
@@ -385,6 +385,9 @@ class RustCompiler(Compiler):
     def get_coverage_link_args(self) -> T.List[str]:
         return rustc_link_args(super().get_coverage_link_args())
 
+    def gen_vs_module_defs_args(self, defsfile: str) -> T.List[str]:
+        return rustc_link_args(super().gen_vs_module_defs_args(defsfile))
+
     def get_profile_generate_args(self) -> T.List[str]:
         return ['-C', 'profile-generate']
 

commit 2b0800d02da143ef77a9d8de7051dea5ead372c8

linkers: fix passing a .def file to lld-link via clang/clang++
While clang-cl is able to handle /DEF:... just fine, the same
is not true when using clang++ together with lld-link.  This
results in a failure:

    [2/3] Linking target qga/vss-win32/qga-vss.dll
    FAILED: qga/vss-win32/qga-vss.dll qga/vss-win32/qga-vss.pdb
    "clang++"  -Wl,/OUT:qga/vss-win32/qga-vss.dll qga/vss-win32/qga-vss.dll.p/requester.cpp.obj qga/vss-win32/qga-vss.dll.p/provider.cpp.obj qga/vss-win32/qga-vss.dll.p/install.cpp.obj qga/vss-win32/qga-vss.dll.p/vss-debug.cpp.obj "-Wl,/nodefaultlib:libcmt" "-Wl,/nologo" "-Wl,/DEBUG" "-Wl,/PDB:qga\vss-win32\qga-vss.pdb" "-Wl,/DLL" "/DEF:..\qga\vss-win32\qga-vss.def" "-Wl,/IMPLIB:qga\vss-win32\qga-vss.lib" "-std=gnu++14" "-fuse-ld=lld" "-lws2_32" "-lole32" "-loleaut32" "-lshlwapi" "-luuid" "-lkernel32" "-luser32" "-lgdi32" "-lwinspool" "-lshell32" "-lcomdlg32" "-ladvapi32"
    clang++: error: no such file or directory: /DEF:..qgavss-win32qga-vss.def

This is due to a missing call to _apply_prefix.

Fixes: b1b229871 ("compilers: forward gen_vs_module_defs_args to linker", 2025-11-23)
Reported-by: Konstantin Kostiuk <kkostiuk@redhat.com>
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/linkers/linkers.py
@@ -1489,7 +1489,7 @@ class VisualStudioLikeLinkerMixin(DynamicLinkerBase):
     def gen_vs_module_defs_args(self, defsfile: str) -> T.List[str]:
         # With MSVC, DLLs only export symbols that are explicitly exported,
         # so if a module defs file is specified, we use that to export symbols
-        return ['/DEF:' + defsfile]
+        return self._apply_prefix(['/DEF:' + defsfile])
 
     def get_soname_args(self, prefix: str, shlib_name: str, suffix: str,
                         soversion: str, darwin_versions: T.Tuple[str, str]