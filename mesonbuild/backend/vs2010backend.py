mesonbuild/meson

commit 6835b6ed6ba7e0ad2d70c6c80a12b611766a7d06

vs2010backend: integrate b_lto/b_pgo with vcxproj backends

mesonbuild/backend/vs2010backend.py
@@ -1303,7 +1303,7 @@ class Vs2010Backend(backends.Backend):
             self,
             root: ET.Element,
             type_config: ET.Element,
-            target,
+            target: build.BuildTarget,
             platform: str,
             subsystem,
             build_args,
@@ -1321,8 +1321,20 @@ class Vs2010Backend(backends.Backend):
         # FIXME: Should the following just be set in create_basic_project(), even if
         # irrelevant for current target?
 
-        # FIXME: Meson's LTO support needs to be integrated here
-        ET.SubElement(type_config, 'WholeProgramOptimization').text = 'false'
+        lto = self.get_target_option(target, 'b_lto')
+        pgo = self.get_target_option(target, 'b_pgo')
+
+        if lto:
+            if pgo == 'off':
+                ET.SubElement(type_config, 'WholeProgramOptimization').text = 'true'
+            elif pgo == 'generate':
+                ET.SubElement(type_config, 'WholeProgramOptimization').text = 'PGInstrument'
+            elif pgo == 'use':
+                ET.SubElement(type_config, 'WholeProgramOptimization').text = 'PGOptimize'
+        else:
+            if pgo != 'off':
+                raise MesonException("b_pgo requires b_lto on MSVC.")
+
         # Let VS auto-set the RTC level
         ET.SubElement(type_config, 'BasicRuntimeChecks').text = 'Default'
         # Incremental linking increases code size

commit 0f350b2b2a377d2b35f3c6d72568d74bb1df4733

vs2010backend: allow missing b_lto or b_pgo options
If the options are missing, do not enable LTO or PGO.

As a side effect of cleaning up the code, the WholeProgramOptimization
element is always emitted as was the case before commit 6835b6ed6
("vs2010backend: integrate b_lto/b_pgo with vcxproj backends", 2026-02-06).

Fixes: 6835b6ed6 ("vs2010backend: integrate b_lto/b_pgo with vcxproj backends", 2026-02-06)
Fixes: #15702
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/backend/vs2010backend.py
@@ -1234,6 +1234,22 @@ class Vs2010Backend(backends.Backend):
 
         return (nmake_base_meson_command, exe_search_paths)
 
+    def get_target_whole_program_opt(self, target: build.BuildTarget) -> str:
+        lto = False
+        pgo = 'off'
+        try:
+            lto = self.get_target_option(target, 'b_lto')
+            pgo = self.get_target_option(target, 'b_pgo')
+        except KeyError:
+            pass
+
+        if pgo == 'off':
+            return 'true' if lto else 'false'
+
+        if not lto:
+            raise MesonException("b_pgo requires b_lto on MSVC.")
+        return 'PGInstrument' if pgo == 'generate' else 'PGOptimize'
+
     def add_gen_lite_makefile_vcxproj_elements(self,
                                                root: ET.Element,
                                                platform: str,
@@ -1322,19 +1338,7 @@ class Vs2010Backend(backends.Backend):
         # FIXME: Should the following just be set in create_basic_project(), even if
         # irrelevant for current target?
 
-        lto = self.get_target_option(target, 'b_lto')
-        pgo = self.get_target_option(target, 'b_pgo')
-
-        if lto:
-            if pgo == 'off':
-                ET.SubElement(type_config, 'WholeProgramOptimization').text = 'true'
-            elif pgo == 'generate':
-                ET.SubElement(type_config, 'WholeProgramOptimization').text = 'PGInstrument'
-            elif pgo == 'use':
-                ET.SubElement(type_config, 'WholeProgramOptimization').text = 'PGOptimize'
-        else:
-            if pgo != 'off':
-                raise MesonException("b_pgo requires b_lto on MSVC.")
+        ET.SubElement(type_config, 'WholeProgramOptimization').text = self.get_target_whole_program_opt(target)
 
         # Let VS auto-set the RTC level
         ET.SubElement(type_config, 'BasicRuntimeChecks').text = 'Default'