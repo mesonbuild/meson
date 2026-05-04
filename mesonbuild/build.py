362653ca61b4c66ace5d56a80f3fbddddddf422d
Dylan Baker <dylan@pnwbakers.com>
2026-05-04 12:19:14 -0700

    build: use copy.copy instead of open coding
    This fixes some typing issues, as well as being more generically
    correct.

--- mesonbuild/build.py
+++ mesonbuild/build.py
@@ -440,18 +440,9 @@ class Build:
         return custom_targets
 
     def copy(self) -> Build:
-        def copy_value(v: _T) -> _T:
-            if isinstance(v, PerMachine):
-                build = copy_value(v.build)
-                host = build if v.build is v.host else copy_value(v.host)
-                return PerMachine(build=build, host=host)
-            if isinstance(v, (list, dict, set, OrderedDict)):
-                return v.copy()
-            return v
-
         other = Build.__new__(Build)
         for k, v in self.__dict__.items():
-            other.__dict__[k] = copy_value(v)
+            other.__dict__[k] = copy.copy(v)
         return other
 
     def merge(self, other: Build) -> None:
