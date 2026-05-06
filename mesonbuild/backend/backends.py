8aef8627d40c8089cd0d30036a666eb286c06d7e
Paolo Bonzini <pbonzini@redhat.com>
2026-05-06 10:31:37 +0200

    backends: fix type for get_target_depend_files()
    Both the ninja and vs2010 backends call get_target_depend_files on a
    GeneratedList.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- mesonbuild/backend/backends.py
+++ mesonbuild/backend/backends.py
@@ -1469,7 +1469,7 @@ class Backend:
             srcs += fname
         return srcs
 
-    def get_target_depend_files(self, target: T.Union[build.CustomTarget, build.BuildTarget], absolute_paths: bool = False) -> T.List[str]:
+    def get_target_depend_files(self, target: T.Union[build.CustomTarget, build.BuildTarget, build.GeneratedList], absolute_paths: bool = False) -> T.List[str]:
         deps: T.List[str] = []
         for i in target.depend_files:
             if isinstance(i, mesonlib.File):
