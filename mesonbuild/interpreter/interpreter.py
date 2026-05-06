mesonbuild/meson

commit 57bf0c857626cc71e715886d6d83e78ad5f6e31d

cargo: allow overriding Meson's Cargo interpreter
Some projects may want to override Meson's AST generation for Cargo
projects.  This was not really doable before without hard coding the
results of feature resolution; however, now it will be possible by
accessing the results of the global feature resolution from the Rust
module's workspace object.

At the same time, the subproject must keep using the Cargo method,
which is forced by the workspace object's subproject() method,
because otherwise the interpreter is not propagated.  So just
skip the interpretation phase if a Meson.build is present.

Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/interpreter/interpreter.py
@@ -1058,7 +1058,11 @@ class Interpreter(InterpreterBase, HoldableObject):
             except cargo.TomlImplementationMissing as e:
                 raise MesonException(f'Failed to load Cargo.lock: {e!s}')
 
-            ast = cargo_int.interpret(subdir)
+            if os.path.exists(os.path.join(self.environment.get_source_dir(), subdir, environment.build_filename)):
+                ast = None
+            else:
+                ast = cargo_int.interpret(subdir)
+
             return self._do_subproject_meson(
                 subp_name, subdir, default_options, kwargs, ast,
                 relaxations={InterpreterRuleRelaxation.CARGO_SUBDIR},

commit a477b56072f020b8e0aafa0861f31cbe4b83e506

interpreter: do not apply CARGO_SUBDIR if subproject has a meson.build
Commit 57bf0c857 ("cargo: allow overriding Meson's Cargo interpreter")
allow using Cargo subprojects to share the parent's Cargo.lock file,
even for subprojects that have their own meson.build file.

However, in that case subdir() does not represent delayed generation
of the meson.build rules for a workspace member---that would not even
work if the subdir() is not for a crate but, say, for documentation.
Drop CARGO_SUBDIR from the interpreter relaxations if _do_subproject_cargo()
did not generate the subproject's AST.

Fixes: 57bf0c857 ("cargo: allow overriding Meson's Cargo interpreter")
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/interpreter/interpreter.py
@@ -1068,7 +1068,7 @@ class Interpreter(InterpreterBase, HoldableObject):
 
             return self._do_subproject_meson(
                 subp_name, subdir, default_options, kwargs, ast,
-                relaxations={InterpreterRuleRelaxation.CARGO_SUBDIR},
+                relaxations={InterpreterRuleRelaxation.CARGO_SUBDIR} if ast is not None else None,
                 cargo=cargo_int)
 
     @typed_pos_args('get_option', str)