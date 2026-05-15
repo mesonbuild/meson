6cabb53045454cc453cbf480e407eb3aa20e5882
Paolo Bonzini <pbonzini@redhat.com>
2026-05-15 17:52:53 +0200

    interpreter: fix typos in typed_kwargs
    Both add_project_link_arguments and add_global_link_arguments had a
    cut-and-paste mistake in the @typed_kwargs() decorator, fix it
    (again).
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- mesonbuild/interpreter/interpreter.py
+++ mesonbuild/interpreter/interpreter.py
@@ -2963,7 +2963,7 @@ class Interpreter(InterpreterBase, HoldableObject):
         self._add_global_arguments(node, self.build.global_args[kwargs['native']], args[0], kwargs)
 
     @typed_pos_args('add_global_link_arguments', varargs=str)
-    @typed_kwargs('add_global_arguments', NATIVE_KW, LANGUAGE_KW)
+    @typed_kwargs('add_global_link_arguments', NATIVE_KW, LANGUAGE_KW)
     def func_add_global_link_arguments(self, node: mparser.FunctionNode, args: T.Tuple[T.List[str]], kwargs: 'kwtypes.FuncAddProjectArgs') -> None:
         self._add_global_arguments(node, self.build.global_link_args[kwargs['native']], args[0], kwargs)
 
@@ -2974,7 +2974,7 @@ class Interpreter(InterpreterBase, HoldableObject):
                                     args[0], kwargs)
 
     @typed_pos_args('add_project_link_arguments', varargs=str)
-    @typed_kwargs('add_project_arguments', NATIVE_KW, LANGUAGE_KW)
+    @typed_kwargs('add_project_link_arguments', NATIVE_KW, LANGUAGE_KW)
     def func_add_project_link_arguments(self, node: mparser.FunctionNode, args: T.Tuple[T.List[str]], kwargs: 'kwtypes.FuncAddProjectArgs') -> None:
         self._add_project_arguments(node, self.current_build_project().project_link_args[kwargs['native']],
                                     args[0], kwargs)
