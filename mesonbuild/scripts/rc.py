b86eb777c3dc5d1c1c05d8153f3c14b5f35f2f4f
L. E. Segovia <amy@centricular.com>
2026-06-06 21:46:39 -0300

    windows: Implement RC depfile generation for clang targeting MSVC

--- mesonbuild/scripts/rc.py
+++ mesonbuild/scripts/rc.py
@@ -1,41 +1,37 @@
 # SPDX-License-Identifier: Apache-2.0
 
-"""Wrapper for rc.exe to make it output included files."""
+"""Wrapper for manually generating depfiles of resource files."""
 
 from __future__ import annotations
 
 import argparse
 import subprocess
-import shutil
+import sys
 import typing as T
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument('--rc')
-parser.add_argument('--cl')
+parser.add_argument('--rc', nargs='+', required=True)
+parser.add_argument('--cl', nargs='+', required=True)
+parser.add_argument('--Xarg', action='append')
 
 
 def run(args: T.List[str]) -> int:
     options, rc_args = parser.parse_known_args(args)
     target = rc_args[-1] if rc_args else None
 
-    rc = options.rc if options.rc else shutil.which('rc.exe')
-    if rc is None:
-        print('rc.exe not found')
-        return 1
-
-    cl = options.cl if options.cl else shutil.which('cl.exe')
-    if cl is None:
-        print('cl.exe not found')
-        return 1
+    rc = options.rc
+    cl = options.cl
 
     # Use preprocessor to display include files
     include_args = [a for a in rc_args if a.startswith(('/I', '-I'))]
-    cmd = [cl, '/showIncludes', '/EP', '/nologo', '/DRC_INVOKED'] + include_args + ['/Tc' + target]
+    cmd = [*cl, *include_args, *options.Xarg]
+    if target:
+        cmd += [target]
     result = subprocess.call(cmd, stdout=subprocess.DEVNULL)
     if result != 0:
-        print('Error running preprocessor to find resource dependencies')
-        # continue anyway. rc.exe should catch the error later
+        print('Error running preprocessor to find resource dependencies', file=sys.stderr)
+        # continue anyway. the resource compiler should catch the error later
 
-    cmd = [rc] + rc_args
+    cmd = [*rc, *rc_args]
     return subprocess.call(cmd)
