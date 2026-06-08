044576c5f1deda3c8562af65aeab50a742e8a512
Dylan Baker <dylan@pnwbakers.com>
2026-06-08 10:53:30 -0700

    interpreter: Add `embed_directories` keyword argument
    This is added to `declare_dependency()` and `build_target()`, which is
    meant to be used for C23 and C++26 `#embed` support.

--- mesonbuild/interpreter/type_checking.py
+++ mesonbuild/interpreter/type_checking.py
@@ -425,6 +425,11 @@ INCLUDE_DIRECTORIES: KwargInfo[T.List[T.Union[str, IncludeDirs]]] = KwargInfo(
     default=[],
 )
 
+EMBED_DIRECTORIES = INCLUDE_DIRECTORIES.evolve(
+    name='embed_directories',
+    since='1.13.0',
+)
+
 def _default_options_convertor(raw: T.Union[str, T.List[str], T.Dict[str, ElementaryOptionValues]]) -> T.Dict[OptionKey, ElementaryOptionValues]:
     d = _override_options_convertor(raw)
     return {OptionKey.from_string(k): v for k, v in d.items()}
@@ -800,6 +805,7 @@ _BUILD_TARGET_KWS: T.List[KwargInfo] = [
     *_LANGUAGE_KWS,
     BT_SOURCES_KW,
     INCLUDE_DIRECTORIES.evolve(name='d_import_dirs'),
+    EMBED_DIRECTORIES,
     LINK_ARGS_KW,
     LINK_WHOLE_KW.evolve(
         since='0.40.0',
