mesonbuild/meson

commit 61c5859b771eb938f20935cc3da54a9075d24c64

envconfig: Cannot run executables if subsystem doesn't match
Without this, building for iOS while using subsystem names instead of
`system = 'ios'` doesn't work correctly.

mesonbuild/envconfig.py
@@ -751,7 +751,10 @@ def machine_info_can_run(machine_info: MachineInfo) -> bool:
     detect_cpu_family() here because we always want to know the OS
     architecture, not what the compiler environment tells us.
     """
-    if machine_info.system != detect_system():
+    system = detect_system()
+    if machine_info.system != system:
+        return False
+    if machine_info.subsystem != detect_subsystem(system):
         return False
     true_build_cpu_family = detect_cpu_family({})
     assert machine_info.cpu_family is not None, 'called on incomplete machine_info'

commit 1892dd0822c4b7911fb9ef16adebdca854fd65ac

envconfig: allow running executables if subsystem is not set
detect_subsystem(system) is just a simple transformation if
system; do not require setting machine_info.subsystem at all
costs, and if it is unset just assume that it is okay since
the system has already been matched.

Regression introduced by commit 61c5859b7 ("envconfig: Cannot
run executables if subsystem doesn't match", 2026-02-04).

Fixes: #15840
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/envconfig.py
@@ -758,7 +758,7 @@ def machine_info_can_run(machine_info: MachineInfo) -> bool:
     system = detect_system()
     if machine_info.system != system:
         return False
-    if machine_info.subsystem != detect_subsystem(system):
+    if machine_info.subsystem and machine_info.subsystem != detect_subsystem(system):
         return False
     true_build_cpu_family = detect_cpu_family({})
     assert machine_info.cpu_family is not None, 'called on incomplete machine_info'