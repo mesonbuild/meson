2a418f9b25e6f74b4d619e502395adc15207b775
Aleksandr Kovalko <gistrec@gmail.com>
2026-07-07 19:35:24 +0200

    compilers/cpp: add vc++23 to ALL_STDS
    ClangClCPPCompiler.get_options() registers 'vc++23' as a valid cpp_std
    value (since commit 3b7c81d4, 'clang-cl: add c++23 support ...'), but
    'vc++23' was never added to the ALL_STDS list that backs the cpp_std
    UserStdOption. UserStdOption.set_versions() therefore trips its
    
        assert all(std in self.all_stds for std in versions)
    
    as soon as a clang-cl C++ compiler is detected, aborting configuration
    with an AssertionError ("Unhandled python exception") for every C++
    project built with clang-cl. MSVC is unaffected because it never
    registers the vc++23 standard.
    
    Add the missing 'vc++23' entry so the compiler's advertised stds are a
    subset of ALL_STDS again.

--- mesonbuild/compilers/cpp.py
+++ mesonbuild/compilers/cpp.py
@@ -48,7 +48,7 @@ else:
 ALL_STDS = ['c++98', 'c++0x', 'c++03', 'c++1y', 'c++1z', 'c++11', 'c++14', 'c++17']
 ALL_STDS += ['c++2a', 'c++2b', 'c++2c', 'c++20', 'c++23', 'c++26']
 ALL_STDS += [f'gnu{std[1:]}' for std in ALL_STDS]
-ALL_STDS += ['vc++11', 'vc++14', 'vc++17', 'vc++20', 'vc++latest', 'c++latest']
+ALL_STDS += ['vc++11', 'vc++14', 'vc++17', 'vc++20', 'vc++23', 'vc++latest', 'c++latest']
 
 
 def non_msvc_eh_options(eh: str, args: T.List[str]) -> None:
