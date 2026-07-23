0a7bc034fa5c6ca5b9d3cba3f260c046f6bd8218
Dylan Baker <dylan@pnwbakers.com>
2026-07-23 16:46:46 +0000

    interpreter/type_checking: quote unknown langauge validator error

--- mesonbuild/interpreter/type_checking.py
+++ mesonbuild/interpreter/type_checking.py
@@ -61,7 +61,7 @@ def _language_validator(l: T.List[str]) -> T.Optional[str]:
     """
     diff = {a.lower() for a in l}.difference(compilers.all_languages)
     if diff:
-        return f'unknown languages: {", ".join(diff)}'
+        return f'unknown languages: {", ".join(_quote(diff))}'
     return None
 
 
