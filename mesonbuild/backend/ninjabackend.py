mesonbuild/meson

commit 1e50fe1adb64b5b7dae8b73df1e5b6ca5f6f37bc

ninjabackend: msvc: make detect_vs_dep_prefix independent of stdlib
this function does not pass enough args from the compiler object to find
windows sdk without relying on environment variables.
including a file from the same directory removes the dependency on stdlib.

mesonbuild/backend/ninjabackend.py
@@ -567,9 +567,13 @@ class NinjaBackend(backends.Backend):
                                 filebase)
         with open(filename, 'w', encoding='utf-8') as f:
             f.write(dedent('''\
-                #include<stdio.h>
+                #include"incdetect2"
                 int dummy;
             '''))
+        filename_inc = os.path.join(self.environment.get_scratch_dir(),
+                                    'incdetect2')
+        with open(filename_inc, 'w', encoding='utf-8') as f:
+            pass
 
         # The output of cl dependency information is language
         # and locale dependent. Any attempt at converting it to
@@ -588,18 +592,18 @@ class NinjaBackend(backends.Backend):
                               stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         (stdout, stderr) = pc.communicate()
 
-        # We want to match 'Note: including file: ' in the line
-        # 'Note: including file: d:\MyDir\include\stdio.h', however
+        # 'Note: including file: d:\build\meson-private\incdetect2', however
         # different locales have different messages with a different
-        # number of colons. Match up to the drive name 'd:\'.
+        # number of colons. Match up to the drive name 'd:\' or
+        # a relative path '.\'.
         # When used in cross compilation, the path separator is a
         # forward slash rather than a backslash so handle both; i.e.
-        # the path is /MyDir/include/stdio.h.
+        # the path is /build/meson-private/incdetect or ./incdetect2.
         # With certain cross compilation wrappings of MSVC, the paths
         # use backslashes, but without the leading drive name, so
         # allow the path to start with any path separator, i.e.
-        # \MyDir\include\stdio.h.
-        matchre = re.compile(rb"^(.*\s)([a-zA-Z]:[\\/]|[\\\/]).*stdio.h$")
+        # \build\meson-private\incdetect2
+        matchre = re.compile(rb"^(.*\s)([a-zA-Z]:[\\/]|\.?[\\/]).*incdetect2$")
 
         def detect_prefix(out: bytes) -> T.TextIO:
             for line in re.split(rb'\r?\n', out):

commit e0edeb56077eaa693ab75e4b2d255b8c9bfb1f00

Fix dep dependency prefix detection
Fix regression introduced by 1e50fe1.

It is possible that the reported relative path has not ./ prefix at
all: e.g. "Note: including file: incdetect2". This fixes the regex
for that case.

mesonbuild/backend/ninjabackend.py
@@ -653,7 +653,7 @@ class NinjaBackend(backends.Backend):
         # 'Note: including file: d:\build\meson-private\incdetect2', however
         # different locales have different messages with a different
         # number of colons. Match up to the drive name 'd:\' or
-        # a relative path '.\'.
+        # a relative path '.\', or no path at all.
         # When used in cross compilation, the path separator is a
         # forward slash rather than a backslash so handle both; i.e.
         # the path is /build/meson-private/incdetect or ./incdetect2.
@@ -661,7 +661,7 @@ class NinjaBackend(backends.Backend):
         # use backslashes, but without the leading drive name, so
         # allow the path to start with any path separator, i.e.
         # \build\meson-private\incdetect2
-        matchre = re.compile(rb"^(.*\s)([a-zA-Z]:[\\/]|\.?[\\/]).*incdetect2$")
+        matchre = re.compile(rb"^(.*\s)([a-zA-Z]:[\\/].*|\.?[\\/].*|)incdetect2$")
 
         def detect_prefix(out: bytes) -> T.TextIO:
             for line in re.split(rb'\r?\n', out):