14933d4bba945e1e0ba6f674014b7c906284f941
Jussi Pakkanen <jussi.pakkanen@mailbox.org>
2026-08-13 13:52:23 +0300

    Ensure coverage info is not empty.
    This seems to be caused by a change in lcov. Originally detected
    in Debian:
    
    https: //bugs.debian.org/cgi-bin/bugreport.cgi?bug=1144176
    (cherry picked from commit aa397a895ea18e409377293c3c172c695c6817a0)

--- test cases/common/1 trivial/trivial.c
+++ test cases/common/1 trivial/trivial.c
@@ -1,6 +1,11 @@
 #include<stdio.h>
 
-int main(void) {
+int main(int argc, char **argv) {
+    /* Have a non-elidable branch to make sure coverage info is not empty. */
+    if(argc > 1) {
+        printf("A command line argument was passed to the program.\n");
+        (void)argv;
+    }
     printf("Trivial test is working.\n");
     return 0;
 }
--- test cases/common/243 escape++/test.c
+++ test cases/common/243 escape++/test.c
@@ -1,3 +1,9 @@
-int main(void) {
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+    if(argc > 1) {
+        printf("A command line argument was passed to this program.\n");
+        (void)argv;
+    }
     return 0;
 }
