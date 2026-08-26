d64a1f4d1f2511d82592b4ef92adf246cda6a528
Paolo Bonzini <pbonzini@redhat.com>
2026-08-26 15:31:04 +0200

    build: introduce AnyTargetProto
    Document precisely what is required, and in particular that
    AnyTargetProto is not hashable.  The old type union
    is not needed anymore.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- mesonbuild/interpreter/kwargs.py
+++ mesonbuild/interpreter/kwargs.py
@@ -201,7 +201,7 @@ class FuncAddLanguages(ExtractRequired):
 class RunTarget(TypedDict):
 
     command: T.List[build.CommandTypes]
-    depends: T.List[build.AnyTargetType | Program]
+    depends: T.List[build.AnyTargetProto | Program]
     env: EnvironmentVariables
 
 
