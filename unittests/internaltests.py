mesonbuild/meson

commit 300d7c39fca1c57afee2ae3d274a1a8de29507a8

compilers: add link always args if CompileCheckMode.LINK
On OS/2, -Zomf should be used to link against both aout and omf libs. If
-Zomf is missing, omf libs cannot be linked.

When using --prefer-static, -Zomf is missed and has_function() test
fails if dependency is specified.

mesonbuild/compilers/compilers.py
@@ -617,7 +617,7 @@ class Compiler(HoldableObject, metaclass=abc.ABCMeta):
         return self.linker.get_accepts_rsp()
 
     def get_linker_always_args(self) -> T.List[str]:
-        return self.linker.get_always_args()
+        return self.linker.get_always_args() if self.linker else []
 
     def get_linker_lib_prefix(self) -> str:
         return self.linker.get_lib_prefix()
@@ -819,6 +819,7 @@ class Compiler(HoldableObject, metaclass=abc.ABCMeta):
             args += self.get_preprocess_only_args()
         else:
             assert mode is CompileCheckMode.LINK
+            args += self.get_linker_always_args()
         return args
 
     def compiler_args(self, args: T.Optional[T.Iterable[str]] = None) -> CompilerArgs:

commit b33d81f5c77fae611a267939703a6836fe503c74

compilers: pass linker always args as linker, not compiler arguments
Commit 300d7c39f ("compilers: add link always args if CompileCheckMode.LINK",
2026-01-07) added get_linker_always_args() to get_compiler_args_for_mode(),
but that function returns *compiler* arguments, and it is applied twice
to a compile check: once by CLikeCompiler._get_basic_compiler_args()
and once by Compiler.compile().

For MSVC this puts linker-only flags on the compiler command line twice
*and* in the wrong place:

  cl ... /release /nologo ... /release /nologo ... /link foo.lib

Add them to the linker argument list that is passed through
linker_to_compiler_args() instead of going through get_compiler_args_for_mode().
While that appears twice (in build_wrapper_args() and in the sanity check),
it is not necessary anymore to add linker always args in find_library()
now that build_wrapper_args() adds them.  Note that this does not include
the Ninja backend which has a call to get_linker_always_args() of its own.

Fixes: #16086
Fixes: 300d7c39f ("compilers: add link always args if CompileCheckMode.LINK", 2026-01-07)
Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

mesonbuild/compilers/compilers.py
@@ -909,7 +909,6 @@ class Compiler(HoldableObject, metaclass=SimpleABC):
             args += self.get_preprocess_only_args()
         else:
             assert mode is CompileCheckMode.LINK
-            args += self.get_linker_always_args()
         return args
 
     def compiler_args(self, args: T.Optional[T.Iterable[str]] = None) -> CompilerArgs:
@@ -1611,6 +1610,7 @@ class Compiler(HoldableObject, metaclass=SimpleABC):
             args += T.cast('T.List[str]', self.environment.coredata.optstore.get_value_for(
                 OptionKey(f'{self.language}_args', machine=self.for_machine)))
         elif mode is CompileCheckMode.LINK:
+            args += self.get_linker_always_args()
             # Add LDFLAGS from the env
             args += T.cast('T.List[str]', self.environment.coredata.optstore.get_value_for(
                 OptionKey(f'{self.language}_link_args', machine=self.for_machine)))

mesonbuild/compilers/mixins/clike.py
@@ -368,6 +368,8 @@ class CLikeCompiler(Compiler):
         cargs += cleaned_sys_args
 
         if mode is CompileCheckMode.LINK:
+            largs += self.get_linker_always_args()
+
             ld_value = self.environment.lookup_binary_entry(self.for_machine, self.language + '_ld')
             if ld_value is not None:
                 cargs += self.use_linker_args(ld_value[0], self.version)
@@ -1133,7 +1135,7 @@ class CLikeCompiler(Compiler):
         if ((not extra_dirs and libtype is LibType.PREFER_SHARED) or
                 libname in self.internal_libs):
             cargs = ['-l' + libname]
-            largs = self.get_linker_always_args() + self.get_allow_undefined_link_args()
+            largs = self.get_allow_undefined_link_args()
             extra_args = cargs + self.linker_to_compiler_args(largs)
 
             if self.links(code, extra_args=extra_args, disable_cache=True)[0]:
@@ -1155,7 +1157,7 @@ class CLikeCompiler(Compiler):
         except (mesonlib.MesonException, KeyError): # TODO evaluate if catching KeyError is wanted here
             elf_class = 0
         # Search in the specified dirs, and then in the system libraries
-        largs = self.get_linker_always_args() + self.get_allow_undefined_link_args()
+        largs = self.get_allow_undefined_link_args()
         lcargs = self.linker_to_compiler_args(largs)
         for d in itertools.chain(extra_dirs, [] if ignore_system_dirs else self.get_library_dirs(elf_class)):
             for p in patterns:

unittests/internaltests.py
@@ -505,6 +505,29 @@ Thread model: posix'''), '21.9.0')
                             ManyInOneLinkerOptionStyle('-Wl,', ','), [])
         return GnuCCompiler([], [], 'fake', MachineChoice.HOST, env, linker=linker)
 
+    def assertAfterLink(self, args: T.List[str], flag: str) -> None:
+        '''Assert that a linker-only flag is passed exactly once, after /link.'''
+        self.assertEqual(args.count(flag), 1, f'{flag} not passed exactly once in {args}')
+        self.assertIn('/link', args)
+        self.assertLess(args.index('/link'), args.index(flag), f'{flag} passed before /link in {args}')
+
+    def test_sanity_check_args_msvc(self):
+        cc = self._fake_msvc_cc()
+        args, largs = cc._sanity_check_compile_args('t.c', 't.exe')
+        # external link args are passed once, and after /link
+        self.assertAfterLink(largs, '/SUBSYSTEM:CONSOLE')
+        self.assertNotIn('/SUBSYSTEM:CONSOLE', args)
+        # so are the linker's own always args
+        self.assertAfterLink(largs, '/release')
+        self.assertNotIn('/release', args)
+        # external compile args are passed to the compiler, not to the linker
+        self.assertNotIn('-DCFLAG', largs)
+        self.assertEqual(args.count('-DCFLAG'), 1, args)
+        # linking, so name the executable with /Fe even if it is not a .exe
+        self.assertIn('/Fet.exe', args)
+        dll_args, _ = cc._sanity_check_compile_args('t.c', 't.dll')
+        self.assertIn('/Fet.dll', dll_args)
+
     def test_sanity_check_args_gnu(self):
         cc = self._fake_gnu_cc()
         args, largs = cc._sanity_check_compile_args('t.c', 't.exe')
@@ -512,6 +535,16 @@ Thread model: posix'''), '21.9.0')
         self.assertEqual(args.count('-DCFLAG'), 1, args)
         self.assertEqual((args + largs).count('-Wl,-O1'), 1, (args, largs))
         self.assertIn('-Wl,-O1', largs)
+
+    def test_compiler_check_args_msvc(self):
+        cc = self._fake_msvc_cc()
+        args = cc.build_wrapper_args(None, None, CompileCheckMode.LINK).to_native()
+        # linker-only flags belong after /link, not on the compiler command line
+        self.assertAfterLink(args, '/release')
+        self.assertAfterLink(args, '/SUBSYSTEM:CONSOLE')
+        args = cc.build_wrapper_args(None, None, CompileCheckMode.COMPILE).to_native()
+        self.assertNotIn('/release', args)
+        self.assertNotIn('/link', args)
 
     def test_compiler_check_args_os2(self):
         # the linker's always args (-Zomf on OS/2) must reach link mode checks
@@ -525,7 +558,7 @@ Thread model: posix'''), '21.9.0')
 
         def fake_links(code, *, extra_args=None, **kwargs):
             args = cc.build_wrapper_args(extra_args, None, CompileCheckMode.LINK).to_native()
-            self.assertIn('/release', args)
+            self.assertAfterLink(args, '/release')
             return (True, False)
 
         with mock.patch.object(cc, 'links', fake_links):