c63b2c67ebdaef9f949b44f069d693140ea4f2d6
Paolo Bonzini <pbonzini@redhat.com>
2026-09-07 11:17:56 +0200

    cargo: flatten [target] into the unconditional tables
    This simplifies the parsing and makes it possible to handle correctly
    dependency-named features, which apply equally to all dependencies,
    even those that are disabled.  In other words, even if a "win"
    dependency is only declared under [target.'cfg(windows)'.dependencies],
    "cargo build --features win" succeeds on non-Windows platforms as well.
    
    Due to the new representation, move the merged table into
    PackageConfiguration.  This is cleaner, because it doesn't modify the
    manifest, and it makes it possible to fully separate dependencies for
    the host and build machines.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- mesonbuild/modules/rust.py
+++ mesonbuild/modules/rust.py
@@ -293,7 +293,7 @@ class RustCrate(ModuleObject):
     @noKwargs
     def rust_dependency_map_method(self, state: ModuleState, args: T.List, kwargs: TYPE_kwargs) -> T.Dict[str, str]:
         """Returns rust dependency mapping for this package."""
-        return self.cfg.get_dependency_map(self.package.manifest)
+        return self.cfg.get_dependency_map()
 
 
 class RustPackage(RustCrate):
@@ -383,7 +383,7 @@ class RustPackage(RustCrate):
         kwargs['dependencies'].extend(deps)
 
         depmap = kwargs['rust_dependency_map']
-        kwargs['rust_dependency_map'] = cfg.get_dependency_map(self.package.manifest)
+        kwargs['rust_dependency_map'] = cfg.get_dependency_map()
         kwargs['rust_dependency_map'].update(depmap)
 
         rust_args = kwargs['rust_args']
