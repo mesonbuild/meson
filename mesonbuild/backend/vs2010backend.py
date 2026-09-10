d8d6aa5e8ab01921f01d221dde04ecacbe1a86d8
Paolo Bonzini <pbonzini@redhat.com>
2026-09-10 08:54:14 +0200

    vs2010backend: unify branches for SharedLibrary/Executable
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- mesonbuild/backend/vs2010backend.py
+++ mesonbuild/backend/vs2010backend.py
@@ -1532,11 +1532,11 @@ class Vs2010Backend(backends.Backend):
             # Undocumented, but you can find a reference in WDK props
             minver = ET.SubElement(link, 'MinimumRequiredVersion')
             minver.text = version
-        if isinstance(target, (build.SharedLibrary, build.Executable)) and target.get_import_filename():
-            # DLLs built with MSVC always have an import library except when
-            # they're data-only DLLs, but we don't support those yet.
-            ET.SubElement(link, 'ImportLibrary').text = target.get_import_filename()
         if isinstance(target, (build.SharedLibrary, build.Executable)):
+            if target.get_import_filename():
+                # DLLs built with MSVC always have an import library except when
+                # they're data-only DLLs, but we don't support those yet.
+                ET.SubElement(link, 'ImportLibrary').text = target.get_import_filename()
             # Add module definitions file, if provided
             if target.vs_module_defs:
                 relpath = os.path.join(down, target.vs_module_defs.rel_to_builddir(self.build_to_src))
