d6d3d63b665d6d55adce7911f9522e046d1b41fd
Paolo Bonzini <pbonzini@redhat.com>
2026-09-12 12:30:31 +0200

    cargo: reject path dependencies from outside the subproject
    This is effectively a sandbox violation.

--- mesonbuild/cargo/interpreter.py
+++ mesonbuild/cargo/interpreter.py
@@ -696,6 +696,10 @@ class Interpreter:
         assert dep.path is not None
         ws = self.workspaces[pkg.ws_subdir]
         dep_member = os.path.normpath(os.path.join(pkg.ws_member, dep.path))
+        dep_subdir = os.path.normpath(os.path.join(pkg.ws_subdir, dep_member))
+        if is_parent_path(self.subprojects_dir, pkg.ws_subdir) and \
+                not is_parent_path(pkg.ws_subdir, dep_subdir):
+            raise MesonException(f'path dependency "{dep.package}" points outside the current subproject')
         if is_parent_path(self.subprojects_dir, dep_member):
             if len(pathlib.PurePath(dep_member).parts) != 2:
                 raise MesonException('found "{self.subprojects_dir}" in path but it is not a valid subproject path')
