13736b1e6f815624fbe466a1f5bba32fbb55fa4e
Paolo Bonzini <pbonzini@redhat.com>
2026-09-12 12:14:49 +0200

    cargo: allow path dependency to come from another workspace
    A path dependency may belong to the top-level workspace even when
    its consumer belongs to a nested Cargo workspace such as a subproject
    (this happens especially in the presence of patching).
    
    In that case, when _process_member() unconditionally tries to access
    ws.packages[member] it gets a KeyError.  Only process dependencies
    that are actually part of the workspace being generated, and expect
    that other dependencies are provided via meson.override_dependency()
    by the parent process.
    
    This case was already happening for excluded dependencies, and the
    existing conditional is subsumed in the more general one.

--- mesonbuild/cargo/interpreter.py
+++ mesonbuild/cargo/interpreter.py
@@ -463,7 +463,7 @@ class Interpreter:
                     dep = cfg.dependencies[depname]
                     if dep.path:
                         dep_member = os.path.normpath(os.path.join(pkg.ws_member, dep.path))
-                        if not ws.workspace.is_excluded(dep_member):
+                        if dep_member in ws.packages:
                             _process_member(dep_member)
                 found = True
             if not found:
