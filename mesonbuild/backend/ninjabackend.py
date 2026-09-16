d70b5adfcdc54aed7d077a3d6e565c4ca7620799
Paolo Bonzini <pbonzini@redhat.com>
2026-09-16 10:48:13 +0200

    ninjabackend: simplify looking for the main Rust file in the non-copy case
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>

--- mesonbuild/backend/ninjabackend.py
+++ mesonbuild/backend/ninjabackend.py
@@ -2061,27 +2061,12 @@ class NinjaBackend(backends.Backend):
             if target.structured_sources.needs_copy():
                 _ods, main_rust_file = self.__generate_sources_structure(Path(
                     self.get_target_private_dir(target)) / 'structured', target.structured_sources, '.rs')
-                if main_rust_file is None:
-                    raise MesonException('Could not find a rust file to treat as the main file for ', target.name)
             else:
-                # The only way to get here is to have only files in the "root"
-                # positional argument, which are all generated into the same
-                # directory
+                # The only way to get here is to have only non-generated files
                 for g in target.structured_sources.sources['']:
-                    if isinstance(g, File):
-                        if g.endswith('.rs'):
-                            main_rust_file = g.rel_to_builddir(self.build_to_src)
-                    elif isinstance(g, GeneratedList):
-                        for h in g.get_outputs():
-                            if h.endswith('.rs'):
-                                main_rust_file = os.path.join(self.get_target_private_dir(target), h)
-                                break
-                    else:
-                        for h in g.get_outputs():
-                            if h.endswith('.rs'):
-                                main_rust_file = os.path.join(g.get_builddir(), h)
-                                break
-                    if main_rust_file is not None:
+                    assert isinstance(g, File)
+                    if g.endswith('.rs'):
+                        main_rust_file = g.rel_to_builddir(self.build_to_src)
                         break
 
                 _ods = []
@@ -2091,6 +2076,8 @@ class NinjaBackend(backends.Backend):
                     else:
                         _ods.extend([os.path.join(self.build_to_src, f.subdir, s)
                                      for s in f.get_outputs()])
+            if main_rust_file is None:
+                raise MesonException('Could not find a rust file to treat as the main file for ', target.name)
             self.all_structured_sources.update(_ods)
             orderdeps.extend(_ods)
             return orderdeps, main_rust_file
